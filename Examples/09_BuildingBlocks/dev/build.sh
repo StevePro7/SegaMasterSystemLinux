bfefda2cafc0ba8b6d8a692c7dcac3d56e044b7a
StevePro7 <steveprozune@hotmail.com>
2021-10-18 21:42:06 +0100

    building blocks

--- Examples/09_BuildingBlocks/dev/build.sh
+++ Examples/09_BuildingBlocks/dev/build.sh
@@ -14,50 +14,64 @@ cd ..
 cd engine
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 enemy_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 hack_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gamer_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 input_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 path_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 screen_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 sound_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 sprite_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 text_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 tree_manager.c
+cd ..
+
+cd object
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 enemy_object.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gamer_object.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 path_object.c
 cd ..
 
 cd screen
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 none_screen.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 start_screen.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 music_screen.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 sound_screen.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 joint_screen.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 title_screen.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 ready_screen.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 play_screen.c
 cd ..
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
 # Link
-sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 /
-../crt0/crt0_sms.rel main.rel /
-../lib/SMSlib.lib /
-../lib/PSGlib.rel /
-devkit/_sms_manager.rel /
-devkit/_snd_manager.rel /
-engine/asm_manager.rel /
-engine/content_manager.rel /
-engine/enemy_manager.rel /
-engine/font_manager.rel /
-engine/gamer_manager.rel /
-engine/hack_manager.rel /
-engine/input_manager.rel /
-engine/screen_manager.rel /
-engine/sound_manager.rel /
-engine/sprite_manager.rel /
-engine/text_manager.rel /
-engine/tree_manager.rel /
-screen/none_screen.rel /
-screen/start_screen.rel /
-screen/music_screen.rel /
-screen/sound_screen.rel /
-screen/joint_screen.rel /
-gfx.rel /
+sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 \
+../crt0/crt0_sms.rel main.rel \
+../lib/SMSlib.lib \
+../lib/PSGlib.rel \
+devkit/_sms_manager.rel \
+devkit/_snd_manager.rel \
+engine/asm_manager.rel \
+engine/content_manager.rel \
+engine/enemy_manager.rel \
+engine/font_manager.rel \
+engine/gamer_manager.rel \
+engine/hack_manager.rel \
+engine/input_manager.rel \
+engine/path_manager.rel \
+engine/screen_manager.rel \
+engine/sound_manager.rel \
+engine/sprite_manager.rel \
+engine/text_manager.rel \
+engine/tree_manager.rel \
+object/enemy_object.rel \
+object/gamer_object.rel \
+object/path_object.rel \
+screen/none_screen.rel \
+screen/start_screen.rel \
+screen/title_screen.rel \
+screen/ready_screen.rel \
+screen/play_screen.rel \
+gfx.rel \
 psg.rel
 
 
--- Examples/09_BuildingBlocks/dev/object/path_object.adb
+++ Examples/09_BuildingBlocks/dev/object/path_object.adb
@@ -1,2 +1,2 @@
 M:path_object
-T:Fpath_object$tag_struct_path_object[({0}S:S$gamer_route$0$0({448}DA8d,DA56d,SC:U),Z,0,0)]
+T:Fpath_object$tag_struct_path_object[({0}S:S$gamer_route$0_0$0({448}DA8d,DA56d,SC:U),Z,0,0)]
