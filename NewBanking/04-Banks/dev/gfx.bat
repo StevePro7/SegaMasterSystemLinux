cba3627b972a3cceb98ec33def8b0210b8c60f64
StevePro7 <steveprozune@hotmail.com>
2021-11-18 19:22:23 +0000

    no message

---
+++ NewBanking/04-Banks/dev/gfx.bat
@@ -0,0 +1,8 @@
+cd ..
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw\font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
+
+cd ..
+cd dev
