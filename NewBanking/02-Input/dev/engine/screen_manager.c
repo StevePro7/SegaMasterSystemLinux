f7670b23588962052e18ac7c3ba2df005f4d2e85
StevePro7 <steveprozune@hotmail.com>
2021-11-18 18:58:23 +0000

    no message

---
+++ NewBanking/02-Input/dev/engine/screen_manager.c
@@ -0,0 +1,11 @@
+#include "screen_manager.h"
+#include "font_manager.h"
+
+void engine_screen_manager_init()
+{
+	engine_font_manager_draw_text("HELLO WORLD", 10, 10);
+}
+
+void engine_screen_manager_update()
+{
+}
