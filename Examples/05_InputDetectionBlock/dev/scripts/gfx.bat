91f22f26b3cc545eb0a0cd5086ed930971988c6e
StevePro7 <steveprozune@hotmail.com>
2021-12-19 14:20:03 +0000

    no message

--- Examples/05_InputDetectionBlock/dev/scripts/gfx.bat
+++ Examples/05_InputDetectionBlock/dev/scripts/gfx.bat
@@ -1,3 +1,7 @@
+@echo off
+REM gfx		Commonly used gfx	not banked
+
+cd ..
 cd ..
 cd gfx
 
@@ -8,5 +12,13 @@ bmp2tile.exe raw\game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremo
 :: Sprites
 bmp2tile.exe raw/sprites.bmp -savetiles "sprites (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
 
-cd ..
-cd dev
+cd ..\dev
+
+folder2c ..\gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 gfx.c
+
+del *.asm > nul; del *.lst > nul; del *.sym > nul
+
+::cd ..
+cd scripts
