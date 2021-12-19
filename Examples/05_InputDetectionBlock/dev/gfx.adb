1ae6fcd00d71a328d4e5eefd2e67f7dc2b0bf6da
StevePro7 <steveprozune@hotmail.com>
2021-12-19 14:57:58 +0000

    no message

--- Examples/05_InputDetectionBlock/dev/gfx.adb
+++ Examples/05_InputDetectionBlock/dev/gfx.adb
@@ -6,4 +6,4 @@ S:G$game_tiles__palette__bin$0$0({16}DA16d,SC:U),D,0,0
 S:G$game_tiles__tilemap__bin$0$0({208}DA208d,SC:U),D,0,0
 S:G$game_tiles__tiles__psgcompr$0$0({1376}DA1376d,SC:U),D,0,0
 S:G$sprites__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$sprites__tiles__psgcompr$0$0({62}DA62d,SC:U),D,0,0
+S:G$sprites__tiles__psgcompr$0$0({1090}DA1090d,SC:U),D,0,0
--- Examples/05_InputDetectionBlock/dev/gfx.h
+++ Examples/05_InputDetectionBlock/dev/gfx.h
@@ -20,5 +20,5 @@ extern const unsigned char	sprites__palette__bin[];
 #define				sprites__palette__bin_size 16
 
 extern const unsigned char	sprites__tiles__psgcompr[];
-#define				sprites__tiles__psgcompr_size 62
+#define				sprites__tiles__psgcompr_size 1090
 
