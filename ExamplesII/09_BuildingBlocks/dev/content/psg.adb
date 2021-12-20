e2d4cd6b1073ffc2a4f1be31f4f0cbee4920d5a5
StevePro7 <steveprozune@hotmail.com>
2021-12-20 21:14:20 +0000

    no message

---
+++ ExamplesII/09_BuildingBlocks/dev/content/psg.adb
@@ -0,0 +1,4 @@
+M:psg
+S:G$sfx_accept_psg$0$0({32}DA32d,SC:U),D,0,0
+S:G$sfx_over_psg$0$0({64}DA64d,SC:U),D,0,0
+S:G$sfx_reset_psg$0$0({35}DA35d,SC:U),D,0,0
---
+++ ExamplesII/09_BuildingBlocks/dev/content/psg.h
@@ -0,0 +1,9 @@
+extern const unsigned char	sfx_accept_psg[];
+#define				sfx_accept_psg_size 32
+
+extern const unsigned char	sfx_over_psg[];
+#define				sfx_over_psg_size 64
+
+extern const unsigned char	sfx_reset_psg[];
+#define				sfx_reset_psg_size 35
+
