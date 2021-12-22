b0ace4f01135114b243770af2597eb0ff2718a35
StevePro7 <steveprozune@hotmail.com>
2021-12-22 20:56:25 +0000

    no message

--- NewBanking/05-Banks/dev/scripts/gfx.bat
+++ NewBanking/05-Banks/dev/scripts/gfx.bat
@@ -9,8 +9,8 @@ cd gfx
 bmp2tile.exe raw/font.bmp -savetiles "font (tiles).psgcompr" -removedupes -tileoffset 0 -savetilemap "font (tilemap).bin" -savepalette "font (palette).bin" -exit
 
 
-cd ../dev
-
+cd ..
+cd dev
 folder2c ../gfx gfx
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 gfx.c
