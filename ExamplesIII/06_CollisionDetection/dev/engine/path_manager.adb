fe5300cc390115d239bf402f2084e73f04d80fc6
StevePro7 <steveprozune@hotmail.com>
2021-12-27 21:46:25 +0000

    no message

---
+++ ExamplesIII/06_CollisionDetection/dev/engine/path_manager.adb
@@ -0,0 +1,5 @@
+M:path_manager
+F:G$engine_path_manager_init$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
+S:Lpath_manager.engine_path_manager_init$po$1_0$1({2}DG,STtag_struct_path_object:S),R,0,0,[]
+T:Fpath_manager$tag_struct_path_object[({0}S:S$gamer_route$0_0$0({448}DA8d,DA56d,SC:U),Z,0,0)]
+S:G$global_path_object$0_0$0({448}STtag_struct_path_object:S),E,0,0
---
+++ ExamplesIII/06_CollisionDetection/dev/screen/sound_screen.h
@@ -0,0 +1,7 @@
+#ifndef _SOUND_SCREEN_H_
+#define _SOUND_SCREEN_H_
+
+void screen_sound_screen_load();
+void screen_sound_screen_update( unsigned char *screen_type );
+
+#endif//_SOUND_SCREEN_H_
