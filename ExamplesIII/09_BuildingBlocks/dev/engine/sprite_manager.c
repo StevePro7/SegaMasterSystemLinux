73c33c5adad34fa8cbcb768817162eaca328c155
StevePro7 <steveprozune@hotmail.com>
2021-12-27 14:01:51 +0000

    no message

--- ExamplesIII/09_BuildingBlocks/dev/engine/sprite_manager.c
+++ ExamplesIII/09_BuildingBlocks/dev/engine/sprite_manager.c
@@ -5,6 +5,6 @@ void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned char
 {
 	devkit_SMS_addSprite( x + 0, y + 0, tile + 0 );
 	devkit_SMS_addSprite( x + 8, y + 0, tile + 1 );
-	devkit_SMS_addSprite( x + 0, y + 8, tile + 8 );
-	devkit_SMS_addSprite( x + 8, y + 8, tile + 9 );
+	devkit_SMS_addSprite( x + 0, y + 8, tile + 6 );
+	devkit_SMS_addSprite( x + 8, y + 8, tile + 7 );
 }
