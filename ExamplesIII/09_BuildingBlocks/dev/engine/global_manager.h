1e7424aaa9d7972a9513456d9a2bf2ab5d86379b
StevePro7 <steveprozune@hotmail.com>
2021-12-27 13:42:06 +0000

    no message

---
+++ ExamplesIII/09_BuildingBlocks/dev/engine/global_manager.h
@@ -0,0 +1,20 @@
+#ifndef _GLOBAL_MANAGER_H_
+#define _GLOBAL_MANAGER_H_
+
+// Sprites.
+#define SPRITE_TILES		256
+
+// Steps.
+#define GAMER_MAX_STEPS		8
+#define GAMER_STD_STEPS		1
+
+// Delay.
+#define SPLASH_DELAY		150
+#define ENEMY_STD_DELAY		75
+#define ENEMY_MIN_DELAY		25
+
+// Paths.
+#define GAMER_MAX_FRAME		56
+#define GAMER_MAX_PATHS		8
+
+#endif//_GLOBAL_MANAGER_H_
---
+++ ExamplesIII/09_BuildingBlocks/dev/engine/route_manager.h
@@ -0,0 +1,1 @@
+extern const unsigned char	gamer_route0[];
