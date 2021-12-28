715a6ebbe6ed66aede99ac9f996e8c034feb414c
StevePro7 <steveprozune@hotmail.com>
2021-12-28 20:47:41 +0000

    no message

--- ExamplesIII/05_InputDetection/dev/engine/sprite_manager.c
+++ ExamplesIII/05_InputDetection/dev/engine/sprite_manager.c
@@ -1,7 +1,7 @@
 #include "sprite_manager.h"
 #include "../devkit/_sms_manager.h"
 
-void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned char tile )
+void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned int tile )
 {
 	devkit_SMS_addSprite( x + 0, y + 0, tile + 0 );
 	devkit_SMS_addSprite( x + 8, y + 0, tile + 1 );
