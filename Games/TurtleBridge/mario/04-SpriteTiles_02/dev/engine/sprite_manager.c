e97475da9709e2689711ec2ede7ee40377e813ce
StevePro7 <steveprozune@hotmail.com>
2021-12-30 23:05:20 +0000

    no message

--- Games/TurtleBridge/mario/04-SpriteTiles_02/dev/engine/sprite_manager.c
+++ Games/TurtleBridge/mario/04-SpriteTiles_02/dev/engine/sprite_manager.c
@@ -5,6 +5,21 @@ void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned int 
 {
 	devkit_SMS_addSprite( x + 0, y + 0, tile + 0 );
 	devkit_SMS_addSprite( x + 8, y + 0, tile + 1 );
-	devkit_SMS_addSprite( x + 0, y + 8, tile + 2 );
-	devkit_SMS_addSprite( x + 8, y + 8, tile + 3 );
+	devkit_SMS_addSprite( x + 16, y + 0, tile + 2 );
+	devkit_SMS_addSprite( x + 24, y + 0, tile + 3 );
+
+	devkit_SMS_addSprite( x + 0, y + 8, tile + 4 );
+	devkit_SMS_addSprite( x + 8, y + 8, tile + 5 );
+	devkit_SMS_addSprite( x + 16, y + 8, tile + 6 );
+	devkit_SMS_addSprite( x + 24, y + 8, tile + 7 );
+
+	devkit_SMS_addSprite( x + 0, y + 16, tile + 8 );
+	devkit_SMS_addSprite( x + 8, y + 16, tile + 9 );
+	devkit_SMS_addSprite( x + 16, y + 16, tile + 10 );
+	devkit_SMS_addSprite( x + 24, y + 16, tile + 11 );
+
+	devkit_SMS_addSprite( x + 0, y + 24, tile + 12 );
+	devkit_SMS_addSprite( x + 8, y + 24, tile + 13 );
+	devkit_SMS_addSprite( x + 16, y + 24, tile + 14 );
+	devkit_SMS_addSprite( x + 24, y + 24, tile + 15 );
 }
