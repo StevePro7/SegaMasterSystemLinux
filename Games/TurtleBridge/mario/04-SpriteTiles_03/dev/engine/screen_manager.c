19061f62e8c3e89134377f9271c8eaa9cae918ae
StevePro7 <steveprozune@hotmail.com>
2021-12-31 20:40:48 +0000

    no message

--- Games/TurtleBridge/mario/04-SpriteTiles_03/dev/engine/screen_manager.c
+++ Games/TurtleBridge/mario/04-SpriteTiles_03/dev/engine/screen_manager.c
@@ -7,42 +7,74 @@
 #include "sprite_manager.h"
 
 static unsigned char index = 0;
-static unsigned char x = 32;
+static unsigned char x = 0;
+static unsigned char z = 0;
 
 void engine_screen_manager_init()
 {
 	engine_font_manager_draw_text( "STEVEPRO STUDIOS()", 4, 4 );
 	index = 0;
-	x = 32;
+	x = 96;
+	z = 0;
 }
 
 void engine_screen_manager_update()
 {
 	unsigned char input = 0;
-	input = engine_input_manager_hold_up();
+	//input = engine_input_manager_hold_up();
+	//if( input )
+	//{
+	//	index = 0;
+	//}
+	input = engine_input_manager_move_right();
+	//input = engine_input_manager_hold_right();
 	if( input )
 	{
-		index = 0;
-	}
-	input = engine_input_manager_hold_right();
-	if( input )
-	{
-		index = 1;
-	}
-	input = engine_input_manager_hold_down();
-	if( input )
-	{
-		index = 2;
-	}
-	input = engine_input_manager_hold_left();
-	if( input )
-	{
-		index = 3;
+		//x++;
+		z++;
+		if( z >= 16 )
+		{
+			z = 0;
+			index++;
+			if( index >= 3 )
+			{
+				index = 0;
+			}
+		}
 	}
 
-	if( input != 0 )
-	{
-		x++;
-	}
+	//input = engine_input_manager_move_left();
+	////input = engine_input_manager_hold_left();
+	//if( input )
+	//{
+	//	x--;
+	//	z--;
+	//	if( z <= 0 )
+	//	{
+	//		z = 4;
+	//		index--;
+	//		if( index >= 3 )
+	//		{
+	//			index = 0;
+	//		}
+	//	}
+	//}
+
+	//input = engine_input_manager_hold_down();
+	//if( input )
+	//{
+	//	index = 2;
+	//}
+	//input = engine_input_manager_hold_left();
+	//if( input )
+	//{
+	//	index = 3;
+	//}
+
+	//if( input != 0 )
+	//{
+	//	x++;
+	//}
+
 	engine_sprite_manager_draw( x, 96, SPRITE_TILES + index * 4 );
 }
