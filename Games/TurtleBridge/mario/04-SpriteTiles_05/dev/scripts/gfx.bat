e8459e5fa5438b9290c1e3982950460618f9d9fb
StevePro7 <steveprozune@hotmail.com>
2021-12-31 21:31:21 +0000

    no message

---
+++ Games/TurtleBridge/mario/04-SpriteTiles_05/dev/scripts/gfx.bat
@@ -0,0 +1,33 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
+
+:: Sprites
+bmp2tile.exe raw/left03.png -savetiles "left01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "mario01 (palette).bin" -fullpalette  -spritepalette -exit
+bmp2tile.exe raw/left13.png -savetiles "left02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+bmp2tile.exe raw/left23.png -savetiles "left03 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+bmp2tile.exe raw/left33.png -savetiles "left04 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+
+bmp2tile.exe raw/rght03.png -savetiles "rght01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+bmp2tile.exe raw/rght13.png -savetiles "rght02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+bmp2tile.exe raw/rght23.png -savetiles "rght03 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+bmp2tile.exe raw/rght33.png -savetiles "rght04 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
