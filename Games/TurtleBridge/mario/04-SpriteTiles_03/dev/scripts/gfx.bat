75d0f51483d656a40b71aaa0f82ca566764a5732
StevePro7 <steveprozune@hotmail.com>
2021-12-31 17:16:14 +0000

    no message

---
+++ Games/TurtleBridge/mario/04-SpriteTiles_03/dev/scripts/gfx.bat
@@ -0,0 +1,25 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
+
+:: Sprites
+bmp2tile.exe raw/mario.png -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
