e56da843fdf9ecbf07ed7e27b9dafefa919f93f7
StevePro7 <steveprozune@hotmail.com>
2021-12-31 20:08:40 +0000

    no message

--- Games/TurtleBridge/mario/04-SpriteTiles_03/dev/content/gfx.h
+++ Games/TurtleBridge/mario/04-SpriteTiles_03/dev/content/gfx.h
@@ -7,9 +7,6 @@ extern const unsigned char	font_tiles__tilemap__bin[];
 extern const unsigned char	font_tiles__tiles__psgcompr[];
 #define				font_tiles__tiles__psgcompr_size 663
 
-extern const unsigned char	mario__palette__bin[];
-#define				mario__palette__bin_size 16
-
 extern const unsigned char	left01__tiles__psgcompr[];
 #define				left01__tiles__psgcompr_size 80
 
@@ -21,6 +18,9 @@ extern const unsigned char	left03__tiles__psgcompr[];
 
 extern const unsigned char	left04__tiles__psgcompr[];
 #define				left04__tiles__psgcompr_size 77
+
+extern const unsigned char	mario01__palette__bin[];
+#define				mario01__palette__bin_size 16
 
 extern const unsigned char	rght01__tiles__psgcompr[];
 #define				rght01__tiles__psgcompr_size 80
