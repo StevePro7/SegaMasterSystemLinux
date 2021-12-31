3c9e115ea0ed00c2deca7746abeedd6568061fd6
StevePro7 <steveprozune@hotmail.com>
2021-12-31 21:33:32 +0000

    no message

--- Games/TurtleBridge/mario/04-SpriteTiles_05/dev/content/gfx.h
+++ Games/TurtleBridge/mario/04-SpriteTiles_05/dev/content/gfx.h
@@ -8,29 +8,14 @@ extern const unsigned char	font_tiles__tiles__psgcompr[];
 #define				font_tiles__tiles__psgcompr_size 663
 
 extern const unsigned char	left01__tiles__psgcompr[];
-#define				left01__tiles__psgcompr_size 81
+#define				left01__tiles__psgcompr_size 221
 
 extern const unsigned char	left02__tiles__psgcompr[];
-#define				left02__tiles__psgcompr_size 82
+#define				left02__tiles__psgcompr_size 213
 
 extern const unsigned char	left03__tiles__psgcompr[];
-#define				left03__tiles__psgcompr_size 81
-
-extern const unsigned char	left04__tiles__psgcompr[];
-#define				left04__tiles__psgcompr_size 82
+#define				left03__tiles__psgcompr_size 214
 
 extern const unsigned char	mario01__palette__bin[];
 #define				mario01__palette__bin_size 16
-
-extern const unsigned char	rght01__tiles__psgcompr[];
-#define				rght01__tiles__psgcompr_size 80
-
-extern const unsigned char	rght02__tiles__psgcompr[];
-#define				rght02__tiles__psgcompr_size 84
-
-extern const unsigned char	rght03__tiles__psgcompr[];
-#define				rght03__tiles__psgcompr_size 80
-
-extern const unsigned char	rght04__tiles__psgcompr[];
-#define				rght04__tiles__psgcompr_size 84
 
