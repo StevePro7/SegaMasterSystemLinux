e35a7e911c679078ee50ac34c3d8a577e25adea7
StevePro7 <steveprozune@hotmail.com>
2022-01-01 20:59:44 +0000

    no message

---
+++ Games/TurtleBridge/mario/05-SpriteTiles_Turtle01/dev/engine/content_manager.h
@@ -0,0 +1,9 @@
+#ifndef _CONTENT_MANAGER_H_
+#define _CONTENT_MANAGER_H_
+
+void engine_content_manager_load_tiles();
+void engine_content_manager_load_sprites();
+//void engine_content_manager_load_sprites01();
+//void engine_content_manager_load_sprites02();
+
+#endif//_CONTENT_MANAGER_H_
