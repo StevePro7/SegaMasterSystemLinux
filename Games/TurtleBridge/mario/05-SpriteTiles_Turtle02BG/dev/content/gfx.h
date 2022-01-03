c632c78d538b261bc726ebd7bf8678bb6777f6a2
StevePro7 <steveprozune@hotmail.com>
2022-01-03 19:35:24 +0000

    no message

---
+++ Games/TurtleBridge/mario/05-SpriteTiles_Turtle02BG/dev/content/gfx.h
@@ -0,0 +1,15 @@
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	turtle__palette__bin[];
+#define				turtle__palette__bin_size 16
+
+extern const unsigned char	turtle__tiles__psgcompr[];
+#define				turtle__tiles__psgcompr_size 199
+
