952edb01f747947f1ab2bafb3669177743fba95d
StevePro7 <steveprozune@hotmail.com>
2022-01-05 22:27:41 +0000

    no message

---
+++ Examples/02-SplashScreenWinLAND/dev/gfx.bat
@@ -0,0 +1,7 @@
+cd ..
+cd gfx
+
+bmp2tile.exe raw\splash.png -savetiles "splash (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 128 -savetilemap "splash (tilemap).stmcompr" -savepalette "splash (palette).bin" -fullpalette
+
+cd ..
+cd dev
