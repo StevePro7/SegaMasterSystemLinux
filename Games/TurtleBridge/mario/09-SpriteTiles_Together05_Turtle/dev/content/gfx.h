25785a2c17e437a9619a1953fccf94a56cb1833d
StevePro7 <steveprozune@hotmail.com>
2022-01-05 21:46:48 +0000

    no message

---
+++ Games/TurtleBridge/mario/09-SpriteTiles_Together05_Turtle/dev/content/gfx.h
@@ -0,0 +1,54 @@
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	left01__tiles__psgcompr[];
+#define				left01__tiles__psgcompr_size 80
+
+extern const unsigned char	left02__tiles__psgcompr[];
+#define				left02__tiles__psgcompr_size 78
+
+extern const unsigned char	left03__tiles__psgcompr[];
+#define				left03__tiles__psgcompr_size 81
+
+extern const unsigned char	left04__tiles__psgcompr[];
+#define				left04__tiles__psgcompr_size 77
+
+extern const unsigned char	mario01__palette__bin[];
+#define				mario01__palette__bin_size 16
+
+extern const unsigned char	rght01__tiles__psgcompr[];
+#define				rght01__tiles__psgcompr_size 80
+
+extern const unsigned char	rght02__tiles__psgcompr[];
+#define				rght02__tiles__psgcompr_size 80
+
+extern const unsigned char	rght03__tiles__psgcompr[];
+#define				rght03__tiles__psgcompr_size 80
+
+extern const unsigned char	rght04__tiles__psgcompr[];
+#define				rght04__tiles__psgcompr_size 77
+
+extern const unsigned char	turtle_tiles_01__palette__bin[];
+#define				turtle_tiles_01__palette__bin_size 16
+
+extern const unsigned char	turtle_tiles_01__tilemap__bin[];
+#define				turtle_tiles_01__tilemap__bin_size 32
+
+extern const unsigned char	turtle_tiles_01__tiles__psgcompr[];
+#define				turtle_tiles_01__tiles__psgcompr_size 199
+
+extern const unsigned char	turtle_tiles_02__palette__bin[];
+#define				turtle_tiles_02__palette__bin_size 16
+
+extern const unsigned char	turtle_tiles_02__tilemap__bin[];
+#define				turtle_tiles_02__tilemap__bin_size 32
+
+extern const unsigned char	turtle_tiles_02__tiles__psgcompr[];
+#define				turtle_tiles_02__tiles__psgcompr_size 195
+
---
+++ Games/TurtleBridge/mario/09-SpriteTiles_Together05_Turtle/dev/engine/screen_manager.c
@@ -0,0 +1,57 @@
+#include "screen_manager.h"
+#include "audio_manager.h"
+#include "content_manager.h"
+#include "font_manager.h"
+#include "global_manager.h"
+#include "input_manager.h"
+#include "sprite_manager.h"
+#include <stdlib.h>
+
+static void draw_turtles();
+static void draw_turtle( unsigned char i );
+static unsigned char flag[ 6 ] = { 0,0,0,0,0 };
+static unsigned char wide[ 6 ] = { 2, 7, 12, 17, 22, 27 };
+
+void engine_screen_manager_init()
+{
+	engine_font_manager_draw_text( "STEVEPRO STUDIOS", 4, 4 );
+	draw_turtles();
+
+	engine_music_manager_play();
+}
+
+void engine_screen_manager_update()
+{
+	unsigned char test;
+	rand();
+
+	test = rand() % 20;
+	if( 1 == test )
+	{
+		test = rand() % 6;
+		flag[ test ] = 1 - flag[ test ];
+		draw_turtle( test );
+	}
+
+	engine_sprite_manager_draw( 88, 64, SPRITE_TILES );
+}
+
+static void draw_turtles()
+{
+	unsigned char i;
+	for( i = 0; i < 6; i++ )
+	{
+		draw_turtle( i );
+	}
+}
+static void draw_turtle( unsigned char i )
+{
+	if( 0 == flag[ i ] )
+	{
+		engine_turtle_manager_draw_01( wide[ i ], 18 );
+	}
+	else
+	{
+		engine_turtle_manager_draw_02( wide[ i ], 18 );
+	}
+}
