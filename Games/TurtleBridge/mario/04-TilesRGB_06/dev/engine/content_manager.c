fe0b946e6469373ab8f15c3702b6b47dc658ffca
StevePro7 <steveprozune@hotmail.com>
2022-01-05 20:32:47 +0000

    no message

--- Games/TurtleBridge/mario/04-TilesRGB_06/dev/engine/content_manager.c
+++ Games/TurtleBridge/mario/04-TilesRGB_06/dev/engine/content_manager.c
@@ -8,26 +8,30 @@
 void engine_content_manager_load_tiles()
 {
 	// Font tiles.
-	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
-	devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
+	//devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+
+	// Game tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) game_tiles__tiles__psgcompr, FONT_TILES );
+	devkit_SMS_loadBGPalette( ( void * ) game_tiles__palette__bin );
 }
 
 void engine_content_manager_load_sprites()
 {
-	// Sprite tiles.
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) mario01__tiles__psgcompr, SPRITE_TILES );
-	devkit_SMS_loadSpritePalette( ( void * ) mario01__palette__bin );
+	//// Sprite tiles.
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) mario01__tiles__psgcompr, SPRITE_TILES );
+	//devkit_SMS_loadSpritePalette( ( void * ) mario01__palette__bin );
 
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght02__tiles__psgcompr, SPRITE_TILES + ( 1 * 4 ) );
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght03__tiles__psgcompr, SPRITE_TILES + ( 2 * 4 ) );
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght04__tiles__psgcompr, SPRITE_TILES + ( 3 * 4 ) );
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght02__tiles__psgcompr, SPRITE_TILES + ( 1 * 4 ) );
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght03__tiles__psgcompr, SPRITE_TILES + ( 2 * 4 ) );
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght04__tiles__psgcompr, SPRITE_TILES + ( 3 * 4 ) );
 
-	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
-	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left02__tiles__psgcompr, SPRITE_TILES + ( 1 * 4 ) );
-	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 2 * 4 ) );
-	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left03__tiles__psgcompr, SPRITE_TILES + ( 3 * 4 ) );
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left04__tiles__psgcompr, SPRITE_TILES + ( 7 * 4 ) );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left02__tiles__psgcompr, SPRITE_TILES + ( 1 * 4 ) );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 2 * 4 ) );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left03__tiles__psgcompr, SPRITE_TILES + ( 3 * 4 ) );
+	////devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left04__tiles__psgcompr, SPRITE_TILES + ( 7 * 4 ) );
 }
 
 //void engine_content_manager_load_sprites01()
