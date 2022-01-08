f667fdd166dd9209f3f0c638ebe2fdd682d24855
StevePro7 <steveprozune@hotmail.com>
2022-01-08 19:59:53 +0000

    no message

---
+++ Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/content_manager.c
@@ -0,0 +1,36 @@
+#include "content_manager.h"
+#include "../engine/global_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+#define FONT_TILES			0
+
+void engine_content_manager_load_tiles()
+{
+	// Font tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
+	devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+}
+
+void engine_content_manager_load_turtle()
+{
+	// Turtle tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) game_tiles__tiles__psgcompr, TURTLE_TILES );
+	devkit_SMS_loadBGPalette( ( void * ) game_tiles__palette__bin );
+}
+
+void engine_content_manager_load_sprites()
+{
+	// Sprite tiles.
+	devkit_SMS_loadSpritePalette( ( void * ) mario01__palette__bin );
+
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght02__tiles__psgcompr, SPRITE_TILES + ( 1 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght03__tiles__psgcompr, SPRITE_TILES + ( 2 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) rght04__tiles__psgcompr, SPRITE_TILES + ( 3 * 4 ) );
+
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 4 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left02__tiles__psgcompr, SPRITE_TILES + ( 5 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left03__tiles__psgcompr, SPRITE_TILES + ( 6 * 4 ) );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left04__tiles__psgcompr, SPRITE_TILES + ( 7 * 4 ) );
+}
---
+++ Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/sprite_manager.c
@@ -0,0 +1,88 @@
+#include "sprite_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+//void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned int tile )
+//{
+//	devkit_SMS_addSprite( x + 0, y + 0, tile + 0 );
+//	devkit_SMS_addSprite( x + 8, y + 0, tile + 1 );
+//	devkit_SMS_addSprite( x + 16, y + 0, tile + 2 );
+//	devkit_SMS_addSprite( x + 24, y + 0, tile + 3 );
+//
+//	devkit_SMS_addSprite( x + 0, y + 8, tile + 4 );
+//	devkit_SMS_addSprite( x + 8, y + 8, tile + 5 );
+//	devkit_SMS_addSprite( x + 16, y + 8, tile + 6 );
+//	devkit_SMS_addSprite( x + 24, y + 8, tile + 7 );
+//
+//	devkit_SMS_addSprite( x + 0, y + 16, tile + 8 );
+//	devkit_SMS_addSprite( x + 8, y + 16, tile + 9 );
+//	devkit_SMS_addSprite( x + 16, y + 16, tile + 10 );
+//	devkit_SMS_addSprite( x + 24, y + 16, tile + 11 );
+//
+//	devkit_SMS_addSprite( x + 0, y + 24, tile + 12 );
+//	devkit_SMS_addSprite( x + 8, y + 24, tile + 13 );
+//	devkit_SMS_addSprite( x + 16, y + 24, tile + 14 );
+//	devkit_SMS_addSprite( x + 24, y + 24, tile + 15 );
+//}
+
+void engine_turtle_manager_draw_01( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + 0 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + 1 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + 2 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + 3 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + 4 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + 5 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + 6 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + 7 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + 8 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + 9 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + 10 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + 11 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 3 );	devkit_SMS_setTile( *pnt + 12 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 3 );	devkit_SMS_setTile( *pnt + 13 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 3 );	devkit_SMS_setTile( *pnt + 14 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 15 );
+}
+
+void engine_turtle_manager_draw_02( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + 16 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + 17 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + 18 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + 19 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + 20 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + 21 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + 22 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + 23 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + 24 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + 25 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + 26 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + 27 );
+
+	devkit_SMS_setNextTileatXY( x + 0, y + 3 );	devkit_SMS_setTile( *pnt + 28 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 3 );	devkit_SMS_setTile( *pnt + 29 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 3 );	devkit_SMS_setTile( *pnt + 30 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 31 );
+}
+
+
+// zoom sprites
+void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned int tile )
+{
+	const unsigned char size = 2;
+
+	devkit_SMS_addSprite( x + size * 0, y + size * 0, tile + 0 );
+	devkit_SMS_addSprite( x + size * 8, y + size * 0, tile + 1 );
+	devkit_SMS_addSprite( x + size * 0, y + size * 8, tile + 2 );
+	devkit_SMS_addSprite( x + size * 8, y + size * 8, tile + 3 );
+}
