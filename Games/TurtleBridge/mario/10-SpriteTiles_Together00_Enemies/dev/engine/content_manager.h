1c338b7c8d02a9f5009390af4d588bc5d8ac5af4
StevePro7 <steveprozune@hotmail.com>
2022-01-08 20:06:40 +0000

    no message

--- Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/content_manager.h
+++ Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/content_manager.h
@@ -2,7 +2,7 @@
 #define _CONTENT_MANAGER_H_
 
 void engine_content_manager_load_tiles();
-void engine_content_manager_load_turtle();
+//void engine_content_manager_load_turtle();
 void engine_content_manager_load_sprites();
 
 #endif//_CONTENT_MANAGER_H_
--- Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/sprite_manager.c
+++ Games/TurtleBridge/mario/10-SpriteTiles_Together00_Enemies/dev/engine/sprite_manager.c
@@ -27,7 +27,7 @@
 
 void engine_turtle_manager_draw_01( unsigned char x, unsigned char y )
 {
-	const unsigned char *pnt = game_tiles__tilemap__bin;
+	/*const unsigned char *pnt = game_tiles__tilemap__bin;
 
 	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + 0 );
 	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + 1 );
@@ -46,13 +46,13 @@ void engine_turtle_manager_draw_01( unsigned char x, unsigned char y )
 
 	devkit_SMS_setNextTileatXY( x + 0, y + 3 );	devkit_SMS_setTile( *pnt + 12 );
 	devkit_SMS_setNextTileatXY( x + 1, y + 3 );	devkit_SMS_setTile( *pnt + 13 );
-	devkit_SMS_setNextTileatXY( x + 2, y + 3 );	devkit_SMS_setTile( *pnt + 14 );
-	devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 15 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 3 );	devkit_SMS_setTile( *pnt + 14 );*/
+	//devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 15 );
 }
 
 void engine_turtle_manager_draw_02( unsigned char x, unsigned char y )
 {
-	const unsigned char *pnt = game_tiles__tilemap__bin;
+	/*const unsigned char *pnt = game_tiles__tilemap__bin;
 
 	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + 16 );
 	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + 17 );
@@ -72,7 +72,7 @@ void engine_turtle_manager_draw_02( unsigned char x, unsigned char y )
 	devkit_SMS_setNextTileatXY( x + 0, y + 3 );	devkit_SMS_setTile( *pnt + 28 );
 	devkit_SMS_setNextTileatXY( x + 1, y + 3 );	devkit_SMS_setTile( *pnt + 29 );
 	devkit_SMS_setNextTileatXY( x + 2, y + 3 );	devkit_SMS_setTile( *pnt + 30 );
-	devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 31 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 3 );	devkit_SMS_setTile( *pnt + 31 );*/
 }
 
 
