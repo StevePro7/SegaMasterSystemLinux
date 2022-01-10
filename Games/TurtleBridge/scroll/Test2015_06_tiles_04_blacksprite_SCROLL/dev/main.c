ec31db60a459d1ebd978b4ef2e6fa71272d7c3f6
StevePro7 <steveprozune@hotmail.com>
2022-01-10 20:39:34 +0000

    no message

--- Games/TurtleBridge/scroll/Test2015_06_tiles_04_blacksprite_SCROLL/dev/main.c
+++ Games/TurtleBridge/scroll/Test2015_06_tiles_04_blacksprite_SCROLL/dev/main.c
@@ -1,17 +1,9 @@
 #include "main.h"
 
-
-
 void main(void)
 {
-	SMS_displayOn();
+	//SMS_displayOn();
 	for( ;;)
 	{
 	}
 }
-
-#ifdef _CONSOLE
-#else
-	SMS_EMBED_SEGA_ROM_HEADER(9999, 0);
-	SMS_EMBED_SDSC_HEADER(1, 0, 2017, 3, 17, "StevePro Studios", "Candy Kid Demo", "DESC");
-#endif
