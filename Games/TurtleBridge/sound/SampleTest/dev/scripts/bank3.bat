2688acfc26d7131d63bbe47db38b15cdb480e006
StevePro7 <steveprozune@hotmail.com>
2022-01-13 19:14:20 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest/dev/scripts/bank3.bat
+++ Games/TurtleBridge/sound/SampleTest/dev/scripts/bank3.bat
@@ -1,18 +1,12 @@
-::@echo off
+REM bank12
+@echo off
 
 cd ..
 cd banks
-cd bank3
 
-bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0 -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
-
-cd ..
-
-:: Convert.
 folder2c bank3 bank3 3
 
-:: Compile
-sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
+sdcc -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
 
 del *.asm > nul
 del *.lst > nul
