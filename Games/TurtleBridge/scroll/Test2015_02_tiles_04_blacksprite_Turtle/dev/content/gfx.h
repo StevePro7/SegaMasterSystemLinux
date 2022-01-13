14a84996edf82421174834277470b8a15e0e889a
StevePro7 <steveprozune@hotmail.com>
2022-01-13 20:22:21 +0000

    no message

--- Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
+++ Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
@@ -1,3 +1,6 @@
+extern const unsigned char	back_tiles__tiles__psgcompr[];
+#define				back_tiles__tiles__psgcompr_size 2
+
 extern const unsigned char	font_tiles__palette__bin[];
 #define				font_tiles__palette__bin_size 16
 
@@ -6,22 +9,4 @@ extern const unsigned char	font_tiles__tilemap__bin[];
 
 extern const unsigned char	font_tiles__tiles__psgcompr[];
 #define				font_tiles__tiles__psgcompr_size 663
-
-extern const unsigned char	turtle_tiles_01__palette__bin[];
-#define				turtle_tiles_01__palette__bin_size 16
-
-extern const unsigned char	turtle_tiles_01__tilemap__bin[];
-#define				turtle_tiles_01__tilemap__bin_size 32
-
-extern const unsigned char	turtle_tiles_01__tiles__psgcompr[];
-#define				turtle_tiles_01__tiles__psgcompr_size 199
-
-extern const unsigned char	turtle_tiles_02__palette__bin[];
-#define				turtle_tiles_02__palette__bin_size 16
-
-extern const unsigned char	turtle_tiles_02__tilemap__bin[];
-#define				turtle_tiles_02__tilemap__bin_size 32
-
-extern const unsigned char	turtle_tiles_02__tiles__psgcompr[];
-#define				turtle_tiles_02__tiles__psgcompr_size 195
 
