05fce60b3e8cf6ffc338d07c631768420fb464e6
StevePro7 <steveprozune@hotmail.com>
2022-01-13 20:38:33 +0000

    no message

--- Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
+++ Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
@@ -5,7 +5,7 @@ extern const unsigned char	back_tiles__tilemap__bin[];
 #define				back_tiles__tilemap__bin_size 64
 
 extern const unsigned char	back_tiles__tiles__psgcompr[];
-#define				back_tiles__tiles__psgcompr_size 195
+#define				back_tiles__tiles__psgcompr_size 383
 
 extern const unsigned char	font_tiles__palette__bin[];
 #define				font_tiles__palette__bin_size 16
--- Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/scripts/gfx.bat
+++ Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/scripts/gfx.bat
@@ -7,7 +7,7 @@ cd gfx
 
 :: Tiles
 bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
-bmp2tile.exe raw/back_tiles.png -savetiles "back_tiles (tiles).psgcompr" -removedupes -planar -tileoffset 64 -savetilemap "back_tiles (tilemap).bin" -savepalette "back_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/back_tiles.png -savetiles "back_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "back_tiles (tilemap).bin" -savepalette "back_tiles (palette).bin" -fullpalette -exit
 
 
 cd ..
