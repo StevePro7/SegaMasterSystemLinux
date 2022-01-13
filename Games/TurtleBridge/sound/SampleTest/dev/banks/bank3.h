b65f4fd5343917993090a3b9056dc2fce50fe712
StevePro7 <steveprozune@hotmail.com>
2022-01-13 19:16:24 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest/dev/banks/bank3.h
+++ Games/TurtleBridge/sound/SampleTest/dev/banks/bank3.h
@@ -1,12 +1,4 @@
-extern const unsigned char	font_tiles__palette__bin[];
-#define				font_tiles__palette__bin_size 16
-#define				font_tiles__palette__bin_bank 3
-
-extern const unsigned char	font_tiles__tilemap__bin[];
-#define				font_tiles__tilemap__bin_size 128
-#define				font_tiles__tilemap__bin_bank 3
-
-extern const unsigned char	font_tiles__tiles__psgcompr[];
-#define				font_tiles__tiles__psgcompr_size 663
-#define				font_tiles__tiles__psgcompr_bank 3
+extern const unsigned char	bank3_txt[];
+#define				bank3_txt_size 5
+#define				bank3_txt_bank 3
 
