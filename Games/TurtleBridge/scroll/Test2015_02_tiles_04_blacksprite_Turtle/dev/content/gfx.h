47c35bbd4622cf5a2c3c766f3adee46ea884ed39
StevePro7 <steveprozune@hotmail.com>
2022-01-13 20:44:11 +0000

    no message

--- Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
+++ Games/TurtleBridge/scroll/Test2015_02_tiles_04_blacksprite_Turtle/dev/content/gfx.h
@@ -2,10 +2,10 @@ extern const unsigned char	back_tiles__palette__bin[];
 #define				back_tiles__palette__bin_size 16
 
 extern const unsigned char	back_tiles__tilemap__bin[];
-#define				back_tiles__tilemap__bin_size 64
+#define				back_tiles__tilemap__bin_size 48
 
 extern const unsigned char	back_tiles__tiles__psgcompr[];
-#define				back_tiles__tiles__psgcompr_size 383
+#define				back_tiles__tiles__psgcompr_size 384
 
 extern const unsigned char	font_tiles__palette__bin[];
 #define				font_tiles__palette__bin_size 16
