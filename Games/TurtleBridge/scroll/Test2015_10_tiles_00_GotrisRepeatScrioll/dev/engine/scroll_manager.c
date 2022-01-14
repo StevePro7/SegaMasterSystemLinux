6769d895f3bba17b63eb30ce8d9bba3ac946b6ef
StevePro7 <steveprozune@hotmail.com>
2022-01-14 22:00:05 +0000

    no message

--- Games/TurtleBridge/scroll/Test2015_10_tiles_00_GotrisRepeatScrioll/dev/engine/scroll_manager.c
+++ Games/TurtleBridge/scroll/Test2015_10_tiles_00_GotrisRepeatScrioll/dev/engine/scroll_manager.c
@@ -1,11 +1,27 @@
 #include "scroll_manager.h"
 #include "font_manager.h"
+#include "sprite_manager.h"
 #include "../devkit/_sms_manager.h"
 
 #ifdef _CONSOLE
 #else
 #pragma disable_warning 158
 #endif
+
+unsigned int BG_TILE_WIDTH = 64;
+unsigned int X_TILE_MAX = 32;
+unsigned int X_TILE_DIFF = 6; // 64 - 32;
+unsigned int Y_TILE_MAX = 24;
+
+unsigned char test = 0;
+unsigned int test2 = 0;
+unsigned char scroll = 0;
+//unsigned int scrollDivided8 = 0;
+unsigned char scrollRight = 0;
+unsigned char scrollRightDivided8 = 0;
+//unsigned int xtile = 0;
+unsigned int ytile = 0;
+unsigned int yDelta = 0;
 
 unsigned int scroll_x[6];
 unsigned char lineCnt;
@@ -30,6 +46,22 @@ void engine_scroll_manager_init()
 
 void engine_scroll_manager_update()
 {
+	const unsigned char delta = 1;
+	scroll -= delta;
+	scrollRight += delta;
+	scrollRightDivided8 = scrollRight / 8;
+	devkit_SMS_setBGScrollX( scroll );
+
+	// This works but does not update the top row
+	if( ( scrollRight % 8 ) == delta )
+	{
+		test = scrollRightDivided8 % 2;
+
+		ytile = 18;
+
+		engine_tilemap_manager_draw_tile( X_TILE_MAX + scrollRightDivided8, ytile - 1 );
+	}
+
 	//const unsigned char h1 = 128;
 	//const unsigned char h2 = 144;
 	//scroll_x[0] = scroll_x[0] - h1; // 128;                             // 0.5<<8
