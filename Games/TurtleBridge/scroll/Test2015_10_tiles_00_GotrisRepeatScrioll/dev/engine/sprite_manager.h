74a806e94598bfa6e2402f3ad91cec57d19150af
StevePro7 <steveprozune@hotmail.com>
2022-01-14 21:45:57 +0000

    no message

---
+++ Games/TurtleBridge/scroll/Test2015_10_tiles_00_GotrisRepeatScrioll/dev/engine/sprite_manager.h
@@ -0,0 +1,16 @@
+#ifndef _SPRITE_MANAGER_H_
+#define _SPRITE_MANAGER_H_
+
+void engine_content_manager_draw_tile( unsigned char x, unsigned char y );
+void engine_content_manager_draw_tile2( unsigned char x, unsigned char y );
+//void engine_content_manager_draw_tile( unsigned char x, unsigned char y, unsigned int i );
+
+void engine_tilemap_manager_draw_tile( unsigned char x, unsigned char y );
+
+//void engine_tile_manager_draw( unsigned char x, unsigned char y );
+//void engine_tile_manager_tile( unsigned char x, unsigned char y, unsigned char i );
+
+//void engine_turtle_manager_draw_01( unsigned char x, unsigned char y );
+//void engine_turtle_manager_draw_02( unsigned char x, unsigned char y );
+
+#endif//_SPRITE_MANAGER_H_
