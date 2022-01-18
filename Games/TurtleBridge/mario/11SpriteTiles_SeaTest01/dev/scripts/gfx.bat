f58b732a637d910692cbe87a6d97199b2f0ed716
StevePro7 <steveprozune@hotmail.com>
2022-01-18 22:24:57 +0000

    no message

--- Games/TurtleBridge/mario/11SpriteTiles_SeaTest01/dev/scripts/gfx.bat
+++ Games/TurtleBridge/mario/11SpriteTiles_SeaTest01/dev/scripts/gfx.bat
@@ -9,17 +9,6 @@ cd gfx
 bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0   -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
 bmp2tile.exe raw/game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64  -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
 
-:: Sprites
-bmp2tile.exe raw/left01.png -savetiles "left01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "mario01 (palette).bin" -fullpalette  -spritepalette -exit
-bmp2tile.exe raw/left02.png -savetiles "left02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-bmp2tile.exe raw/left03.png -savetiles "left03 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-bmp2tile.exe raw/left04.png -savetiles "left04 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-
-bmp2tile.exe raw/rght01.png -savetiles "rght01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-bmp2tile.exe raw/rght02.png -savetiles "rght02 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-bmp2tile.exe raw/rght03.png -savetiles "rght03 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-bmp2tile.exe raw/rght04.png -savetiles "rght04 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -exit
-
 
 cd ..
 folder2c gfx gfx
