d3d6a5970fae4e5a60bf042cd55ac682b4fca425
StevePro7 <steveprozune@hotmail.com>
2022-01-19 20:50:47 +0000

    no message

--- Games/TurtleBridge/mario/11SpriteTiles_SeaTest01/dev/content/gfx.adb
+++ Games/TurtleBridge/mario/11SpriteTiles_SeaTest01/dev/content/gfx.adb
@@ -1,4 +1,4 @@
 M:gfx
 S:G$game_tiles__palette__bin$0$0({16}DA16d,SC:U),D,0,0
 S:G$game_tiles__tilemap__bin$0$0({88}DA88d,SC:U),D,0,0
-S:G$game_tiles__tiles__psgcompr$0$0({669}DA669d,SC:U),D,0,0
+S:G$game_tiles__tiles__psgcompr$0$0({655}DA655d,SC:U),D,0,0
