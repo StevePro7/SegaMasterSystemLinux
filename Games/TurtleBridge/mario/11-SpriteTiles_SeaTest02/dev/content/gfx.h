bf926e641be3051d78b25126fdd2391b14f8a183
StevePro7 <steveprozune@hotmail.com>
2022-01-21 14:52:35 +0000

    no message

--- Games/TurtleBridge/mario/11-SpriteTiles_SeaTest02/dev/content/gfx.h
+++ Games/TurtleBridge/mario/11-SpriteTiles_SeaTest02/dev/content/gfx.h
@@ -7,3 +7,39 @@ extern const unsigned char	game_tiles__tilemap__bin[];
 extern const unsigned char	game_tiles__tiles__psgcompr[];
 #define				game_tiles__tiles__psgcompr_size 655
 
+extern const unsigned char	turtle00__palette__bin[];
+#define				turtle00__palette__bin_size 16
+
+extern const unsigned char	turtle00__tilemap__bin[];
+#define				turtle00__tilemap__bin_size 24
+
+extern const unsigned char	turtle00__tiles__psgcompr[];
+#define				turtle00__tiles__psgcompr_size 206
+
+extern const unsigned char	turtle01__palette__bin[];
+#define				turtle01__palette__bin_size 16
+
+extern const unsigned char	turtle01__tilemap__bin[];
+#define				turtle01__tilemap__bin_size 24
+
+extern const unsigned char	turtle01__tiles__psgcompr[];
+#define				turtle01__tiles__psgcompr_size 199
+
+extern const unsigned char	turtle02__palette__bin[];
+#define				turtle02__palette__bin_size 16
+
+extern const unsigned char	turtle02__tilemap__bin[];
+#define				turtle02__tilemap__bin_size 24
+
+extern const unsigned char	turtle02__tiles__psgcompr[];
+#define				turtle02__tiles__psgcompr_size 193
+
+extern const unsigned char	turtle03__palette__bin[];
+#define				turtle03__palette__bin_size 16
+
+extern const unsigned char	turtle03__tilemap__bin[];
+#define				turtle03__tilemap__bin_size 24
+
+extern const unsigned char	turtle03__tiles__psgcompr[];
+#define				turtle03__tiles__psgcompr_size 216
+
