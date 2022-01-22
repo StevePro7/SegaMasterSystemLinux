0803a76d029181b3c4ff3352d820c06abbf970d8
StevePro7 <steveprozune@hotmail.com>
2022-01-22 19:40:01 +0000

    no message

---
+++ Games/TurtleBridge/mario/12-SpriteTiles_JumpTest00/dev/engine/content_manager.c
@@ -0,0 +1,35 @@
+#include "content_manager.h"
+#include "../engine/global_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+#define FONT_TILES			0
+
+void engine_content_manager_load_tiles()
+{
+	// Font tiles.
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
+	//devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+}
+
+void engine_content_manager_load_turtle()
+{
+	// Turtle tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) game_tiles__tiles__psgcompr, 0 );
+	//devkit_SMS_loadBGPalette( ( void * ) game_tiles__palette__bin );
+
+	// Turtle tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) turtle00__tiles__psgcompr, 128 );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) turtle01__tiles__psgcompr, 144 );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) turtle02__tiles__psgcompr, 160 );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) turtle03__tiles__psgcompr, 176 );
+	//devkit_SMS_loadBGPalette( ( void * ) turtle00__palette__bin );
+
+	devkit_SMS_loadBGPalette( ( void * ) game_tiles__palette__bin );
+
+
+	// Sprite tiles.
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) mario01__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) left01__tiles__psgcompr, SPRITE_TILES + ( 0 * 4 ) );
+	devkit_SMS_loadSpritePalette( ( void * ) mario01__palette__bin );
+}
---
+++ Games/TurtleBridge/mario/12-SpriteTiles_JumpTest00/dev/scripts/gfx.bat
@@ -0,0 +1,32 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+::bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0   -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
+
+bmp2tile.exe raw/turtle00.png -savetiles "turtle00 (tiles).psgcompr" -noremovedupes -planar -tileoffset 128  -savetilemap "turtle00 (tilemap).bin" -savepalette "turtle00 (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/turtle01.png -savetiles "turtle01 (tiles).psgcompr" -noremovedupes -planar -tileoffset 144  -savetilemap "turtle01 (tilemap).bin" -savepalette "turtle01 (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/turtle02.png -savetiles "turtle02 (tiles).psgcompr" -noremovedupes -planar -tileoffset 160  -savetilemap "turtle02 (tilemap).bin" -savepalette "turtle02 (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/turtle03.png -savetiles "turtle03 (tiles).psgcompr" -noremovedupes -planar -tileoffset 176  -savetilemap "turtle03 (tilemap).bin" -savepalette "turtle03 (palette).bin" -fullpalette -exit
+
+:: Sprites
+bmp2tile.exe raw/f00_cent.png -savetiles "left01 (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "mario01 (palette).bin" -fullpalette  -spritepalette -exit
+
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
