24dcc9247a3fd683b63c761ef814883da74d18bf
StevePro7 <steveprozune@hotmail.com>
2022-01-23 20:15:07 +0000

    no message

---
+++ Games/TurtleBridge/mario/09b-SpriteTiles_Turtle_Wings/dev/content/gfx.h
@@ -0,0 +1,45 @@
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	game_tiles__palette__bin[];
+#define				game_tiles__palette__bin_size 16
+
+extern const unsigned char	game_tiles__tilemap__bin[];
+#define				game_tiles__tilemap__bin_size 64
+
+extern const unsigned char	game_tiles__tiles__psgcompr[];
+#define				game_tiles__tiles__psgcompr_size 403
+
+extern const unsigned char	left01__tiles__psgcompr[];
+#define				left01__tiles__psgcompr_size 80
+
+extern const unsigned char	left02__tiles__psgcompr[];
+#define				left02__tiles__psgcompr_size 78
+
+extern const unsigned char	left03__tiles__psgcompr[];
+#define				left03__tiles__psgcompr_size 81
+
+extern const unsigned char	left04__tiles__psgcompr[];
+#define				left04__tiles__psgcompr_size 77
+
+extern const unsigned char	mario01__palette__bin[];
+#define				mario01__palette__bin_size 16
+
+extern const unsigned char	rght01__tiles__psgcompr[];
+#define				rght01__tiles__psgcompr_size 80
+
+extern const unsigned char	rght02__tiles__psgcompr[];
+#define				rght02__tiles__psgcompr_size 80
+
+extern const unsigned char	rght03__tiles__psgcompr[];
+#define				rght03__tiles__psgcompr_size 80
+
+extern const unsigned char	rght04__tiles__psgcompr[];
+#define				rght04__tiles__psgcompr_size 77
+
---
+++ Games/TurtleBridge/mario/09b-SpriteTiles_Turtle_Wings/dev/scripts/gfx.bat
@@ -0,0 +1,24 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw/wingdown.png -savetiles "wingdown (tiles).psgcompr" -noremovedupes -planar -tileoffset 64   -savetilemap "wingdown (tilemap).bin" -savepalette "wings (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/wingup.png -savetiles "wingup (tiles).psgcompr" -noremovedupes -planar -tileoffset 80  -savetilemap "wingup (tilemap).bin" -savepalette "wings2 (palette).bin" -fullpalette -exit
+
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
