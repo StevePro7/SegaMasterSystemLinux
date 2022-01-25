6b763fa03eb4dce01532df0690ee3259d72dc1b2
StevePro7 <steveprozune@hotmail.com>
2022-01-25 20:54:24 +0000

    no message

--- Games/TurtleBridge/mario/14-BGTiles_SectionGround/dev/engine/sprite_manager.c
+++ Games/TurtleBridge/mario/14-BGTiles_SectionGround/dev/engine/sprite_manager.c
@@ -3,9 +3,22 @@
 #include "../devkit/_sms_manager.h"
 #include "../content/gfx.h"
 
-void engine_tile_manager_draw( unsigned char x, unsigned char y, unsigned char tile )
+void engine_tile_manager_draw( unsigned char x, unsigned char y )
 {
 	const unsigned char *pnt = game_tiles__tilemap__bin;
-	devkit_SMS_setNextTileatXY( x, y );
-	devkit_SMS_setTile( *pnt + tile );
+	unsigned char tile = 0;
+
+	unsigned char w;
+	unsigned char h;
+
+	for( h = 0; h < 6; h++ )
+	{
+		for( w = 0; w < 6; w++ )
+		{
+			devkit_SMS_setNextTileatXY( x + w, y + h );
+			devkit_SMS_setTile( *pnt + tile );
+			tile++;
+		}
+	}
+	
 }
--- Games/TurtleBridge/mario/14-BGTiles_SectionGround/dev/engine/sprite_manager.h
+++ Games/TurtleBridge/mario/14-BGTiles_SectionGround/dev/engine/sprite_manager.h
@@ -1,6 +1,6 @@
 #ifndef _SPRITE_MANAGER_H_
 #define _SPRITE_MANAGER_H_
 
-void engine_tile_manager_draw( unsigned char x, unsigned char y, unsigned char tile );
+void engine_tile_manager_draw( unsigned char x, unsigned char y );
 
 #endif//_SPRITE_MANAGER_H_
