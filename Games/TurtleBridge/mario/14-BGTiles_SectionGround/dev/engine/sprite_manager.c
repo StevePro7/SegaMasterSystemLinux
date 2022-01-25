c8d8f2c70e8155b4fbeef3308fc9084ea8c112d0
StevePro7 <steveprozune@hotmail.com>
2022-01-25 20:30:07 +0000

    no message

---
+++ Games/TurtleBridge/mario/14-BGTiles_SectionGround/dev/engine/sprite_manager.c
@@ -0,0 +1,14 @@
+#include "sprite_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+// zoom sprites
+void engine_sprite_manager_draw( unsigned char x, unsigned char y, unsigned int tile )
+{
+	const unsigned char size = 2;
+
+	devkit_SMS_addSprite( x + size * 0, y + size * 0, tile + 0 );
+	devkit_SMS_addSprite( x + size * 8, y + size * 0, tile + 1 );
+	devkit_SMS_addSprite( x + size * 0, y + size * 8, tile + 2 );
+	devkit_SMS_addSprite( x + size * 8, y + size * 8, tile + 3 );
+}
