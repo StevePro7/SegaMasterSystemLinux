bc7ef89bbb9caadcd361ad55e2d5efe1e43090c4
StevePro7 <steveprozune@hotmail.com>
2022-01-25 19:31:24 +0000

    no message

---
+++ Games/TurtleBridge/utils/01-Textures_Import/ScreenShotTest/AnGame.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Configuration;
+
+namespace ScreenShotTest
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	public class AnGame : Microsoft.Xna.Framework.Game
+	{
+		//const string file = "f02_rght";
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		RenderTarget2D renderTarget;
+		private Texture2D image;
+		private Texture2D pixel;
+		private const int offset = 0;
+
+		private bool save;
+		private string file;
+
+		private int width = 256 / 1;
+		private int height = 192 / 1;
+
+		//private IDictionary<string, Texture2D> dict = new Dictionary<string, Texture2D>();
+		//private int size = 1;
+		//private int pixl = 16;
+		//private string[] lines;
+
+		public AnGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = width; // pixl * size;
+			graphics.PreferredBackBufferHeight = height;// pixl * size;
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			file = "wb01a";
+			//if (null != ConfigurationManager.AppSettings["file"])
+			//{
+			//	file = Convert.ToString(ConfigurationManager.AppSettings["file"]);
+			//}
+			save = false;
+			//if (null != ConfigurationManager.AppSettings["save"])
+			//{
+			//	save = Convert.ToBoolean(ConfigurationManager.AppSettings["save"]);
+			//}
+			//save = true;
+			IsMouseVisible = true;
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			image = Content.Load<Texture2D>(file);
+
+			//const int hole = 32;
+			//const int half = 16;
+			Color[] texColors = new Color[(width * 1) * (height * 1)];
+			Color[] newColors = new Color[(width * 1) * (height * 1)];
+			image.GetData(texColors);
+			//lines = File.ReadAllLines("mario_sprite.csv");
+
+			pixel = new Texture2D(graphics.GraphicsDevice, width, height);
+			int j = 0;
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					int i = j;
+					//int i = y * height + x;
+					//if (x % 2 == 0)
+					{
+						newColors[j] = texColors[i];
+						j++;
+					}
+				}
+			}
+			pixel.SetData<Color>(newColors);
+
+			PresentationParameters pp = GraphicsDevice.PresentationParameters;
+			pp.BackBufferWidth = width;
+			pp.BackBufferHeight = height;
+			//width = pp.BackBufferWidth;
+			//height = pp.BackBufferHeight;
+			//renderTarget = new RenderTarget2D(GraphicsDevice, width, height, 1, GraphicsDevice.DisplayMode.Format);
+			renderTarget = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Color, DepthFormat.Depth24);
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// all content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// Allows the game to exit
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				this.Exit();
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			if (save)
+			{
+				//GraphicsDevice.SetRenderTarget(0, renderTarget);
+				GraphicsDevice.SetRenderTarget(renderTarget);
+				GraphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.CornflowerBlue, 1, 0);
+
+				Draw();
+				base.Draw(gameTime);
+
+				//GraphicsDevice.SetRenderTarget(0, null);
+				GraphicsDevice.SetRenderTarget(null);
+				//Texture2D resolvedTexture = renderTarget.GetTexture();
+				Texture2D resolvedTexture = (Texture2D)renderTarget;
+				//resolvedTexture.Save("00.jpg", ImageFileFormat.Jpg);
+				Stream stream = File.Create(file + ".png");
+				//resolvedTexture.SaveAsJpeg(stream, width, height);
+				resolvedTexture.SaveAsPng(stream, width, height);
+		
+				Exit();
+			}
+			else
+			{
+				Draw();
+				base.Draw(gameTime);
+			}
+		}
+
+		private void Draw()
+		{
+			graphics.GraphicsDevice.Clear(Color.Black);
+			spriteBatch.Begin();
+			spriteBatch.Draw(pixel, Vector2.Zero, Color.White);
+			spriteBatch.End();
+		}
+
+	}
+
+}
