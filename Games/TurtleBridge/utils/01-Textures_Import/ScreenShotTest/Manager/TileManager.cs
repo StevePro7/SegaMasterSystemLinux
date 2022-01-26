b5450dec54eb575a0fed137b4d845faed77213e0
StevePro7 <steveprozune@hotmail.com>
2022-01-26 19:40:19 +0000

    no message

--- Games/TurtleBridge/utils/01-Textures_Import/ScreenShotTest/Manager/TileManager.cs
+++ Games/TurtleBridge/utils/01-Textures_Import/ScreenShotTest/Manager/TileManager.cs
@@ -2,19 +2,28 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ScreenShotTest
 {
 	public class TileManager
 	{
 		ImageManager imageManager;
+		Dictionary<string, string> tilesDict;
+		List<string> tilemapList;
+
+		int keyTile, keyTileMap;
 		int px, py;
 		int tx, ty;
 
 		public TileManager(ImageManager imageManager)
 		{
 			this.imageManager = imageManager;
+			tilesDict = new Dictionary<string, string>();
+			tilemapList = new List<string>();
+			keyTile = 0;
+			keyTileMap = 0;
 		}
 
 		public void LoadContent(Texture2D texture)
@@ -28,13 +37,30 @@ namespace ScreenShotTest
 
 		public void Process()
 		{
+			tilesDict.Clear();
+			tilemapList.Clear();
+
 			int ix, iy;
 			ix = 0;
 			iy = 0;
 			int out_index = 0;
 			int inp_index = iy * ty + ix;
 
+			string txtTile;
 			var hash = imageManager.Process(inp_index, out_index);
+			var dictTiles = tilesDict.FirstOrDefault(x => x.Value == hash);
+			if (null == dictTiles.Key)
+			{
+				txtTile = keyTile.ToString().PadLeft(2, '0');
+				tilesDict.Add(txtTile, hash);
+				keyTileMap++;
+			}
+			else
+			{
+				txtTile = dictTiles.Key;
+			}
+
+			tilemapList.Add(txtTile);
 		}
 	}
 }
