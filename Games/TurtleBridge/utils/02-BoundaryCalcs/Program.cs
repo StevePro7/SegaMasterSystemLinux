8d12b39e5ea2a23d5fc12d23ecdcecbe43a5ebbd
StevePro7 <steveprozune@hotmail.com>
2022-01-27 09:13:35 +0000

    no message

---
+++ Games/TurtleBridge/utils/02-BoundaryCalcs/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace BinaryFileWrite
+{
+	class Program
+	{
+		static void Main()
+		{
+			//var fileName = ConfigurationManager.AppSettings["fileName"];
+			Console.WriteLine("Update sections:" );
+			var fm = new FileManager();
+			fm.GetFiles();
+
+			var fileNames = fm.Files;
+			foreach (var fileName in fileNames)
+			{
+				Console.WriteLine($"{fileName}");
+				fm.Process(fileName);
+
+				var inpLines = fm.Lines;
+				var outLines = new List<string>();
+
+				var firstTime = true;
+				var count = inpLines.Length;
+				for (int index = 0; index < count; index++)
+				{
+					var inpLine = inpLines[index];
+					var txtLine = inpLine;
+					var tmpLine = inpLine.Trim();
+
+					if (tmpLine.EndsWith(":"))
+					{
+						if (txtLine.StartsWith("-") || txtLine.StartsWith("+"))
+						{
+						}
+						else
+						{
+							if (firstTime)
+							{
+								firstTime = false;
+							}
+							else
+							{
+								var endLine = ".ends";
+								outLines.Add(endLine);
+								outLines.Add(String.Empty);
+							}
+
+							var section = txtLine.Substring(0, txtLine.Length - 1);
+							var sctLine = $".section \"{section}\" free";
+							outLines.Add(sctLine);
+						}
+					}
+
+					outLines.Add(txtLine);
+				}
+
+				if (!firstTime)
+				{
+					outLines.Add(".ends");
+				}
+
+				fm.Saving(outLines, fileName);
+			}
+
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
