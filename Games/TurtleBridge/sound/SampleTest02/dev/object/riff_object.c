86d97136c1e1e14b83c8f6f57adb2add488650b9
StevePro7 <steveprozune@hotmail.com>
2022-01-29 10:10:34 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest02/dev/object/riff_object.c
+++ Games/TurtleBridge/sound/SampleTest02/dev/object/riff_object.c
@@ -5,23 +5,32 @@
 #include "../banks/bank5.h"
 #include "../banks/bank6.h"
 #include "../banks/bank7.h"
+#include "../banks/bank8.h"
+#include "../banks/bank9.h"
+#include "../banks/bank10.h"
 
 const unsigned char *riff_sample_data[] =
 {
-	SoundFx_wav_pcmenc,
-	SoundFx03_wav_pcmenc,
-	SoundFx04_wav_pcmenc,
-	SoundFx05_wav_pcmenc,
-	SoundFx06_wav_pcmenc,
-	SoundFx07_wav_pcmenc,
+	Riff__01_wav_pcmenc,
+	Riff__02_wav_pcmenc,
+	Riff__03_wav_pcmenc,
+	Riff__04_wav_pcmenc,
+	Riff__05_wav_pcmenc,
+	Riff__06_wav_pcmenc,
+	Riff__07_wav_pcmenc,
+	Riff__08_wav_pcmenc,
+	Riff__09_wav_pcmenc,
 };
 
 const unsigned char riff_sample_bank[] =
 {
-	SoundFx_wav_pcmenc_bank,
-	SoundFx03_wav_pcmenc_bank,
-	SoundFx04_wav_pcmenc_bank,
-	SoundFx05_wav_pcmenc_bank,
-	SoundFx06_wav_pcmenc_bank,
-	SoundFx07_wav_pcmenc_bank,
+	Riff__01_wav_pcmenc_bank,
+	Riff__02_wav_pcmenc_bank,
+	Riff__03_wav_pcmenc_bank,
+	Riff__04_wav_pcmenc_bank,
+	Riff__05_wav_pcmenc_bank,
+	Riff__06_wav_pcmenc_bank,
+	Riff__07_wav_pcmenc_bank,
+	Riff__08_wav_pcmenc_bank,
+	Riff__09_wav_pcmenc_bank
 };
