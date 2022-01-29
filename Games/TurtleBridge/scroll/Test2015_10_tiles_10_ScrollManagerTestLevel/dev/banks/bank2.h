bd51f4037668d4a9754ddce03bf6bcffaddcd456
StevePro7 <steveprozune@hotmail.com>
2022-01-29 21:14:14 +0000

    no message

---
+++ Games/TurtleBridge/scroll/Test2015_10_tiles_10_ScrollManagerTestLevel/dev/banks/bank2.h
@@ -0,0 +1,4 @@
+extern const unsigned char	level0201_txt[];
+#define				level0201_txt_size 216
+#define				level0201_txt_bank 2
+
---
+++ Games/TurtleBridge/scroll/Test2015_10_tiles_10_ScrollManagerTestLevel/dev/banks/bank3.h
@@ -0,0 +1,3 @@
+extern const unsigned char	level0301_txt[];
+#define				level0301_txt_size 256
+#define				level0301_txt_bank 2
