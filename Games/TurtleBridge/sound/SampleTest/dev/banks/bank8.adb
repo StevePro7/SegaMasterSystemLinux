79063377fa1172a96c6e631e95f45e8c18dc00ed
StevePro7 <steveprozune@hotmail.com>
2022-01-30 21:47:01 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest/dev/banks/bank8.adb
+++ Games/TurtleBridge/sound/SampleTest/dev/banks/bank8.adb
@@ -1,2 +1,2 @@
 M:bank8
-S:G$SoundFx08_wav_pcmenc$0$0({15412}DA15412d,SC:U),D,0,0
+S:G$SoundFx08_wav_pcmenc$0$0({16145}DA16145d,SC:U),D,0,0
--- Games/TurtleBridge/sound/SampleTest/dev/banks/bank8.h
+++ Games/TurtleBridge/sound/SampleTest/dev/banks/bank8.h
@@ -1,4 +1,4 @@
 extern const unsigned char	SoundFx08_wav_pcmenc[];
-#define				SoundFx08_wav_pcmenc_size 15412
+#define				SoundFx08_wav_pcmenc_size 16145
 #define				SoundFx08_wav_pcmenc_bank 8
 
