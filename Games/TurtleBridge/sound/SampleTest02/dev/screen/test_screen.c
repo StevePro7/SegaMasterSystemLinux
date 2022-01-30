6ed8bd48b30d05c8572dcd21bd73a824f789267f
StevePro7 <steveprozune@hotmail.com>
2022-01-30 20:52:22 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest02/dev/screen/test_screen.c
+++ Games/TurtleBridge/sound/SampleTest02/dev/screen/test_screen.c
@@ -13,11 +13,43 @@ void screen_test_screen_load()
 	devkit_SMS_displayOff();
 	engine_content_manager_load_splash_screen();
 	devkit_SMS_displayOn();
-	engine_font_manager_draw_text( "PRESS FIRE 2 THEN FIRE 1", 4, 23 );
+	//engine_font_manager_draw_text( "PRESS FIRE 2 THEN FIRE 1", 4, 23 );
 	check = 0;
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
+{
+	//unsigned char input2;
+	unsigned char input1;
+	unsigned char value;
+
+	//input2 = engine_input_manager_hold( input_type_fire2 );
+	//if( input2 )
+	{
+		if( check == 0 )
+		{
+			check = 1;
+			for( value = 0; value < 9; value++ )
+			{
+			//	engine_font_manager_draw_text( "RIFF", 4, value );
+			//	engine_font_manager_draw_data( value, 24, value );
+				engine_riff_manager_play( value );
+
+				engine_input_manager_update();
+				input1 = engine_input_manager_move( input_type_fire1 );
+				if( input1 )
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	*screen_type = screen_type_test;
+}
+
+
+void screen_test_screen_update_input( unsigned char *screen_type )
 {
 	unsigned char input2;
 	unsigned char input1;
