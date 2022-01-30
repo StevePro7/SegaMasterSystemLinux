aaa6f9673cca1f9895ac77f0f51948a6b68d6754
StevePro7 <steveprozune@hotmail.com>
2022-01-30 20:31:08 +0000

    no message

--- Games/TurtleBridge/mario/18-GotrisFadeTest/dev/build.bat
+++ Games/TurtleBridge/mario/18-GotrisFadeTest/dev/build.bat
@@ -1,29 +1,29 @@
 @echo off
 
 :: Content
-folder2c ..\gfx gfx
-sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 gfx.c
+::folder2c ../gfx gfx
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 gfx.c
 
 :: Compile
 cd devkit
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 _sms_manager.c
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
 cd ..
 
 cd engine
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 asm_manager.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ..\peep-rules.txt --std-c99 content_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
 cd ..
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
 :: Link
 sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
-..\crt0\crt0_sms.rel main.rel ^
-..\lib\SMSlib.lib ^
-devkit\_sms_manager.rel ^
-engine\asm_manager.rel ^
-engine\content_manager.rel ^
-gfx.rel
+../crt0/crt0_sms.rel main.rel ^
+../lib/SMSlib.lib ^
+devkit/_sms_manager.rel ^
+engine/asm_manager.rel ^
+engine/content_manager.rel ^
+content/gfx.rel
 
 :: Execute
 ihx2sms output.ihx output.sms
@@ -49,5 +49,5 @@ if exist "*.noi" del "*.noi" > nul
 if exist "*.sym" del "*.sym" > nul
 
 :: Run
-java -jar C:\SEGA\Emulicious\Emulicious.jar output.sms
+java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
 ::output.sms
