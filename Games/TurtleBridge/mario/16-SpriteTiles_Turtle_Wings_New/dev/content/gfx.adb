477498469cb8a0fae41f3db47c83bc187126391c
StevePro7 <steveprozune@hotmail.com>
2022-01-30 12:26:56 +0000

    no message

--- Games/TurtleBridge/mario/16-SpriteTiles_Turtle_Wings_New/dev/content/gfx.adb
+++ Games/TurtleBridge/mario/16-SpriteTiles_Turtle_Wings_New/dev/content/gfx.adb
@@ -1,7 +1,7 @@
 M:gfx
+S:G$turtle__palette__bin$0$0({16}DA16d,SC:U),D,0,0
+S:G$turtle2__palette__bin$0$0({16}DA16d,SC:U),D,0,0
 S:G$turtleAA_32x24__tilemap__bin$0$0({24}DA24d,SC:U),D,0,0
 S:G$turtleAA_32x24__tiles__psgcompr$0$0({206}DA206d,SC:U),D,0,0
 S:G$turtleBB_32x24__tilemap__bin$0$0({24}DA24d,SC:U),D,0,0
 S:G$turtleBB_32x24__tiles__psgcompr$0$0({205}DA205d,SC:U),D,0,0
-S:G$wings__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$wings2__palette__bin$0$0({16}DA16d,SC:U),D,0,0
--- Games/TurtleBridge/mario/16-SpriteTiles_Turtle_Wings_New/dev/content/gfx.h
+++ Games/TurtleBridge/mario/16-SpriteTiles_Turtle_Wings_New/dev/content/gfx.h
@@ -1,3 +1,9 @@
+extern const unsigned char	turtle__palette__bin[];
+#define				turtle__palette__bin_size 16
+
+extern const unsigned char	turtle2__palette__bin[];
+#define				turtle2__palette__bin_size 16
+
 extern const unsigned char	turtleAA_32x24__tilemap__bin[];
 #define				turtleAA_32x24__tilemap__bin_size 24
 
@@ -9,10 +15,4 @@ extern const unsigned char	turtleBB_32x24__tilemap__bin[];
 
 extern const unsigned char	turtleBB_32x24__tiles__psgcompr[];
 #define				turtleBB_32x24__tiles__psgcompr_size 205
-
-extern const unsigned char	wings__palette__bin[];
-#define				wings__palette__bin_size 16
-
-extern const unsigned char	wings2__palette__bin[];
-#define				wings2__palette__bin_size 16
 
