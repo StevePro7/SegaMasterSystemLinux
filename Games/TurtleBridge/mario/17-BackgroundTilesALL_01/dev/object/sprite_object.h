bcb1606fd3a13fcf192e37b45b0b200e9e711f9e
StevePro7 <steveprozune@hotmail.com>
2022-01-30 14:33:22 +0000

    no message

---
+++ Games/TurtleBridge/mario/17-BackgroundTilesALL_01/dev/object/sprite_object.h
@@ -0,0 +1,8 @@
+//#ifndef _SPRITE_OBJECT_H_
+//#define _SPRITE_OBJECT_H_
+//
+//// Player.
+//extern const unsigned char *player_anim_data[];
+////extern const unsigned char player_anim_bank[];
+//
+//#endif//_SPRITE_OBJECT_H_
---
+++ Games/TurtleBridge/mario/17-BackgroundTilesALL_01/dev/scripts/gfx.bat
@@ -0,0 +1,24 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw/turtleAA_32x24.png -savetiles "turtleAA_32x24 (tiles).psgcompr" -noremovedupes -planar -tileoffset 64   -savetilemap "turtleAA_32x24 (tilemap).bin" -savepalette "turtle (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/turtleBB_32x24.png -savetiles "turtleBB_32x24 (tiles).psgcompr" -noremovedupes -planar -tileoffset 96  -savetilemap "turtleBB_32x24 (tilemap).bin" -savepalette "turtle2 (palette).bin" -fullpalette -exit
+
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
