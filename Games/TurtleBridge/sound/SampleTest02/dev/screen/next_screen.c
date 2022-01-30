2716addb47bf5dbf184ea7b1972aad9a68f8d5c1
StevePro7 <steveprozune@hotmail.com>
2022-01-30 21:56:42 +0000

    no message

--- Games/TurtleBridge/sound/SampleTest02/dev/screen/next_screen.c
+++ Games/TurtleBridge/sound/SampleTest02/dev/screen/next_screen.c
@@ -1,20 +1,19 @@
 #include "next_screen.h"
-#include "../engine/asm_manager.h"
-#include "../engine/content_manager.h"
 #include "../engine/enum_manager.h"
 #include "../engine/font_manager.h"
-#include "../devkit/_sms_manager.h"
 
 void screen_next_screen_load()
 {
 	devkit_SMS_displayOff();
-	engine_asm_manager_clear_VRAM();
-	engine_content_manager_load_tiles();
+	engine_content_manager_load_splash_screen();
 	devkit_SMS_displayOn();
-	engine_font_manager_draw_text( "NEXT SCREEN...", 4, 4 );
+	//engine_font_manager_draw_text( "PRESS FIRE 2 THEN FIRE 1", 4, 23 );
+	check = 0;
 }
 
 void screen_next_screen_update( unsigned char *screen_type )
 {
+	
+
 	*screen_type = screen_type_next;
 }
