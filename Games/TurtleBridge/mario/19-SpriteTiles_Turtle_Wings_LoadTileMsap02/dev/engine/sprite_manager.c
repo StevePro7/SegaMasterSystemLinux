56b1a405530592242518bc7cb6ca8e87b52b813e
StevePro7 <steveprozune@hotmail.com>
2022-01-31 21:10:02 +0000

    no message

--- Games/TurtleBridge/mario/19-SpriteTiles_Turtle_Wings_LoadTileMsap02/dev/engine/sprite_manager.c
+++ Games/TurtleBridge/mario/19-SpriteTiles_Turtle_Wings_LoadTileMsap02/dev/engine/sprite_manager.c
@@ -96,22 +96,58 @@ void engine_turtle_manager_draw_02( unsigned char x, unsigned char y )
 void engine_turtle_manager_draw_03( unsigned char x, unsigned char y )
 {
 	const unsigned char *pnt = turtleAA_32x24__tilemap__bin;
-	unsigned char index = 12;
+	unsigned char start = 12 * 2;
+	unsigned char index = 0;
+	unsigned char count = 0;
 
-	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 0 ] );
-	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 1 ] );
-	devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 2 ] );
-	devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 3 ] );
+	unsigned char ax, ay;
+	unsigned char mx, my;
+	unsigned char row = 0;
+	mx = 4;
+	my = 3;
 
-	devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 4 ] );
-	devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 5 ] );
-	devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 6 ] );
-	devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 7 ] );
+	for( ay = 0; ay < my; ay++ )
+	{
+		for( ax = 0; ax < mx; ax++ )
+		{
+			index = turtleAA_32x24__tilemap__bin[ start + count * 2 ];
+			count++;
 
-	devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 8 ] );
-	devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 9 ] );
-	devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 10 ] );
-	devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 11 ] );
+			devkit_SMS_setNextTileatXY( x + ax, y + ay );
+			devkit_SMS_setTile( *pnt + index );
+		}
+	}
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 0 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 1 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 2 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 3 * 2 ] );
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 4 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 5 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 6 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 7 * 2 ] );
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 8 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 9 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 10 * 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + turtleAA_32x24__tilemap__bin[ index + 11 * 2 ] );
+
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 0 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 1 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 2 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + array[ index + 3 ] );
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 4 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 5 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 6 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + array[ index + 7 ] );
+
+	//devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 8 ] );
+	//devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 9 ] );
+	//devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 10 ] );
+	//devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + array[ index + 11 ] );
 }
 
 void engine_turtle_manager_draw_04( unsigned char x, unsigned char y )
