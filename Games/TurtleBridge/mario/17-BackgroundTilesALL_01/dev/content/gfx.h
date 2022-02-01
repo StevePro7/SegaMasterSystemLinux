21c146a8a1c8dd8ea599e57f66515ff2978fb9fb
StevePro7 <steveprozune@hotmail.com>
2022-02-01 21:03:41 +0000

    no message

--- Games/TurtleBridge/mario/17-BackgroundTilesALL_01/dev/content/gfx.h
+++ Games/TurtleBridge/mario/17-BackgroundTilesALL_01/dev/content/gfx.h
@@ -2,8 +2,8 @@ extern const unsigned char	game_tiles__palette__bin[];
 #define				game_tiles__palette__bin_size 16
 
 extern const unsigned char	game_tiles__tilemap__bin[];
-#define				game_tiles__tilemap__bin_size 512
+#define				game_tiles__tilemap__bin_size 1216
 
 extern const unsigned char	game_tiles__tiles__psgcompr[];
-#define				game_tiles__tiles__psgcompr_size 3493
+#define				game_tiles__tiles__psgcompr_size 3837
 
