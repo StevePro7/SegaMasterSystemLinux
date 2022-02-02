ea61bc427751e7b81dbe56ae7cb43bbcdd4d0844
StevePro7 <steveprozune@hotmail.com>
2022-02-02 20:44:09 +0000

    no message

--- Games/TurtleBridge/mario/20-BackgroundTilesALL_02_New/dev/engine/tile_manager.c
+++ Games/TurtleBridge/mario/20-BackgroundTilesALL_02_New/dev/engine/tile_manager.c
@@ -27,50 +27,37 @@ void engine_tile_manager_turtle01( unsigned char x, unsigned char y )
 void engine_tile_manager_turtle02( unsigned char x, unsigned char y )
 {
 	const unsigned char *pnt = game_tiles__tilemap__bin;
-	unsigned char array[ 24 ] = { 64,65,66,0,67,68,69,70,71,72,73,74,0, };
+	unsigned int array[ 24 ] = { 152,154,156,0,256,186,260,262,216,218,220,294, };
 
-	unsigned char idx, val;
-	unsigned char ax, ay;
-	ax = 0;
-	ay = 0;
+	unsigned char idx;
+	unsigned int val;
+	unsigned char ix, iy;
 
-	//idx = 0;
-	//val = 128;// val = array[ idx ] * 2;
-	//devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ val ], 2 );
+	for( iy = 0; iy < 3; iy++ )
+	{
+		for( ix = 0; ix < 4; ix++ )
+		{
+			idx = iy * 4 + ix;
+			val = array[ idx ];
 
-	//ax = 1;
-	//idx = 1;
-	//val = array[ idx ] * 2;
-	//devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ val ], 2 );
-	//ax = 2;
-	//idx = 2;
-	//val = array[ idx ] * 2;
-	//devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ val ], 2 );
+			devkit_SMS_loadTileMap( x + ix, y + iy, ( void * ) &pnt[ val ], 2 );
+		}
+	}
 
-	ax = 0;
-	ay = 1;
-	idx = 4;
-	val = array[ idx ] * 2;
-	val = 11 * 16;
-	devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 0 ];	devkit_SMS_loadTileMap( x + 0, y + 0, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 1 ];	devkit_SMS_loadTileMap( x + 1, y + 0, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 2 ];	devkit_SMS_loadTileMap( x + 2, y + 0, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 3 ];	devkit_SMS_loadTileMap( x + 3, y + 0, ( void * ) &pnt[ val ], 2 );
 
-	/*ax = 1;
-	ay = 1;
-	idx = 5;
-	val = array[ idx ] * 2;
-	devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ val ], 2 );*/
-	//unsigned char ax, ay;
-	//unsigned char mx, my;
-	//unsigned char row = 0;
-	//mx = 4;
-	//my = 3;
+	//val = array[ 4 ];	devkit_SMS_loadTileMap( x + 0, y + 1, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 5 ];	devkit_SMS_loadTileMap( x + 1, y + 1, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 6 ];	devkit_SMS_loadTileMap( x + 2, y + 1, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 7 ];	devkit_SMS_loadTileMap( x + 3, y + 1, ( void * ) &pnt[ val ], 2 );
 
-	//for( ax = 0; ax < mx; ax++ )
-	//{
-	//	for( ay = 0 + ( row*my ); ay < my + ( row*my ); ay++ )
-	//	{
-	//		index = ( ( ay * mx ) + ax ) * 2;
-	//		devkit_SMS_loadTileMap( x + ax, y + ay, ( void * ) &pnt[ index ], 2 );
-	//	}
-	//}
+	//val = array[ 8 ];	devkit_SMS_loadTileMap( x + 0, y + 2, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 9 ];	devkit_SMS_loadTileMap( x + 1, y + 2, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 10 ];	devkit_SMS_loadTileMap( x + 2, y + 2, ( void * ) &pnt[ val ], 2 );
+	//val = array[ 11 ];	devkit_SMS_loadTileMap( x + 3, y + 2, ( void * ) &pnt[ val ], 2 );
+
+	
 }
