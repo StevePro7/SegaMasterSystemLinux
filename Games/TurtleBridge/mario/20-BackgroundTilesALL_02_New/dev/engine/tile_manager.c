90b81c6a8f034df27a2d16936f491c688f02f81a
StevePro7 <steveprozune@hotmail.com>
2022-02-02 18:51:37 +0000

    no message

--- Games/TurtleBridge/mario/20-BackgroundTilesALL_02_New/dev/engine/tile_manager.c
+++ Games/TurtleBridge/mario/20-BackgroundTilesALL_02_New/dev/engine/tile_manager.c
@@ -5,26 +5,42 @@
 void engine_tile_manager_turtle01( unsigned char x, unsigned char y )
 {
 	const unsigned char *pnt = game_tiles__tilemap__bin;
+	unsigned char start = 64;
 
-	unsigned char start = 0;
-	unsigned char index = 0;
-	unsigned char count = 64;
+	devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + start + 0 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 0 );	devkit_SMS_setTile( *pnt + start + 1 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 0 );	devkit_SMS_setTile( *pnt + start + 2 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 0 );	devkit_SMS_setTile( *pnt + 0 );
 
-	unsigned char ax, ay;
-	unsigned char mx, my;
-	unsigned char row = 0;
-	mx = 4;
-	my = 3;
+	devkit_SMS_setNextTileatXY( x + 0, y + 1 );	devkit_SMS_setTile( *pnt + start + 3 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 1 );	devkit_SMS_setTile( *pnt + start + 4 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 1 );	devkit_SMS_setTile( *pnt + start + 5 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 1 );	devkit_SMS_setTile( *pnt + start + 6 );
 
-	for( ay = 0; ay < my; ay++ )
-	{
-		for( ax = 0; ax < mx; ax++ )
-		{
-			index = pnt[ start + count * 2 ];
-			count++;
+	devkit_SMS_setNextTileatXY( x + 0, y + 2 );	devkit_SMS_setTile( *pnt + start + 7 );
+	devkit_SMS_setNextTileatXY( x + 1, y + 2 );	devkit_SMS_setTile( *pnt + start + 8 );
+	devkit_SMS_setNextTileatXY( x + 2, y + 2 );	devkit_SMS_setTile( *pnt + start + 9 );
+	devkit_SMS_setNextTileatXY( x + 3, y + 2 );	devkit_SMS_setTile( *pnt + start + 10 );
 
-			devkit_SMS_setNextTileatXY( x + ax, y + ay );
-			devkit_SMS_setTile( *pnt + index );
-		}
-	}
+	//unsigned char start = 64 * 2;
+	//unsigned char index = 0;
+	//unsigned char count = 0;
+
+	//unsigned char ax, ay;
+	//unsigned char mx, my;
+	//unsigned char row = 0;
+	//mx = 4;
+	//my = 3;
+
+	//for( ay = 0; ay < my; ay++ )
+	//{
+	//	for( ax = 0; ax < mx; ax++ )
+	//	{
+	//		index = pnt[ start + count * 2 ];
+	//		count++;
+
+	//		devkit_SMS_setNextTileatXY( x + ax, y + ay );
+	//		devkit_SMS_setTile( *pnt + start + index );
+	//	}
+	//}
 }
