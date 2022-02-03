6d9cecb3cf5d3c50b24a2e4d4be4f54ccdac626b
StevePro7 <steveprozune@hotmail.com>
2022-02-03 21:41:51 +0000

    no message

--- TurtleBridge/mario/21-TitleTurtleBridgeTesting/dev/content/gfx.h
+++ TurtleBridge/mario/21-TitleTurtleBridgeTesting/dev/content/gfx.h
@@ -1,9 +1,81 @@
-extern const unsigned char	game_tiles__palette__bin[];
-#define				game_tiles__palette__bin_size 16
+extern const unsigned char	game_tilesB__palette__bin[];
+#define				game_tilesB__palette__bin_size 16
 
-extern const unsigned char	game_tiles__tilemap__bin[];
-#define				game_tiles__tilemap__bin_size 506
+extern const unsigned char	game_tilesB__tilemap__bin[];
+#define				game_tilesB__tilemap__bin_size 8
 
-extern const unsigned char	game_tiles__tiles__psgcompr[];
-#define				game_tiles__tiles__psgcompr_size 1319
+extern const unsigned char	game_tilesB__tiles__psgcompr[];
+#define				game_tilesB__tiles__psgcompr_size 34
+
+extern const unsigned char	game_tilesD__palette__bin[];
+#define				game_tilesD__palette__bin_size 16
+
+extern const unsigned char	game_tilesD__tilemap__bin[];
+#define				game_tilesD__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesD__tiles__psgcompr[];
+#define				game_tilesD__tiles__psgcompr_size 30
+
+extern const unsigned char	game_tilesE__palette__bin[];
+#define				game_tilesE__palette__bin_size 16
+
+extern const unsigned char	game_tilesE__tilemap__bin[];
+#define				game_tilesE__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesE__tiles__psgcompr[];
+#define				game_tilesE__tiles__psgcompr_size 30
+
+extern const unsigned char	game_tilesG__palette__bin[];
+#define				game_tilesG__palette__bin_size 16
+
+extern const unsigned char	game_tilesG__tilemap__bin[];
+#define				game_tilesG__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesG__tiles__psgcompr[];
+#define				game_tilesG__tiles__psgcompr_size 37
+
+extern const unsigned char	game_tilesI__palette__bin[];
+#define				game_tilesI__palette__bin_size 16
+
+extern const unsigned char	game_tilesI__tilemap__bin[];
+#define				game_tilesI__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesI__tiles__psgcompr[];
+#define				game_tilesI__tiles__psgcompr_size 21
+
+extern const unsigned char	game_tilesL__palette__bin[];
+#define				game_tilesL__palette__bin_size 16
+
+extern const unsigned char	game_tilesL__tilemap__bin[];
+#define				game_tilesL__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesL__tiles__psgcompr[];
+#define				game_tilesL__tiles__psgcompr_size 21
+
+extern const unsigned char	game_tilesR__palette__bin[];
+#define				game_tilesR__palette__bin_size 16
+
+extern const unsigned char	game_tilesR__tilemap__bin[];
+#define				game_tilesR__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesR__tiles__psgcompr[];
+#define				game_tilesR__tiles__psgcompr_size 35
+
+extern const unsigned char	game_tilesT__palette__bin[];
+#define				game_tilesT__palette__bin_size 16
+
+extern const unsigned char	game_tilesT__tilemap__bin[];
+#define				game_tilesT__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesT__tiles__psgcompr[];
+#define				game_tilesT__tiles__psgcompr_size 25
+
+extern const unsigned char	game_tilesU__palette__bin[];
+#define				game_tilesU__palette__bin_size 16
+
+extern const unsigned char	game_tilesU__tilemap__bin[];
+#define				game_tilesU__tilemap__bin_size 8
+
+extern const unsigned char	game_tilesU__tiles__psgcompr[];
+#define				game_tilesU__tiles__psgcompr_size 28
 
