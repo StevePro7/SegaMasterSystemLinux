0f895afbef27a2f272b474d7e024dc81236e1b3a
StevePro7 <steveprozune@hotmail.com>
2022-02-04 10:56:35 +0000

    no message

--- TurtleBridge/mario/21-TitleTurtleBridgeTesting/dev/content/gfx.adb
+++ TurtleBridge/mario/21-TitleTurtleBridgeTesting/dev/content/gfx.adb
@@ -1,4 +1,4 @@
 M:gfx
 S:G$game_tiles__palette__bin$0$0({16}DA16d,SC:U),D,0,0
 S:G$game_tiles__tilemap__bin$0$0({96}DA96d,SC:U),D,0,0
-S:G$game_tiles__tiles__psgcompr$0$0({329}DA329d,SC:U),D,0,0
+S:G$game_tiles__tiles__psgcompr$0$0({2}DA2d,SC:U),D,0,0
