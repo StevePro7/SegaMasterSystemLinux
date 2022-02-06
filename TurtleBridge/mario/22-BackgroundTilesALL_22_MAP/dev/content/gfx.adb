33cd19753f3f450bc3c4f83bd27eb13913f3a063
StevePro7 <steveprozune@hotmail.com>
2022-02-06 17:33:29 +0000

    no message

--- TurtleBridge/mario/22-BackgroundTilesALL_22_MAP/dev/content/gfx.adb
+++
@@ -1,4 +0,0 @@
-M:gfx
-S:G$game_tiles__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$game_tiles__tilemap__bin$0$0({64}DA64d,SC:U),D,0,0
-S:G$game_tiles__tiles__psgcompr$0$0({340}DA340d,SC:U),D,0,0
--- TurtleBridge/mario/22-BackgroundTilesALL_22_MAP/dev/content/gfx.h
+++
@@ -1,9 +0,0 @@
-extern const unsigned char	game_tiles__palette__bin[];
-#define				game_tiles__palette__bin_size 16
-
-extern const unsigned char	game_tiles__tilemap__bin[];
-#define				game_tiles__tilemap__bin_size 64
-
-extern const unsigned char	game_tiles__tiles__psgcompr[];
-#define				game_tiles__tiles__psgcompr_size 340
-
