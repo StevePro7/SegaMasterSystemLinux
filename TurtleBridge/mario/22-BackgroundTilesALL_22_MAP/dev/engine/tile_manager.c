ce8bb2b294c924cd89371da32285c1c1ee56e7dc
StevePro7 <steveprozune@hotmail.com>
2022-02-06 21:22:17 +0000

    no message

--- TurtleBridge/mario/22-BackgroundTilesALL_22_MAP/dev/engine/tile_manager.c
+++ TurtleBridge/mario/22-BackgroundTilesALL_22_MAP/dev/engine/tile_manager.c
@@ -60,32 +60,100 @@ void engine_tile_manager_section01( unsigned char x, unsigned char y )
 	draw_tile_full( array, x, y, w, h );
 }
 
+//void engine_tile_manager_section01_left( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *tiles = game_tiles__tilemap__bin;
+//	const unsigned char *array = tile_object_data[ tile_type_section01 ];
+//	const unsigned char w = 16;
+//	const unsigned char h = 10;
+//
+//	unsigned char idx;
+//	unsigned char val;
+//	unsigned int off;
+//	unsigned char col, row;
+//
+//	unsigned char *tile;
+//	unsigned int flip = devkit_TILE_FLIPPED_Y();
+//	for( row = 0; row < h; row++ )
+//	{
+//		for( col = 0; col < w; col++ )
+//		{
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			off = val * 2;
+//
+//			tile = ( void * ) &tiles[ off ];
+//			*tile = *tile | flip;
+//
+//			devkit_SMS_loadTileMap( x + col, y + row, tile, 2 );
+//			devkit_SMS_loadTileMap( x + col, y + row, ( void * ) &tiles[ off ], 2 );
+//		}
+//	}
+//}
+
 void engine_tile_manager_section01_left( unsigned char x, unsigned char y )
 {
 	const unsigned char *tiles = game_tiles__tilemap__bin;
 	const unsigned char *array = tile_object_data[ tile_type_section01 ];
+
 	const unsigned char w = 16;
 	const unsigned char h = 10;
+	unsigned int flip = devkit_TILE_FLIPPED_X();
 
 	unsigned char idx;
-	unsigned int val;
 	unsigned char row, col;
+	unsigned char val;
+	unsigned char tmp;
+	row = 0;
+	col = 0;
+	//val = 163;
 
-	for( row = 0; row < h; row++ )
+	//row = 1;
+	for( col = w; col >0; col-- )
 	{
-		for( col = 0; col < w; col++ )
-		{
-			idx = row * w + col;
-			val = array[ idx ];
-			if( val != 64 )
-			{
-				val--;
-			}
-			devkit_SMS_setNextTileatXY( x + col, y + row );
-			devkit_SMS_setTile( *tiles + val );
-		}
+		tmp = col - 1;
+		idx = row * w + tmp;
+		val = array[ idx ];
+		val--;
+		devkit_SMS_setNextTileatXY( x + tmp, y + row );	devkit_SMS_setTile( ( *tiles + val ) | flip );
 	}
+
+	col = 0;
+	row = 1;
+	//devkit_SMS_setNextTileatXY( x + col, y + row );	devkit_SMS_setTile( ( *tiles + 167 ) | flip );
 }
+
+//void engine_tile_manager_section01_left( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *tiles = game_tiles__tilemap__bin;
+//	const unsigned char *array = tile_object_data[ tile_type_section01 ];
+//	const unsigned char w = 16;
+//	const unsigned char h = 10;
+//
+//	unsigned char idx;
+//	unsigned int val;
+//	unsigned char row, col;
+//
+//	unsigned char tmp;
+//	//unsigned int flip = devkit_TILE_FLIPPED_X();
+//
+//	for( row = 0; row < h; row++ )
+//	{
+//		for( col = w; col > 0; col++ )
+//		{
+//			tmp = col - 1;
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			//if( val != 64 )
+//			//{
+//			//	val--;
+//			//}
+//			devkit_SMS_setNextTileatXY( x + col, y + row );
+//			//devkit_SMS_setTile( ( *tiles + val ) | flip );
+//			devkit_SMS_setTile( *tiles + val );
+//		}
+//	}
+//}
 
 void engine_tile_manager_section02( unsigned char x, unsigned char y )
 {
