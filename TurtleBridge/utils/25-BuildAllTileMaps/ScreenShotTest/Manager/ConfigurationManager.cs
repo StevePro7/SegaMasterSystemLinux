44f09082a6f30b9c77c06a0a21ed12cee460b830
StevePro7 <steveprozune@hotmail.com>
2022-02-06 17:27:53 +0000

    no message

--- TurtleBridge/utils/25-BuildAllTileMaps/ScreenShotTest/Manager/ConfigurationManager.cs
+++ TurtleBridge/utils/25-BuildAllTileMaps/ScreenShotTest/Manager/ConfigurationManager.cs
@@ -6,16 +6,37 @@ namespace ScreenShotTest
 	{
 		public void Initialize()
 		{
-			string files = String.Empty;
-			if (System.Configuration.ConfigurationManager.AppSettings["files"] != null)
+			//string files = String.Empty;
+			//if (System.Configuration.ConfigurationManager.AppSettings["files"] != null)
+			//{
+			//	files = System.Configuration.ConfigurationManager.AppSettings["files"];
+			//	Files = files.Split(new char[] { ',' });
+			//}
+			//else
+			//{
+			//	Files = new string[0];
+			//}
+
+			Files = new string[17]
 			{
-				files = System.Configuration.ConfigurationManager.AppSettings["files"];
-				Files = files.Split(new char[] { ',' });
-			}
-			else
-			{
-				Files = new string[0];
-			}
+				"font_tiles",
+				"wave_strip",
+				"sea_turtleAA01_32x24",
+				"sea_turtleAA02_32x24",
+				"sea_turtleAA03_32x24",
+				"sea_turtleBB01_32x24",
+				"sea_turtleBB02_32x24",
+				"sea_turtleBB03_32x24",
+				"flying_turtle01_32x24",
+				"flying_turtle02_32x24",
+				"cloud01_32x24",
+				"cloud02_48x24",
+				"goal_sign",
+				"sign02",
+				"wonderboy32_01",
+				"wonderboy32_02",
+				"wonderboy32_03",
+			};
 		}
 
 		public string[] Files { get; private set; }
