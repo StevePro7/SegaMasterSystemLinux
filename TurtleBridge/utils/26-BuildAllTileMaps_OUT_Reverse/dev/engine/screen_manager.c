f283c8c888d65a16dfb1f2671168f7a4be0726b6
StevePro7 <steveprozune@hotmail.com>
2022-02-07 15:07:41 +0000

    no message

---
+++ TurtleBridge/utils/26-BuildAllTileMaps_OUT_Reverse/dev/engine/screen_manager.c
@@ -0,0 +1,41 @@
+#include "screen_manager.h"
+#include "audio_manager.h"
+#include "content_manager.h"
+#include "font_manager.h"
+#include "global_manager.h"
+#include "input_manager.h"
+#include "sprite_manager.h"
+#include "tile_manager.h"
+
+void engine_screen_manager_init()
+{
+	//engine_turtle_manager_draw_sea();
+	engine_font_manager_draw_text( "NEW TEST 7", 2, 0 );
+
+	engine_tile_manager_cloud01( 10, 4 );
+	engine_tile_manager_cloud02( 20, 2 );
+	//engine_turtle_manager_draw_title( 6, 2 );
+
+	//engine_tile_manager_turtle01( 5, 15 );
+	//engine_tile_manager_turtle04( 9, 5 );
+	//engine_tile_manager_turtle01( 5, 10 );
+	//engine_tile_manager_turtle02( 10, 11 );
+	//engine_tile_manager_turtle03( 15, 12 );
+	//engine_tile_manager_turtle04( 5, 16 );
+	//engine_tile_manager_turtle05( 10, 17 );
+	//engine_tile_manager_turtle06( 15, 18 );
+	//engine_tile_manager_turtle07( 25, 10 );
+	//engine_tile_manager_turtle08( 25, 16 );
+
+	//engine_tile_manager_section01( 12, 12 );
+	//engine_tile_manager_section02( 12, 2 );
+
+	//engine_tile_manager_ground01( 5, 5 );
+	//engine_tile_manager_ground02( 7, 14 );
+}
+
+void engine_screen_manager_update()
+{
+	//unsigned char input;
+	//input = engine_input_manager_hold_down();
+}
---
+++ TurtleBridge/utils/26-BuildAllTileMaps_OUT_Reverse/dev/engine/sprite_manager.c
@@ -0,0 +1,105 @@
+#include "sprite_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+void engine_turtle_manager_draw_sea()
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	unsigned char x, y;
+	for( y = 0; y < 24; y++ )
+	{
+		for( x = 0; x < 32; x++ )
+		{
+			devkit_SMS_setNextTileatXY( x + 0, y + 0 );	devkit_SMS_setTile( *pnt + 19 );
+		}
+	}
+}
+
+void draw_tops( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	unsigned char z;
+
+	for( z = 0; z < 23; z++ )
+	{
+		devkit_SMS_setNextTileatXY( x + z, y + 0 );
+		devkit_SMS_setTile( *pnt + 139 );
+	}
+}
+
+
+void draw_ones( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	devkit_SMS_setNextTileatXY( x + 0, y + 0 );
+	devkit_SMS_setTile( *pnt + 139 );
+}
+
+void draw_four( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	unsigned char z;
+
+	for( z = 0; z < 3; z++ )
+	{
+		devkit_SMS_setNextTileatXY( x + z, y + 0 );
+		devkit_SMS_setTile( *pnt + 139 );
+	}
+}
+void draw_five( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	unsigned char z;
+
+	for( z = 0; z < 5; z++ )
+	{
+		devkit_SMS_setNextTileatXY( x + 0, y + z );
+		devkit_SMS_setTile( *pnt + 139 );
+	}
+}
+
+void engine_turtle_manager_draw_title( unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = game_tiles__tilemap__bin;
+	draw_tops( x, y - 2 );
+
+	draw_four( x + 0, y + 0 );
+	draw_five( x + 1, y + 0);
+
+	draw_five( x + 4, y + 0 );
+	draw_five( x + 6, y + 0 );
+	draw_four( x + 4, y + 4 );
+
+	draw_five( x + 8, y + 0);
+	draw_ones( x + 9, y + 0 );
+	draw_ones( x + 9, y + 2 );
+	draw_ones( x + 10, y + 1 );
+	draw_ones( x + 10, y + 3 );
+	draw_ones( x + 10, y + 4 );
+
+	draw_four( x + 12, y + 0 );
+	draw_five( x + 13, y + 0 );
+
+	draw_five( x + 16, y + 0 );
+	draw_four( x + 16, y + 4 );
+
+	draw_four( x + 20, y + 0 );
+	draw_four( x + 20, y + 2 );
+	draw_four( x + 20, y + 4 );
+	draw_five( x + 20, y + 0 );
+
+	//draw_ones( x + 9, y + 2 );
+	//draw_ones( x + 10, y + 1 );
+	//draw_ones( x + 10, y + 3 );
+	//draw_ones( x + 10, y + 4 );
+	
+//	draw_five( x + 13, y );
+//
+//	draw_five( x + 15, y );
+//	draw_four( x + 15, y + 6 );
+//
+//	draw_five( x + 19, y );
+//	draw_four( x + 19, y + 0 );
+//	draw_four( x + 19, y + 2 );
+//	draw_four( x + 19, y + 4 );
+}
