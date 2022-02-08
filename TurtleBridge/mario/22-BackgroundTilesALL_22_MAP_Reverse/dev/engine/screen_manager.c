63757c5d03e3f4136f26c972b11b02e21df84d18
StevePro7 <steveprozune@hotmail.com>
2022-02-08 20:09:17 +0000

    no message

--- TurtleBridge/mario/22-BackgroundTilesALL_22_MAP_Reverse/dev/engine/screen_manager.c
+++ TurtleBridge/mario/22-BackgroundTilesALL_22_MAP_Reverse/dev/engine/screen_manager.c
@@ -15,8 +15,8 @@ void engine_screen_manager_init()
 	engine_tile_manager_sky();
 	engine_font_manager_draw_text( "REVERSE!!", 2, 2 );
 
-	//engine_tile_manager_section01_left( 0, 14 );
-	//engine_tile_manager_section01( 16, 14 );
+	engine_tile_manager_section01_left( 0, 14 );
+	engine_tile_manager_section01( 16, 14 );
 
 	//engine_tile_manager_section02( 10, 4 );
 	engine_tile_manager_section03( 10, 4 );
