e1d72828e8af31f07690f9808d85259a97a27848
StevePro7 <steveprozune@hotmail.com>
2022-02-08 21:37:15 +0000

    no message

--- TurtleBridge/mario/22-BackgroundTilesALL_22_MAP_Reverse/dev/engine/tile_manager.c
+++ TurtleBridge/mario/22-BackgroundTilesALL_22_MAP_Reverse/dev/engine/tile_manager.c
@@ -52,6 +52,27 @@ void engine_tile_manager_sea()
 		devkit_SMS_setNextTileatXY( col + 2, row );	devkit_SMS_setTile( *tiles + idx + 1 );
 		devkit_SMS_setNextTileatXY( col + 3, row );	devkit_SMS_setTile( *tiles + idx + 2 );
 	}
+
+	idx = 73;
+	row = 22;
+	for( col = 0; col < SCREEN_WIDE; col += 4 )
+	{
+		devkit_SMS_setNextTileatXY( col + 3, row ); devkit_SMS_setTile( *tiles + idx + 0 );
+		//devkit_SMS_setNextTileatXY( col + 2, row ); devkit_SMS_setTile( *tiles + idx + 1 );
+	}
+	
+	//devkit_SMS_setNextTileatXY( 30, row );	devkit_SMS_setTile( *tiles + 70 );
+	//devkit_SMS_setNextTileatXY( 29, row );	devkit_SMS_setTile( *tiles + 69 );
+
+	idx = 69;
+	row = 23;
+	for( col = 0; col < SCREEN_WIDE; col += 4 )
+	{
+		devkit_SMS_setNextTileatXY( col + 1, row ); devkit_SMS_setTile( *tiles + idx + 0 );
+		devkit_SMS_setNextTileatXY( col + 2, row ); devkit_SMS_setTile( *tiles + idx + 1 );
+	}
+
+	//devkit_SMS_setNextTileatXY( 31, row );	devkit_SMS_setTile( *tiles + 73 + 0 );
 }
 
 void engine_tile_manager_turtle( unsigned char type, unsigned char x, unsigned char y )
