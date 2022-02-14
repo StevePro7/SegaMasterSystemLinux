cbbe219ee95e31e505fe3ed1ffd459a4b8f65d03
StevePro7 <steveprozune@hotmail.com>
2022-02-14 18:55:13 +0000

    no message

--- NewBanking/07-BanksMacSMS/dev/engine/content_manager.c
+++ NewBanking/07-BanksMacSMS/dev/engine/content_manager.c
@@ -9,7 +9,7 @@
 void engine_content_manager_load_tiles()
 {	
 	// Font tiles.
-	devkit_SMS_loadPSGaidencompressedTiles( font__tiles__psgcompr, FONT_TILES );
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char* ) font__tiles__psgcompr, FONT_TILES );
 	devkit_SMS_loadBGPalette( ( void * ) font__palette__bin );
 
 }
