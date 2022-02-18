d783856b03a5f7d27f9deed499575fd2a30d30a2
StevePro7 <steveprozune@hotmail.com>
2022-02-18 21:22:24 +0000

    no message

--- TurtleBridge/mario/25-BackgroundSscrollTilesTest/dev/engine/level_manager.c
+++ TurtleBridge/mario/25-BackgroundSscrollTilesTest/dev/engine/level_manager.c
@@ -1,5 +1,6 @@
 #include "level_manager.h"
 #include "enum_manager.h"
+#include "global_manager.h"
 #include "tile_manager.h"
 
 // Global variable.
@@ -12,9 +13,12 @@ void engine_level_manager_init()
 void engine_level_manager_update( unsigned char column_X, unsigned int scroll_X )
 {
 	unsigned char sect;
-
-	engine_tile_manager_blank_column( column_X );
+	//unsigned char four;
+	//engine_tile_manager_blank_column( column_X );
 
 	sect = level_object_sect[ scroll_X ];
-	engine_tile_manager_draw_pipe( tile_type_section03, column_X, 15, sect );
+	engine_tile_manager_draw_pipe( tile_type_section03, column_X, WAVES_HIGH - 3, sect );
+
+	//four = column_X % 4;
+	//engine_tile_manager_draw_pipe( tile_type_sea_tiles, column_X, WAVES_HIGH, four );
 }
