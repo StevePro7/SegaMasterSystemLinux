93cb5c6010a5ab0f1b6943fdeedecf7c616c12b6
StevePro7 <steveprozune@hotmail.com>
2022-02-22 19:44:06 +0000

    no message

---
+++ TurtleBridge/maths/03-JumpTestUpRight/dev/content/gfx.h
@@ -0,0 +1,15 @@
+extern const unsigned char	font_tiles__palette__bin[];
+#define				font_tiles__palette__bin_size 16
+
+extern const unsigned char	font_tiles__tilemap__bin[];
+#define				font_tiles__tilemap__bin_size 128
+
+extern const unsigned char	font_tiles__tiles__psgcompr[];
+#define				font_tiles__tiles__psgcompr_size 663
+
+extern const unsigned char	left01__tiles__psgcompr[];
+#define				left01__tiles__psgcompr_size 88
+
+extern const unsigned char	mario01__palette__bin[];
+#define				mario01__palette__bin_size 16
+
