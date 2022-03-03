826581bcd20dc00c7e9bc0601cc60a36a816d33d
StevePro7 <steveprozune@hotmail.com>
2022-03-03 20:48:37 +0000

    no message

--- TurtleBridge/level/02-LevelScreen/LevelScreen/Manager/FileManager.cs
+++ TurtleBridge/level/02-LevelScreen/LevelScreen/Manager/FileManager.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace LevelScreen
 {
@@ -20,13 +14,18 @@ namespace LevelScreen
 
 		public void Initialize()
 		{
-			int index = 0;
 			int gridX = configManager.GridsXWide;
 
 			PlaneA = new byte[gridX];
 			PlaneB = new byte[gridX];
 
-			var file = "PlaneA.csv";
+			InitPlane(gridX, "PlaneA.csv", PlaneA);
+			InitPlane(gridX, "PlaneB.csv", PlaneB);
+		}
+
+		private void InitPlane(int gridX, string file, byte[] input)
+		{
+			int index = 0;
 			var lines = File.ReadAllLines(file);
 			foreach (var line in lines)
 			{
@@ -35,16 +34,21 @@ namespace LevelScreen
 				{
 					text = line.Substring(0, line.Length - 1);
 					var datas = text.Split(new char[] { ',' });
-					foreach(var data in datas)
+					foreach (var data in datas)
 					{
-						// https://theburningmonk.com/2010/02/converting-hex-to-int-in-csharp/
-						PlaneA[index] = Convert.ToByte(data, 16);
+						// https://theburningmonk.com/2010/02/converting-hex-to-int-in-csharp
+						input[index] = Convert.ToByte(data, 16);
 						index++;
 					}
 				}
 			}
-		}
 
+			if (index != gridX)
+			{
+				var msg = $"File: {file} Expect:{gridX} Actual:{index}";
+				throw new Exception(msg);
+			}
+		}
 
 		public byte[] PlaneA { get; private set; }
 		public byte[] PlaneB { get; private set; }
