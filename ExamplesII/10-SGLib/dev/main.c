b03c1a3958982e1375f7e3a7e6d696edc917ffa7
StevePro7 <steveprozune@hotmail.com>
2022-03-04 19:17:30 +0000

    no message

--- ExamplesII/10-SGLib/dev/main.c
+++ ExamplesII/10-SGLib/dev/main.c
@@ -2,7 +2,7 @@
 
 void main(void)
 {
-  SG_setBackdropColor(SG_COLOR_LIGHT_RED);
+  SG_setBackdropColor(SG_COLOR_MAGENTA);
   SG_displayOn();
   for (;;)
   {
@@ -10,5 +10,5 @@ void main(void)
   }
 }
 
-//SMS_EMBED_SEGA_ROM_HEADER(9999, 0);
-//SMS_EMBED_SDSC_HEADER(1, 0, 2017, 9, 15, "StevePro Studios", "Hello World", "Simple Sega Master System demo to run on real hardware");
+// SMS_EMBED_SEGA_ROM_HEADER(9999, 0);
+// SMS_EMBED_SDSC_HEADER(1, 0, 2017, 9, 15, "StevePro Studios", "Hello World", "Simple Sega Master System demo to run on real hardware");
