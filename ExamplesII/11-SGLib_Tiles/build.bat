e1b2ca04bd32fde7fa42d6ad6bf792f826c8ca2c
StevePro7 <steveprozune@hotmail.com>
2022-03-04 19:13:56 +0000

    no message

---
+++ ExamplesII/11-SGLib_Tiles/build.bat
@@ -0,0 +1,22 @@
+@echo off
+echo Build
+::sdcc --debug -c -mz80 --std-sdcc99 main.c
+sdcc -c -mz80 --std-sdcc99 main.c
+
+echo Link
+::sdcc --debug -mz80 --no-std-crt0 --data-loc 0xC000 -o output.ihx crt0_sg.rel main.rel SGlib.rel
+sdcc -mz80 --no-std-crt0 --data-loc 0xC000 -o output.ihx crt0_sg.rel main.rel SGlib.rel
+
+echo Run
+ihx2sms output.ihx output.sg
+
+:: Cleanup
+if exist "*.asm" del "*.asm" > nul
+if exist "*.ihx" del "*.ihx" > nul
+if exist "*.lk"  del "*.lk"  > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.noi" del "*.noi" > nul
+if exist "*.sym" del "*.sym" > nul
+
+java -jar C:\SEGA\Emulicious\Emulicious.jar output.sg
+::output.sms
---
+++ ExamplesII/11-SGLib_Tiles/main.c
@@ -0,0 +1,14 @@
+#include "SGlib.h"
+
+void main(void)
+{
+  SG_setBackdropColor(SG_COLOR_LIGHT_RED);
+  SG_displayOn();
+  for (;;)
+  {
+    SG_waitForVBlank();
+  }
+}
+
+//SMS_EMBED_SEGA_ROM_HEADER(9999, 0);
+//SMS_EMBED_SDSC_HEADER(1, 0, 2017, 9, 15, "StevePro Studios", "Hello World", "Simple Sega Master System demo to run on real hardware");
