b5398ce5ef76d6335b85acb091f317a058319971
StevePro7 <steveprozune@hotmail.com>
2022-03-06 20:28:21 +0000

    no message

--- TurtleBridge/sound/skazkasound01/dev/object/riff_object.c
+++ TurtleBridge/sound/skazkasound01/dev/object/riff_object.c
@@ -4,27 +4,23 @@
 #include "../banks/bank4.h"
 #include "../banks/bank5.h"
 #include "../banks/bank6.h"
-#include "../banks/bank7.h"
-#include "../banks/bank8.h"
+//#include "../banks/bank7.h"
+//#include "../banks/bank8.h"
 
 const unsigned char *riff_sample_data[] =
 {
-	SoundFx_wav_pcmenc,
-	SoundFx03_wav_pcmenc,
-	SoundFx04_wav_pcmenc,
-	SoundFx05_wav_pcmenc,
-	SoundFx06_wav_pcmenc,
-	SoundFx07_wav_pcmenc,
-	SoundFx08_wav_pcmenc,
+	Riff__01_wav_pcmenc,
+	Riff__02_wav_pcmenc,
+	Riff__03_wav_pcmenc,
+	Riff__04_wav_pcmenc,
+	Riff__05_wav_pcmenc,
 };
 
 const unsigned char riff_sample_bank[] =
 {
-	SoundFx_wav_pcmenc_bank,
-	SoundFx03_wav_pcmenc_bank,
-	SoundFx04_wav_pcmenc_bank,
-	SoundFx05_wav_pcmenc_bank,
-	SoundFx06_wav_pcmenc_bank,
-	SoundFx07_wav_pcmenc_bank,
-	SoundFx08_wav_pcmenc_bank,
+	Riff__01_wav_pcmenc_bank,
+	Riff__02_wav_pcmenc_bank,
+	Riff__03_wav_pcmenc_bank,
+	Riff__04_wav_pcmenc_bank,
+	Riff__05_wav_pcmenc_bank,
 };
--- TurtleBridge/sound/skazkasound01/dev/screen/test_screen.c
+++ TurtleBridge/sound/skazkasound01/dev/screen/test_screen.c
@@ -6,64 +6,22 @@
 
 void screen_test_screen_load()
 {
-	engine_font_manager_draw_text( "PRESS LEFT / UP / RIGHT", 4, 6 );
+	engine_font_manager_draw_text( "PRESS FIRE1", 4, 6 );
 }
 
 void screen_test_screen_update( unsigned char *screen_type )
 {	
-	unsigned char input = engine_input_manager_hold( input_type_left );
+	unsigned char input;
+
+	input = engine_input_manager_hold( input_type_fire1 );
 	if( input )
 	{
-		engine_font_manager_draw_text( "PLAY SOUND FX 01!!", 10, 8 );
-		engine_riff_manager_play( 6 );
-	}
-	else
-	{
-		input = engine_input_manager_hold( input_type_up );
-		if( input )
-		{
-			engine_font_manager_draw_text( "PLAY SOUND FX 02", 10, 9 );
-			engine_riff_manager_play( 0 );
-		}
-		else
-		{
-			input = engine_input_manager_hold( input_type_right );
-			if( input )
-			{
-				engine_font_manager_draw_text( "PLAY SOUND FX 03", 10, 10 );
-				engine_riff_manager_play( 3 );
-			}
-			else
-			{
-				input = engine_input_manager_hold( input_type_right );
-				if( input )
-				{
-					//engine_font_manager_draw_text( "PLAY WRONG1", 10, 12 );
-					//engine_riff_manager_play( 3 );
-				}
-				else
-				{
-					input = engine_input_manager_hold( input_type_fire1 );
-					if( input )
-					{
-						engine_font_manager_draw_text( "PLAY SOUND FX", 10, 13 );
-						engine_riff_manager_play( 1 );
-						engine_riff_manager_play( 2 );
-						engine_riff_manager_play( 3 );
-					}
-					else
-					{
-						input = engine_input_manager_hold( input_type_fire2 );
-						if( input )
-						{
-							engine_font_manager_draw_text( "PLAY GAME OVER", 10, 14 );
-							engine_riff_manager_play( 4 );
-							engine_riff_manager_play( 5 );
-						}
-					}
-				}
-			}
-		}
+		engine_font_manager_draw_text( "PLAY SOUND FX..!!", 10, 13 );
+		engine_riff_manager_play( 0 );
+		engine_riff_manager_play( 1 );
+		engine_riff_manager_play( 2 );
+		engine_riff_manager_play( 3 );
+		engine_riff_manager_play( 4 );
 	}
 
 	*screen_type = screen_type_test;
