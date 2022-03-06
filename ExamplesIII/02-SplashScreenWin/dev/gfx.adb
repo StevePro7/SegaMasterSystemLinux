a8a414826827df6e807d3a5877f4faa1d99c7a60
StevePro7 <steveprozune@hotmail.com>
2022-03-06 21:00:55 +0000

    no message

--- ExamplesIII/02-SplashScreenWin/dev/gfx.adb
+++ ExamplesIII/02-SplashScreenWin/dev/gfx.adb
@@ -1,4 +1,4 @@
 M:gfx
 S:G$splash__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$splash__tilemap__stmcompr$0$0({71}DA71d,SC:U),D,0,0
-S:G$splash__tiles__psgcompr$0$0({2089}DA2089d,SC:U),D,0,0
+S:G$splash__tilemap__stmcompr$0$0({75}DA75d,SC:U),D,0,0
+S:G$splash__tiles__psgcompr$0$0({15093}DA15093d,SC:U),D,0,0
