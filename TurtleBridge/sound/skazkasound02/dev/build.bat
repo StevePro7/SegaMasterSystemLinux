f57e71fc14bfa0fb3e70bd4d292b5d17ee57100a
StevePro7 <steveprozune@hotmail.com>
2022-03-06 20:43:44 +0000

    no message

--- TurtleBridge/sound/skazkasound02/dev/build.bat
+++ TurtleBridge/sound/skazkasound02/dev/build.bat
@@ -65,12 +65,14 @@ echo.
 REM echo Linking
 sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
 -Wl-b_BANK2=0x8000 -Wl-b_BANK3=0x8000 -Wl-b_BANK4=0x8000 -Wl-b_BANK5=0x8000 ^
--Wl-b_BANK6=0x8000 ^
+-Wl-b_BANK6=0x8000 -Wl-b_BANK7=0x8000 -Wl-b_BANK8=0x8000 -Wl-b_BANK9=0x8000 ^
+-Wl-b_BANK10=0x8000 ^
 ../crt0/crt0_sms.rel main.rel ^
 ../lib/SMSlib.lib ^
 ../lib/PSGlib.rel ^
 banks/bank2.rel banks/bank3.rel banks/bank4.rel banks/bank5.rel ^
-banks/bank6.rel ^
+banks/bank6.rel banks/bank7.rel banks\bank8.rel banks\bank9.rel ^
+banks\bank10.rel ^
 devkit/_sms_manager.rel ^
 devkit/_snd_manager.rel ^
 engine/asm_manager.rel ^
