218ba36f3c6bd5af7fb211f80103686d50f3abcf
StevePro7 <steveprozune@hotmail.com>
2022-03-10 20:22:07 +0000

    no message

--- Examples/08_Music_SoundFX/dev/object/audio_object.c
+++ Examples/08_Music_SoundFX/dev/object/audio_object.c
@@ -3,6 +3,7 @@
 
 const unsigned char *audio_effect[ MAX_EFFECTS ] = 
 {
+	m_gover_psg,
 	sound1_psg,
 	sound2_psg,
 	sound3_psg,
--- Examples/08_Music_SoundFX/dev/screen/sound_screen.c
+++ Examples/08_Music_SoundFX/dev/screen/sound_screen.c
@@ -1,21 +1,49 @@
 #include "sound_screen.h"
-#include "../devkit/_snd_manager.h"
 #include "../engine/audio_manager.h"
 #include "../engine/enum_manager.h"
 #include "../engine/font_manager.h"
+#include "../engine/global_manager.h"
 #include "../engine/input_manager.h"
+#include "../devkit/_snd_manager.h"
+
+static unsigned char index;
+static void draw_index();
 
 void screen_sound_screen_load()
 {
 	engine_font_manager_draw_text( "AUDIO", 10, 2 );
+	index = 0;
+	draw_index();
 }
 
 void screen_sound_screen_update( unsigned char *screen_type )
 {
-	unsigned char input = engine_input_manager_hold_fire1();
+	unsigned char input;
+
+	input = engine_input_manager_hold_up();
+	if (input )
+	{
+		if( index <= 0 )
+		{
+			index = MAX_EFFECTS;
+		}
+
+		index--;
+		draw_index();
+	}
+
+
+	input = engine_input_manager_hold_fire1();
 	if( input )
 	{
-		engine_audio_manager_play( 1 );
+		engine_audio_manager_play( index );
+	}
+
+	input = engine_input_manager_hold_fire2();
+	if( input )
+	{
+		devkit_PSGSFXStop();
+		devkit_PSGSilenceChannels();
 	}
 
 	//input = engine_input_manager_hold_fire2();
@@ -34,4 +62,9 @@ void screen_sound_screen_update( unsigned char *screen_type )
 	//}
 
 	*screen_type = screen_type_sound;
+}
+
+static void draw_index()
+{
+	engine_font_manager_draw_data( index, 20, 2 );
 }
