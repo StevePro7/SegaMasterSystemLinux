63a0827872550ee2ee9b1a6218a3496a60d34fe9
StevePro7 <steveprozune@hotmail.com>
2022-03-11 11:10:57 +0000

    no message

---
+++ Games/Z80HeartOnFire/dev/object/riff_object.adb
@@ -0,0 +1,6 @@
+M:riff_object
+S:G$riff_sample_data$0$0({6}DA3d,DG,SC:U),E,0,0
+S:G$Riff__01_wav_pcmenc$0$0({0}DA0d,SC:U),D,0,0
+S:G$Riff__02_wav_pcmenc$0$0({0}DA0d,SC:U),D,0,0
+S:G$Riff__03_wav_pcmenc$0$0({0}DA0d,SC:U),D,0,0
+S:G$riff_sample_bank$0$0({3}DA3d,SC:U),D,0,0
