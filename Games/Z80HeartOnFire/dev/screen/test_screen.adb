e92505abd6b746858e0efd8f8ab935ab48362c6c
StevePro7 <steveprozune@hotmail.com>
2022-03-11 19:35:56 +0000

    no message

--- Games/Z80HeartOnFire/dev/screen/test_screen.adb
+++ Games/Z80HeartOnFire/dev/screen/test_screen.adb
@@ -3,13 +3,14 @@ F:G$screen_test_screen_load$0$0({2}DF,SV:S),Z,0,0,0,0,0
 F:G$screen_test_screen_update$0$0({2}DF,SV:S),Z,0,0,0,0,0
 S:Ltest_screen.screen_test_screen_update$screen_type$1$25({2}DG,SC:U),B,1,4
 S:Ltest_screen.screen_test_screen_update$value$1$26({1}SC:U),R,0,0,[l]
+S:Ftest_screen$index$0$0({1}SC:U),E,0,0
+S:G$engine_audio_manager_play$0$0({2}DF,SV:S),C,0,0
 S:G$engine_font_manager_draw_char$0$0({2}DF,SV:S),C,0,0
 S:G$engine_font_manager_draw_text$0$0({2}DF,SV:S),C,0,0
 S:G$engine_font_manager_draw_data$0$0({2}DF,SV:S),C,0,0
 S:G$engine_input_manager_update$0$0({2}DF,SV:S),C,0,0
 S:G$engine_input_manager_hold$0$0({2}DF,SC:U),C,0,0
 S:G$engine_input_manager_move$0$0({2}DF,SC:U),C,0,0
-S:G$engine_riff_manager_play$0$0({2}DF,SV:S),C,0,0
 S:G$devkit_PSG_STOPPED$0$0({2}DF,SC:U),C,0,0
 S:G$devkit_PSG_PLAYING$0$0({2}DF,SC:U),C,0,0
 S:G$devkit_SFX_CHANNEL2$0$0({2}DF,SC:U),C,0,0
