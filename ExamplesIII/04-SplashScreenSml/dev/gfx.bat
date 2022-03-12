7548e78d269edea7847bc2e9c7a766bb7795f15f
StevePro7 <steveprozune@hotmail.com>
2022-03-12 18:29:13 +0000

    no message

--- ExamplesIII/04-SplashScreenSml/dev/gfx.bat
+++ ExamplesIII/04-SplashScreenSml/dev/gfx.bat
@@ -11,3 +11,7 @@ cd dev
 
 folder2c ../gfx gfx
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
