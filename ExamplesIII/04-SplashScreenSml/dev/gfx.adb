182884eb646e3a108e5b9e971f2e88f0be0ef47a
StevePro7 <steveprozune@hotmail.com>
2022-03-12 19:21:32 +0000

    no message

--- ExamplesIII/04-SplashScreenSml/dev/gfx.adb
+++ ExamplesIII/04-SplashScreenSml/dev/gfx.adb
@@ -1,7 +1,1 @@
 M:gfx
-S:G$font_tiles__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$font_tiles__tilemap__bin$0$0({448}DA448d,SC:U),D,0,0
-S:G$font_tiles__tiles__psgcompr$0$0({1480}DA1480d,SC:U),D,0,0
-S:G$title__palette__bin$0$0({16}DA16d,SC:U),D,0,0
-S:G$title__tilemap__stmcompr$0$0({41}DA41d,SC:U),D,0,0
-S:G$title__tiles__psgcompr$0$0({2194}DA2194d,SC:U),D,0,0
--- ExamplesIII/04-SplashScreenSml/dev/gfx.bat
+++ ExamplesIII/04-SplashScreenSml/dev/gfx.bat
@@ -2,11 +2,7 @@ cd ..
 cd gfx
 
 :: Tiles
-::bmp2tile.exe raw\title.bmp -savetiles "title (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 0 -savetilemap "title (tilemap).stmcompr" -savepalette "title (palette).bin" -fullpalette -exit
-
-bmp2tile.exe raw/title_sega.bmp -savetiles "title (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 0 -savetilemap "title (tilemap).stmcompr" -savepalette "title (palette).bin" -fullpalette -exit
-
-bmp2tile.exe raw\font_tiles.png -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 128  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette
 
 cd ..
 cd dev
