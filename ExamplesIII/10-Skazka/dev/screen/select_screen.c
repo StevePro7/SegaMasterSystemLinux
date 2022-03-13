e022c9210f408eec937c3cc787dcd76e83acdfd2
StevePro7 <steveprozune@hotmail.com>
2022-03-13 21:16:15 +0000

    no message

--- ExamplesIII/10-Skazka/dev/screen/select_screen.c
+++ ExamplesIII/10-Skazka/dev/screen/select_screen.c
@@ -23,7 +23,7 @@ void screen_select_screen_update( unsigned char *screen_type )
 	selection = engine_select_manager_update( select_type_option );
 	if( INVALID_INDEX != selection )
 	{
-		engine_font_manager_data( selection, 15, 18 );
+		engine_font_manager_data( selection + 1, 15, 18 );
 	}
 
 	*screen_type = screen_type_select;
