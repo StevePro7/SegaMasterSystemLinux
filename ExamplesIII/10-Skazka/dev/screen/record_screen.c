6b3b40d4dfee8d78b0bf0e37816550c616776f3b
StevePro7 <steveprozune@hotmail.com>
2022-03-14 20:10:26 +0000

    no message

--- ExamplesIII/10-Skazka/dev/screen/record_screen.c
+++ ExamplesIII/10-Skazka/dev/screen/record_screen.c
@@ -4,11 +4,13 @@
 
 void screen_record_screen_load()
 {
-	engine_font_manager_text( "RECORD SCREEN!!", 2, 10 );
-	engine_font_manager_char( 'X', 2, 12 );
+	engine_font_manager_text( "HELLO WORLD", 2, 10 );
+	engine_font_manager_char( 'A', 2, 12 );
 
-	engine_font_manager_data( 8, 12, 14 );
-	engine_font_manager_zero( 28, 12, 16 );
+	engine_font_manager_data( 1234, 12, 14 );
+	engine_font_manager_data( 5678, 12, 15 );
+	engine_font_manager_data( 1234, 12, 16 );
+	engine_font_manager_zero( 90, 12, 17 );
 }
 
 void screen_record_screen_update( unsigned char *screen_type )
