f3fad562f69b3fc103f256f0afd0977a9e2d9430
StevePro7 <steveprozune@hotmail.com>
2022-03-16 21:12:17 +0000

    no message

--- ExamplesIII/10-Skazka/dev/screen/talk_screen.c
+++ ExamplesIII/10-Skazka/dev/screen/talk_screen.c
@@ -11,13 +11,16 @@
 #include "../banks/fixedbank.h"
 #include <stdlib.h>
 
-#define MAX_CHAT	3
+//#define MAX_CHAT	3
 
 void screen_talk_screen_load()
 {
 	unsigned char row;
-	unsigned char idx, val;
-	unsigned char beg, end;
+	unsigned char idx = 0;
+	unsigned char val;
+	unsigned char beg = 2;
+	unsigned char txt = 0;
+	//unsigned char max = 3;
 
 	row = 1;
 	engine_player_manager_calc();
@@ -27,24 +30,27 @@ void screen_talk_screen_load()
 	engine_text_manager_border();
 	engine_text_manager_clear( row + 2, row + 9 );
 
-	val = rand() % MAX_RANDOM;
-	val = 7;
-	beg = val * MAX_CHAT;
-	end = beg + MAX_CHAT;
+	rand(); //rand(); rand(); //rand(); rand(); rand(); rand();
+	val = ( unsigned char ) ( rand() % MAX_RANDOM );
+	//val = 7;
+	beg = val * 4;
+	//end = beg + 3;
 
 	// TODO delete
+	engine_font_manager_data( val, 20, 19 );
 	engine_font_manager_data( beg, 20, 20 );
-	engine_font_manager_data( end, 20, 21 );
+	//engine_font_manager_data( end, 20, 21 );
 	// TODO delete
 
 	idx = 0;
 	row = 7;
 
 	devkit_SMS_mapROMBank( FIXED_BANK );
-	for( idx = beg; idx < end; idx++ )
+	for( idx = 0; idx < 3; idx++ )
 	{
+		txt = beg + idx;
 	//	engine_font_manager_text( "SPLAT", LEFT_X + 4, row++ );
-		engine_font_manager_text( ( unsigned char * ) villager_texts[ idx ], LEFT_X + 3, row );
+		engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
 		row++;
 	}
 
