543f2e4ccf125d0fb7458400a96ca031c3920614
StevePro7 <steveprozune@hotmail.com>
2022-03-16 21:29:15 +0000

    no message

--- ExamplesIII/10-Skazka/dev/screen/talk_screen.c
+++ ExamplesIII/10-Skazka/dev/screen/talk_screen.c
@@ -22,56 +22,26 @@ void screen_talk_screen_load()
 	unsigned char val;
 	unsigned char beg = 0;
 	unsigned char txt = 0;
-	//unsigned char max = 3;
 
 	row = 1;
 	engine_player_manager_calc();
 
-//	devkit_SMS_displayOff();
+	devkit_SMS_displayOff();
 	engine_content_manager_load_title( row );
 	engine_text_manager_border();
 	engine_text_manager_clear( row + 2, row + 9 );
 
-
-	rand(); rand(); rand(); //rand(); rand(); rand(); rand();
 	val = ( unsigned char ) ( rand() % MAX_RANDOM );
 	print( val );
-	//val = 7;
-	//beg = val + val;
-	//txt = beg;
-	////end = beg + 3;
 
-	//// TODO delete
-	//engine_font_manager_data( val, 20, 19 );
-	//engine_font_manager_data( txt, 20, 20 );
-	////engine_font_manager_data( end, 20, 21 );
-	//// TODO delete
-
-	//idx = 0;
-	//row = 7;
-
-	//devkit_SMS_mapROMBank( FIXED_BANK );
-	//
-	////txt = beg + idx;
-	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
-
-	//for( idx = 0; idx < 3; idx++ )
-	//{
-	//	txt = beg + idx;
-	////	engine_font_manager_text( "SPLAT", LEFT_X + 4, row++ );
-	//	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
-	//	row++;
-	//}
-
-	//engine_text_manager_fire();
-//	devkit_SMS_displayOn();
+	engine_text_manager_fire();
+	devkit_SMS_displayOn();
 }
 
 void screen_talk_screen_update( unsigned char *screen_type )
 {
 	*screen_type = screen_type_talk;
 }
-
 
 static void print( unsigned char val )
 {
@@ -80,30 +50,14 @@ static void print( unsigned char val )
 	unsigned char beg = 0;
 	unsigned char txt = 0;
 
-	beg = val * MAX_CHAT;// val + val;
-	//txt = beg;
-	//end = beg + 3;
-
-	// TODO delete
-	engine_font_manager_data( val, 20, 19 );
-	engine_font_manager_data( beg, 20, 20 );
-	//engine_font_manager_data( end, 20, 21 );
-	// TODO delete
-
-	idx = 0;
+	beg = val * MAX_CHAT;
 	row = 7;
 
 	devkit_SMS_mapROMBank( FIXED_BANK );
-
-	//txt = beg + idx;
-	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
-
 	for( idx = 0; idx < MAX_CHAT; idx++ )
 	{
 		txt = beg + idx;
 		engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
 		row++;
 	}
-
-	engine_text_manager_fire();
 }
