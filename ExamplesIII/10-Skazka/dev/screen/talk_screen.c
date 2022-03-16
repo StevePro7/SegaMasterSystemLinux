9d400a61a58e75afefdbbf9d29226201be5bd171
StevePro7 <steveprozune@hotmail.com>
2022-03-16 21:27:23 +0000

    no message

--- ExamplesIII/10-Skazka/dev/screen/talk_screen.c
+++ ExamplesIII/10-Skazka/dev/screen/talk_screen.c
@@ -11,14 +11,16 @@
 #include "../banks/fixedbank.h"
 #include <stdlib.h>
 
-//#define MAX_CHAT	3
+#define MAX_CHAT	3
+
+static void print( unsigned char val );
 
 void screen_talk_screen_load()
 {
 	unsigned char row;
 	unsigned char idx = 0;
 	unsigned char val;
-	unsigned char beg = 2;
+	unsigned char beg = 0;
 	unsigned char txt = 0;
 	//unsigned char max = 3;
 
@@ -30,10 +32,56 @@ void screen_talk_screen_load()
 	engine_text_manager_border();
 	engine_text_manager_clear( row + 2, row + 9 );
 
-	rand(); //rand(); rand(); //rand(); rand(); rand(); rand();
+
+	rand(); rand(); rand(); //rand(); rand(); rand(); rand();
 	val = ( unsigned char ) ( rand() % MAX_RANDOM );
+	print( val );
 	//val = 7;
-	beg = val * 4;
+	//beg = val + val;
+	//txt = beg;
+	////end = beg + 3;
+
+	//// TODO delete
+	//engine_font_manager_data( val, 20, 19 );
+	//engine_font_manager_data( txt, 20, 20 );
+	////engine_font_manager_data( end, 20, 21 );
+	//// TODO delete
+
+	//idx = 0;
+	//row = 7;
+
+	//devkit_SMS_mapROMBank( FIXED_BANK );
+	//
+	////txt = beg + idx;
+	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
+
+	//for( idx = 0; idx < 3; idx++ )
+	//{
+	//	txt = beg + idx;
+	////	engine_font_manager_text( "SPLAT", LEFT_X + 4, row++ );
+	//	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
+	//	row++;
+	//}
+
+	//engine_text_manager_fire();
+//	devkit_SMS_displayOn();
+}
+
+void screen_talk_screen_update( unsigned char *screen_type )
+{
+	*screen_type = screen_type_talk;
+}
+
+
+static void print( unsigned char val )
+{
+	unsigned char row;
+	unsigned char idx = 0;
+	unsigned char beg = 0;
+	unsigned char txt = 0;
+
+	beg = val * MAX_CHAT;// val + val;
+	//txt = beg;
 	//end = beg + 3;
 
 	// TODO delete
@@ -46,19 +94,16 @@ void screen_talk_screen_load()
 	row = 7;
 
 	devkit_SMS_mapROMBank( FIXED_BANK );
-	for( idx = 0; idx < 3; idx++ )
+
+	//txt = beg + idx;
+	//engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
+
+	for( idx = 0; idx < MAX_CHAT; idx++ )
 	{
 		txt = beg + idx;
-	//	engine_font_manager_text( "SPLAT", LEFT_X + 4, row++ );
 		engine_font_manager_text( ( unsigned char * ) villager_texts[ txt ], LEFT_X + 3, row );
 		row++;
 	}
 
 	engine_text_manager_fire();
-//	devkit_SMS_displayOn();
-}
-
-void screen_talk_screen_update( unsigned char *screen_type )
-{
-	*screen_type = screen_type_talk;
 }
