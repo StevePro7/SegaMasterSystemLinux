43a5f7cd7c4e8d2f9d2b2b31ea9a97856612217e
StevePro7 <steveprozune@hotmail.com>
2022-03-17 12:41:50 +0000

    no message

---
+++ ExamplesIII/10-Skazka/sms/screen/fight_screen.c
@@ -0,0 +1,47 @@
+#include "fight_screen.h"
+#include "../engine/content_manager.h"
+#include "../engine/enum_manager.h"
+#include "../engine/font_manager.h"
+#include "../engine/global_manager.h"
+#include "../engine/input_manager.h"
+#include "../engine/select_manager.h"
+#include "../engine/text_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../banks/fixedbank.h"
+
+static void print();
+
+void screen_fight_screen_load()
+{
+	unsigned char row;
+	unsigned char idx;
+
+	row = 1;
+	devkit_SMS_displayOff();
+	engine_content_manager_load_title( row );
+	engine_text_manager_border();
+	engine_text_manager_clear( row + 2, row + 9 );
+	print();
+	row = 14;
+	engine_select_manager_load( select_type_boss, LEFT_X + 5, row, 2 );
+	devkit_SMS_displayOn();
+}
+
+void screen_fight_screen_update( unsigned char *screen_type )
+{
+	*screen_type = screen_type_fight;
+}
+
+static void print()
+{
+	unsigned char row;
+	unsigned char idx;
+	row = 7;
+	engine_font_manager_data( row, 20, 20 );
+	devkit_SMS_mapROMBank( FIXED_BANK );
+	for( idx = 0; idx < 9; idx++ )
+	{
+		engine_font_manager_text( ( unsigned char * ) prep_texts[ idx ], LEFT_X + 3, row );
+		row += 1;
+	}
+}
