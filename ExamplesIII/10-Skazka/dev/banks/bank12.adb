59f30206188c0745eda41618680a976629ca3b7b
StevePro7 <steveprozune@hotmail.com>
2022-03-20 00:51:38 +0000

    no message

--- ExamplesIII/10-Skazka/dev/banks/bank12.adb
+++ ExamplesIII/10-Skazka/dev/banks/bank12.adb
@@ -1,2 +1,15 @@
 M:bank12
-S:G$bank12_txt$0$0({6}DA6d,SC:U),D,0,0
+S:G$sound0_psg$0$0({43}DA43d,SC:U),D,0,0
+S:G$sound1_psg$0$0({32}DA32d,SC:U),D,0,0
+S:G$sound10_psg$0$0({34}DA34d,SC:U),D,0,0
+S:G$sound11_psg$0$0({21}DA21d,SC:U),D,0,0
+S:G$sound12_psg$0$0({19}DA19d,SC:U),D,0,0
+S:G$sound13_psg$0$0({20}DA20d,SC:U),D,0,0
+S:G$sound2_psg$0$0({35}DA35d,SC:U),D,0,0
+S:G$sound3_psg$0$0({19}DA19d,SC:U),D,0,0
+S:G$sound4_psg$0$0({23}DA23d,SC:U),D,0,0
+S:G$sound5_psg$0$0({97}DA97d,SC:U),D,0,0
+S:G$sound6_psg$0$0({21}DA21d,SC:U),D,0,0
+S:G$sound7_psg$0$0({72}DA72d,SC:U),D,0,0
+S:G$sound8_psg$0$0({37}DA37d,SC:U),D,0,0
+S:G$sound9_psg$0$0({34}DA34d,SC:U),D,0,0
