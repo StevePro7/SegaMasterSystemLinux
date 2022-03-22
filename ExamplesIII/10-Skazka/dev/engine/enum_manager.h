2ec043558157052045c769a1179515da54b98818
StevePro7 <steveprozune@hotmail.com>
2022-03-22 21:32:57 +0000

    no message

--- ExamplesIII/10-Skazka/dev/engine/enum_manager.h
+++ ExamplesIII/10-Skazka/dev/engine/enum_manager.h
@@ -157,16 +157,16 @@ typedef enum tag_enum_sound_type
 	sound_type_4 = 4,
 	sound_type_5 = 5,
 	sound_type_6 = 6,
-	sound_type_7 = 7,
+	//sound_type_7 = 7,
 	sound_type_8 = 8,
 	sound_type_9 = 9,
 	sound_type_10 = 10,
 	sound_type_11 = 11,
-	sound_type_12 = 12,
-	sound_type_13 = 13,
-	sound_type_14 = 14,
-	sound_type_15 = 15,
-	sound_type_16 = 16,
+	//sound_type_12 = 12,
+	//sound_type_13 = 13,
+	//sound_type_14 = 14,
+	//sound_type_15 = 15,
+	//sound_type_16 = 16,
 
 } enum_sound_type;
 
