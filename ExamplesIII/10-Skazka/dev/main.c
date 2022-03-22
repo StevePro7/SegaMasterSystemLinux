4fc28fe0aa5ce5ce14d16db44c6e1790005bd1ba
StevePro7 <steveprozune@hotmail.com>
2022-03-22 20:23:26 +0000

    no message

--- ExamplesIII/10-Skazka/dev/main.c
+++ ExamplesIII/10-Skazka/dev/main.c
@@ -24,7 +24,7 @@ void main( void )
 	//open_screen_type = screen_type_title;
 	//open_screen_type = screen_type_intro;
 	//open_screen_type = screen_type_rest;
-	//open_screen_type = screen_type_stats;
+	open_screen_type = screen_type_stats;
 	//open_screen_type = screen_type_forest;
 	//open_screen_type = screen_type_select;
 	//open_screen_type = screen_type_record;
@@ -32,7 +32,7 @@ void main( void )
 	//open_screen_type = screen_type_over;
 	//open_screen_type = screen_type_shop;
 	//open_screen_type = screen_type_prep;
-	open_screen_type = screen_type_boss;
+	//open_screen_type = screen_type_boss;
 	//open_screen_type = screen_type_complete;
 	//open_screen_type = screen_type_detail;
 	//open_screen_type = screen_type_test;
