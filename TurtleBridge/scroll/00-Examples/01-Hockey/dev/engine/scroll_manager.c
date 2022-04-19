1bcf1e2570ab668098c268d7654ffdc9749ee4b2
StevePro7 <steveprozune@hotmail.com>
2022-04-19 20:38:53 +0100

    no message

--- TurtleBridge/scroll/00-Examples/01-Hockey/dev/engine/scroll_manager.c
+++ TurtleBridge/scroll/00-Examples/01-Hockey/dev/engine/scroll_manager.c
@@ -2,6 +2,7 @@
 #include "content_manager.h"
 #include "../devkit/_sms_manager.h"
 #include "../gfx.h"
+#include <stdlib.h>
 
 // 400px wide / 8px tiles
 unsigned int BG_TILE_WIDTH = 50;		// Hockey = 400 x 192	i.e. 50 x 8 tiles wide
@@ -16,12 +17,17 @@ static unsigned int scrollRightDivided8 = 0;
 
 void engine_scroll_manager_load()
 {
-	unsigned int ytile = 0;
+	unsigned char *src = NULL;
+	unsigned char ytile = 0;
+	unsigned int index = 0;
+	unsigned int size = 0;
+
 	for( ytile = 0; ytile < Y_TILE_MAX; ytile++ )
 	{
 		// 32 tiles * 2 bytes each.
-		unsigned char *src = ( void * ) &hockey__tilemap__bin[ BG_TILE_WIDTH * ytile * 2 ];
-		int size = X_TILE_MAX * 2;
+		index = BG_TILE_WIDTH * ytile * 2;
+		src = ( void * ) &hockey__tilemap__bin[ index ];
+		size = X_TILE_MAX * 2;
 
 		devkit_SMS_loadTileMap( 0, ytile, src, size );
 	}
@@ -29,7 +35,14 @@ void engine_scroll_manager_load()
 
 void engine_scroll_manager_update_left()
 {
-	unsigned int ytile = 0;
+	unsigned char *src = NULL;
+	unsigned char ytile = 0;
+	unsigned int index = 0;
+	const unsigned int size = 2;
+
+	unsigned int x = 0;
+	unsigned char y = 0;
+
 	if( scrollRight > 0 )
 	{
 		scroll += delta;
@@ -42,11 +55,12 @@ void engine_scroll_manager_update_left()
 			for( ytile = 1; ytile < Y_TILE_MAX; ytile++ )
 			{
 				// 1 tile * 2 bytes.
-				unsigned char x = X_TILE_MAX + scrollRightDivided8;
-				unsigned char y = ytile - 1;
-				unsigned char *src = ( void * ) &hockey__tilemap__bin[ ( ( BG_TILE_WIDTH*ytile ) + scrollRightDivided8 ) * 2 ];
+				x = X_TILE_MAX + scrollRightDivided8;
+				y = ytile - 1;
+				index = ( ( BG_TILE_WIDTH*ytile ) + scrollRightDivided8 ) * 2;
+				src = ( void * ) &hockey__tilemap__bin[ index ];
 
-				devkit_SMS_loadTileMap( x, y, src, 2 );
+				devkit_SMS_loadTileMap( x, y, src, size );
 			}
 		}
 	}
@@ -54,7 +68,14 @@ void engine_scroll_manager_update_left()
 
 void engine_scroll_manager_update_right()
 {
-	unsigned int ytile = 0;
+	unsigned char *src = NULL;
+	unsigned char ytile = 0;
+	unsigned int index = 0;
+	const unsigned int size = 2;
+
+	unsigned int x = 0;
+	unsigned char y = 0;
+
 	if( scrollRightDivided8 < ( BG_TILE_WIDTH - X_TILE_MAX ) )
 	{
 		scroll -= delta;
@@ -66,11 +87,12 @@ void engine_scroll_manager_update_right()
 		{
 			for( ytile = 1; ytile < Y_TILE_MAX; ytile++ )
 			{
-				unsigned char x = X_TILE_MAX + scrollRightDivided8;
-				unsigned char y = ytile - 1;
-				unsigned char *src = ( void * ) &hockey__tilemap__bin[ ( ( BG_TILE_WIDTH*ytile ) + ( X_TILE_MAX + scrollRightDivided8 ) ) * 2 ];
+				x = X_TILE_MAX + scrollRightDivided8;
+				y = ytile - 1;
+				index = ( ( BG_TILE_WIDTH*ytile ) + ( X_TILE_MAX + scrollRightDivided8 ) ) * 2;
+				src = ( void * ) &hockey__tilemap__bin[ index ];
 
-				devkit_SMS_loadTileMap( x, y, src, 2 );
+				devkit_SMS_loadTileMap( x, y, src, size );
 			}
 		}
 	}
