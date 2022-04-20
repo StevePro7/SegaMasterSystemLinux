1a9054f06e9f7952128b7079ca65e0ca07947764
StevePro7 <steveprozune@hotmail.com>
2022-04-20 18:33:18 +0100

    no message

---
+++ TurtleBridge/scroll/10-DragonBlaster/dev/engine/global_manager.h
@@ -0,0 +1,15 @@
+#ifndef _GLOBAL_MANAGER_H_
+#define _GLOBAL_MANAGER_H_
+
+// Sprites.
+#define SPRITE_TILES		256
+#define SKYBLUE_TILE		64
+
+#define SCREEN_WIDE			32
+#define SCREEN_HIGH			24
+
+#define TREES_HIGH			8
+#define EARTH_HIGH			18
+#define WAVES_HIGH			21
+
+#endif//_GLOBAL_MANAGER_H_
---
+++ TurtleBridge/scroll/10-DragonBlaster/dev/engine/scroll_manager.h
@@ -0,0 +1,15 @@
+#ifndef _SCROLL_MANAGER_H_
+#define _SCROLL_MANAGER_H_
+
+#include "../object/scroll_object.h"
+
+// Global variable.
+extern struct_scroll_object global_scroll_object;
+
+// Methods.
+void engine_scroll_manager_init();
+void engine_scroll_manager_load();
+void engine_scroll_manager_update();
+unsigned char engine_scroll_manager_getPosY( unsigned int col );
+
+#endif//_SCROLL_MANAGER_H_
