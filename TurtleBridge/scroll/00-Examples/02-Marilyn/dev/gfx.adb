b3f501fa3f3ddcb80d8efd09253b308a6c45bc53
StevePro7 <steveprozune@hotmail.com>
2022-04-26 19:31:13 +0100

    no message

--- TurtleBridge/scroll/00-Examples/02-Marilyn/dev/gfx.adb
+++
@@ -1,4 +0,0 @@
-M:gfx
-S:G$marilyn__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$marilyn__tilemap__bin$0_0$0({3584}DA3584d,SC:U),D,0,0
-S:G$marilyn__tiles__psgcompr$0_0$0({7775}DA7775d,SC:U),D,0,0
--- TurtleBridge/scroll/00-Examples/02-Marilyn/dev/gfx.h
+++
@@ -1,9 +0,0 @@
-extern const unsigned char	marilyn__palette__bin[];
-#define				marilyn__palette__bin_size 16
-
-extern const unsigned char	marilyn__tilemap__bin[];
-#define				marilyn__tilemap__bin_size 3584
-
-extern const unsigned char	marilyn__tiles__psgcompr[];
-#define				marilyn__tiles__psgcompr_size 7775
-
