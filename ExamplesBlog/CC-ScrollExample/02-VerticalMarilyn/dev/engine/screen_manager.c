4737eaff82510dafb4008150e2b4adae2f5809ff
StevePro7 <steveprozune@hotmail.com>
2022-04-27 21:44:11 +0100

    no message

---
+++ ExamplesBlog/CC-ScrollExample/02-VerticalMarilyn/dev/engine/screen_manager.c
@@ -0,0 +1,32 @@
+#include "screen_manager.h"
+#include "audio_manager.h"
+#include "content_manager.h"
+#include "enum_manager.h"
+#include "font_manager.h"
+#include "global_manager.h"
+#include "input_manager.h"
+#include "map_manager.h"
+#include "scroll_manager.h"
+#include "sprite_manager.h"
+#include "../content/gfx.h"
+	
+void engine_screen_manager_init()
+{
+	//engine_music_manager_play();
+}
+
+void engine_screen_manager_update()
+{
+	unsigned char input;
+	input = engine_input_manager_move_down();
+	if( input )
+	{
+		engine_scroll_manager_down();
+	}
+	
+	input = engine_input_manager_move_up();
+	if( input )
+	{
+		engine_scroll_manager_up();
+	}
+}
---
+++ ExamplesBlog/CC-ScrollExample/02-VerticalMarilyn/dev/object/scroll_object.h
@@ -0,0 +1,24 @@
+#ifndef _SCROLL_OBJECT_H_
+#define _SCROLL_OBJECT_H_
+
+typedef struct tag_struct_scroll_object
+{
+	//unsigned char scroll_left;
+	//unsigned char scroll_right;
+	unsigned int scroll;
+	unsigned int height;
+	unsigned int size;
+
+	unsigned char scrollleft;
+	unsigned char scrollRight;
+	unsigned char scrollRightDivided8;
+
+	//unsigned int offset_left;
+	//unsigned int offset_right;
+
+	unsigned char column_X;
+	unsigned int scroll_X;
+
+} struct_scroll_object;
+
+#endif//_SCROLL_OBJECT_H_
