c48c13015ce45747acc89269dddb87bcf817c519
StevePro7 <steveprozune@hotmail.com>
2022-04-27 11:04:19 +0100

    no message

--- ExamplesBlog/AA-SplashScreen/01-ORGsplash/dev/build.sh
+++ ExamplesBlog/AA-SplashScreen/01-ORGsplash/dev/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo off
+## echo off
 ## Time build START
 ## Reference: https://stackoverflow.com/questions/673523/how-do-i-measure-execution-time-of-a-command-on-the-windows-command-line
 ##set _time=%time: =0%
@@ -26,9 +26,9 @@ sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main
 ##set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
 ##set /a _duration=_hours*60*60*100+_min*60*100+_sec*100+_cs-_started
 ##set /a _hours=_duration/60/60/100,_min=100+_duration/60/100%%60,_sec=100+(_duration/100%%60%%60),_cs=100+_duration%%100
-echo.
-echo Time taken: %_sec:~-2%.%_cs:~-2% secs
-echo.
+##echo.
+##echo Time taken: %_sec:~-2%.%_cs:~-2% secs
+##echo.
 
 
 ## Link
