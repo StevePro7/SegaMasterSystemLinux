97dd40d8a0051d7c718474d6ee5913df0e17479d
StevePro7 <steveprozune@hotmail.com>
2022-04-27 21:36:02 +0100

    no message

--- ExamplesBlog/AA-HelloWorld/02-DevkitExample/dev/build.bat
+++ ExamplesBlog/AA-HelloWorld/02-DevkitExample/dev/build.bat
@@ -25,9 +25,9 @@ echo.
 
 
 :: Link
-sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 \
-../crt0/crt0_sms.rel main.rel \
-../lib/SMSlib.lib \
+sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
+../crt0/crt0_sms.rel main.rel ^
+../lib/SMSlib.lib ^
 devkit/_sms_manager.rel
 
 :: Execute
@@ -36,10 +36,6 @@ ihx2sms output.ihx output.sms
 
 :: Delete
 cd devkit
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
-cd ..
-
-cd engine
 if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
