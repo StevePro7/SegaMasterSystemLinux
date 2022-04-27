a4ef2eec2c523aac9ead82ef311914647f60655e
StevePro7 <steveprozune@hotmail.com>
2022-04-27 22:09:38 +0100

    no message

--- ExamplesBlog/CC-ScrollExample/03-VerticalDragon/dev/engine/screen_manager.c
+++ ExamplesBlog/CC-ScrollExample/03-VerticalDragon/dev/engine/screen_manager.c
@@ -17,7 +17,8 @@ void engine_screen_manager_update()
 	unsigned char input2;
 	input1 = engine_input_manager_move_up();
 	input2 = engine_input_manager_move_fire1();
-	if( input1 || input2 )
+
+	//if( input1 || input2 )
 	{
 		engine_map_manager_draw_map();
 	}
