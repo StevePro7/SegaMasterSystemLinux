0a97b4d5afd3669482551a7537e3963a0a3a5c7f
StevePro7 <steveprozune@hotmail.com>
2022-04-27 22:11:49 +0100

    no message

--- ExamplesBlog/CC-ScrollExample/03-VerticalDragon/dev/engine/level_manager.adb
+++
@@ -1,7 +0,0 @@
-M:level_manager
-F:G$engine_level_manager_init$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-F:G$engine_level_manager_update$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-S:Llevel_manager.engine_level_manager_update$column_X$1_0$4({1}SC:U),B,1,4
-S:Llevel_manager.engine_level_manager_update$scroll_X$1_0$4({2}SI:U),B,1,5
-T:Flevel_manager$tag_struct_level_object[({0}S:S$byte$0_0$0({1}SC:U),Z,0,0)]
-S:G$global_level_object$0_0$0({1}STtag_struct_level_object:S),E,0,0
