e81223d3e785a8f23e8e6f2cbc567abc41ffbe42
StevePro7 <steveprozune@hotmail.com>
2022-04-28 20:29:11 +0100

    no message

--- ExamplesBlog/EE-BankedCode/01-BasicExample/dev/engine/content_manager.c
+++ ExamplesBlog/EE-BankedCode/01-BasicExample/dev/engine/content_manager.c
@@ -1,6 +1,6 @@
 #include "content_manager.h"
 #include "../devkit/_sms_manager.h"
-#include "../gfx.h"
+#include "../content/gfx.h"
 
 #define TREE_TILES		64
 #define FONT_TILES		0
@@ -11,5 +11,4 @@ void engine_content_manager_load_tiles()
 	// Font tiles.
 	devkit_SMS_loadPSGaidencompressedTiles( font_tiles__tiles__psgcompr, FONT_TILES );
 	devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
-
 }
