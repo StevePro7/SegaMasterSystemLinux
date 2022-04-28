3607571c05d3c69a368aeca3994befaeed2c34bd
StevePro7 <steveprozune@hotmail.com>
2022-04-28 20:37:56 +0100

    no message

--- ExamplesBlog/EE-BankedCode/01-BasicExample/dev/build.bat
+++ ExamplesBlog/EE-BankedCode/01-BasicExample/dev/build.bat
@@ -68,11 +68,11 @@ if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist
 cd ..
 
 cd devkit
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nulcd ..
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
 cd engine
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nulcd ..
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
 if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
--- ExamplesBlog/EE-BankedCode/02-MacroExample/dev/build.bat
+++ ExamplesBlog/EE-BankedCode/02-MacroExample/dev/build.bat
@@ -1,5 +1,11 @@
 @echo off
 
+:: Time build START
+:: Reference: https://stackoverflow.com/questions/673523/how-do-i-measure-execution-time-of-a-command-on-the-windows-command-line
+set _time=%time: =0%
+set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
+set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
+
 
 ::Compile
 cd banks
@@ -11,7 +17,6 @@ cd ..
 
 cd devkit
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
 cd ..
 
 cd engine
@@ -22,7 +27,19 @@ sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 i
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 screen_manager.c
 cd ..
 
+
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
+
+
+:: Time build -END-
+set _time=%time: =0%
+set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2%%%100,_cs=%_time:~9,2%
+set /a _duration=_hours*60*60*100+_min*60*100+_sec*100+_cs-_started
+set /a _hours=_duration/60/60/100,_min=100+_duration/60/100%%60,_sec=100+(_duration/100%%60%%60),_cs=100+_duration%%100
+echo.
+echo Time taken: %_sec:~-2%.%_cs:~-2% secs
+echo.
+
 
 ::Link
 sdcc --debug -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ^
@@ -32,20 +49,18 @@ sdcc --debug -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ^
 -Wl-b_BANK4=0x44000 ^
 ../crt0/crt0b_sms.rel ^
 ../lib/SMSlib.lib ^
-../lib/PSGlib.rel ^
 main.rel ^
 banks/banked_code_1.rel ^
 banks/banked_code_2.rel ^
 banks/banked_code_3.rel ^
 banks/banked_code_4.rel ^
 devkit/_sms_manager.rel ^
-devkit/_snd_manager.rel ^
 engine/asm_manager.rel ^
 engine/content_manager.rel ^
 engine/font_manager.rel ^
 engine/input_manager.rel ^
 engine/screen_manager.rel ^
-gfx.rel
+content/gfx.rel
 
 ::Execute
 makesms output.ihx output.sms
@@ -53,30 +68,20 @@ makesms output.ihx output.sms
 
 :: Delete
 cd banks
-if exist "*.asm" del "*.asm" > nul
-if exist "*.lst" del "*.lst" > nul
-if exist "*.sym" del "*.sym" > nul
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
 cd devkit
-if exist "*.asm" del "*.asm" > nul
-if exist "*.lst" del "*.lst" > nul
-if exist "*.sym" del "*.sym" > nul
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
 cd engine
-if exist "*.asm" del "*.asm" > nul
-if exist "*.lst" del "*.lst" > nul
-if exist "*.sym" del "*.sym" > nul
+if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
-if exist "*.asm" del "*.asm" > nul
-if exist "*.ihx" del "*.ihx" > nul
-if exist "*.lk"  del "*.lk"  > nul
-if exist "*.lst" del "*.lst" > nul
-::if exist "*.noi" del "*.noi" > nul
-if exist "*.sym" del "*.sym" > nul
-
+if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
+if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
+:: if exist "*.noi"  del "*.noi"  > nul
 :: Run
 ::java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
 output.sms
