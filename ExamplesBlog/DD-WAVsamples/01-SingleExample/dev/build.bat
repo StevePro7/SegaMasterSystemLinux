0f70bb9b675d67d23ba7b1ea7b90d13a27a6430b
StevePro7 <steveprozune@hotmail.com>
2022-05-19 21:32:27 +0100

    no message

--- ExamplesBlog/DD-WAVsamples/01-SingleExample/dev/build.bat
+++ ExamplesBlog/DD-WAVsamples/01-SingleExample/dev/build.bat
@@ -15,7 +15,7 @@ cd ..
 
 cd engine
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_manager.c
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 input_manager.c
@@ -23,7 +23,7 @@ sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 s
 cd ..
 
 cd object
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_object.c
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_object.c
 cd ..
 
 
@@ -43,19 +43,15 @@ echo.
 ::Link
 sdcc --debug -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ../crt0/crt0_sms.rel main.rel ^
--Wl-b_BANK2=0x8000 ^
 ../lib/SMSlib.lib ^
 ../lib/PSGlib.rel ^
-banks\bank2.rel ^
 devkit/_sms_manager.rel ^
 devkit/_snd_manager.rel ^
 engine/asm_manager.rel ^
-engine/audio_manager.rel ^
 engine/content_manager.rel ^
 engine/font_manager.rel ^
 engine/input_manager.rel ^
 engine/screen_manager.rel ^
-object/audio_object.rel ^
 content/gfx.rel
 
 ::Execute
@@ -72,7 +68,7 @@ if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist
 cd ..
 
 cd object
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+::if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
 cd ..
 
 if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
