0f35b6b6f8105c90f341a57b0f5109b4bb67ae4c
StevePro7 <steveprozune@hotmail.com>
2022-05-20 16:57:41 +0100

    no message

--- ExamplesBlog/DD-WAVsamples/01-SingleExample/dev/engine/screen_manager.c
+++ ExamplesBlog/DD-WAVsamples/01-SingleExample/dev/engine/screen_manager.c
@@ -2,10 +2,11 @@
 #include "audio_manager.h"
 #include "font_manager.h"
 #include "input_manager.h"
+#include "riff_manager.h"
 
 void engine_screen_manager_init()
 {
-	engine_font_manager_draw_text( " == PCM SAMPLE TESTER ==!", 4, 6 );
+	engine_font_manager_draw_text( " == PCM SAMPLE TESTER == ", 4, 6 );
 	engine_font_manager_draw_text( "PRESS LEFT, RIGHT OR DOWN", 4, 8 );
 	engine_font_manager_draw_text( "PRESS FIRE TO PLAY SAMPLE", 4, 9 );
 }
@@ -37,5 +38,6 @@ void engine_screen_manager_update()
 	input = engine_input_manager_hold_fire1();
 	if( input )
 	{
+		engine_riff_manager_play( 0 );
 	}
 }
