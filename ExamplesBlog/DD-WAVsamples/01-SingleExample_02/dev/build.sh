0da95414eab47efc260dbf718985f9e9f8c0fc44
StevePro7 <steveprozune@hotmail.com>
2022-05-21 12:05:50 +0100

    no message

--- ExamplesBlog/DD-WAVsamples/01-SingleExample_02/dev/build.sh
+++ ExamplesBlog/DD-WAVsamples/01-SingleExample_02/dev/build.sh
@@ -8,23 +8,24 @@
 
 
 ##Compile
-cd banks
-sdcc --debug -c -mz80 --codeseg BANK1 banked_code_1.c
-sdcc --debug -c -mz80 --codeseg BANK2 banked_code_2.c
-sdcc --debug -c -mz80 --codeseg BANK3 banked_code_3.c
-cd ..
-
 cd devkit
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
 cd ..
 
 cd engine
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
+##sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 input_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 screen_manager.c
 cd ..
+
+cd object
+##sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 audio_object.c
+cd ..
+
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
@@ -41,16 +42,11 @@ sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main
 
 ##Link
 sdcc --debug -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 \
--Wl-b_BANK1=0x14000 \
--Wl-b_BANK2=0x24000 \
--Wl-b_BANK3=0x34000 \
-../crt0/crt0b_sms.rel \
+../crt0/crt0_sms.rel main.rel \
 ../lib/SMSlib.lib \
-main.rel \
-banks/banked_code_1.rel \
-banks/banked_code_2.rel \
-banks/banked_code_3.rel \
+../lib/PSGlib.rel \
 devkit/_sms_manager.rel \
+devkit/_snd_manager.rel \
 engine/asm_manager.rel \
 engine/content_manager.rel \
 engine/font_manager.rel \
@@ -63,15 +59,15 @@ makesms output.ihx output.sms
 
 
 ## Delete
-cd  banks
-rm -f *.asm 2> /dev/null; rm -f *.lst 2> /dev/null; rm -f *.sym 2> /dev/null
-cd ..
-
 cd  devkit
 rm -f *.asm 2> /dev/null; rm -f *.lst 2> /dev/null; rm -f *.sym 2> /dev/null
 cd ..
 
 cd  engine
+rm -f *.asm 2> /dev/null; rm -f *.lst 2> /dev/null; rm -f *.sym 2> /dev/null
+cd ..
+
+cd  object
 rm -f *.asm 2> /dev/null; rm -f *.lst 2> /dev/null; rm -f *.sym 2> /dev/null
 cd ..
 
