bac636f109be19fb8cd38654013088da0ebd59e0
StevePro7 <steveprozune@hotmail.com>
2022-05-25 20:55:19 +0100

    update #1

--- ExamplesBlog/BB-SplashScreen/02-NEWsplash/dev/build.bat
+++ ExamplesBlog/BB-SplashScreen/02-NEWsplash/dev/build.bat
@@ -34,7 +34,6 @@ echo.
 sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ../crt0/crt0_sms.rel main.rel ^
 ../lib/SMSlib.lib ^
-../lib/PSGlib.rel ^
 devkit/_sms_manager.rel ^
 engine/asm_manager.rel ^
 engine/content_manager.rel ^
