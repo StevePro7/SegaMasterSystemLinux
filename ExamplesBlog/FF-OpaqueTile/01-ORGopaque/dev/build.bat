222bffa3af249e006289f281d3d1759cbba4d988
StevePro7 <steveprozune@hotmail.com>
2022-05-26 22:01:31 +0100

    no message

--- ExamplesBlog/FF-OpaqueTile/01-ORGopaque/dev/build.bat
+++ ExamplesBlog/FF-OpaqueTile/01-ORGopaque/dev/build.bat
@@ -15,6 +15,18 @@ cd ..
 cd engine
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 asm_manager.c
 sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 content_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 enum_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 font_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gamer_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 input_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 screen_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 sprite_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 tile_manager.c
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 tree_manager.c
+cd ..
+
+cd object
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gamer_object.c
 cd ..
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
@@ -34,28 +46,49 @@ echo.
 sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ../crt0/crt0_sms.rel main.rel ^
 ../lib/SMSlib.lib ^
-devkit/_sms_manager.rel ^
 engine/asm_manager.rel ^
 engine/content_manager.rel ^
+engine/enum_manager.rel ^
+engine/font_manager.rel ^
+engine/gamer_manager.rel ^
+engine/input_manager.rel ^
+engine/screen_manager.rel ^
+engine/sprite_manager.rel ^
+engine/tile_manager.rel ^
+engine/tree_manager.rel ^
+object/gamer_object.rel ^
+devkit/_sms_manager.rel ^
 content/gfx.rel
-
 
 :: Execute
 ihx2sms output.ihx output.sms
 
 :: Delete
 cd devkit
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
 cd ..
 
 cd engine
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
 cd ..
 
-if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
-if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
-:: if exist "*.noi"  del "*.noi"  > nul
+cd object
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+cd ..
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.ihx" del "*.ihx" > nul
+if exist "*.lk"  del "*.lk"  > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.noi" del "*.noi" > nul
+if exist "*.sym" del "*.sym" > nul
 
 :: Run
-::java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
-output.sms
+java -jar C:/SEGA/Emulicious/Emulicious.jar output.sms
+::output.sms
