acc654458bb5c361a5eb99efaa168e6a6c5ab975
StevePro7 <steveprozune@hotmail.com>
2022-05-27 16:29:08 +0100

    In front of sprites

--- ExamplesBlog/FF-OpaqueTile/02-NEWopaque/dev/scripts/gfx.bat
+++ ExamplesBlog/FF-OpaqueTile/02-NEWopaque/dev/scripts/gfx.bat
@@ -7,7 +7,7 @@ cd gfx
 
 :: Tiles
 bmp2tile.exe raw/font_tiles.bmp -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "font_tiles (palette).bin" -fullpalette -exit
-bmp2tile.exe raw/game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "game_tiles (tilemap).bin" -savepalette "game_tiles (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/game_tiles.png -savetiles "game_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 64 -savetilemap "game_tiles (tilemap).bin" -infrontofsprites -savepalette "game_tiles (palette).bin" -fullpalette -exit
 
 :: Sprites
 bmp2tile.exe raw/sprites.png -savetiles "sprites (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprites (palette).bin" -fullpalette  -spritepalette -exit
