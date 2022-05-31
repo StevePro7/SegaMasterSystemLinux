5e5b9eaff0ad9eabaf9a551fbed8988d4f8c1680
StevePro7 <steveprozune@hotmail.com>
2022-05-31 19:16:19 +0100

    no message

---
+++ ExamplesBlog/DD-WAVsamples/10-Ohlalal/dev/scripts/bank3.bat
@@ -0,0 +1,13 @@
+:: bank3
+@echo off
+
+cd ..
+cd banks
+folder2c bank3 bank3 2
+
+sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK3 bank3.c
+
+del *.asm > nul; del *.lst > nul; del *.sym > nul
+
+cd ..
+cd scripts
