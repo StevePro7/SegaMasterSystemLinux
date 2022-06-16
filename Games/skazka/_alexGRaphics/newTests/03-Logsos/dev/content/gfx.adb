0369da39a45083e05af2ab35071800763d42481c
StevePro7 <steveprozune@hotmail.com>
2022-06-16 20:02:05 +0100

    no message

---
+++ Games/skazka/_alexGRaphics/newTests/03-Logsos/dev/content/gfx.adb
@@ -0,0 +1,7 @@
+M:gfx
+S:G$font_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
+S:G$font_tiles__tilemap__bin$0_0$0({96}DA96d,SC:U),D,0,0
+S:G$font_tiles__tiles__psgcompr$0_0$0({385}DA385d,SC:U),D,0,0
+S:G$koschey__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
+S:G$koschey__tilemap__bin$0_0$0({360}DA360d,SC:U),D,0,0
+S:G$koschey__tiles__psgcompr$0_0$0({2680}DA2680d,SC:U),D,0,0
