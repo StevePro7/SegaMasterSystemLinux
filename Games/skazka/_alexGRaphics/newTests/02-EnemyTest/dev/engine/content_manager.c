3e42e2d3da2265bb6ef51e55e37c5b10300afe83
StevePro7 <steveprozune@hotmail.com>
2022-06-16 19:41:24 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/02-EnemyTest/dev/engine/content_manager.c
+++ Games/skazka/_alexGRaphics/newTests/02-EnemyTest/dev/engine/content_manager.c
@@ -3,7 +3,6 @@
 #include "../content/gfx.h"
 
 #define FONT_TILES		0
-#define GAME_TILES		64
 #define SPRITE_TILES	256
 
 void engine_content_manager_load_tiles()
@@ -11,4 +10,8 @@ void engine_content_manager_load_tiles()
 	// Font tiles.
 	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
 	devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+
+	// Enemy tiles
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) koschey__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadSpritePalette( ( void * ) koschey__palette__bin );
 }
