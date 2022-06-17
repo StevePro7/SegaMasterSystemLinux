0d388aee54af82595001713a2bc367e5416165bb
StevePro7 <steveprozune@hotmail.com>
2022-06-17 18:14:26 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/05-Graphics/dev/engine/content_manager.c
+++ Games/skazka/_alexGRaphics/newTests/05-Graphics/dev/engine/content_manager.c
@@ -6,14 +6,21 @@
 #define BORDER_TILES	48
 #define SPRITE_TILES	436
 
+#define LGOG_TILES		64
+
 void engine_content_manager_load_tiles()
 {
-	// Font tiles.
+	// Background tiles.
 	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font__tiles__psgcompr, FONT_TILES );
 	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) border__tiles__psgcompr, BORDER_TILES );
 	devkit_SMS_loadBGPalette( ( void * ) background__palette__bin );
 
-	// Village tiles
+	// Sprite tiles
 	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) battle_enemies_leshy__tiles__psgcompr, SPRITE_TILES );
 	devkit_SMS_loadSpritePalette( ( void * ) sprite__palette__bin );
+}
+
+void engine_content_manager_load_logo_big()
+{
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font__tiles__psgcompr, FONT_TILES );
 }
--- Games/skazka/_alexGRaphics/newTests/05-Graphics/dev/engine/content_manager.h
+++ Games/skazka/_alexGRaphics/newTests/05-Graphics/dev/engine/content_manager.h
@@ -2,5 +2,6 @@
 #define _CONTENT_MANAGER_H_
 
 void engine_content_manager_load_tiles();
+void engine_content_manager_load_logo_big();
 
 #endif//_CONTENT_MANAGER_H_
