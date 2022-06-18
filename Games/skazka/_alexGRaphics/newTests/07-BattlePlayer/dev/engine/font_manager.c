185fb9e84ceadaf83b73d596ccbd2b746037ffd0
StevePro7 <steveprozune@hotmail.com>
2022-06-18 20:26:40 +0100

    no message

---
+++ Games/skazka/_alexGRaphics/newTests/07-BattlePlayer/dev/engine/font_manager.c
@@ -0,0 +1,117 @@
+#include "font_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+#define TEXT_ROOT	64		// 64 is "A" (65-01).
+#define DATA_ROOT	27		// 27 is "0" (48-27).
+#define UNIT_ROOT	10		// 10 is decimal
+#define DATA_LONG	3		// 3 placeholder
+
+// Private helper functions.
+static unsigned char get_tile( unsigned char ch );
+static void draw_char( unsigned int tile, unsigned char x, unsigned char y );
+
+void engine_font_manager_draw_punc( unsigned char ch, unsigned char x, unsigned char y )
+{
+	unsigned char tile = get_tile( ch );
+	draw_char( tile, x, y );
+}
+void engine_font_manager_draw_flip( unsigned char ch, unsigned char x, unsigned char y )
+{
+	unsigned char tile = get_tile( ch );
+	unsigned int flipX = devkit_TILE_FLIPPED_X();
+	draw_char( ( tile | flipX ), x, y );
+}
+
+void engine_font_manager_draw_char( unsigned char ch, unsigned char x, unsigned char y )
+{
+	unsigned char tile = ch - TEXT_ROOT;
+	draw_char( tile, x, y );
+}
+
+void engine_font_manager_draw_text( unsigned char *text, unsigned char x, unsigned char y )
+{
+	unsigned char idx = 0;
+	while( '\0' != text[idx] )
+	{
+		unsigned char ch = text[ idx ];
+		unsigned char tile = ch - TEXT_ROOT;
+		draw_char( tile, x, y );
+		x++;
+		idx++;
+	}
+}
+
+void engine_font_manager_draw_data( unsigned int data, unsigned char x, unsigned char y )
+{
+	unsigned char idx;
+	signed char tile;
+
+	unsigned int quotient = 0;
+	unsigned char remainder = 0;
+
+	char hold[DATA_LONG];
+	for( idx = 0; idx < DATA_LONG; ++idx )
+	{
+		quotient = data / UNIT_ROOT;
+		remainder = data % UNIT_ROOT;
+
+		hold[idx] = remainder;
+		data /= UNIT_ROOT;
+
+		tile = hold[idx] + DATA_ROOT;
+		if( 0 == quotient && 0 == remainder && idx > 0 )
+		{
+			// Replace with space!
+			tile = 0;
+		}
+
+		draw_char( tile, x, y );
+		x--;
+	}
+}
+
+void engine_font_manager_draw_numb( unsigned char numb, unsigned char x, unsigned char y )
+{
+	unsigned char tile = numb + DATA_ROOT;
+	draw_char( tile, x, y );
+}
+
+void engine_font_manager_draw_zero( unsigned int data, unsigned char x, unsigned char y )
+{
+	unsigned char idx;
+	unsigned char tile;
+
+	char hold[DATA_LONG];
+	for( idx = 0; idx < DATA_LONG; ++idx )
+	{
+		hold[idx] = data % UNIT_ROOT;
+		data /= UNIT_ROOT;
+
+		tile = hold[idx] + DATA_ROOT;
+		draw_char( tile, x, y );
+		x--;
+	}
+}
+
+static unsigned char get_tile( unsigned char ch )
+{
+	if( '+' == ch ) { return 37; }
+	else if( '-' == ch ) { return 38; }
+	else if( '\'' == ch ) { return 39; }
+	else if( '.' == ch ) { return 40; }
+	else if( ',' == ch ) { return 41; }
+	else if( ':' == ch ) { return 42; }
+	else if( '?' == ch ) { return 43; }
+	else if( '!' == ch ) { return 44; }
+	else if( '/' == ch ) { return 45; }
+	else if( ')' == ch ) { return 46; }
+	else if( '>' == ch ) { return 47; }
+	return 0;
+}
+static void draw_char( unsigned int tile, unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = font__tilemap__bin;
+	devkit_SMS_setNextTileatXY( x, y );
+	devkit_SMS_setTile( *pnt + tile );
+}
---
+++ Games/skazka/_alexGRaphics/newTests/07-BattlePlayer/dev/engine/screen_manager.c
@@ -0,0 +1,103 @@
+#include "screen_manager.h"
+#include "content_manager.h"
+#include "font_manager.h"
+#include "graphics_manager.h"
+#include "locale_manager.h"
+#include "text_manager.h"
+
+static void draw_title();
+static void draw_boss();
+
+static void draw_text();
+static void draw_punc();
+static void draw_flip();
+
+void engine_screen_manager_init()
+{
+	//draw_title();		// screen_01_title
+	draw_boss();
+}
+
+void engine_screen_manager_update()
+{
+}
+
+static void draw_boss()
+{
+	engine_graphics_manager_draw_border();
+
+	engine_content_manager_load_koschey();
+
+	// TODO - replace hard coded values!
+	engine_graphics_manager_draw_koschey( 10, 2 );
+
+	engine_font_manager_draw_text( "SO YOU VE COME TO CHALLENGE", 2, 19 );
+	engine_font_manager_draw_text( "ME FOOLISH MORTAL  VERY WELL", 2, 20 );
+
+	engine_font_manager_draw_punc( '\'', 8, 19 );
+	engine_font_manager_draw_punc( '?', 19, 20 );
+}
+
+static void draw_title()
+{
+	engine_graphics_manager_draw_border();
+
+	engine_content_manager_load_logo_big();
+
+	// TODO - replace hard coded values!
+	engine_graphics_manager_draw_logo_big( 2, 3 );
+
+	engine_font_manager_draw_text( LOCALE_TITLE_MSG1, 6, 10 );
+	engine_font_manager_draw_text( LOCALE_TITLE_MSG2, 3, 15 );
+
+	// IMPORTANT - must draw text before number!
+	engine_font_manager_draw_numb( 8, 16, 15 );
+
+	engine_text_manager_cont();
+}
+
+static void draw_text()
+{
+	engine_font_manager_draw_numb( 8, 10, 15 );
+	engine_font_manager_draw_zero( 7, 10, 18 );
+	engine_font_manager_draw_zero( 12, 10, 19 );
+	engine_font_manager_draw_zero( 512, 10, 20 );
+
+	engine_font_manager_draw_text( "STATS VILLAGE", 10, 20 );
+	engine_font_manager_draw_char( 'X', 20, 1 );
+	engine_font_manager_draw_punc( '?', 12, 1 );
+}
+
+static void draw_punc()
+{
+	unsigned char idx = 0;
+
+	engine_font_manager_draw_punc( '+', 10, idx++ );
+	engine_font_manager_draw_punc( '-', 10, idx++ );
+	engine_font_manager_draw_punc( '\'', 10, idx++ );
+	engine_font_manager_draw_punc( '.', 10, idx++ );
+	engine_font_manager_draw_punc( ',', 10, idx++ );
+	engine_font_manager_draw_punc( ':', 10, idx++ );
+	engine_font_manager_draw_punc( '?', 10, idx++ );
+	engine_font_manager_draw_punc( '!', 10, idx++ );
+	engine_font_manager_draw_punc( '/', 10, idx++ );
+	engine_font_manager_draw_punc( ')', 10, idx++ );
+	engine_font_manager_draw_punc( '>', 10, idx++ );
+}
+
+static void draw_flip()
+{
+	unsigned char idx = 0;
+
+	engine_font_manager_draw_flip( '+', 20, idx++ );
+	engine_font_manager_draw_flip( '-', 20, idx++ );
+	engine_font_manager_draw_flip( '\'', 20, idx++ );
+	engine_font_manager_draw_flip( '.', 20, idx++ );
+	engine_font_manager_draw_flip( ',', 20, idx++ );
+	engine_font_manager_draw_flip( ':', 20, idx++ );
+	engine_font_manager_draw_flip( '?', 20, idx++ );
+	engine_font_manager_draw_flip( '!', 20, idx++ );
+	engine_font_manager_draw_flip( '/', 20, idx++ );
+	engine_font_manager_draw_flip( ')', 20, idx++ );
+	engine_font_manager_draw_flip( '>', 20, idx++ );
+}
