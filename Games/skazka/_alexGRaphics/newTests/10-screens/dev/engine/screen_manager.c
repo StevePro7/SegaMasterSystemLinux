ec0a8dbab25f3b147b52cf4861512535f59e527b
StevePro7 <steveprozune@hotmail.com>
2022-06-18 16:55:12 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/engine/screen_manager.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/engine/screen_manager.c
@@ -21,10 +21,10 @@ void engine_screen_manager_init()
 {
 	//engine_graphics_manager_draw_border();
 
-	//draw_title();		// screen_01_title
+	draw_title();		// screen_01_title
 	//draw_boss();
 	//draw_intro();
-	draw_leshy();
+	//draw_leshy();
 }
 
 void engine_screen_manager_update()
