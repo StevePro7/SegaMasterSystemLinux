6a04d70f2c88e2b3a8d142ac252284329f181cd1
StevePro7 <steveprozune@hotmail.com>
2022-06-21 20:41:23 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
@@ -2,11 +2,34 @@
 
 void main( void )
 {
+	// Global variables.
+	static bool global_pause;
+	unsigned char open_screen_type;
+
 	devkit_SMS_init();
 	devkit_SMS_displayOff();
 	engine_asm_manager_clear_VRAM();
 
 	devkit_SMS_useFirstHalfTilesforSprites( false );
+
+	open_screen_type = screen_type_splash;
+	//open_screen_type = screen_type_title;
+	//open_screen_type = screen_type_load;
+	//open_screen_type = screen_type_diff;
+
+
+	// Initialize player first!
+	//engine_player_manager_init();
+	//engine_hack_manager_init();
+	//engine_hack_manager_load();
+	//engine_hack_manager_invert();
+
+	//engine_enemy_manager_init();
+	//engine_game_manager_init();
+	//engine_sound_manager_init();
+
+	//engine_select_manager_init();
+	//engine_screen_manager_init( open_screen_type );
 
 	engine_content_manager_load_tiles();
 	engine_screen_manager_init();
--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.h
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.h
@@ -4,10 +4,20 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// LIBRARIES.
 #include "devkit/_sms_manager.h"
+//#include "devkit/_snd_manager.h"
 
 #include "engine/asm_manager.h"
+//#include "engine/audio_manager.h"
 #include "engine/content_manager.h"
+#include "engine/enum_manager.h"
+//#include "engine/enemy_manager.h"
+//#include "engine/game_manager.h"
+//#include "engine/hack_manager.h"
+//#include "engine/input_manager.h"
+//#include "engine/player_manager.h"
 #include "engine/screen_manager.h"
+//#include "engine/select_manager.h"
 
 #endif//__MAIN__
