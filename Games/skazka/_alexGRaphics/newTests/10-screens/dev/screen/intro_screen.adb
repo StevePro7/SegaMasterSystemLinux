8d857bf498fbacd8994f849627d51e1ffec17a36
StevePro7 <steveprozune@hotmail.com>
2022-06-22 21:04:16 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/intro_screen.adb
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/intro_screen.adb
@@ -1,10 +1,10 @@
 M:intro_screen
 F:G$screen_intro_screen_load$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-S:Lintro_screen.screen_intro_screen_load$row$1_0$124({1}SC:U),B,1,-1
-S:Lintro_screen.screen_intro_screen_load$idx$1_0$124({1}SC:U),R,0,0,[c]
+S:Lintro_screen.screen_intro_screen_load$row$1_0$126({1}SC:U),B,1,-1
+S:Lintro_screen.screen_intro_screen_load$idx$1_0$126({1}SC:U),R,0,0,[c]
 S:Lintro_screen.screen_intro_screen_load$sloc0$0_1$0({1}SC:U),B,1,-1
 F:G$screen_intro_screen_update$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-S:Lintro_screen.screen_intro_screen_update$screen_type$1_0$127({2}DG,SC:U),B,1,4
+S:Lintro_screen.screen_intro_screen_update$screen_type$1_0$129({2}DG,SC:U),B,1,4
 T:Fintro_screen$tag_struct_game_object[({0}S:S$intro_once$0_0$0({1}:S),Z,0,0)({1}S:S$play_music$0_0$0({1}:S),Z,0,0)({2}S:S$flash_arrow$0_0$0({1}:S),Z,0,0)({3}S:S$difficulty$0_0$0({1}SC:U),Z,0,0)]
 T:Fintro_screen$tag_struct_timer_object[({0}S:S$timer_count$0_0$0({2}SI:U),Z,0,0)({2}S:S$timer_value$0_0$0({2}SI:U),Z,0,0)]
 S:G$global_game_object$0_0$0({4}STtag_struct_game_object:S),E,0,0
@@ -30,6 +30,7 @@ S:G$over_texts$0_0$0({0}DA0d,DG,SC:U),E,0,0
 S:G$beat_texts$0_0$0({0}DA0d,DG,SC:U),E,0,0
 S:G$engine_content_manager_load_tiles$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_content_manager_load_logo_big$0_0$0({2}DF,SV:S),C,0,0
+S:G$engine_content_manager_load_logo_small$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_content_manager_load_koschey$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_font_manager_draw_char$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_font_manager_draw_punc$0_0$0({2}DF,SV:S),C,0,0
@@ -52,7 +53,9 @@ S:G$engine_game_manager_print_player$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_game_manager_print_version$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_game_manager_print_texts$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_graphics_manager_draw_border$0_0$0({2}DF,SV:S),C,0,0
+S:G$engine_graphics_manager_draw_underline$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_graphics_manager_draw_logo_big$0_0$0({2}DF,SV:S),C,0,0
+S:G$engine_graphics_manager_draw_logo_small$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_graphics_manager_draw_koschey$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_graphics_manager_draw_leshy$0_0$0({2}DF,SV:S),C,0,0
 S:G$engine_input_manager_update$0_0$0({2}DF,SV:S),C,0,0
