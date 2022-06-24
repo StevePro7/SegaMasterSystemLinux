64998485cf7ebf52c5e0c764deb3f31796b226be
StevePro7 <steveprozune@hotmail.com>
2022-06-24 21:11:39 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
@@ -12,21 +12,21 @@ void main( void )
 
 	devkit_SMS_useFirstHalfTilesforSprites( false );
 
-	//open_screen_type = screen_type_splash;
-	open_screen_type = screen_type_title;
-	open_screen_type = screen_type_diff;
-	open_screen_type = screen_type_intro;
-	open_screen_type = screen_type_stats;
-	open_screen_type = screen_type_victory;
-	open_screen_type = screen_type_relive;	// TODO counter broken
-	open_screen_type = screen_type_prep;
+	////open_screen_type = screen_type_splash;
+	//open_screen_type = screen_type_title;
+	//open_screen_type = screen_type_diff;
+	//open_screen_type = screen_type_intro;
+	//open_screen_type = screen_type_stats;
+	//open_screen_type = screen_type_victory;
+	//open_screen_type = screen_type_relive;	// TODO counter broken
+	//open_screen_type = screen_type_prep;
 	open_screen_type = screen_type_boss;
-	open_screen_type = screen_type_over;
-	open_screen_type = screen_type_complete;
-	open_screen_type = screen_type_menu;
-	//open_screen_type = screen_type_load;
-	open_screen_type = screen_type_test;
-	//open_screen_type = screen_type_func;
+	//open_screen_type = screen_type_over;
+	//open_screen_type = screen_type_complete;
+	//open_screen_type = screen_type_menu;
+	////open_screen_type = screen_type_load;
+	//open_screen_type = screen_type_test;
+	////open_screen_type = screen_type_func;
 
 	// Initialize player first!
 	engine_player_manager_init();
