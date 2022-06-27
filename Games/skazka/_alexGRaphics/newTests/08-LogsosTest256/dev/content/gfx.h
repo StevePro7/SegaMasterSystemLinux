4a48f57f1ad046680e1b81dfb23fa56d96418d9b
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:26:42 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/08-LogsosTest256/dev/content/gfx.h
+++ Games/skazka/_alexGRaphics/newTests/08-LogsosTest256/dev/content/gfx.h
@@ -1,14 +1,11 @@
-extern const unsigned char	font_tiles__palette__bin[];
-#define				font_tiles__palette__bin_size 16
+extern const unsigned char	background__palette__bin[];
+#define				background__palette__bin_size 16
 
 extern const unsigned char	font_tiles__tilemap__bin[];
 #define				font_tiles__tilemap__bin_size 96
 
 extern const unsigned char	font_tiles__tiles__psgcompr[];
 #define				font_tiles__tiles__psgcompr_size 385
-
-extern const unsigned char	logo_big__palette__bin[];
-#define				logo_big__palette__bin_size 16
 
 extern const unsigned char	logo_big__tilemap__bin[];
 #define				logo_big__tilemap__bin_size 280
