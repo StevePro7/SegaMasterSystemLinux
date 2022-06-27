078e98113ed3a3dd2cc27447324a277f0285e609
StevePro7 <steveprozune@hotmail.com>
2022-06-27 19:21:06 +0100

    no message

---
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/engine/content_manager.c
@@ -0,0 +1,95 @@
+#include "content_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+#define FONT_TILES		0
+#define LOGO_TILES		64
+#define SPRITE_TILES	256
+
+void engine_content_manager_load_tiles()
+{
+	// Font tiles.
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) font_tiles__tiles__psgcompr, FONT_TILES );
+	devkit_SMS_loadBGPalette( ( void * ) font_tiles__palette__bin );
+
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) logo_big__tiles__psgcompr, LOGO_TILES );
+	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) logo_small__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadSpritePalette( ( void * ) font_tiles__palette__bin );
+}
+
+void engine_content_manager_draw_logo1()
+{
+	const unsigned char wide = 28;
+	const unsigned char high = 5;
+	unsigned char x, y;
+	unsigned int index = 0;
+	unsigned int value = 0;
+	unsigned int tile = 0;
+	unsigned char i, j;
+
+	x = 2;
+	y = 2;
+	for( j = 0; j < high; j++ )
+	{
+		for( i = 0; i < wide; i++ )
+		{
+			index = j * wide + i;
+			value = index * 2;
+			tile = logo_big__tilemap__bin[ value ];
+			devkit_SMS_setNextTileatXY( x + i, y + j );
+			devkit_SMS_setTile( tile );
+		}
+	}
+}
+
+void engine_content_manager_draw_logo2()
+{
+	const unsigned char *pnt = logo_big__tilemap__bin;
+	unsigned char x, y;
+	unsigned char i, j;
+	unsigned char idx = 0;
+	unsigned int tile;
+	//unsigned int palette;
+
+	//palette = devkit_TILE_USE_SPRITE_PALETTE();
+	j = 0;
+	i = 0;
+	for( j = 0; j < 5; j++ )
+	{
+		for( i = 0; i < 28; i++ )
+		{
+			x = 2 + i;
+			y = 2 + j;
+			tile = ( 0 + idx );
+			devkit_SMS_setNextTileatXY( x, y );
+			devkit_SMS_setTile( *pnt + tile );
+			idx++;
+		}
+	}
+}
+//
+//void engine_content_manager_draw_logo2()
+//{
+//	const unsigned char *pnt = logo_small__tilemap__bin;
+//	unsigned char x, y;
+//	unsigned char i, j;
+//	unsigned char idx = 0;
+//	unsigned int tile;
+//	unsigned int palette;
+//
+//	palette = devkit_TILE_USE_SPRITE_PALETTE();
+//	j = 0;
+//	i = 0;
+//	for( j = 0; j < 3; j++ )
+//	{
+//		for( i = 0; i < 30; i++ )
+//		{
+//			x = 1 + i;
+//			y = 12 + j;
+//			tile = ( 256 + idx ) | palette;
+//			devkit_SMS_setNextTileatXY( x, y );
+//			devkit_SMS_setTile( *pnt + tile );
+//			idx++;
+//		}
+//	}
+//}
---
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/engine/screen_manager.c
@@ -0,0 +1,16 @@
+#include "screen_manager.h"
+#include "font_manager.h"
+
+void engine_screen_manager_init()
+{
+	//engine_font_manager_draw_data( 89, 10, 10 );
+	//engine_font_manager_draw_zero( 0, 10, 10 );
+
+	//engine_font_manager_draw_text( "BOSS FIGHT", 10, 20 );
+	//engine_font_manager_draw_char( ')', 10, 10 );
+}
+
+void engine_screen_manager_update()
+{
+//	engine_sprite_manager_draw( 64, 64, 256 );
+}
