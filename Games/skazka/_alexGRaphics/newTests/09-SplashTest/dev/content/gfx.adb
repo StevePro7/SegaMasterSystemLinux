d5783810c4e2c44048cd8805fd0b056c5273a2cc
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:08:09 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/content/gfx.adb
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/content/gfx.adb
@@ -1,7 +1,7 @@
 M:gfx
+S:G$background__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
 S:G$font_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
 S:G$font_tiles__tilemap__bin$0_0$0({96}DA96d,SC:U),D,0,0
 S:G$font_tiles__tiles__psgcompr$0_0$0({385}DA385d,SC:U),D,0,0
-S:G$logo_big__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$logo_big__tilemap__bin$0_0$0({1536}DA1536d,SC:U),D,0,0
-S:G$logo_big__tiles__psgcompr$0_0$0({2692}DA2692d,SC:U),D,0,0
+S:G$splash__tilemap__bin$0_0$0({1536}DA1536d,SC:U),D,0,0
+S:G$splash__tiles__psgcompr$0_0$0({2692}DA2692d,SC:U),D,0,0
--- Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/content/gfx.h
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest/dev/content/gfx.h
@@ -1,3 +1,6 @@
+extern const unsigned char	background__palette__bin[];
+#define				background__palette__bin_size 16
+
 extern const unsigned char	font_tiles__palette__bin[];
 #define				font_tiles__palette__bin_size 16
 
@@ -7,12 +10,9 @@ extern const unsigned char	font_tiles__tilemap__bin[];
 extern const unsigned char	font_tiles__tiles__psgcompr[];
 #define				font_tiles__tiles__psgcompr_size 385
 
-extern const unsigned char	logo_big__palette__bin[];
-#define				logo_big__palette__bin_size 16
+extern const unsigned char	splash__tilemap__bin[];
+#define				splash__tilemap__bin_size 1536
 
-extern const unsigned char	logo_big__tilemap__bin[];
-#define				logo_big__tilemap__bin_size 1536
-
-extern const unsigned char	logo_big__tiles__psgcompr[];
-#define				logo_big__tiles__psgcompr_size 2692
+extern const unsigned char	splash__tiles__psgcompr[];
+#define				splash__tiles__psgcompr_size 2692
 
