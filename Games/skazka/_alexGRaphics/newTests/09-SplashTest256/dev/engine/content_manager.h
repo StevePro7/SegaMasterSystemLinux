147f550c89f57a431e57197a641d9abfc48524c7
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:32:22 +0100

    no message

---
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/engine/content_manager.h
@@ -0,0 +1,8 @@
+#ifndef _CONTENT_MANAGER_H_
+#define _CONTENT_MANAGER_H_
+
+void engine_content_manager_load_tiles();
+void engine_content_manager_draw_logo1();
+void engine_content_manager_draw_logo2();
+
+#endif//_CONTENT_MANAGER_H_
---
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/scripts/gfx.bat
@@ -0,0 +1,25 @@
+@echo off
+::gfx		Commonly used gfx	not banked
+
+cd ..
+cd content
+cd gfx
+
+:: Tiles
+bmp2tile.exe raw/font_tiles.png -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "background (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/splash.png -savetiles "splash (tiles).psgcompr" -removedupes -mirror -planar -tileoffset 64  -savetilemap "splash (tilemap).bin" -savepalette "background (palette).bin" -fullpalette -exit
+::bmp2tile.exe raw/logo_small.png -savetiles "logo_small (tiles).psgcompr" -noremovedupes -planar -tileoffset 256  -savetilemap "logo_small (tilemap).bin" -savepalette "logo_small (palette).bin" -fullpalette -exit
+
+
+cd ..
+folder2c gfx gfx
+
+sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+
+if exist "*.asm" del "*.asm" > nul
+if exist "*.lst" del "*.lst" > nul
+if exist "*.sym" del "*.sym" > nul
+
+
+cd ..
+cd scripts
