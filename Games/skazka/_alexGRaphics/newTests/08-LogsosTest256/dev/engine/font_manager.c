48bf1553fe988fe165933be6986413f9bfbc629c
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:25:05 +0100

    no message

---
+++ Games/skazka/_alexGRaphics/newTests/08-LogsosTest256/dev/engine/font_manager.c
@@ -0,0 +1,82 @@
+#include "font_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+#define TEXT_ROOT	64		// 64 is "A" (65-01).
+#define DATA_ROOT	27		// 27 is "0" (48-27).
+#define UNIT_ROOT	10		// 10 is decimal
+#define DATA_LONG	3		// 3 placeholder
+
+void engine_font_manager_draw_char( unsigned char ch, unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = font_tiles__tilemap__bin;
+	unsigned char tile = ch - TEXT_ROOT;
+	devkit_SMS_setNextTileatXY( x, y );
+	devkit_SMS_setTile( *pnt + tile );
+}
+
+void engine_font_manager_draw_text( unsigned char *text, unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = font_tiles__tilemap__bin;
+	unsigned char idx = 0;
+
+	while( '\0' != text[idx] )
+	{
+		unsigned char ch = text[ idx ];
+		unsigned char tile = text[idx] - TEXT_ROOT;
+		devkit_SMS_setNextTileatXY( x++, y );
+		devkit_SMS_setTile( *pnt + tile );
+		idx++;
+	}
+}
+
+void engine_font_manager_draw_data( unsigned int data, unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = font_tiles__tilemap__bin;
+
+	unsigned char idx;
+	signed char tile;
+
+	unsigned int quotient = 0;
+	unsigned char remainder = 0;
+
+	char hold[DATA_LONG];
+	for( idx = 0; idx < DATA_LONG; ++idx )
+	{
+		quotient = data / UNIT_ROOT;
+		remainder = data % UNIT_ROOT;
+
+		hold[idx] = remainder;
+		data /= UNIT_ROOT;
+
+		tile = hold[idx] + DATA_ROOT;
+		if( 0 == quotient && 0 == remainder && idx > 0 )
+		{
+			// Replace with space!
+			tile = 0;
+		}
+
+		devkit_SMS_setNextTileatXY( x--, y );
+		devkit_SMS_setTile( *pnt + tile );
+	}
+}
+
+void engine_font_manager_draw_zero( unsigned int data, unsigned char x, unsigned char y )
+{
+	const unsigned char *pnt = font_tiles__tilemap__bin;
+
+	unsigned char idx;
+	unsigned char tile;
+
+	char hold[DATA_LONG];
+	for( idx = 0; idx < DATA_LONG; ++idx )
+	{
+		hold[idx] = data % UNIT_ROOT;
+		data /= UNIT_ROOT;
+
+		tile = hold[idx] + DATA_ROOT;
+
+		devkit_SMS_setNextTileatXY( x--, y );
+		devkit_SMS_setTile( *pnt + tile );
+	}
+}
