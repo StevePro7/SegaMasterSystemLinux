ccbeb0c98163aca0afb46e257747f40eecfe4dd9
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:53:08 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/engine/content_manager.c
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/engine/content_manager.c
@@ -13,8 +13,9 @@ void engine_content_manager_load_tiles()
 	devkit_SMS_loadBGPalette( ( void * ) background__palette__bin );
 
 	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) splash__tiles__psgcompr, LOGO_TILES );
-	//devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) logo_small__tiles__psgcompr, SPRITE_TILES );
-	devkit_SMS_loadSpritePalette( ( void * ) background__palette__bin );
+
+	devkit_SMS_loadPSGaidencompressedTiles( ( unsigned char * ) logo_big__tiles__psgcompr, SPRITE_TILES );
+	devkit_SMS_loadSpritePalette( ( void * ) logo_big__palette__bin );
 }
 
 void engine_content_manager_draw_logo1()
@@ -40,7 +41,6 @@ void engine_content_manager_draw_logo1()
 			index = j * wide + i;
 			value = index * 2;
 			tile = splash__tilemap__bin[ value ];
-			//tile = logo_big__tilemap__bin[ value ] | palette;
 			devkit_SMS_setNextTileatXY( x + i, y + j );
 			devkit_SMS_setTile( tile );
 		}
