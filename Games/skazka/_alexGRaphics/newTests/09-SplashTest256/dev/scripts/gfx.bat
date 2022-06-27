8e87ffe8e82cf9b0f3e80afaa343f395a5da8810
StevePro7 <steveprozune@hotmail.com>
2022-06-27 20:49:35 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/scripts/gfx.bat
+++ Games/skazka/_alexGRaphics/newTests/09-SplashTest256/dev/scripts/gfx.bat
@@ -8,7 +8,7 @@ cd gfx
 :: Tiles
 bmp2tile.exe raw/font_tiles.png -savetiles "font_tiles (tiles).psgcompr" -noremovedupes -planar -tileoffset 0  -savetilemap "font_tiles (tilemap).bin" -savepalette "background (palette).bin" -fullpalette -exit
 bmp2tile.exe raw/splash.png -savetiles "splash (tiles).psgcompr" -removedupes -mirror -planar -tileoffset 64  -savetilemap "splash (tilemap).bin" -savepalette "background (palette).bin" -fullpalette -exit
-::bmp2tile.exe raw/logo_small.png -savetiles "logo_small (tiles).psgcompr" -noremovedupes -planar -tileoffset 256  -savetilemap "logo_small (tilemap).bin" -savepalette "logo_small (palette).bin" -fullpalette -exit
+bmp2tile.exe raw/logo_big.png -savetiles "logo_big (tiles).psgcompr" -noremovedupes -planar -tileoffset 256  -savetilemap "logo_big (tilemap).bin" -savepalette "logo_big (palette).bin" -fullpalette -exit
 
 
 cd ..
