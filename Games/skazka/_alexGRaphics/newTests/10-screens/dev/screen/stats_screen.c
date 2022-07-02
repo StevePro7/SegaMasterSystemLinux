3c01c7da04df0e877377a165bcc7f44508661fc6
StevePro7 <steveprozune@hotmail.com>
2022-07-02 17:37:56 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/stats_screen.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/stats_screen.c
@@ -1,7 +1,8 @@
 #include "stats_screen.h"
+#include "../engine/audio_manager.h"
 #include "../engine/content_manager.h"
 #include "../engine/enum_manager.h"
-#include "../engine/font_manager.h"
+//#include "../engine/font_manager.h"
 #include "../engine/game_manager.h"
 #include "../engine/global_manager.h"
 #include "../engine/graphics_manager.h"
@@ -11,33 +12,40 @@
 #include "../engine/text_manager.h"
 #include "../engine/timer_manager.h"
 #include "../devkit/_sms_manager.h"
+#include "../devkit/_snd_manager.h"
 #include "../banks/fixedbank.h"
 
 static unsigned char select_type;
 
 void screen_stats_screen_load()
 {
+	struct_game_object *go = &global_game_object;
+	bool flash_arrow = go->flash_arrow;
+
 	select_type = select_type_stats;
 	engine_player_manager_calc();
 
-	devkit_SMS_displayOff();		// TODO try comment this line out for smooth screen transition??
-	engine_content_manager_load_logo_small();
-	engine_content_manager_load_village();
-	engine_content_manager_load_inventory();
+	if( !flash_arrow )
+	{
+		devkit_SMS_displayOff();		// TODO try comment this line out for smooth screen transition??
+		engine_content_manager_load_logo_small();
+		engine_content_manager_load_village();
+		engine_content_manager_load_inventory();
 
-	engine_text_manager_clear( TOP_Y + 5, TOP_Y + 22 );
-	engine_graphics_manager_draw_logo_small( LEFT_X + 1, TOP_Y + 1 );
-	engine_graphics_manager_draw_village( LEFT_X + 12, TOP_Y + 6 );
+		engine_text_manager_clear( TOP_Y + 5, TOP_Y + 22 );
+		engine_graphics_manager_draw_logo_small( LEFT_X + 1, TOP_Y + 1 );
+		engine_graphics_manager_draw_village( LEFT_X + 12, TOP_Y + 6 );
 
-	engine_player_manager_draw_inventory( LEFT_X + 2, TOP_Y + 14 );
+		engine_player_manager_draw_inventory( LEFT_X + 2, TOP_Y + 14 );
 
-	engine_game_manager_print_stats();
-	engine_game_manager_print_texts();
+		engine_game_manager_print_stats();
+		engine_game_manager_print_texts();
 
-	engine_graphics_manager_draw_border();
-	engine_graphics_manager_draw_underline( TOP_Y + 4 );
+		engine_graphics_manager_draw_border();
+		engine_graphics_manager_draw_underline( TOP_Y + 4 );
 
-	devkit_SMS_displayOn();			// TODO try comment this line out for smooth screen transition??
+		devkit_SMS_displayOn();			// TODO try comment this line out for smooth screen transition??
+	}
 
 	engine_select_manager_load( select_type, LEFT_X + 12, TOP_Y + OPTION_ROW, 6 );
 	engine_game_manager_flash_off();
@@ -54,10 +62,10 @@ void screen_stats_screen_update( unsigned char *screen_type )
 
 	// Wait until SFX completed...
 	*screen_type = screen_type_stats;
-	//if( devkit_PSGSFXGetStatus() )
-	//{
-	//	return;
-	//}
+	if( devkit_PSGSFXGetStatus() )
+	{
+		return;
+	}
 
 	switch( selection )
 	{
@@ -68,9 +76,9 @@ void screen_stats_screen_update( unsigned char *screen_type )
 		*screen_type = screen_type_shop;
 		break;
 	case action_type_rest:
-		//engine_player_manager_rest();
-		//engine_player_manager_stats();
-		//engine_sound_manager_play( sound_type_9 );
+		engine_player_manager_rest();
+		engine_player_manager_stats();
+		engine_sound_manager_play( sound_type_9 );
 		break;
 	case action_type_talk:
 		*screen_type = screen_type_talk;
