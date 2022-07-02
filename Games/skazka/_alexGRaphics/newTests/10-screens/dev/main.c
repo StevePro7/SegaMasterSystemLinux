b812bf96b326fb6be1b4581c8d19cf6b3c3453dc
StevePro7 <steveprozune@hotmail.com>
2022-07-02 17:34:16 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/main.c
@@ -17,7 +17,7 @@ void main( void )
 	//open_screen_type = screen_type_credit;
 	//open_screen_type = screen_type_menu;
 	open_screen_type = screen_type_diff;
-	//open_screen_type = screen_type_intro;
+	open_screen_type = screen_type_intro;
 	//open_screen_type = screen_type_load;
 	//open_screen_type = screen_type_start;
 	//open_screen_type = screen_type_stats;
