32aac6e2be55fa01abe890cbf5f68c5f89452981
StevePro7 <steveprozune@hotmail.com>
2022-07-03 12:39:40 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/kill_screen.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/kill_screen.c
@@ -1,14 +1,52 @@
 #include "kill_screen.h"
+#include "../engine/audio_manager.h"
+#include "../engine/content_manager.h"
 #include "../engine/enum_manager.h"
-#include "../engine/font_manager.h"
+#include "../engine/global_manager.h"
+#include "../engine/graphics_manager.h"
+#include "../engine/timer_manager.h"
+#include "../devkit/_sms_manager.h"
+#include <stdbool.h>
+
+#define KILL_SCREEN_DELAY		50
+
+static unsigned int palette;
+static unsigned char count;
+static bool flag;
 
 void screen_kill_screen_load()
 {
-	engine_font_manager_draw_text( "KILL SCREEN", 10, 2 );
+	engine_content_manager_load_koschey();
+	engine_graphics_manager_draw_koschey( LEFT_X + 10, TOP_Y + 2, devkit_TILE_USE_SPRITE_PALETTE() );
+
+	engine_timer_manager_load( KILL_SCREEN_DELAY );
+	engine_sound_manager_kill();
+	count = 0;
+	flag = true;
 }
 
 void screen_kill_screen_update( unsigned char *screen_type )
 {
+	unsigned char timer;
+
+	timer = engine_timer_manager_update();
+	if( timer )
+	{
+		count++;
+		if( count < 5 )
+		{
+			flag = !flag;
+			palette = flag ? devkit_TILE_USE_SPRITE_PALETTE() : 0;
+			engine_graphics_manager_draw_koschey( LEFT_X + 10, TOP_Y + 2, palette );
+		}
+	}
+
+	if( count > 5 )
+	{
+		*screen_type = screen_type_complete;
+		return;
+	}
+
 	*screen_type = screen_type_kill;
 }
 
