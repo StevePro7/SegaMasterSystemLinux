63d681c1c4454d85d69547756c84ee5842783fe1
StevePro7 <steveprozune@hotmail.com>
2022-07-03 14:32:42 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/engine/player_manager.c
+++ Games/skazka/_alexGRaphics/newTests/10-screens/dev/engine/player_manager.c
@@ -20,9 +20,6 @@ void engine_player_manager_init()
 	po->def_weapon = weapon_type_dagger;	// Q$	ATK
 	po->def_armor = armor_type_none;		// W$	ARM
 	po->def_life = life_type_none;			// E$	UP
-
-	po->def_armor = armor_type_kolchuga;		// W$	ARM
-	po->def_life = life_type_oneup;			// E$	UP
 }
 
 void engine_player_manager_load()
