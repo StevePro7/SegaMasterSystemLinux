5abf8f85a52331185637eb94726bb15d55f7e964
StevePro7 <steveprozune@hotmail.com>
2022-07-03 12:13:42 +0100

    no message

--- Games/skazka/_alexGRaphics/newTests/10-screens/dev/screen/select_screen.h
+++
@@ -1,7 +0,0 @@
-#ifndef _SELECT_SCREEN_H_
-#define _SELECT_SCREEN_H_
-
-void screen_select_screen_load();
-void screen_select_screen_update( unsigned char *screen_type );
-
-#endif//_SELECT_SCREEN_H_
