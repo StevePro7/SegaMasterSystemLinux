d14c82a1fb5689519e93eb7211eb6385d6c992d5
StevePro7 <steveprozune@hotmail.com>
2022-07-13 20:40:52 +0100

    no message

---
+++ sgdk/00-skazka-tests/04-HelloMusic/dev/res/resources.h
@@ -0,0 +1,23 @@
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+#ifdef _CONSOLE
+#include "_genesis.h"
+#else
+#include <genesis.h>
+#endif
+
+extern const u8 sfx_sound_type_1[ 2304 ];
+extern const u8 sfx_sound_type_2[ 2048 ];
+extern const u8 sfx_sound_type_3[ 2048 ];
+extern const u8 sfx_sound_type_4[ 3072 ];
+extern const u8 sfx_sound_type_5[ 6912 ];
+extern const u8 sfx_sound_type_6[ 8448 ];
+extern const u8 sfx_sound_type_7[ 8448 ];
+extern const u8 sfx_sound_type_8[ 3840 ];
+extern const u8 sfx_sound_type_9[ 12800 ];
+extern const u8 sfx_sound_type_10[ 3072 ];
+extern const u8 sfx_sound_type_11[ 199936 ];
+extern const u8 sfx_sound_type_12[ 49408 ];
+
+#endif // _RES_RESOURCES_H_
---
+++ sgdk/00-skazka-tests/04-HelloMusic/sfx/res/resources.c
@@ -0,0 +1,14 @@
+#include "resources.h"
+
+const u8 sfx_sound_type_1[ 2304 ] = { 0 };
+const u8 sfx_sound_type_2[ 2048 ] = { 0 };
+const u8 sfx_sound_type_3[ 2048 ] = { 0 };
+const u8 sfx_sound_type_4[ 3072 ] = { 0 };
+const u8 sfx_sound_type_5[ 6912 ] = { 0 };
+const u8 sfx_sound_type_6[ 8448 ] = { 0 };
+const u8 sfx_sound_type_7[ 8448 ] = { 0 };
+const u8 sfx_sound_type_8[ 3840 ] = { 0 };
+const u8 sfx_sound_type_9[ 12800 ] = { 0 };
+const u8 sfx_sound_type_10[ 3072 ] = { 0 };
+const u8 sfx_sound_type_11[ 199936 ] = { 0 };
+const u8 sfx_sound_type_12[ 49408 ] = { 0 };
