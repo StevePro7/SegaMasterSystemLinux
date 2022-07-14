d8f1f8aa29f21af4832c666aabc1bbfdd3a581c0
StevePro7 <steveprozune@hotmail.com>
2022-07-14 17:00:49 +0100

    no message

--- sgdk/00-skazka-tests/05-HelloFolders/dev/inc/object/game_object.h
+++ sgdk/00-skazka-tests/05-HelloFolders/dev/inc/object/game_object.h
@@ -1,13 +1,13 @@
 #ifndef _GAME_OBJECT_H_
 #define _GAME_OBJECT_H_
 
-#include <stdbool.h>
+//#include <stdbool.h>
 
 typedef struct tag_struct_game_object
 {
-	bool intro_once;
-	bool play_music;
-	bool flash_arrow;
+//	bool intro_once;
+//	bool play_music;
+//	bool flash_arrow;
 	unsigned char difficulty;
 
 } struct_game_object;
