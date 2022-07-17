b23c5e6fbfa2e163577333dc5a79762cc816b3a9
StevePro7 <steveprozune@hotmail.com>
2022-07-17 20:50:23 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
@@ -11,8 +11,8 @@ int main()
 	engine_graphics_manager_init();
 	engine_graphics_manager_load();
 
-	print00();
-	//engine_graphics_manager_draw_splash();
+	//print00();
+	engine_graphics_manager_draw_splash();
 	while( 1 )
 	{
 		VDP_waitVSync();
