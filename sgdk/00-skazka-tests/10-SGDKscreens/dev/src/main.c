29cbc9e879c19c54450e6a6afd7f02223dc32e04
StevePro7 <steveprozune@hotmail.com>
2022-07-17 20:50:57 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
@@ -3,7 +3,7 @@
 static void print00()
 {
 	engine_player_manager_draw( 2, 1, LEFT_X + 2, TOP_Y + 16 );
-	engine_enemy_manager_draw( 4, LEFT_X + 27, TOP_Y + 16 );
+	engine_enemy_manager_draw( 3, LEFT_X + 27, TOP_Y + 16 );
 }
 
 int main()
@@ -11,8 +11,8 @@ int main()
 	engine_graphics_manager_init();
 	engine_graphics_manager_load();
 
-	//print00();
-	engine_graphics_manager_draw_splash();
+	print00();
+	//engine_graphics_manager_draw_splash();
 	while( 1 )
 	{
 		VDP_waitVSync();
