d27595da2b8f9ed7712d3baf23f7164dbb49637a
StevePro7 <steveprozune@hotmail.com>
2022-07-18 20:32:24 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/engine/hack_manager.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/engine/hack_manager.c
@@ -9,13 +9,8 @@ struct_hack_object global_hack_object;
 #define PEEK( addr)			(* ( unsigned char *)( addr ) )
 #define POKE( addr, data )	(* ( unsigned char *)( addr ) = ( data ) )
 
-#define HACKER_START		0x01A0
-
-//#ifdef _CONSOLE
-//#else
-//#pragma disable_warning 110
-//#pragma disable_warning 126
-//#endif
+#define HACKER_START		0x01E0
+#define HACKER_SPACE		32
 
 void engine_hack_manager_init()
 {
@@ -40,17 +35,17 @@ void engine_hack_manager_load()
 
 #ifndef _CONSOLE
 
-	ho->hack_delays = PEEK( HACKER_START - 2 );			// 0x019E		// Used to speed through any game delay.
-	ho->hack_nodead = PEEK( HACKER_START - 1 );			// 0x019F		// Non-zero value enables invincibility.
-	ho->hack_currxp = PEEK( HACKER_START + 0 );			// 0x01A0		// Non-zero sets default experience pts.
-	ho->hack_currhp = PEEK( HACKER_START + 1 );			// 0x01A1		// Non-zero sets default healthy points.
-	ho->hack_goldno = PEEK( HACKER_START + 2 );			// 0x01A2		// Non-zero sets default gold available.
-	ho->hack_weapon = PEEK( HACKER_START + 3 );			// 0x01A3		// Set value to 1 or 2 for stock else 0.
-	ho->hack_armors = PEEK( HACKER_START + 4 );			// 0x01A4		// Set value to 1 or 2 for armor else 0.
-	ho->hack_oneups = PEEK( HACKER_START + 5 );			// 0x01A5		// Set value to 1=Life otherwise 0=None.
-	ho->hack_talker = PEEK( HACKER_START + 6 );			// 0x01A6		// Set value to 1=repeat villagers talk.
-	ho->hack_musics = PEEK( HACKER_START + 7 );			// 0x01A7		// Set 0=Music to play otherwise silent.
-	ho->hack_sounds = PEEK( HACKER_START + 8 );			// 0x01A8		// Set 0=Sound to play otherwise silent.
+	ho->hack_delays = PEEK( HACKER_START - 2 );			// 0x01DE		// Used to speed through any game delay.
+	ho->hack_nodead = PEEK( HACKER_START - 1 );			// 0x01DF		// Non-zero value enables invincibility.
+	ho->hack_currxp = PEEK( HACKER_START + 0 );			// 0x01E0		// Non-zero sets default experience pts.
+	ho->hack_currhp = PEEK( HACKER_START + 1 );			// 0x01E1		// Non-zero sets default healthy points.
+	ho->hack_goldno = PEEK( HACKER_START + 2 );			// 0x01E2		// Non-zero sets default gold available.
+	ho->hack_weapon = PEEK( HACKER_START + 3 );			// 0x01E3		// Set value to 1 or 2 for stock else 0.
+	ho->hack_armors = PEEK( HACKER_START + 4 );			// 0x01E4		// Set value to 1 or 2 for armor else 0.
+	ho->hack_oneups = PEEK( HACKER_START + 5 );			// 0x01E5		// Set value to 1=Life otherwise 0=None.
+	ho->hack_talker = PEEK( HACKER_START + 6 );			// 0x01E6		// Set value to 1=repeat villagers talk.
+	ho->hack_musics = PEEK( HACKER_START + 7 );			// 0x01E7		// Set 0=Music to play otherwise silent.
+	ho->hack_sounds = PEEK( HACKER_START + 8 );			// 0x01E8		// Set 0=Sound to play otherwise silent.
 
 #endif
 }
@@ -58,6 +53,15 @@ void engine_hack_manager_load()
 void engine_hack_manager_invert()
 {
 	struct_hack_object *ho = &global_hack_object;
+	if( ho->hack_delays && HACKER_SPACE == ho->hack_delays )
+	{
+		ho->hack_delays = 0;
+	}
+	if( ho->hack_nodead && HACKER_SPACE == ho->hack_nodead )
+	{
+		ho->hack_nodead = 0;
+	}
+
 	//if( ho->hack_currxp > 0 && ho->hack_currxp <= MAX_XP )
 	//{
 	//	engine_player_manager_def_currxp( ho->hack_currxp );
@@ -83,9 +87,20 @@ void engine_hack_manager_invert()
 	//	engine_player_manager_def_oneups( ho->hack_oneups );
 	//}
 
+	if( ho->hack_musics && HACKER_SPACE == ho->hack_musics )
+	{
+		ho->hack_musics = 0;
+	}
+	if( ho->hack_sounds && HACKER_SPACE == ho->hack_sounds )
+	{
+		ho->hack_sounds = 0;
+	}
+
+
 	// Invert default values.
 	ho->hack_musics = !ho->hack_musics;
 	ho->hack_sounds = !ho->hack_sounds;
 
-	ho->hack_musics = 1;
+	ho->hack_musics = 0;
+	ho->hack_sounds = 0;
 }
