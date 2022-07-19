0773b8c6798a488cdc0d2dc938e879dca4d4f004
StevePro7 <steveprozune@hotmail.com>
2022-07-19 20:05:11 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/main.c
@@ -8,9 +8,9 @@ int main()
 	open_screen_type = screen_type_title;
 	//open_screen_type = screen_type_diff;
 	//open_screen_type = screen_type_intro;
-	//open_screen_type = screen_type_load;
-	open_screen_type = screen_type_over;
-	open_screen_type = screen_type_relive;
+	open_screen_type = screen_type_load;
+	//open_screen_type = screen_type_over;
+	//open_screen_type = screen_type_relive;
 	//open_screen_type = screen_type_complete;
 	//open_screen_type = screen_type_credit;
 	//open_screen_type = screen_type_test;
--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/load_screen.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/load_screen.c
@@ -1,13 +1,27 @@
 #include "load_screen.h"
 #include "enum_manager.h"
-#include "font_manager.h"
+#include "game_manager.h"
+#include "player_manager.h"
+#include "select_manager.h"
 
 void screen_load_screen_load()
 {
-	engine_font_manager_draw_text( "LOAD SCREEN!!", 10, 2 );
+	engine_player_manager_load();
+	engine_player_manager_calc();
+
+	engine_select_manager_once();
 }
 
 void screen_load_screen_update( unsigned char *screen_type )
 {
-	*screen_type = screen_type_load;
+	struct_game_object *go = &global_game_object;
+	unsigned char flash_arrow = go->flash_arrow;
+
+	if( flash_arrow )
+	{
+		*screen_type = screen_type_start;
+		return;
+	}
+
+	*screen_type = screen_type_stats;
 }
