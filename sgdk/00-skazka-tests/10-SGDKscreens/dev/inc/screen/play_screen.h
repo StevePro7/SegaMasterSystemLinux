6c697d089eb27914bcef6f8a4f646125627dbc3d
StevePro7 <steveprozune@hotmail.com>
2022-07-19 20:49:49 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/inc/screen/play_screen.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PLAY_SCREEN__
-#define __PLAY_SCREEN___
-
-int screen_play_screen_init();
-
-#endif//__PLAY_SCREEN__
--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/play_screen.c
+++
@@ -1,7 +0,0 @@
-#include "play_screen.h"
-#include "data_manager.h"
-
-int screen_play_screen_init()
-{
-	return engine_data_manager_test();
-}
