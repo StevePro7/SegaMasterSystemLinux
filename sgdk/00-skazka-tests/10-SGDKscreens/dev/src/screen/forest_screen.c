6baa36ae4febb7ad54cb41f9dafc06ee383c8724
StevePro7 <steveprozune@hotmail.com>
2022-07-20 19:42:17 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/forest_screen.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/forest_screen.c
@@ -27,7 +27,7 @@ static unsigned char select_type;
 static unsigned char run_away_val;
 
 static void setup();
-//static unsigned char calc_add_armor();
+static unsigned char calc_add_armor();
 
 unsigned char run_away_hit[ 2 ] = { 1, 2 };
 
@@ -68,6 +68,57 @@ void screen_forest_screen_update( unsigned char *screen_type )
 
 	if( scene_type_pushon == event_stage )
 	{
+		input = engine_input_manager_hold_buttonA();
+		if( input )
+		{
+			engine_font_manager_draw_text( LOCALE_FIGHT_BLANKS, LEFT_X + 7, TOP_Y + 17 );
+			engine_font_manager_draw_text( LOCALE_FIGHT_BLANKS, LEFT_X + 7, TOP_Y + 18 );
+
+			if( fight_type_battle == curr_selection )
+			{
+				// Calculate whether to add armor on hard difficulty.
+				add_armor = calc_add_armor();
+				if( add_armor )
+				{
+					engine_player_manager_armor( po->armor );
+				}
+
+				// If both you and enemy have 0 HP then you get game over first!
+				engine_player_manager_hit( player_damage );
+				if( engine_player_manager_dead() )
+				{
+					// Check if player has extra life!
+					if( engine_player_manager_life() )
+					{
+						*screen_type = screen_type_relive;
+						return;
+					}
+
+					*screen_type = screen_type_over;
+					return;
+				}
+
+				engine_enemy_manager_hit( enemys_damage );
+				if( engine_enemy_manager_dead() )
+				{
+					engine_audio_manager_stop();
+
+					engine_fight_manager_gold( &xp, &player_gold );
+					engine_player_manager_inc_gold( xp, player_gold );
+
+					*screen_type = screen_type_victory;
+					return;
+				}
+			}
+
+			// Display updated HP after checking deaths.
+			engine_player_manager_hplo();
+			engine_enemy_manager_hplo();
+
+			event_stage = scene_type_select;
+			*screen_type = screen_type_forest;
+			return;
+		}
 	}
 
 	if( scene_type_select == event_stage )
@@ -213,28 +264,28 @@ static void setup()
 	engine_enemy_manager_hplo();
 }
 
-//static unsigned char calc_add_armor()
-//{
-//	struct_game_object *go = &global_game_object;
-//	struct_enemy_object *eo = &global_enemy_object;
-//	struct_player_object *po = &global_player_object;
-//
-//	unsigned char add_armor = 1;
-//	if( diff_type_hard == go->difficulty )
-//	{
-//		if( po->level >= 2 )
-//		{
-//			// For weaker enemies on hard difficulty do not factor in armor.
-//			if( enemy_type_razboynik == eo->index || enemy_type_hungry_wolf == eo->index )
-//			{
-//				add_armor = 0;
-//			}
-//		}
-//		if( add_armor )
-//		{
-//			add_armor = engine_random_manager_diff( po->level );
-//		}
-//	}
-//
-//	return add_armor;
-//}
+static unsigned char calc_add_armor()
+{
+	struct_game_object *go = &global_game_object;
+	struct_enemy_object *eo = &global_enemy_object;
+	struct_player_object *po = &global_player_object;
+
+	unsigned char add_armor = 1;
+	if( diff_type_hard == go->difficulty )
+	{
+		if( po->level >= 2 )
+		{
+			// For weaker enemies on hard difficulty do not factor in armor.
+			if( enemy_type_razboynik == eo->index || enemy_type_hungry_wolf == eo->index )
+			{
+				add_armor = 0;
+			}
+		}
+		if( add_armor )
+		{
+			add_armor = engine_random_manager_diff( po->level );
+		}
+	}
+
+	return add_armor;
+}
