e8792512dfb16696ca5c41e54eef4f5cf9d2e0c9
StevePro7 <steveprozune@hotmail.com>
2022-07-20 20:00:27 +0100

    no message

--- sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/boss_screen.c
+++ sgdk/00-skazka-tests/10-SGDKscreens/dev/src/screen/boss_screen.c
@@ -22,14 +22,14 @@
 #include <genesis.h>
 #endif
 
-static unsigned char select_type;
+//static unsigned char select_type;
 static unsigned char event_stage;
 static unsigned char enemys_damage;
 static unsigned char player_damage;
 static unsigned char player_weapon;
 static unsigned char player_armor;
 static unsigned char beg_boss_val;
-static unsigned char first_time;
+//static unsigned char first_time;
 
 static void boss_init( unsigned char *p_weapon, unsigned char *p_armor );
 static void boss_stats( unsigned char *p_weapon, unsigned char *p_armor );
@@ -81,7 +81,6 @@ void screen_boss_screen_load()
 	}
 
 	beg_boss_val = beg_boss_hit[ go->difficulty ];
-
 	//first_time = 1;
 }
 
