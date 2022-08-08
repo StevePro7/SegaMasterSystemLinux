5a25029fd88d93fac8112b4361d03c3280149a55
StevePro7 <steveprozune@hotmail.com>
2022-08-08 19:57:18 +0100

    no message

--- TurtleBridge/tilesNew/04-NewSpritesZoom/dev/content/gfx.h
+++ TurtleBridge/tilesNew/04-NewSpritesZoom/dev/content/gfx.h
@@ -11,5 +11,5 @@ extern const unsigned char	sprite_tiles__palette__bin[];
 #define				sprite_tiles__palette__bin_size 16
 
 extern const unsigned char	sprite_tiles__tiles__psgcompr[];
-#define				sprite_tiles__tiles__psgcompr_size 2278
+#define				sprite_tiles__tiles__psgcompr_size 853
 
