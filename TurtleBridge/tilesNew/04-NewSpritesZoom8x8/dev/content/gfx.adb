c7a89377a0174dd84fdf5113b1407ca2695d5519
StevePro7 <steveprozune@hotmail.com>
2022-08-09 21:46:55 +0100

    no message

---
+++ TurtleBridge/tilesNew/04-NewSpritesZoom8x8/dev/content/gfx.adb
@@ -0,0 +1,6 @@
+M:gfx
+S:G$bggame_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
+S:G$bggame_tiles__tilemap__bin$0_0$0({512}DA512d,SC:U),D,0,0
+S:G$bggame_tiles__tiles__psgcompr$0_0$0({4172}DA4172d,SC:U),D,0,0
+S:G$sprite_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
+S:G$sprite_tiles__tiles__psgcompr$0_0$0({853}DA853d,SC:U),D,0,0
---
+++ TurtleBridge/tilesNew/04-NewSpritesZoom8x8/dev/content/gfx.h
@@ -0,0 +1,15 @@
+extern const unsigned char	bggame_tiles__palette__bin[];
+#define				bggame_tiles__palette__bin_size 16
+
+extern const unsigned char	bggame_tiles__tilemap__bin[];
+#define				bggame_tiles__tilemap__bin_size 512
+
+extern const unsigned char	bggame_tiles__tiles__psgcompr[];
+#define				bggame_tiles__tiles__psgcompr_size 4172
+
+extern const unsigned char	sprite_tiles__palette__bin[];
+#define				sprite_tiles__palette__bin_size 16
+
+extern const unsigned char	sprite_tiles__tiles__psgcompr[];
+#define				sprite_tiles__tiles__psgcompr_size 853
+
