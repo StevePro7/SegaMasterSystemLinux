85c4be3976f0dcf8c4777a8ee81f38828a89cc07
StevePro7 <steveprozune@hotmail.com>
2022-08-09 21:50:16 +0100

    no message

--- TurtleBridge/tilesNew/04-NewSpritesZoom8x8/dev/scripts/gfx.bat
+++ TurtleBridge/tilesNew/04-NewSpritesZoom8x8/dev/scripts/gfx.bat
@@ -7,17 +7,17 @@ cd gfx
 
 
 :: Tiles
-bmp2tile.exe raw/bggame_tiles.png -savetiles "bggame_tiles (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 0  -savetilemap "bggame_tiles (tilemap).bin" -savepalette "bggame_tiles (palette).bin" -fullpalette -exit
+"C:\SEGA\bmp2tile043\bmp2tile.exe" raw/bggame_tiles.png -savetiles "bggame_tiles (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 0  -savetilemap "bggame_tiles (tilemap).bin" -savepalette "bggame_tiles (palette).bin" -fullpalette -exit
 
 
 :: Sprites
-bmp2tile.exe raw/sprite_tiles.png -savetiles "sprite_tiles (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprite_tiles (palette).bin" -fullpalette  -spritepalette -exit
+"C:\SEGA\bmp2tile043\bmp2tile.exe" raw/sprite_tiles.png -savetiles "sprite_tiles (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprite_tiles (palette).bin" -fullpalette  -spritepalette -exit
 
 
 cd ..
-folder2c gfx gfx
+"C:\Program Files\SDCC\bin]folder2c.exe" gfx gfx
 
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 gfx.c
 
 if exist "*.asm" del "*.asm" > nul
 if exist "*.lst" del "*.lst" > nul
