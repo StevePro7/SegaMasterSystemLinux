2c0fc92e7816ecbbeb8198b4c54e90490997abea
StevePro7 <steveprozune@hotmail.com>
2022-10-04 20:45:37 +0100

    no message

---
+++ TurtleBridge/physics/10-Triangle01/02-Test/Game1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace _02_Test
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D triangle;
+		Texture2D block;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = 600;
+			graphics.PreferredBackBufferHeight = 500;
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void Initialize()
+		{
+			IsMouseVisible = true;
+			Logger.Initialize();
+			IsFixedTimeStep = true;
+			var fps = 50;
+			TargetElapsedTime = TimeSpan.FromSeconds(1.0f / fps);
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			triangle = Content.Load<Texture2D>("triangle345");
+			block = Content.Load<Texture2D>("block");
+		}
+
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(triangle, new Vector2(100, 100), Color.White);
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
