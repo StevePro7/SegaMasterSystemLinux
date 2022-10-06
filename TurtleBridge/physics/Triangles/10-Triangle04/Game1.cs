7cc8efda9b48f9675aa93549830105fb99237739
StevePro7 <steveprozune@hotmail.com>
2022-10-06 20:58:50 +0100

    no message

---
+++ TurtleBridge/physics/Triangles/10-Triangle04/Game1.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace _02_Test
+{
+	public class Game1 : Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D[] triangle;
+		private const int High = 256-80;
+		private readonly MyRocketManager myRocketManager;
+		private readonly MyConfigManger myConfigManger;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = 256;
+			graphics.PreferredBackBufferHeight = 256;
+			Content.RootDirectory = "Content";
+			myConfigManger = new MyConfigManger();
+			myRocketManager = new MyRocketManager(myConfigManger);
+			myRocketManager.Initialize();
+		}
+
+		protected override void Initialize()
+		{
+			IsMouseVisible = true;
+			Logger.Initialize();
+			IsFixedTimeStep = true;
+			var fps = 50;
+			TargetElapsedTime = TimeSpan.FromSeconds(1.0f / fps);
+			myConfigManger.Initialize();
+			myRocketManager.Initialize();
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			triangle = new Texture2D[2];
+			triangle[0] = Content.Load<Texture2D>("triangle45_01");
+			triangle[1] = Content.Load<Texture2D>("triangle45_02");
+			myRocketManager.LoadContent(Content);
+		}
+
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
+
+			//if (!myRocketManager.IsRocketFlying)
+			//{
+			//	if (Keyboard.GetState().IsKeyDown(Keys.D1))
+			//	{
+			//		myRocketManager.Launch(1);
+			//	}
+			//	if (Keyboard.GetState().IsKeyDown(Keys.D2))
+			//	{
+			//		myRocketManager.Launch(2);
+			//	}
+			//	if (Keyboard.GetState().IsKeyDown(Keys.D3))
+			//	{
+			//		myConfigManger.Initialize();
+			//		myRocketManager.Launch(3);
+			//	}
+			//	if (Keyboard.GetState().IsKeyDown(Keys.D4))
+			//	{
+			//		myRocketManager.Launch(4);
+			//	}
+			//	if (Keyboard.GetState().IsKeyDown(Keys.D5))
+			//	{
+			//		myRocketManager.Launch(5);
+			//	}
+			//}
+
+			//myRocketManager.Update(gameTime);
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			//GraphicsDevice.Clear(Color.White);
+
+			spriteBatch.Begin();
+			spriteBatch.Draw(triangle[0], new Vector2(32, High), Color.White);
+			spriteBatch.Draw(triangle[1], new Vector2(256-80-32, High), Color.White);
+			myRocketManager.Draw(spriteBatch);
+
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+
+	}
+}
---
+++ TurtleBridge/physics/Triangles/10-Triangle04/MyRocketManager.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace _02_Test
+{
+	public class MyRocketManager
+	{
+		private readonly MyConfigManger myConfigManger;
+
+		private bool _rocketFlying = false;
+		private Vector2 _rocketPosition;
+		private float angle;
+		private float radians;
+		private float speed;
+		private float veliX, veliY;
+		private float timer;
+		float delta;
+		private IList<Vector2> _rocketPositionList;
+
+		private Texture2D _rocketImage01, _rocketImage03;
+		private int Wide = 0;
+		private int High = 256- 32;
+		private const float gravity = -9.8f;
+		private byte index = 0;
+
+		public MyRocketManager(MyConfigManger myConfigManger)
+		{
+			this.myConfigManger = myConfigManger;
+			index = 2;
+		}
+
+		public void Initialize()
+		{
+			_rocketPositionList = new List<Vector2>();
+			_rocketPositionList.Clear();
+
+			InitPos(index);
+			_rocketPosition = new Vector2(Wide, High);
+			//_rocketPositionList.Add(_rocketPosition);
+			//_rocketAngle = 0.0f;
+
+			angle = myConfigManger.Angle;
+			radians = MathHelper.ToRadians(angle);
+			speed = myConfigManger.Speed;
+			veliX = (float)(speed * Math.Cos(radians));
+			veliY = (float)(speed * Math.Sin(radians));
+			timer = 0.0f;
+		}
+
+		public void InitPos(byte myIndex)
+		{
+			delta = 0.0f;
+			index = myIndex;
+			if (1 == index)
+			{
+				Wide = 0; High = 256 - 32;
+			}
+			if (2 == index)
+			{
+				Wide = 80; High = 256- 80-32;
+			}
+			if (3 == index)
+			{
+				Wide = 200 - 16; High = 400 - 32;
+			}
+			if (4 == index)
+			{
+				Wide = 600 - 16; High = 400 - 32;
+			}
+			if (5 == index)
+			{
+				Wide = 600 +57-16; High = 400 + 80 - 32;
+			}
+
+			var msg = $"(W,H)=({Wide},{High})";
+			Logger.Info(msg);
+		}
+
+		public void LoadContent(ContentManager content)
+		{
+			_rocketImage01 = content.Load<Texture2D>("steve01");
+			_rocketImage03 = content.Load<Texture2D>("steve03");
+		}
+
+		public void Launch(byte myIndex)
+		{
+			angle = myConfigManger.Angle;
+			radians = MathHelper.ToRadians(angle);
+
+			if (_rocketFlying)
+			{
+				return;
+			}
+			index = myIndex;
+			if (3==index)
+			{
+				Initialize();
+			}
+			else
+			{
+				InitPos(myIndex);
+			}
+
+			_rocketFlying = true;
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			if (!_rocketFlying)
+			{
+				return;
+			}
+
+			if (1 == index)
+			{
+				Update01(gameTime);
+			}
+			if (2 == index)
+			{
+				Update02(gameTime);
+			}
+			if (3 == index)
+			{
+				Update03(gameTime);
+			}
+			if (4 == index)
+			{
+				Update04(gameTime);
+			}
+			if (5 == index)
+			{
+				Update05(gameTime);
+			}
+		}
+
+		private void Update01(GameTime gameTime)
+		{
+			Vector2 prevPos = _rocketPosition;
+			_rocketPosition = new Vector2(prevPos.X + 1, High);
+			if (_rocketPosition.X >= 200 - 57 - 16)
+			{
+				_rocketFlying = false;
+			}
+		}
+		private void Update02(GameTime gameTime)
+		{
+			Vector2 prevPos = _rocketPosition;
+			var posY = GetY(delta);
+			delta++;
+			_rocketPosition = new Vector2(prevPos.X + 1, High - posY);
+			if (_rocketPosition.X >= 200 - 16)
+			{
+				_rocketFlying = false;
+				delta = 0;
+			}
+		}
+		private void Update03(GameTime gameTime)
+		{
+			float delta = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000f;
+			timer += delta;
+			float posX = veliX * timer;
+			float posY = (float)(veliY * timer + 0.5 * gravity * timer * timer);
+
+			_rocketPosition = new Vector2(Wide + posX, High - posY);
+			//_rocketPositionList.Add(_rocketPosition);
+
+			if (_rocketPosition.Y >= High)
+			{
+				_rocketFlying = false;
+				foreach (var pos in _rocketPositionList)
+				{
+					//Logger.Info($"{pos.X},{pos.Y}");
+				}
+			}
+		}
+		private void Update04(GameTime gameTime)
+		{
+			Vector2 prevPos = _rocketPosition;
+			var posY = GetY(delta);
+			delta++;
+			_rocketPosition = new Vector2(prevPos.X + 1, High + posY);
+			if (_rocketPosition.X >= 600 + 57 - 16)
+			{
+				_rocketFlying = false;
+				delta = 0;
+			}
+		}
+		private void Update05(GameTime gameTime)
+		{
+			Vector2 prevPos = _rocketPosition;
+			_rocketPosition = new Vector2(prevPos.X + 1, High);
+			if (_rocketPosition.X >= 750)
+			{
+				_rocketFlying = false;
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			if (1 == index || 5==index)
+			{
+				spriteBatch.Draw(_rocketImage01, _rocketPosition, Color.White);
+			}
+			else
+			{
+				spriteBatch.Draw(_rocketImage03, _rocketPosition, Color.White);
+			}
+		}
+
+		public bool IsRocketFlying { get { return _rocketFlying; } }
+
+		private float GetY(float x)
+		{
+			var rads = MathHelper.ToRadians(angle);
+			var angl = Math.Tan(rads);
+			var high = x * angl;
+			return (float)high;
+		}
+	}
+}
