405c41d366c8ef678cd18d06d77b20d25281fcdb
StevePro7 <steveprozune@hotmail.com>
2022-10-15 18:35:25 +0100

    no message

--- TurtleBridge/physics/Catapult/20-Modified/Catapult.WIN/Catapult.cs
+++ TurtleBridge/physics/Catapult/20-Modified/Catapult.WIN/Catapult.cs
@@ -83,16 +83,11 @@ namespace CatapultMiniGame
 
 		public override void  Initialize()
 		{
-			baseTexture = 
-				curGame.Content.Load<Texture2D>("Textures/body_front");
-			baseTextureBack = 
-				curGame.Content.Load<Texture2D>("Textures/body_back");
-			pumpkinTexture = 
-				curGame.Content.Load<Texture2D>("Textures/pumpkin");
-			pumpkinSmashTexture = 
-				curGame.Content.Load<Texture2D>("Textures/pumpkinsmash");
+			baseTexture = curGame.Content.Load<Texture2D>("Textures/body_front");
+			baseTextureBack = curGame.Content.Load<Texture2D>("Textures/body_back");
+			pumpkinTexture = curGame.Content.Load<Texture2D>("Textures/pumpkin");
+			pumpkinSmashTexture = curGame.Content.Load<Texture2D>("Textures/pumpkinsmash");
 			armTexture = curGame.Content.Load<Texture2D>("Textures/arm");
-
 			base.Initialize();
 		}
 
@@ -113,8 +108,7 @@ namespace CatapultMiniGame
 				if (curGame.CurrentKeyboardState.IsKeyDown(Keys.Right))
 					speedAmt = 1.0f;
 
-				baseSpeed += speedAmt * 
-								gameTime.ElapsedGameTime.Milliseconds * 0.001f;
+				baseSpeed += speedAmt * gameTime.ElapsedGameTime.Milliseconds * 0.001f;
 
 				// Move catapult based on speed
 				basePosition.X += baseSpeed * gameTime.ElapsedGameTime.Milliseconds;
@@ -124,10 +118,7 @@ namespace CatapultMiniGame
 				pumpkinPosition.Y = basePosition.Y + 80;
 
 				// Check to see if we fire the pumpkin
-				if ((curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed &&
-					curGame.LastGamePadState.Buttons.A != ButtonState.Pressed) ||
-					(curGame.CurrentKeyboardState.IsKeyDown(Keys.Space) &&
-					curGame.LastKeyboardState.IsKeyUp(Keys.Space)))
+				if ((curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed && curGame.LastGamePadState.Buttons.A != ButtonState.Pressed) || (curGame.CurrentKeyboardState.IsKeyDown(Keys.Space) && curGame.LastKeyboardState.IsKeyUp(Keys.Space)))
 				{
 					Fire();
 				}
@@ -138,24 +129,18 @@ namespace CatapultMiniGame
 				// Rotate the arm
 				if (armRotation < MathHelper.ToRadians(81))
 				{
-					armRotation += 
-						MathHelper.ToRadians(gameTime.ElapsedGameTime.Milliseconds);
+					armRotation += MathHelper.ToRadians(gameTime.ElapsedGameTime.Milliseconds);
 
 					Matrix matTranslate, matTranslateBack, matRotate, matFinal;
-					matTranslate = Matrix.CreateTranslation((-pumpkinRotationPosition.X)
-									- 170, -pumpkinRotationPosition.Y, 0);
-					matTranslateBack = 
-						Matrix.CreateTranslation(pumpkinRotationPosition.X + 170, 
-													pumpkinRotationPosition.Y, 0);
+					matTranslate = Matrix.CreateTranslation((-pumpkinRotationPosition.X) - 170, -pumpkinRotationPosition.Y, 0);
+					matTranslateBack = Matrix.CreateTranslation(pumpkinRotationPosition.X + 170, pumpkinRotationPosition.Y, 0);
 					matRotate = Matrix.CreateRotationZ(armRotation);
 					matFinal = matTranslate * matRotate * matTranslateBack;
 
-					Vector2.Transform(ref pumpkinRotationPosition, ref matFinal, 
-										out pumpkinPosition);
+					Vector2.Transform(ref pumpkinRotationPosition, ref matFinal, out pumpkinPosition);
 					pumpkinLaunchPosition = pumpkinPosition.X;
 
-					pumpkinRotation += MathHelper.ToRadians(
-										gameTime.ElapsedGameTime.Milliseconds / 10.0f);
+					pumpkinRotation += MathHelper.ToRadians(gameTime.ElapsedGameTime.Milliseconds / 10.0f);
 				}
 				// We are done rotating so send the pumpkin flying
 				else
@@ -200,14 +185,11 @@ namespace CatapultMiniGame
 			else if (currentState == CatapultState.ProjectileFlying)
 			{
 				// Update the position of the pumpkin
-				pumpkinPosition += pumpkinVelocity * 
-									gameTime.ElapsedGameTime.Milliseconds;
-				pumpkinVelocity += pumpkinAcceleration * 
-									gameTime.ElapsedGameTime.Milliseconds;
+				pumpkinPosition += pumpkinVelocity * gameTime.ElapsedGameTime.Milliseconds;
+				pumpkinVelocity += pumpkinAcceleration * gameTime.ElapsedGameTime.Milliseconds;
 
 				// Move the catapult away from the pumpkin
-				basePosition.X -= pumpkinVelocity.X * 
-									gameTime.ElapsedGameTime.Milliseconds;
+				basePosition.X -= pumpkinVelocity.X * gameTime.ElapsedGameTime.Milliseconds;
 
 				// Rotate the pumpkin as it flys
 				pumpkinRotation += MathHelper.ToRadians(pumpkinVelocity.X * 3.5f);
@@ -234,12 +216,7 @@ namespace CatapultMiniGame
 			}
 
 			// If the projectile hit or we crashed reset the catapult
-			if ((currentState == CatapultState.Crash || 
-						currentState == CatapultState.ProjectileHit) && 
-						(curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed || 
-						curGame.CurrentKeyboardState.IsKeyDown(Keys.Space)) &&
-						curGame.CurrentGamePadState.Triggers.Left == 0 &&
-						curGame.CurrentKeyboardState.IsKeyUp(Keys.Right))
+			if ((currentState == CatapultState.Crash || currentState == CatapultState.ProjectileHit) && (curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed || curGame.CurrentKeyboardState.IsKeyDown(Keys.Space)) && curGame.CurrentGamePadState.Triggers.Left == 0 && curGame.CurrentKeyboardState.IsKeyUp(Keys.Right))
 			{
 				currentState = CatapultState.Reset;
 			}
@@ -255,33 +232,21 @@ namespace CatapultMiniGame
 
 			if (currentState == CatapultState.Crash)
 			{
-				curGame.SpriteBatch.Draw(baseTextureBack, basePosition, null, 
-					Color.White, MathHelper.ToRadians(-5), 
-					Vector2.Zero, 1.0f, SpriteEffects.None, 0);
-				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, 
-					Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
-				curGame.SpriteBatch.Draw(baseTexture, basePosition, null, Color.White,
-					MathHelper.ToRadians(5), Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+				curGame.SpriteBatch.Draw(baseTextureBack, basePosition, null, Color.White, MathHelper.ToRadians(-5), Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
+				curGame.SpriteBatch.Draw(baseTexture, basePosition, null, Color.White, MathHelper.ToRadians(5), Vector2.Zero, 1.0f, SpriteEffects.None, 0);
 			}
 			else
 			{
 				curGame.SpriteBatch.Draw(baseTextureBack, basePosition, Color.White);
-				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, 
-					Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
+				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
 				curGame.SpriteBatch.Draw(baseTexture, basePosition, Color.White);
 			}
 
-			if (currentState != CatapultState.ProjectileHit && 
-				currentState != CatapultState.Crash)
-				curGame.SpriteBatch.Draw(pumpkinTexture, 
-								new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), 
-								null, Color.White, pumpkinRotation,
-								new Vector2(32, 32), 1.0f, SpriteEffects.None, 0.0f);
+			if (currentState != CatapultState.ProjectileHit && currentState != CatapultState.Crash)
+				curGame.SpriteBatch.Draw(pumpkinTexture, new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), null, Color.White, pumpkinRotation, new Vector2(32, 32), 1.0f, SpriteEffects.None, 0.0f);
 			else
-				curGame.SpriteBatch.Draw(pumpkinSmashTexture, 
-								new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), 
-								null, Color.White, 0,
-								new Vector2(50, 32), 1.0f, SpriteEffects.None, 0.0f);
+				curGame.SpriteBatch.Draw(pumpkinSmashTexture, new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), null, Color.White, 0, new Vector2(50, 32), 1.0f, SpriteEffects.None, 0.0f);
 
 			base.Draw(gameTime);
 		}
