2035b424d3cd54a6a086d8543ed3562e01726d41
StevePro7 <steveprozune@hotmail.com>
2022-10-15 17:51:15 +0100

    no message

---
+++ TurtleBridge/physics/Catapult/01-Original/Catapult.WIN/Game.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace CatapultMiniGame
+{
+	public class CatapultGame : Game
+	{
+		GraphicsDeviceManager graphics;
+
+		public CatapultGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+		}
+
+		protected override void Initialize()
+		{
+			IsMouseVisible = true;
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+		}
+
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ TurtleBridge/physics/Catapult/01-Original/Catapult.WIN/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CatapultMiniGame
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			using (CatapultGame game = new CatapultGame())
+			{
+				game.Run();
+			}
+		}
+	}
+}
