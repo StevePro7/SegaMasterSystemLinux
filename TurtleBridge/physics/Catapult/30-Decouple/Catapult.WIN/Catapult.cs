2631f8bd562da8e8ff4d53e6374c2950fceb8c11
StevePro7 <steveprozune@hotmail.com>
2022-10-16 11:17:15 +0100

    no message

---
+++ TurtleBridge/physics/Catapult/30-Decouple/Catapult.WIN/Catapult.cs
@@ -0,0 +1,282 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CatapultMiniGame
+{
+	public enum CatapultState 
+	{ 
+		Rolling, 
+		Firing, 
+		Crash, 
+		ProjectileFlying, 
+		ProjectileHit,
+		Reset
+	}
+
+	public class Catapult : DrawableGameComponent
+	{
+		// Hold what game I belong to
+		CatapultGame curGame = null;
+
+		// Current state of the Catapult
+		CatapultState currentState;
+		public CatapultState CurrentState
+		{
+			get { return currentState; }
+			set { currentState = value; }
+		}
+
+		// Texture used for base of Catapult
+		Texture2D baseTexture;
+		Texture2D baseTextureBack;
+
+		// Texture for the arm
+		Texture2D armTexture;
+
+		// Textures for pumpkins
+		Texture2D pumpkinTexture;
+		Texture2D pumpkinSmashTexture;
+
+		// Position and speed of catapult base
+		Vector2 basePosition = Vector2.Zero;
+		float baseSpeed;
+
+		// Position and rotation of catapult arm
+		Vector2 armCenter = new Vector2(200, 27);
+		Vector2 armOffset = new Vector2(280, 100);
+		float armRotation;
+
+		// Position, speed, and rotation of pumpkin
+		Vector2 pumpkinPosition = Vector2.Zero;
+		public Vector2 PumpkinPosition
+		{
+			get { return pumpkinPosition; }
+			set { pumpkinPosition = value; }
+		}
+
+		Vector2 pumpkinVelocity = Vector2.Zero;
+		Vector2 pumpkinAcceleration = new Vector2(0, 0.001f);
+		Vector2 pumpkinRotationPosition = Vector2.Zero;
+		float pumpkinLaunchPosition;
+		public float PumpkinLaunchPosition
+		{
+			get { return pumpkinLaunchPosition; }
+			set { pumpkinLaunchPosition = value; }
+		}
+		float pumpkinRotation;
+
+		// Level of boost power
+		int boostPower;
+		public int BoostPower
+		{
+			get { return boostPower; }
+			set { boostPower = value; }
+		}
+
+		public Catapult(Game game) : base(game)
+		{
+			curGame = (CatapultGame)game;
+			ResetCatapult();
+		}
+
+		public override void  Initialize()
+		{
+			baseTexture = curGame.Content.Load<Texture2D>("Textures/body_front");
+			baseTextureBack = curGame.Content.Load<Texture2D>("Textures/body_back");
+			pumpkinTexture = curGame.Content.Load<Texture2D>("Textures/pumpkin");
+			pumpkinSmashTexture = curGame.Content.Load<Texture2D>("Textures/pumpkinsmash");
+			armTexture = curGame.Content.Load<Texture2D>("Textures/arm");
+			base.Initialize();
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			if (gameTime == null)
+				throw new ArgumentNullException("gameTime");
+
+			// Do we need to reset
+			if (currentState == CatapultState.Reset)
+				ResetCatapult();
+
+			// Are we currently rolling?
+			if (currentState == CatapultState.Rolling)
+			{
+				// Add to current speed
+				float speedAmt = curGame.CurrentGamePadState.Triggers.Left;
+				if (curGame.CurrentKeyboardState.IsKeyDown(Keys.Right))
+					speedAmt = 1.0f;
+
+				baseSpeed += speedAmt * gameTime.ElapsedGameTime.Milliseconds * 0.001f;
+
+				// Move catapult based on speed
+				basePosition.X += baseSpeed * gameTime.ElapsedGameTime.Milliseconds;
+
+				// Move pumpkin to match catapult
+				pumpkinPosition.X = pumpkinLaunchPosition = basePosition.X + 120;
+				pumpkinPosition.Y = basePosition.Y + 80;
+
+				// Check to see if we fire the pumpkin
+				if ((curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed && curGame.LastGamePadState.Buttons.A != ButtonState.Pressed) || (curGame.CurrentKeyboardState.IsKeyDown(Keys.Space) && curGame.LastKeyboardState.IsKeyUp(Keys.Space)))
+				{
+					Fire();
+				}
+			}
+			// Are we in the firing state
+			else if (currentState == CatapultState.Firing)
+			{
+				// Rotate the arm
+				if (armRotation < MathHelper.ToRadians(81))
+				{
+					armRotation += MathHelper.ToRadians(gameTime.ElapsedGameTime.Milliseconds);
+
+					Matrix matTranslate, matTranslateBack, matRotate, matFinal;
+					matTranslate = Matrix.CreateTranslation((-pumpkinRotationPosition.X) - 170, -pumpkinRotationPosition.Y, 0);
+					matTranslateBack = Matrix.CreateTranslation(pumpkinRotationPosition.X + 170, pumpkinRotationPosition.Y, 0);
+					matRotate = Matrix.CreateRotationZ(armRotation);
+					matFinal = matTranslate * matRotate * matTranslateBack;
+
+					Vector2.Transform(ref pumpkinRotationPosition, ref matFinal, out pumpkinPosition);
+					pumpkinLaunchPosition = pumpkinPosition.X;
+
+					pumpkinRotation += MathHelper.ToRadians(gameTime.ElapsedGameTime.Milliseconds / 10.0f);
+				}
+				// We are done rotating so send the pumpkin flying
+				else
+				{
+					currentState = CatapultState.ProjectileFlying;
+
+					pumpkinVelocity.X = baseSpeed * 2.0f + 1;
+					pumpkinVelocity.Y = -baseSpeed * 0.75f;
+
+					// Add extra velocity for Right trigger 
+					float rightTriggerAmt = curGame.CurrentGamePadState.Triggers.Right;
+
+					if (rightTriggerAmt > 0.5f)
+						rightTriggerAmt = 1.0f - rightTriggerAmt;
+
+					if (curGame.CurrentKeyboardState.IsKeyDown(Keys.B))
+						rightTriggerAmt = 0.5f;
+
+					rightTriggerAmt *= 2;
+
+					pumpkinVelocity *= 1.0f + rightTriggerAmt;
+
+					// Check for extra boost power
+					if (basePosition.X > 620)
+					{
+						boostPower = 3;
+						pumpkinVelocity *= 2.0f;
+					}
+					else if (basePosition.X > 600)
+					{
+						boostPower = 2;
+						pumpkinVelocity *= 1.6f;
+					}
+					else if (basePosition.X > 580)
+					{
+						boostPower = 1;
+						pumpkinVelocity *= 1.3f;
+					}
+				}
+			}
+			// Pumpkin is in the flying state
+			else if (currentState == CatapultState.ProjectileFlying)
+			{
+				// Update the position of the pumpkin
+				pumpkinPosition += pumpkinVelocity * gameTime.ElapsedGameTime.Milliseconds;
+				pumpkinVelocity += pumpkinAcceleration * gameTime.ElapsedGameTime.Milliseconds;
+
+				// Move the catapult away from the pumpkin
+				basePosition.X -= pumpkinVelocity.X * gameTime.ElapsedGameTime.Milliseconds;
+
+				// Rotate the pumpkin as it flys
+				pumpkinRotation += MathHelper.ToRadians(pumpkinVelocity.X * 3.5f);
+
+				// Is the pumpkin hitting the ground
+				if (pumpkinPosition.Y > 630)
+				{
+					// Move the pumpkin out of the ground and Change the pumkin velocity
+					pumpkinPosition.Y = 630;
+					pumpkinVelocity.Y *= -0.8f;
+					pumpkinVelocity.X *= 0.7f;
+
+					// Stop the pumpkin if the speed is too low
+					if (pumpkinVelocity.X < 0.1f)
+					{
+						currentState = CatapultState.ProjectileHit;
+					}
+				}
+			}
+			// Did we crash into the log
+			if (basePosition.X > 650)
+			{
+				currentState = CatapultState.Crash;
+			}
+
+			// If the projectile hit or we crashed reset the catapult
+			if ((currentState == CatapultState.Crash || currentState == CatapultState.ProjectileHit) && (curGame.CurrentGamePadState.Buttons.A == ButtonState.Pressed || curGame.CurrentKeyboardState.IsKeyDown(Keys.Space)) && curGame.CurrentGamePadState.Triggers.Left == 0 && curGame.CurrentKeyboardState.IsKeyUp(Keys.Right))
+			{
+				currentState = CatapultState.Reset;
+			}
+
+			base.Update(gameTime);
+		}
+
+		// Draw the catapult and pumpkin
+		public override void Draw(GameTime gameTime)
+		{
+			if (gameTime == null)
+				throw new ArgumentNullException("gameTime");
+
+			if (currentState == CatapultState.Crash)
+			{
+				curGame.SpriteBatch.Draw(baseTextureBack, basePosition, null, Color.White, MathHelper.ToRadians(-5), Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
+				curGame.SpriteBatch.Draw(baseTexture, basePosition, null, Color.White, MathHelper.ToRadians(5), Vector2.Zero, 1.0f, SpriteEffects.None, 0);
+			}
+			else
+			{
+				curGame.SpriteBatch.Draw(baseTextureBack, basePosition, Color.White);
+				curGame.SpriteBatch.Draw(armTexture, basePosition + armOffset, null, Color.White, armRotation, armCenter, 1.0f, SpriteEffects.None, 0.0f);
+				curGame.SpriteBatch.Draw(baseTexture, basePosition, Color.White);
+			}
+
+			if (currentState != CatapultState.ProjectileHit && currentState != CatapultState.Crash)
+				curGame.SpriteBatch.Draw(pumpkinTexture, new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), null, Color.White, pumpkinRotation, new Vector2(32, 32), 1.0f, SpriteEffects.None, 0.0f);
+			else
+				curGame.SpriteBatch.Draw(pumpkinSmashTexture, new Vector2(pumpkinLaunchPosition, pumpkinPosition.Y), null, Color.White, 0, new Vector2(50, 32), 1.0f, SpriteEffects.None, 0.0f);
+
+			base.Draw(gameTime);
+		}
+
+		// Reset the catapult and pumpkin to default positions
+		private void ResetCatapult()
+		{
+			basePosition.X = -100;
+			basePosition.Y = 430;
+			baseSpeed = 0;
+
+			pumpkinPosition = Vector2.Zero;
+			armRotation = MathHelper.ToRadians(0);
+
+			currentState = CatapultState.Rolling;
+
+			pumpkinPosition = Vector2.Zero;
+			pumpkinVelocity = Vector2.Zero;
+			pumpkinPosition.X = pumpkinLaunchPosition = basePosition.X + 120;
+			pumpkinPosition.Y = basePosition.Y + 80;
+			pumpkinRotation = 0;
+
+			boostPower = 0;
+		}
+
+		// Change state to firing and play fire sound
+		private void Fire()
+		{
+			currentState = CatapultState.Firing;
+			pumpkinRotationPosition = pumpkinPosition;
+		}
+	}
+}
---
+++ TurtleBridge/physics/Catapult/30-Decouple/Catapult.WIN/Game.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CatapultMiniGame
+{
+	public class CatapultGame : Game
+	{
+		const int screenWidth = 1280;
+		const int screenHeight = 720;
+		const int starup = 0;
+
+		GraphicsDeviceManager graphics;
+
+		SpriteBatch spriteBatch;
+		public SpriteBatch SpriteBatch
+		{
+			get { return spriteBatch; }
+		}
+
+		// Input States
+		GamePadState currentGamePadState;
+		public GamePadState CurrentGamePadState
+		{
+			get { return currentGamePadState; }
+		}
+		GamePadState lastGamePadState;
+		public GamePadState LastGamePadState
+		{
+			get { return lastGamePadState; }
+		}
+		KeyboardState currentKeyboardState;
+		public KeyboardState CurrentKeyboardState
+		{
+			get { return currentKeyboardState; }
+		}
+		KeyboardState lastKeyboardState;
+		public KeyboardState LastKeyboardState
+		{
+			get { return lastKeyboardState; }
+		}
+
+		// Font components
+		SpriteFont tahomaFont;
+
+		bool runGame = true;
+
+		// Used to delay the start of the game
+		bool playingGame = false;
+
+		// Current high score
+		// This is loaded at the start from a file.
+		int highScore = 0;
+		public int HighScore
+		{
+			get { return highScore; }
+		}
+
+		// Textures for background
+		Texture2D backgroundTexture;
+		Texture2D skyTexture;
+
+		// Track where the player is viewing
+		Vector2 screenPosition = Vector2.Zero;
+
+		// Texture for object at the end of the rolling area
+		Texture2D endObjectTexture;
+
+		// Position of the object at the end of the rolling area
+		Vector2 endObjectPos = new Vector2(1000, 500);
+
+		// Our Catapult object
+		Catapult playerCatapult;
+
+		// Current distance of the pumpkin
+		float pumpkinDistance;
+		public float PumpkinDistance
+		{
+			get { return pumpkinDistance; }
+		}
+
+		public CatapultGame()
+		{
+			Logger.Initialize();
+			Logger.Info("catapult game");
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = screenWidth;
+			graphics.PreferredBackBufferHeight = screenHeight;
+			Content.RootDirectory = "Content";
+
+			IsFixedTimeStep = true;
+			playerCatapult = new Catapult(this);
+		}
+
+		protected override void Initialize()
+		{
+			IsMouseVisible = true;
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(graphics.GraphicsDevice);
+			backgroundTexture = Content.Load<Texture2D>("Textures/ground");
+			skyTexture = Content.Load<Texture2D>("Textures/sky");
+			endObjectTexture = Content.Load<Texture2D>("Textures/log");
+			tahomaFont = Content.Load<SpriteFont>("Fonts/TahomaFont");
+			playerCatapult.Initialize();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			// Allow the game to exit
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+				Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				runGame = false;
+			}
+
+			// Update input
+			lastGamePadState = currentGamePadState;
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+			lastKeyboardState = currentKeyboardState;
+			currentKeyboardState = Keyboard.GetState();
+
+			// Wait 3 seconds until we start playing
+			if (gameTime.TotalGameTime.Seconds > starup && !playingGame)
+			{
+				playingGame = true;
+			}
+
+			// After 3 seconds play the game
+			if (playingGame)
+			{
+				// Update the players Catapult
+				playerCatapult.Update(gameTime);
+
+				if (playerCatapult.CurrentState == CatapultState.Reset)
+				{
+					// reset background and log
+					screenPosition = Vector2.Zero;
+
+					endObjectPos.X = 1000;
+					endObjectPos.Y = 500;
+				}
+
+				// Move background
+				if (playerCatapult.CurrentState == CatapultState.ProjectileFlying)
+				{
+					screenPosition.X = (playerCatapult.PumpkinPosition.X - playerCatapult.PumpkinLaunchPosition) * -1.0f;
+					endObjectPos.X = (playerCatapult.PumpkinPosition.X - playerCatapult.PumpkinLaunchPosition) * -1.0f + 1000;
+				}
+
+				// Calculate the pumpkin flying distance
+				if (playerCatapult.CurrentState == CatapultState.ProjectileFlying ||
+					playerCatapult.CurrentState == CatapultState.ProjectileHit)
+				{
+					pumpkinDistance = playerCatapult.PumpkinPosition.X - playerCatapult.PumpkinLaunchPosition;
+					pumpkinDistance /= 15.0f;
+				}
+
+				// Is this a high score
+				if (highScore < pumpkinDistance)
+					highScore = (int)pumpkinDistance;
+			}
+
+			// Exit game
+			if (!runGame)
+			{
+				Exit();
+			}
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			// Where to draw the Sky
+			Vector2 skyDrawPos = Vector2.Zero;
+			skyDrawPos.Y -= 50;
+			skyDrawPos.X = (screenPosition.X / 6) % 3840;
+
+			// Where to draw the background hills
+			Vector2 backgroundDrawPos = Vector2.Zero;
+			backgroundDrawPos.Y += 225;
+			backgroundDrawPos.X = screenPosition.X % 1920;
+
+			string printString;
+			Vector2 FontOrigin;
+
+			// Start Drawing
+			// https://nelsonhurst.com/xna-cheatsheet
+			//sprite.Begin(SpriteSortMode.Deferred, BlendState.AlphaBlend);
+			spriteBatch.Begin();
+
+			// Draw the sky
+			spriteBatch.Draw(skyTexture, skyDrawPos, Color.White);
+			spriteBatch.Draw(skyTexture, skyDrawPos + new Vector2(skyTexture.Width, 0), null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.FlipHorizontally, 0);
+
+			// Check to see if we need to draw another sky
+			if (skyDrawPos.X <= -(skyTexture.Width * 2) + screenWidth)
+			{
+				skyDrawPos.X += skyTexture.Width * 2;
+				spriteBatch.Draw(skyTexture, skyDrawPos, Color.White);
+				spriteBatch.Draw(skyTexture, skyDrawPos + new Vector2(skyTexture.Width, 0), null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.FlipHorizontally, 0);
+			}
+
+			// Draw the background once
+			spriteBatch.Draw(backgroundTexture, backgroundDrawPos, Color.White);
+			spriteBatch.Draw(backgroundTexture, backgroundDrawPos + new Vector2(backgroundTexture.Width, 0), null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.FlipHorizontally, 0);
+
+			// Check to see if we need to draw another background
+			if (backgroundDrawPos.X <= -(backgroundTexture.Width * 2) + screenWidth)
+			{
+				backgroundDrawPos.X += backgroundTexture.Width * 2;
+				spriteBatch.Draw(backgroundTexture, backgroundDrawPos, Color.White);
+				spriteBatch.Draw(backgroundTexture, backgroundDrawPos + new Vector2(backgroundTexture.Width, 0), null, Color.White, 0, Vector2.Zero, 1, SpriteEffects.FlipHorizontally, 0);
+			}
+
+			// Draw the log at the end
+			spriteBatch.Draw(endObjectTexture, endObjectPos, Color.White);
+
+			// Draw the Catapult
+			playerCatapult.Draw(gameTime);
+
+			// If we have not started to play
+			if (!playingGame)
+			{
+				// Draw title
+				printString = "Catapult";
+				FontOrigin = tahomaFont.MeasureString(printString) / 2;
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 252), new Color(new Vector4(0, 0, 0, 3 - (float)gameTime.TotalGameTime.TotalSeconds)), 0, FontOrigin, 2, SpriteEffects.None, 0);
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 250), new Color(new Vector4(0.5f, 1.0f, 0.2f, 3 - (float)gameTime.TotalGameTime.TotalSeconds)), 0, FontOrigin, 2, SpriteEffects.None, 0);
+			}
+			else
+			// We have started
+			{
+				if (playerCatapult.CurrentState == CatapultState.Rolling)
+				{
+					float rightTriggerAmt = currentGamePadState.Triggers.Right;
+					if (rightTriggerAmt > 0.5f)
+						rightTriggerAmt = 1.0f - rightTriggerAmt;
+
+					if (currentKeyboardState.IsKeyDown(Keys.B))
+						rightTriggerAmt = 0.5f;
+
+					rightTriggerAmt *= 2;
+
+					printString = "Power Bonus: " + rightTriggerAmt.ToString("p1");
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(52, 62), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(52, 60), Color.Azure, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+				}
+
+				// Draw the distance
+				printString = "Distance: " + ((int)pumpkinDistance).ToString() + " ft.";
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(802, 17), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(800, 15), Color.Azure, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+				if (playerCatapult.PumpkinPosition.Y < -32)
+				{
+					float pumpkinHeight = -playerCatapult.PumpkinPosition.Y / 15.0f;
+
+					printString = ((int)pumpkinHeight).ToString() + " ft.";
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(playerCatapult.PumpkinLaunchPosition, 62), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(playerCatapult.PumpkinLaunchPosition, 60), Color.Azure, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+				}
+
+				// Draw the high score
+				printString = "High Score: " + highScore.ToString() + " ft.";
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(52, 17), Color.Black, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+				spriteBatch.DrawString(tahomaFont, printString, new Vector2(52, 15), Color.Azure, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
+
+				// Print new high score if over 1000
+				if (playerCatapult.CurrentState == CatapultState.ProjectileHit && highScore == (int)pumpkinDistance && highScore > 1000)
+				{
+					printString = "High Score!";
+					FontOrigin = tahomaFont.MeasureString(printString) / 2; spriteBatch.DrawString(tahomaFont, printString,
+								new Vector2(screenWidth / 2, 252), Color.Black, 0, FontOrigin, 2, SpriteEffects.None, 0);
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 250), Color.Gold, 0, FontOrigin, 2, SpriteEffects.None, 0);
+				}
+				// Print that we crashed
+				else if (playerCatapult.CurrentState == CatapultState.Crash)
+				{
+					printString = "Crash!";
+					FontOrigin = tahomaFont.MeasureString(printString) / 2;
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 252), Color.Black, 0, FontOrigin, 2, SpriteEffects.None, 0);
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 250), Color.Red, 0, FontOrigin, 2, SpriteEffects.None, 0);
+				}
+				// Did we get boost power
+				else if (playerCatapult.BoostPower > 0)
+				{
+					string boostPoints = "";
+					for (int i = 0; i < playerCatapult.BoostPower; i++)
+						boostPoints += "!";
+
+					printString = "Boost Power" + boostPoints;
+					FontOrigin = tahomaFont.MeasureString(printString) / 2;
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 252), Color.Black, 0, FontOrigin, 2, SpriteEffects.None, 0);
+					spriteBatch.DrawString(tahomaFont, printString, new Vector2(screenWidth / 2, 250), Color.Red, 0, FontOrigin, 2, SpriteEffects.None, 0);
+				}
+			}
+
+			spriteBatch.End();
+			base.Draw(gameTime);
+		}
+
+	}
+}
