c75f542f309e9fdc285efed7caf48da0b77c7920
StevePro7 <steveprozune@hotmail.com>
2022-10-22 17:06:20 +0100

    no message

--- TurtleBridge/tilesNew/07-wonderboyspritesJump/dev/content/gfx.adb
+++ TurtleBridge/tilesNew/07-wonderboyspritesJump/dev/content/gfx.adb
@@ -3,4 +3,4 @@ S:G$bggame_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
 S:G$bggame_tiles__tilemap__bin$0_0$0({512}DA512d,SC:U),D,0,0
 S:G$bggame_tiles__tiles__psgcompr$0_0$0({4172}DA4172d,SC:U),D,0,0
 S:G$sprite_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$sprite_tiles__tiles__psgcompr$0_0$0({2544}DA2544d,SC:U),D,0,0
+S:G$sprite_tiles__tiles__psgcompr$0_0$0({5086}DA5086d,SC:U),D,0,0
--- TurtleBridge/tilesNew/07-wonderboyspritesJump/dev/content/gfx.h
+++ TurtleBridge/tilesNew/07-wonderboyspritesJump/dev/content/gfx.h
@@ -11,5 +11,5 @@ extern const unsigned char	sprite_tiles__palette__bin[];
 #define				sprite_tiles__palette__bin_size 16
 
 extern const unsigned char	sprite_tiles__tiles__psgcompr[];
-#define				sprite_tiles__tiles__psgcompr_size 2544
+#define				sprite_tiles__tiles__psgcompr_size 5086
 
