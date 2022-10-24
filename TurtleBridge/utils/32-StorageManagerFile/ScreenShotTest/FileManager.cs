b7bef696b6080b433783785237920e884fc220f1
StevePro7 <steveprozune@hotmail.com>
2022-10-24 21:53:04 +0100

    no message

--- TurtleBridge/utils/32-StorageManagerFile/ScreenShotTest/FileManager.cs
+++ TurtleBridge/utils/32-StorageManagerFile/ScreenShotTest/FileManager.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework.Content;
+using System.IO;
 
 namespace ScreenShotTest
 {
@@ -6,6 +7,31 @@ namespace ScreenShotTest
 	{
 		public void Initialize()
 		{
+			var name = "output.ssm";
+			var file = File.Open(name, FileMode.Create);
+
+			var numLines = 400;
+			var msb = numLines / 256;
+			var lsb = numLines % 256; 
+			using (BinaryWriter bw = new BinaryWriter(file))
+			{
+				// MAGIC
+				bw.Write(0x04);
+				bw.Write(0xB0);
+				bw.Write(0xE0);
+				bw.Write(0xAC);
+
+				// Num lines.
+				bw.Write(lsb);
+				bw.Write(msb);
+
+				// Values.
+				bw.Write(0x12);
+				bw.Write(0x24);
+				bw.Write(0x48);
+
+				bw.Write(0xFE);
+			}
 		}
 
 		public void LoadContent(ContentManager content)
