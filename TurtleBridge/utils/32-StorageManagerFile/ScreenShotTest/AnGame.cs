72a809217045a23e45bcf7334154c17a899f939e
StevePro7 <steveprozune@hotmail.com>
2022-10-24 16:13:32 +0100

    no message

---
+++ TurtleBridge/utils/32-StorageManagerFile/ScreenShotTest/AnGame.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.IO;
+
+namespace ScreenShotTest
+{
+	public class AnGame : Microsoft.Xna.Framework.Game
+	{
+		const string file = "skate01";
+
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		//RenderTarget2D renderTarget;
+		FileManager fileManager;
+		private int numBytes = 0;
+
+		//private bool save;
+		private const int wide = 16;
+		private const int high = 16;
+		private const int size = 1;
+		private string[] lines;
+
+		public AnGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = wide * size;
+			graphics.PreferredBackBufferHeight = high * size;
+			Content.RootDirectory = "Content";
+			fileManager = new FileManager();
+		}
+
+		protected override void Initialize()
+		{
+			//save = false;
+			//if (null != ConfigurationManager.AppSettings["save"])
+			//{
+			//	save = Convert.ToBoolean(ConfigurationManager.AppSettings["save"]);
+			//}
+			//save = true;
+			IsMouseVisible = true;
+			fileManager.Initialize();
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			fileManager.LoadContent(Content);
+			lines = File.ReadAllLines(file + ".csv");
+
+			PresentationParameters pp = GraphicsDevice.PresentationParameters;
+			int width = pp.BackBufferWidth;
+			int height = pp.BackBufferHeight;
+			//renderTarget = new RenderTarget2D(GraphicsDevice, width, height, 1, GraphicsDevice.DisplayMode.Format);
+			//renderTarget = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Color, DepthFormat.Depth24);
+		}
+
+		protected override void UnloadContent()
+		{
+			Content.Unload();
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			// Allows the game to exit
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				this.Exit();
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			//Draw();
+			base.Draw(gameTime);
+		}
+
+	}
+
+}
---
+++ TurtleBridge/utils/32-StorageManagerFile/ScreenShotTest/FileManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace ScreenShotTest
+{
+	public class FileManager
+	{
+		public void Initialize()
+		{
+		}
+
+		public void LoadContent(ContentManager content)
+		{
+		}
+	}
+}
