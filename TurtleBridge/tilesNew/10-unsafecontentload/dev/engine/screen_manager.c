31453d12fb849bc60647787b14774c700e65dafd
StevePro7 <steveprozune@hotmail.com>
2022-10-25 21:03:29 +0100

    no message

--- TurtleBridge/tilesNew/10-unsafecontentload/dev/engine/screen_manager.c
+++ TurtleBridge/tilesNew/10-unsafecontentload/dev/engine/screen_manager.c
@@ -8,14 +8,14 @@
 #include "sprite_manager.h"
 #include "tile_manager.h"
 #include "../devkit/_sms_manager.h"
+#include "../banks/bank6.h"
 
-static void draw_sprite( unsigned char idx, unsigned char x, unsigned char y );
-static unsigned char frame;
+static unsigned char stageframe;
 
 void engine_screen_manager_init()
 {
 	engine_font_manager_draw_text( "ADRIANA HOMEWORK", 2, 2 );
-	frame = 0;
+	stageframe = 25;
 }
 
 void engine_screen_manager_update()
@@ -26,32 +26,7 @@ void engine_screen_manager_update()
 	input = engine_input_manager_move_fire1();
 	if( input )
 	{
-		draw_sprite( 2, 96, 96 );
-	}
-	else
-	{
-		input = engine_input_manager_hold_fire2();
-		if( input )
-		{
-			frame = 1 - frame;
-		}
-
-		draw_sprite( frame, 96, 96 );
-	}
-}
-
-static void draw_sprite( unsigned char idx, unsigned char x, unsigned char y )
-{
-	//unsigned char row, col;
-	unsigned char num;
-	unsigned char i, j;
-
-	for( j = 0; j < 4; j++ )
-	{
-		for( i = 0; i < 3; i++ )
-		{
-			num = ( idx * 12 ) + j * 3 + i;
-			devkit_SMS_addSprite( x + i * 8, y + j * 8, SPRITE_TILES + num );
-		}
+	//	engine_font_manager_draw_text( "ADRIANA HOMEWORK", 2, 4 );
+		devkit_UNSAFE_SMS_VRAMmemcpy32( 0, ( unsigned char * ) watertiles_bin + ( ( ( stageframe >> 1 ) % 8 ) << 5 ) );
 	}
 }
