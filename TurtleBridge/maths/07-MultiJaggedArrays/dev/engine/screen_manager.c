d0f7a360f28622a760c2af93c8353936bb47992f
StevePro7 <steveprozune@hotmail.com>
2022-11-06 18:04:47 +0000

    no message

--- TurtleBridge/maths/07-MultiJaggedArrays/dev/engine/screen_manager.c
+++ TurtleBridge/maths/07-MultiJaggedArrays/dev/engine/screen_manager.c
@@ -1,4 +1,5 @@
 #include "screen_manager.h"
+#include "array_manager.h"
 #include "font_manager.h"
 #include "input_manager.h"
 
@@ -13,10 +14,40 @@ void engine_screen_manager_init()
 
 void engine_screen_manager_update()
 {
+	const signed int *ptr;
 	unsigned char input;
+	input = engine_input_manager_hold_up();
+	if( input )
+	{
+		if ( 0 ==index )
+		{
+			index = 2;
+		}
+		else
+		{
+			index--;
+		}
+		engine_font_manager_draw_data( index, 10, 2 );
+	}
+
+	input = engine_input_manager_hold_down();
+	if( input )
+	{
+		if( 2 == index )
+		{
+			index = 0;
+		}
+		else
+		{
+			index++;
+		}
+		engine_font_manager_draw_data( index, 10, 2 );
+	}
+
 	input = engine_input_manager_hold_fire1();
 	if( input )
 	{
-		engine_font_manager_draw_text( "STEVEPRO STUDIOS", 4, 4 );
+		ptr = jump_array_ptr[ index ];
+		engine_font_manager_draw_text( "STEVEPRO STUDIOS", 4, 5 );
 	}
 }
