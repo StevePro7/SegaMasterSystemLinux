ef0ea903cd962e41a7a775bc23fd94a6772d6f0e
StevePro7 <steveprozune@hotmail.com>
2022-11-09 19:48:51 +0000

    no message

--- TurtleBridge/sound/SampleTest02_Jung/dev/screen/next_screen.c
+++ TurtleBridge/sound/SampleTest02_Jung/dev/screen/next_screen.c
@@ -1,6 +1,7 @@
 #include "next_screen.h"
 #include "../engine/enum_manager.h"
 #include "../engine/font_manager.h"
+#include "../engine/input_manager.h"
 //#include "../devkit/_sms_manager.h"
 
 void screen_next_screen_load()
@@ -13,5 +14,12 @@ void screen_next_screen_load()
 
 void screen_next_screen_update( unsigned char *screen_type )
 {
+	unsigned char input = engine_input_manager_hold( input_type_fire1 );
+	if( input )
+	{
+		*screen_type = screen_type_test;
+		return;
+	}
+
 	*screen_type = screen_type_next;
 }
