457e5ec6457ea837790c18ac5a485f349abda43a
StevePro7 <steveprozune@hotmail.com>
2022-11-11 21:14:25 +0000

    no message

--- TurtleBridge/physics/images/rotateWB/title/01-titleScreen/dev/content/gfx.adb
+++ TurtleBridge/physics/images/rotateWB/title/01-titleScreen/dev/content/gfx.adb
@@ -1,6 +1,4 @@
 M:gfx
-S:G$bggame_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$bggame_tiles__tilemap__bin$0_0$0({512}DA512d,SC:U),D,0,0
-S:G$bggame_tiles__tiles__psgcompr$0_0$0({4172}DA4172d,SC:U),D,0,0
-S:G$sprite_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$sprite_tiles__tiles__psgcompr$0_0$0({2847}DA2847d,SC:U),D,0,0
+S:G$titlescreen__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
+S:G$titlescreen__tilemap__bin$0_0$0({384}DA384d,SC:U),D,0,0
+S:G$titlescreen__tiles__psgcompr$0_0$0({4869}DA4869d,SC:U),D,0,0
--- TurtleBridge/physics/images/rotateWB/title/01-titleScreen/dev/content/gfx.h
+++ TurtleBridge/physics/images/rotateWB/title/01-titleScreen/dev/content/gfx.h
@@ -1,15 +1,9 @@
-extern const unsigned char	bggame_tiles__palette__bin[];
-#define				bggame_tiles__palette__bin_size 16
+extern const unsigned char	titlescreen__palette__bin[];
+#define				titlescreen__palette__bin_size 16
 
-extern const unsigned char	bggame_tiles__tilemap__bin[];
-#define				bggame_tiles__tilemap__bin_size 512
+extern const unsigned char	titlescreen__tilemap__bin[];
+#define				titlescreen__tilemap__bin_size 384
 
-extern const unsigned char	bggame_tiles__tiles__psgcompr[];
-#define				bggame_tiles__tiles__psgcompr_size 4172
-
-extern const unsigned char	sprite_tiles__palette__bin[];
-#define				sprite_tiles__palette__bin_size 16
-
-extern const unsigned char	sprite_tiles__tiles__psgcompr[];
-#define				sprite_tiles__tiles__psgcompr_size 2847
+extern const unsigned char	titlescreen__tiles__psgcompr[];
+#define				titlescreen__tiles__psgcompr_size 4869
 
