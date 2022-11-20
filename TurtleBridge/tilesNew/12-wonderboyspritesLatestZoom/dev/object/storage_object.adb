575fd6caa621c645b536347c9f75909215860b14
StevePro7 <steveprozune@hotmail.com>
2022-11-20 17:49:44 +0000

    no message

---
+++ TurtleBridge/tilesNew/12-wonderboyspritesLatestZoom/dev/object/storage_object.adb
@@ -0,0 +1,2 @@
+M:storage_object
+T:Fstorage_object$tag_struct_storage_object[({0}S:S$Magic$0_0$0({4}SL:U),Z,0,0)({4}S:S$num_jumps$0_0$0({2}SI:U),Z,0,0)({6}S:S$jump_high$0_0$0({1024}DA1024d,SC:U),Z,0,0)({1030}S:S$terminal$0_0$0({1}SC:U),Z,0,0)]
