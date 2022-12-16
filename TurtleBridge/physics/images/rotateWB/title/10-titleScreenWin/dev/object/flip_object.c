6b0a4c203d0e00c2a613e6b430fe881093fc4d62
StevePro7 <steveprozune@hotmail.com>
2022-12-16 16:33:15 +0000

    no message

--- TurtleBridge/physics/images/rotateWB/title/10-titleScreenWin/dev/object/flip_object.c
+++ TurtleBridge/physics/images/rotateWB/title/10-titleScreenWin/dev/object/flip_object.c
@@ -1,16 +1,5 @@
 #include "flip_object.h"
 
-const unsigned char *flip_array_ptr[]=
-{
-    flip_array_00,
-	flip_array_01,
-	flip_array_02,
-	flip_array_03,
-    flip_array_04,
-    flip_array_05,
-    flip_array_06,
-};
-
 const unsigned char flip_array_00[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
 const unsigned char flip_array_01[] = { 2, 2, 6, 6, 2, 6, 6, 2, 2 };
 
@@ -25,3 +14,15 @@ const unsigned char flip_array_05[] = { 2, 5, 4, 8, 4, 8, 4, 3, 2 };
 const unsigned char flip_array_06[] = { 2, 2, 2, 3, 3, 3, 4, 4, 5 };
 
 //const unsigned char flip_array_02[] = { 2, 2, 6, 6, 2, 2, 6, 6, 2 };
+
+
+const unsigned char *flip_array_ptr[] =
+{
+    flip_array_00,
+	flip_array_01,
+	flip_array_02,
+	flip_array_03,
+    flip_array_04,
+    flip_array_05,
+    flip_array_06,
+};
