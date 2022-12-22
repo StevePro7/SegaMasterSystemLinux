637ce0804cba89a41c2e3c7707de52c6efe308a2
StevePro7 <steveprozune@hotmail.com>
2022-12-22 20:43:46 +0000

    no message

---
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/dev/engine/jump_manager.adb
@@ -0,0 +1,15 @@
+M:jump_manager
+F:G$engine_jump_manager_init$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
+S:Ljump_manager.engine_jump_manager_init$num_jumps$1_0$3({2}SI:U),B,1,4
+S:Ljump_manager.engine_jump_manager_init$jo$1_0$4({2}DG,STtag_struct_jump_object:S),R,0,0,[]
+S:Ljump_manager.engine_jump_manager_init$index$1_0$4({2}SI:U),R,0,0,[e,d]
+F:G$engine_jump_manager_load$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
+S:Ljump_manager.engine_jump_manager_load$index$1_0$7({2}SI:U),B,1,4
+S:Ljump_manager.engine_jump_manager_load$value$1_0$7({1}SC:U),B,1,6
+S:Ljump_manager.engine_jump_manager_load$jo$1_0$8({2}DG,STtag_struct_jump_object:S),R,0,0,[]
+F:G$engine_jump_manager_data$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
+S:Ljump_manager.engine_jump_manager_data$jo$1_0$9({2}DG,STtag_struct_jump_object:S),R,0,0,[]
+S:Ljump_manager.engine_jump_manager_data$index$1_0$9({2}SI:U),R,0,0,[c,b]
+S:Ljump_manager.engine_jump_manager_data$sloc0$0_1$0({2}DG,SI:U),B,1,-2
+T:Fjump_manager$tag_struct_jump_object[({0}S:S$num_jumps$0_0$0({2}SI:U),Z,0,0)({2}S:S$jump_high$0_0$0({2048}DA1024d,SI:U),Z,0,0)]
+S:G$global_jump_object$0_0$0({2050}STtag_struct_jump_object:S),E,0,0
