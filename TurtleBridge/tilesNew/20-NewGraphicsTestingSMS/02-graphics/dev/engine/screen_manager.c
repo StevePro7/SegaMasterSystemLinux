96c91dd23a9396904694211480b9ec189aa1629e
StevePro7 <steveprozune@hotmail.com>
2022-12-22 21:14:58 +0000

    no message

---
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/02-graphics/dev/engine/screen_manager.c
@@ -0,0 +1,188 @@
+#include "screen_manager.h"
+#include "audio_manager.h"
+#include "content_manager.h"
+#include "enum_manager.h"
+#include "font_manager.h"
+#include "global_manager.h"
+#include "input_manager.h"
+#include "jump_manager.h"
+#include "sprite_manager.h"
+#include "storage_manager.h"
+#include "tile_manager.h"
+#include "../devkit/_sms_manager.h"
+
+#define UFIX(x)                ((unsigned char)((x)>>8))
+
+//#define MAX_JUMPS	125
+//#define MAX_JUMPS	516
+#define MAX_FLOOR	128
+
+static void draw_sprite( unsigned char idx, unsigned char x, unsigned char y );
+static unsigned char frame;
+static unsigned int index;
+static unsigned char jumps;
+static unsigned int delta;
+static unsigned char posY;
+static unsigned int posY2;
+static unsigned char scrollX, scrollY;
+static unsigned char storage_available;
+
+void engine_screen_manager_init()
+{
+	struct_jump_object *jo = &global_jump_object;
+	unsigned char index;
+
+	scrollX = 1;
+	scrollY = 0;
+	//engine_tile_manager_sky();
+	//engine_tile_manager_draw_groundX( 4, 8, 20, 0, 8 );
+	if( !scrollY )
+	{
+		//engine_tile_manager_draw_groundX( 4, 0, 20, 0, 8 );
+		//engine_tile_manager_draw_groundX( 4, 16, 20, 0, 8 );
+		//engine_tile_manager_draw_groundX( 4, 24, 20, 0, 8 );
+	}
+
+	//engine_jump_manager_data();
+	if( scrollY )
+	{
+		//engine_font_manager_draw_data( storage_available, 12, 4 );
+		//engine_font_manager_draw_data( jo->num_jumps, 22, 5 );
+	}
+
+	frame = 0;
+	index = 0;
+	delta = 0;
+	jumps = 0;
+	posY = MAX_FLOOR;
+	posY2 = posY << 8;
+
+	engine_tile_manager_gfx();
+	//devkit_SMS_setBGScrollX( 0 );
+//	engine_music_manager_play();
+}
+
+void engine_screen_manager_update()
+{
+	//struct_jump_object *jo = &global_jump_object;
+	//unsigned char input;
+
+	//input = engine_input_manager_move_left();
+	//if( input )
+	//{
+	//	if( scrollY )
+	//	{
+	//		devkit_SMS_setBGScrollX( scrollX-- );
+	//	}
+	//}
+	//else
+	//{
+	//	input = engine_input_manager_move_right();
+	//	if( input )
+	//	{
+	//		if( scrollY )
+	//		{
+	//			devkit_SMS_setBGScrollX( scrollX-- );
+	//			devkit_SMS_setBGScrollX( scrollX-- );
+	//			devkit_SMS_setBGScrollX( scrollX-- );
+	//			devkit_SMS_setBGScrollX( scrollX-- );
+	//		}
+	//	}
+	//	if( scrollY )
+	//	{
+	//		devkit_SMS_setBGScrollX( scrollX-- );
+	//		devkit_SMS_setBGScrollX( scrollX-- );
+	//	}
+	//}
+
+	//input = engine_input_manager_hold_fire1();
+	//if( input )
+	//{
+	//	frame++;
+	//	if( frame >= 10 )
+	//	{
+	//		frame = 0;
+	//	}
+	//}
+	//input = engine_input_manager_hold_down();
+	//if( input )
+	//{
+	//	if( frame > 1 )
+	//	{
+	//		if( frame >= 6 )
+	//		{
+	//			frame -= 4;
+	//		}
+	//		else
+	//		{
+	//			frame += 4;
+	//		}
+	//	}
+	//}
+	//if( jumps )
+	//{
+	//	input = 1;// engine_input_manager_move_fire1();
+	//	if( input )
+	//	{
+	//		delta = jo->jump_high[ index ];// deltaY[ index ];
+	//		//engine_font_manager_draw_data( delta, 22, 4 + index );
+	//		index++;
+	//		if( index >= jo->num_jumps )
+	//		{
+	//			delta = 0;
+	//			index = 0;
+	//			jumps = 0;
+	//			posY = MAX_FLOOR;
+	//		}
+	//	}
+	//}
+	//else
+	//{
+	//	input = engine_input_manager_move_fire2();
+	//	if( input )
+	//	{
+	//		delta = 0;
+	//		index = 0;
+	//		jumps = 1;
+	//		//engine_font_manager_draw_text( "STEVEPRO JUMPING!!", 2, 2 );
+	//		//engine_storage_manager_write();
+	//	}
+	//}
+
+	//
+	//if( 0 == delta )
+	//{
+	//	draw_sprite( frame, 72, posY - delta );
+	//}
+	//else
+	//{
+	//	unsigned int tmpY2 = posY2 - delta;
+	//	unsigned char tmpY = UFIX( tmpY2 );
+	//	draw_sprite( frame, 72, tmpY );
+	//}
+}
+
+static void draw_sprite( unsigned char idx, unsigned char x, unsigned char y )
+{
+	const unsigned char wide = 4;
+	const unsigned char high = 4;
+	unsigned char num;
+	
+	unsigned char i, j;
+	if( 0 == idx || 1 == idx || 2 == idx || 4 == idx || 6 == idx || 8 == idx )
+	{
+		x += 4;
+	}
+	else //if( 3 == idx || 1 == idx || 2 == idx )
+	{
+		y += 4;
+	}
+	for( j = 0; j < high; j++ )
+	{
+		for( i = 0; i < wide; i++ )
+		{
+			num = ( idx * wide * high ) + j * 4 + i;
+			devkit_SMS_addSprite( x + i * 8, y + j * 8, SPRITE_TILES + num );
+		}
+	}
+}
---
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/02-graphics/dev/engine/tile_manager.c
@@ -0,0 +1,245 @@
+#include "tile_manager.h"
+#include "enum_manager.h"
+#include "global_manager.h"
+#include "../devkit/_sms_manager.h"
+#include "../content/gfx.h"
+
+void engine_tile_manager_gfx()
+{
+	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+	unsigned int idx;
+	unsigned int val;
+	unsigned char row, col;
+
+	unsigned x, y;
+	unsigned w, h;
+	w = 9;
+	h = 10;
+	x = 7;
+	y = 14;
+
+	for( row = 0; row < h; row++ )
+	{
+		for( col = 0; col < w; col++ )
+		{
+			idx = row * 2 * w + col * 2;
+			val = tiles[ idx ];
+			devkit_SMS_setNextTileatXY( x + col, y + row );
+			devkit_SMS_setTile( ( val ) );
+		}
+	}
+}
+
+//static void draw_tile_fl02( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h, unsigned char s, unsigned char f );
+////static void draw_tile_full( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h );
+//static void draw_tile_flip( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h );
+//static void draw_tile_next( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h );
+//static void draw_tile_scroll( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h, unsigned char col );
+//
+//
+//void engine_tile_manager_draw_pipe( unsigned char type, unsigned int x, unsigned char y, unsigned char wide, unsigned char high, unsigned char col )
+//{
+//	//const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//}
+//
+//void engine_tile_manager_draw_norm( unsigned char type, unsigned char x, unsigned char y, unsigned char wide, unsigned char high, unsigned char beg, unsigned char end )
+//{
+//	
+//}
+//
+//void engine_tile_manager_draw_flip( unsigned char type, unsigned char x, unsigned char y, unsigned char wide, unsigned char high, unsigned char beg, unsigned char end )
+//{
+//	
+//}
+
+
+
+//void engine_tile_manager_draw_tile( unsigned char type, unsigned char x, unsigned char y )
+//{
+//	
+//}
+//
+//void engine_tile_manager_turtle( unsigned char type, unsigned char x, unsigned char y )
+//{
+//	
+//}
+//
+//void engine_tile_manager_sign( unsigned char type, unsigned char x, unsigned char y )
+//{
+//	
+//}
+
+
+//void engine_tile_manager_cloud( unsigned char type, unsigned char x, unsigned char y )
+//{
+//	const unsigned char *array = tile_object_data[ type ];
+//	const unsigned char w = tile_object_wide[ type ];
+//	const unsigned char h = tile_object_high[ type ];
+//	draw_tile_next( array, x, y, w, h );
+//}
+
+
+
+//void engine_tile_manager_section01( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *array = tile_object_data[ tile_type_section01 ];
+//	const unsigned char w = tile_object_wide[ tile_type_section01 ];
+//	const unsigned char h = tile_object_high[ tile_type_section01 ];
+//	//draw_tile_full( array, x, y, w, h );
+//	draw_tile_next( array, x, y, w, h );
+//	//draw_tile_side( array, x, y, w, h, 0, 16 );
+//}
+//
+//void engine_tile_manager_section01_left( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *array = tile_object_data[ tile_type_section01 ];
+//	const unsigned char w = tile_object_wide[ tile_type_section01 ];
+//	const unsigned char h = tile_object_high[ tile_type_section01 ];
+//	//	draw_tile_full( array, x, y, w, h );
+//	//	draw_tile_next( array, x, y, w, h );
+//	draw_tile_fl02( array, x, y, w, h, 0, 16 );
+//}
+//
+//void engine_tile_manager_section02( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *array = tile_object_data[ tile_type_section02 ];
+//	const unsigned char w = tile_object_wide[ tile_type_section02 ];
+//	const unsigned char h = tile_object_high[ tile_type_section02 ];
+//	//draw_tile_full( array, x, y, w, h );
+//	draw_tile_next( array, x, y, w, h );
+//}
+//
+//void engine_tile_manager_section03( unsigned char x, unsigned char y )
+//{
+//	const unsigned char *array = tile_object_data[ tile_type_section03 ];
+//	const unsigned char w = tile_object_wide[ tile_type_section03 ];
+//	const unsigned char h = tile_object_high[ tile_type_section03 ];
+//	//draw_tile_full( array, x, y, w, h );
+//	draw_tile_next( array, x, y, w, h );
+//	//draw_tile_flip( array, x, y, w, h );
+//}
+
+
+//void engine_tile_manager_scroll_test( unsigned char x, unsigned char y, unsigned char col )
+//{
+//	
+//}
+//
+//void engine_tile_manager_sky()
+//{
+//	
+//}
+//
+//void engine_tile_manager_sea()
+//{
+//	
+//}
+//
+//
+//static void draw_tile_scroll( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h, unsigned char col )
+//{
+//	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//	unsigned char idx;
+//	unsigned char val;
+//	unsigned int off;
+//	unsigned char row;
+//
+//	for( row = 0; row < h; row++ )
+//	{
+//		idx = row * w + col;
+//		val = array[ idx ];
+//		off = val * 2;
+//		devkit_SMS_loadTileMap( x + col, y + row, ( void * ) &tiles[ off ], 2 );
+//	}
+//}
+//
+//static void draw_tile_fl02( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h, unsigned char s, unsigned char f )
+//{
+//	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//	unsigned char idx;
+//	unsigned int val;
+//	unsigned char row, col, tmp;
+//
+//	unsigned int flip = devkit_TILE_FLIPPED_X();
+//	for( row = 0; row < h; row++ )
+//	{
+//		//for( tmp = 0; tmp < w; tmp++ )
+//		for( tmp = s; tmp < f; tmp++ )
+//		{
+//			col = w - tmp - 1;
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			devkit_SMS_setNextTileatXY( x + tmp, y + row );
+//			devkit_SMS_setTile( ( *tiles + val ) | flip );
+//		}
+//	}
+//}
+
+//static void draw_tile_full( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h )
+//{
+//	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//	unsigned char idx;
+//	unsigned char val;
+//	unsigned int off;
+//	unsigned char row, col;
+//
+//	for( row = 0; row < h; row++ )
+//	{
+//		for( col = 0; col < w; col++ )
+//		{
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			off = val * 2;
+//			devkit_SMS_loadTileMap( x + col, y + row, ( void * ) &tiles[ off ], 2 );
+//		}
+//	}
+//}
+
+//static void draw_tile_flip( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h )
+//{
+//	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//	unsigned char idx;
+//	unsigned int val;
+//	unsigned char row, col, tmp;
+//
+//	unsigned int flip = devkit_TILE_FLIPPED_X();
+//	for( row = 0; row < h; row++ )
+//	{
+//		for( tmp = 0; tmp < w; tmp++ )
+//		{
+//			col = w - tmp - 1;
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			devkit_SMS_setNextTileatXY( x + tmp, y + row );
+//			devkit_SMS_setTile( ( *tiles + val ) | flip );
+//		}
+//	}
+//}
+//
+//static void draw_tile_next( const unsigned char *array, unsigned char x, unsigned char y, unsigned char w, unsigned char h )
+//{
+//	const unsigned char *tiles = bggame_tiles__tilemap__bin;
+//	unsigned char idx;
+//	unsigned char val;
+//	unsigned char row, col;
+//
+//	for( row = 0; row < h; row++ )
+//	{
+//		for( col = 0; col < w; col++ )
+//		{
+//			idx = row * w + col;
+//			val = array[ idx ];
+//			devkit_SMS_setNextTileatXY( x + col, y + row );
+//			devkit_SMS_setTile( ( *tiles + val ) );
+//		}
+//	}
+//}
+
+
+// Flip
+//void engine_tile_manager_turtle_flip( unsigned char type, unsigned char x, unsigned char y )
+//{
+//	const unsigned char wide = 4;
+//	const unsigned char high = 3;
+//	engine_tile_manager_draw_flip( type, x, y, wide, high, 0, wide );
+//}
