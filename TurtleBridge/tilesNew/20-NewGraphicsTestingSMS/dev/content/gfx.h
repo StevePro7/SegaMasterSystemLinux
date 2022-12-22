4eb7e292361514017068d13ad89260bb9787c23a
StevePro7 <steveprozune@hotmail.com>
2022-12-22 20:47:38 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/dev/content/gfx.h
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/dev/content/gfx.h
@@ -2,14 +2,8 @@ extern const unsigned char	bggame_tiles__palette__bin[];
 #define				bggame_tiles__palette__bin_size 16
 
 extern const unsigned char	bggame_tiles__tilemap__bin[];
-#define				bggame_tiles__tilemap__bin_size 512
+#define				bggame_tiles__tilemap__bin_size 180
 
 extern const unsigned char	bggame_tiles__tiles__psgcompr[];
-#define				bggame_tiles__tiles__psgcompr_size 4172
-
-extern const unsigned char	sprite_tiles__palette__bin[];
-#define				sprite_tiles__palette__bin_size 16
-
-extern const unsigned char	sprite_tiles__tiles__psgcompr[];
-#define				sprite_tiles__tiles__psgcompr_size 2847
+#define				bggame_tiles__tiles__psgcompr_size 771
 
