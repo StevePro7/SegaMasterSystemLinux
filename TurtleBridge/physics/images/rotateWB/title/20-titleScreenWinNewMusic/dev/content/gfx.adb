718579d849a8ee45be0cd006eed95e83381f0571
StevePro7 <steveprozune@hotmail.com>
2022-12-23 19:49:31 +0000

    no message

--- TurtleBridge/physics/images/rotateWB/title/20-titleScreenWinNewMusic/dev/content/gfx.adb
+++ TurtleBridge/physics/images/rotateWB/title/20-titleScreenWinNewMusic/dev/content/gfx.adb
@@ -1,9 +1,6 @@
 M:gfx
-S:G$sprite_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
 S:G$bggame_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$bggame_tiles__tiles__psgcompr$0_0$0({5255}DA5255d,SC:U),D,0,0
-S:G$titlescreen__tilemap__bin$0_0$0({432}DA432d,SC:U),D,0,0
-S:G$titlescreen__tiles__psgcompr$0_0$0({4592}DA4592d,SC:U),D,0,0
-S:G$bggame_tiles__tilemap__bin$0_0$0({512}DA512d,SC:U),D,0,0
+S:G$bggame_tiles__tilemap__bin$0_0$0({544}DA544d,SC:U),D,0,0
+S:G$bggame_tiles__tiles__psgcompr$0_0$0({5424}DA5424d,SC:U),D,0,0
+S:G$sprite_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
 S:G$sprite_tiles__tiles__psgcompr$0_0$0({2847}DA2847d,SC:U),D,0,0
-S:G$titlescreen__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
