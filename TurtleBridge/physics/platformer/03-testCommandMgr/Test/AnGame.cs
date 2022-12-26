ab9a75356c210d94faf4c0bd6476a357fb9a1462
StevePro7 <steveprozune@hotmail.com>
2022-12-26 19:02:44 +0000

    no message

--- TurtleBridge/physics/platformer/03-testCommandMgr/Test/AnGame.cs
+++ TurtleBridge/physics/platformer/03-testCommandMgr/Test/AnGame.cs
@@ -2,17 +2,17 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Test
+namespace WindowsGame
 {
 	/// <summary>
 	/// This is the main type for your game.
 	/// </summary>
-	public class Game1 : Game
+	public class AnGame : Game
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
-		public Game1()
+		public AnGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
