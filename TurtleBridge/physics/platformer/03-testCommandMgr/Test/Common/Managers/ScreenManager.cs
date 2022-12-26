46294f34806e209c3268773d7eaf1414b8672914
StevePro7 <steveprozune@hotmail.com>
2022-12-26 20:49:54 +0000

    no message

--- TurtleBridge/physics/platformer/03-testCommandMgr/Test/Common/Managers/ScreenManager.cs
+++ TurtleBridge/physics/platformer/03-testCommandMgr/Test/Common/Managers/ScreenManager.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
 using WindowsGame.Master;
 
 namespace WindowsGame.Common
@@ -6,27 +8,59 @@ namespace WindowsGame.Common
 	public interface IScreenManager
 	{
 		void Initialize();
+		void LoadContent();
 		void Update(GameTime gameTime);
 		void Draw();
 	}
 
 	public class ScreenManager : IScreenManager
 	{
+		private IDictionary<Int32, IScreen> screens;
+		private Int32 currScreen = (Int32)ScreenType.Start;
+		private Int32 nextScreen = (Int32)ScreenType.Start;
+
 		public void Initialize()
 		{
+			screens = GetScreens();
+		}
+
+		public void LoadContent()
+		{
+			foreach (var key in screens.Keys)
+			{
+				screens[key].Initialize();
+			}
 		}
 
 		public void Update(GameTime gameTime)
 		{
+			if (currScreen != nextScreen)
+			{
+				currScreen = nextScreen;
+				screens[currScreen].LoadContent();
+			}
+
+			nextScreen = screens[currScreen].Update(gameTime);
 			//myPlayerManager.Update(gameTime);
 		}
 
 		public void Draw()
 		{
 			Engine.SpriteBatch.Begin();
+			screens[currScreen].Draw();
 			//myLevelManager.Draw(spriteBatch);
 			//myPlayerManager.Draw(spriteBatch);
 			Engine.SpriteBatch.End();
+		}
+
+		private static Dictionary<Int32, IScreen> GetScreens()
+		{
+			return new Dictionary<Int32, IScreen>
+			{
+				{(Int32)ScreenType.Start, new StartScreen()},
+				{(Int32)ScreenType.Record, new RecordScreen()},
+				{(Int32)ScreenType.Playback, new PlaybackScreen()},
+			};
 		}
 	}
 }
---
+++ TurtleBridge/physics/platformer/03-testCommandMgr/Test/Common/Screens/StartScreen.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using WindowsGame.Master;
+
+namespace WindowsGame.Common
+{
+	public class StartScreen : IScreen
+	{
+		public void Initialize()
+		{
+		}
+
+		public void LoadContent()
+		{
+		}
+
+		public int Update(GameTime gameTime)
+		{
+			return (int)ScreenType.Start;
+		}
+
+		public void Draw()
+		{
+		}
+	}
+}
