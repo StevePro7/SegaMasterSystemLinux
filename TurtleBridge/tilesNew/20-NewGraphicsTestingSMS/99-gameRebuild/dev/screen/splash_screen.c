18a0560698ad8ef9badda371cf2fb3cae73b3933
StevePro7 <steveprozune@hotmail.com>
2023-01-02 17:48:54 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/99-gameRebuild/dev/screen/splash_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/99-gameRebuild/dev/screen/splash_screen.c
@@ -6,8 +6,6 @@
 void screen_splash_screen_load()
 {
 	engine_content_manager_load_tiles_splash();
-	engine_font_manager_data( 1234, 20, 0 );
-	engine_font_manager_text( "TEST", 10, 0 );
 }
 
 void screen_splash_screen_update( unsigned char *screen_type )
