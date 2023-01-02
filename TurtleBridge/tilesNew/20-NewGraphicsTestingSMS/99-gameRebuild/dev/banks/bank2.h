9ae58e533aa489290ca2a47da96ac5cd7938fabe
StevePro7 <steveprozune@hotmail.com>
2023-01-02 17:26:43 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/99-gameRebuild/dev/banks/bank2.h
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/99-gameRebuild/dev/banks/bank2.h
@@ -3,7 +3,7 @@ extern const unsigned char	splash__palette__bin[];
 #define				splash__palette__bin_bank 2
 
 extern const unsigned char	splash__tilemap__stmcompr[];
-#define				splash__tilemap__stmcompr_size 71
+#define				splash__tilemap__stmcompr_size 72
 #define				splash__tilemap__stmcompr_bank 2
 
 extern const unsigned char	splash__tiles__psgcompr[];
