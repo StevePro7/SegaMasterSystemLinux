734435622cc10dba3ed67b6b1d4165a228bf7ed5
StevePro7 <steveprozune@hotmail.com>
2023-01-04 20:59:06 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/05-graphics-tileMap/dev/content/gfx.adb
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/05-graphics-tileMap/dev/content/gfx.adb
@@ -1,4 +1,4 @@
 M:gfx
 S:G$bggame_tiles__palette__bin$0_0$0({16}DA16d,SC:U),D,0,0
-S:G$bggame_tiles__tilemap__bin$0_0$0({2208}DA2208d,SC:U),D,0,0
-S:G$bggame_tiles__tiles__psgcompr$0_0$0({4143}DA4143d,SC:U),D,0,0
+S:G$bggame_tiles__tilemap__bin$0_0$0({128}DA128d,SC:U),D,0,0
+S:G$bggame_tiles__tiles__psgcompr$0_0$0({537}DA537d,SC:U),D,0,0
