2691c38a256402cdf24a1d331c33dd80f60ed9de
StevePro7 <steveprozune@hotmail.com>
2023-01-10 21:44:37 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/splash_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/splash_screen.c
@@ -9,10 +9,10 @@
 void screen_splash_screen_load()
 {
 	devkit_SMS_displayOff();
-	engine_asm_manager_clear_VRAM();
+	//engine_asm_manager_clear_VRAM();
 	engine_content_manager_splash();
-	engine_font_manager_splash_data( 8, 14, 12 );
-	engine_font_manager_titles_text( "SPLASH SCREEN	", 10, 2 );
+	engine_font_manager_data( 28, 14, 12 );
+	engine_font_manager_text( "SPLASH SCREEN!!", 10, 2 );
 	devkit_SMS_displayOn();
 }
 
@@ -21,7 +21,7 @@ void screen_splash_screen_update( unsigned char *screen_type )
 	unsigned char input = engine_input_manager_hold( input_type_down );
 	if( input )
 	{
-		engine_font_manager_splash_data( 9, 14, 14 );
+		engine_font_manager_data( 29, 14, 14 );
 		*screen_type = screen_type_title;
 		return;
 	}
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/title_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/title_screen.c
@@ -9,10 +9,10 @@
 void screen_title_screen_load()
 {
 	devkit_SMS_displayOff();
-	engine_asm_manager_clear_VRAM();
+	//engine_asm_manager_clear_VRAM();
 	engine_content_manager_titles();
-	engine_font_manager_titles_data( 1, 14, 12 );
-	engine_font_manager_titles_text( "TITLES SCREEN	", 10, 2 );
+	engine_font_manager_data( 51, 14, 12 );
+	engine_font_manager_text( "TITLES SCREEN??", 10, 2 );
 	devkit_SMS_displayOn();
 }
 
@@ -21,7 +21,7 @@ void screen_title_screen_update( unsigned char *screen_type )
 	unsigned char input = engine_input_manager_hold( input_type_down );
 	if( input )
 	{
-		engine_font_manager_titles_data( 2, 14, 12 );
+		engine_font_manager_data( 52, 14, 12 );
 	}
 
 	*screen_type = screen_type_title;
