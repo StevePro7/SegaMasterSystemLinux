13185fda70fc85a3a7b29cb4f41eb81f528d3943
StevePro7 <steveprozune@hotmail.com>
2023-01-10 21:45:52 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/content_manager.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/content_manager.c
@@ -23,6 +23,15 @@ static void load_tile( unsigned char bank, const unsigned char *tiles, unsigned 
 //	//devkit_SMS_loadSpritePalette( ( void * ) sprite_tiles__palette__bin );
 //}
 
+void engine_content_manager_bggame()
+{
+	load_tile( bggame_tiles__tiles__psgcompr_bank, bggame_tiles__tiles__psgcompr, BGGAME_TILES, bggame_tiles__palette__bin );
+	//todo
+	//devkit_SMS_mapROMBank( titles_tiles__tiles__psgcompr_bank );
+	//devkit_SMS_loadPSGaidencompressedTiles( titles_tiles__tiles__psgcompr, BGGAME_TILES );
+	//devkit_SMS_loadBGPalette( ( void * ) titles_tiles__palette__bin );
+}
+
 void engine_content_manager_splash()
 {
 	load_tile( splash_tiles__tiles__psgcompr_bank, splash_tiles__tiles__psgcompr, BGGAME_TILES, splash_tiles__palette__bin );
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/content_manager.h
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/content_manager.h
@@ -1,6 +1,7 @@
 #ifndef _CONTENT_MANAGER_H_
 #define _CONTENT_MANAGER_H_
 
+void engine_content_manager_bggame();
 void engine_content_manager_splash();
 void engine_content_manager_titles();
 
