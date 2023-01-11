7313286cfac9ba7d119be10fac1d2808d4fcbffb
StevePro7 <steveprozune@hotmail.com>
2023-01-11 20:26:45 +0000

    no message

---
+++ TurtleBridge/level/15-LevelManual/ScreenShotTest/AssetManager.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace ScreenShotTest
+{
+	public class AssetManager
+	{
+		public void LoadContent(ContentManager myContentManager)
+		{
+			Images = new Dictionary<string, Texture2D>();
+			var files = GetFiles();
+			foreach (var file in files)
+			{
+				var name = file.Substring(0, 1);
+				Images[name] = myContentManager.Load<Texture2D>(file);
+			}
+		}
+
+		private string[] GetFiles()
+		{
+			return new string[]
+			{
+				"AwavesBlock",
+				"BleftBridge",
+				"CmiddBridge",
+				"DrghtBridge",
+				"EsignBridge",
+				"FgoalBridge",
+				"GleftIsland",
+				"HmiddIsland",
+				"IrghtIsland",
+				"JtreeIslLft",
+				"KtreeIslRgt",
+				"LturtleSea1",
+				"MturtleSea2",
+				"NturtleFly1",
+				"OturtleFly2",
+				"PturtleFly3",
+				"QturtleFly4",
+			};
+		}
+
+		public IDictionary<string, Texture2D> Images { get; private set; }
+	}
+}
