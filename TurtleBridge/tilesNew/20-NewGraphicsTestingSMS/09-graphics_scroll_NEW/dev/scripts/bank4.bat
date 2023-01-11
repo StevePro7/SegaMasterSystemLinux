716cee83e273fad1cd928c7e35c226216faf9af6
StevePro7 <steveprozune@hotmail.com>
2023-01-11 18:58:35 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/scripts/bank4.bat
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/scripts/bank4.bat
@@ -4,9 +4,9 @@ REM bank4
 cd ..
 cd banks
 
-folder2c bank4 bank4 4
+"C:\Program Files\SDCC\bin\folder2c.exe" bank4 bank4 4
 
-sdcc --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
+"C:\Program Files\SDCC\bin\sdcc.exe" --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
 
 del *.asm > nul; del *.lst > nul; del *.sym > nul
 
