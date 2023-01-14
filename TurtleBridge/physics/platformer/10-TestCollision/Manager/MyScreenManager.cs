18f361462eb4a9e3069c7b37adcc2a9397c6998e
StevePro7 <steveprozune@hotmail.com>
2023-01-14 17:46:10 +0000

    no message

---
+++ TurtleBridge/physics/platformer/10-TestCollision/Manager/MyScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace Test
+{
+	public class MyScreenManager
+	{
+		MyContentManager myContentManager;
+		MyInputManager myInputManager;
+		MyLevelManager myLevelManager;
+		MyPlayerManager myPlayerManager;
+
+		public MyScreenManager(
+			MyContentManager myContentManager,
+			MyInputManager myInputManager,
+			MyLevelManager myLevelManager,
+			MyPlayerManager myPlayerManager
+			)
+		{
+			this.myContentManager = myContentManager;
+			this.myInputManager = myInputManager;
+			this.myLevelManager = myLevelManager;
+			this.myPlayerManager = myPlayerManager;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Begin();
+			myLevelManager.Draw(spriteBatch);
+			myPlayerManager.Draw(spriteBatch);
+			spriteBatch.End();
+		}
+	}
+}
