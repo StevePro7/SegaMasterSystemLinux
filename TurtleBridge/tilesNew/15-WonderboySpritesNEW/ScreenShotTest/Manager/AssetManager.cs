3844d8539174b69ddf4680524f68cda17f3c5b91
StevePro7 <steveprozune@hotmail.com>
2023-01-14 14:03:27 +0000

    no message

---
+++ TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/Manager/AssetManager.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace ScreenShotTest
+{
+	public class AssetManager
+	{
+		private string[] names =
+		{
+			"00-font_tiles_yellow_192x24_05",
+			"00-font_tiles_yellow_192x24_06",
+			"01-section02_left_64x112",
+			"01-section02_left_goal00",
+			"01-section02_left_sign01",
+			"01-waves_block_32x112",
+			"02-section03_norm_64x32",
+			"02-section03_open_64x32_02",
+			"02-section03_open_64x32_03",
+			"03-section03_tree_64x32_01",
+			"03-section03_tree_64x32_02",
+			"03-section03_tree_64x32_03",
+			"04-flying_turtle01_32x24_00",
+			"04-flying_turtle01_32x24_01",
+			"04-flying_turtle01_32x24_02",
+			"04-flying_turtle01_32x24_03",
+			"04-hover_turtleAA00_32x24_00",
+			"04-hover_turtleAA00_32x24_01",
+			"05-clouds_192x24",
+			"06-titlescreenPlay",
+			"islands01",
+			"islands02",
+			"islands03",
+			"islands04",
+			"islands05",
+			"islands06",
+		};
+
+		protected int[] heights = { 0 };
+			
+		public void LoadContent(ContentManager content)
+		{
+			var myEnumMemberCount = Enum.GetNames(typeof(AssetType)).Length;
+			Images = new Texture2D[myEnumMemberCount];
+
+			for (int index = 0; index < myEnumMemberCount; index++)
+			{
+				var name = names[index];
+				Images[index] = content.Load<Texture2D>(name);
+			}
+		}
+
+		public int GetHeights(int numAssets)
+		{
+			int height = 0;
+			for (int idx = 0; idx < numAssets; idx++)
+			{
+				height += heights[idx];
+			}
+
+			return height;
+		}
+
+		public Texture2D[] Images { get; private set; }
+	}
+}
---
+++ TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/Manager/EnumManager.cs
@@ -0,0 +1,39 @@
+﻿namespace ScreenShotTest
+{
+	public enum AssetType
+	{
+		x00_font_tiles_yellow_192x24_05 = 0,
+		x00_font_tiles_yellow_192x24_06 = 1,
+
+		x01_section02_left_64x112 = 2,
+		x01_section02_left_goal00 = 3,
+		x01_section02_left_sign01 = 4,
+		x01_waves_block_32x112 = 5,
+
+		x02_section03_norm_64x32 = 6,
+		x02_section03_open_64x32_02 = 7,
+		x02_section03_open_64x32_03 = 8,
+
+		x03_section03_tree_64x32_01 = 9,
+		x03_section03_tree_64x32_02 = 10,
+		x03_section03_tree_64x32_03 = 11,
+
+		x04_flying_turtle01_32x24_00 = 12,
+		x04_flying_turtle01_32x24_01 = 13,
+		x04_flying_turtle01_32x24_02 = 14,
+		x04_flying_turtle01_32x24_03 = 15,
+		x04_hover_turtleAA00_32x24_00 = 16,
+		x04_hover_turtleAA00_32x24_01 = 17,
+
+		x05_clouds_192x24 = 18,
+		x06_titlescreenPlay = 19,
+
+		islands01 = 20,
+		islands02 = 21,
+		islands03 = 22,
+		islands04 = 23,
+		islands05 = 24,
+		islands06 = 25,
+	}
+
+}
