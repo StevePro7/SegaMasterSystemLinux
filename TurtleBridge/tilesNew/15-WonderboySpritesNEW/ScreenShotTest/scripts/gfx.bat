6307abc31179a2103f948406874916d52f77b382
StevePro7 <steveprozune@hotmail.com>
2023-01-14 16:50:07 +0000

    no message

--- TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/scripts/gfx.bat
+++ TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/scripts/gfx.bat
@@ -10,7 +10,7 @@
 
 :: Tiles
 ::"C:/SEGA/bmp2tile043/bmp2tile.exe" raw/bggame_tiles.png -savetiles "bggame_tiles (tiles).psgcompr" -removedupes -nomirror -planar -tileoffset 0  -savetilemap "bggame_tiles (tilemap).bin" -savepalette "bggame_tiles (palette).bin" -fullpalette
-"C:/SEGA/bmp2tile043/bmp2tile.exe" raw/sprite_tiles.png -savetiles "sprite_tiles (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprite_tiles (palette).bin" -fullpalette  -spritepalette -exit
+"C:/SEGA/bmp2tile043/bmp2tile.exe" raw/sprite_tiles.png -savetiles "sprite_tiles (tiles).psgcompr" -noremovedupes -nomirror -planar -tileoffset 0 -savepalette "sprite_tiles (palette).bin" -fullpalette  -spritepalette
 
 ::cd ..
 ::cd scripts
