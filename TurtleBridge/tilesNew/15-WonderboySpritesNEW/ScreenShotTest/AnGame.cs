ac8ed9227741029c8cba5cd2ba3c15eb47ab84ff
StevePro7 <steveprozune@hotmail.com>
2023-01-14 15:29:32 +0000

    no message

--- TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/AnGame.cs
+++ TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/AnGame.cs
@@ -7,7 +7,7 @@ namespace ScreenShotTest
 {
 	public class AnGame : Microsoft.Xna.Framework.Game
 	{
-		const string name = "sprite_plat04";
+		const string name = "sprite_flip08";
 
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
@@ -20,7 +20,7 @@ namespace ScreenShotTest
 
 		public AnGame()
 		{
-			output = new Output01();
+			output = new Output02();
 
 			assetManager = new AssetManager();
 			graphics = new GraphicsDeviceManager(this);
---
+++ TurtleBridge/tilesNew/15-WonderboySpritesNEW/ScreenShotTest/Object/Output02.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ScreenShotTest
+{
+	public class Output02 : Output, IOutput
+	{
+		public void Ctor(GraphicsDeviceManager graphics, AssetManager assetManager)
+		{
+			base.Ctor(assetManager);
+			assets.Add(AssetType.sprites_partial);
+			Wide = 32;
+			High = 32;
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			int idx = 7;
+
+			AssetType asset;
+			Texture2D image;
+			Vector2 pos;
+			Rectangle rect;
+			
+			asset = assets[0];
+			image = assetManager.Images[(int)asset];
+			pos = new Vector2(0, 0);
+			rect = new Rectangle(0, idx * 32, 32, 32);
+			spriteBatch.Draw(image, pos, rect, Color.White);
+		}
+
+	}
+}
