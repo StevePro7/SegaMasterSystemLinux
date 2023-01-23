d4637ca84edbfe156023c5b8f8da3a870c537aa7
StevePro7 <steveprozune@hotmail.com>
2023-01-23 22:30:19 +0000

    no message

--- TurtleBridge/level/15-LevelManual/ScreenShotTest/Manager/EnumManager.cs
+++ TurtleBridge/level/15-LevelManual/ScreenShotTest/Manager/EnumManager.cs
@@ -6,14 +6,12 @@
 		tile_type_bridge_midd = 1,
 		tile_type_bridge_side = 2,
 		tile_type_bridge_sign = 3,
-
 		tile_type_island_midd = 4,
 		tile_type_island_left = 5,
 		tile_type_island_rght = 6,
 		tile_type_island_sign = 7,
 		tile_type_islandTreeL = 8,
 		tile_type_islandTreeR = 9,
-
 		tile_type_turtle_sea1 = 10,
 		tile_type_turtle_sea2 = 11,
 		tile_type_turtle_fly1 = 12,
