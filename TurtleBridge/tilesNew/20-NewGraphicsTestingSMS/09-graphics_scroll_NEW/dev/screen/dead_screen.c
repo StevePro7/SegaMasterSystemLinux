adf401b4f4bc97921a6f388d40b6dd0eb85c3da2
StevePro7 <steveprozune@hotmail.com>
2023-01-27 16:58:58 +0000

    no message

---
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/dead_screen.c
@@ -0,0 +1,13 @@
+#include "dead_screen.h"
+#include "../engine/enum_manager.h"
+#include "../engine/font_manager.h"
+
+void screen_dead_screen_load()
+{
+	engine_font_manager_text( "DEAD SCREEN!!", 10, 2 );
+}
+
+void screen_dead_screen_update( unsigned char *screen_type )
+{
+	*screen_type = screen_type_dead;
+}
