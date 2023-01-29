344663fcbbb74a31c653dac592b76ed81c26dc1f
StevePro7 <steveprozune@hotmail.com>
2023-01-29 22:22:34 +0000

    no message

--- TurtleBridge/maths/20-BankBuildSMS/dev/engine/audio_manager.h
+++
@@ -1,7 +0,0 @@
-#ifndef _AUDIO_MANAGER_H_
-#define _AUDIO_MANAGER_H_
-
-void engine_music_manager_play();
-void engine_sound_manager_play();
-
-#endif//_AUDIO_MANAGER_H_
--- TurtleBridge/maths/20-BankBuildSMS/dev/engine/screen_manager.c
+++ TurtleBridge/maths/20-BankBuildSMS/dev/engine/screen_manager.c
@@ -1,15 +1,9 @@
 #include "screen_manager.h"
-#include "audio_manager.h"
 #include "content_manager.h"
 #include "enum_manager.h"
 #include "font_manager.h"
 #include "global_manager.h"
 #include "input_manager.h"
-
-static unsigned char x = 16;
-static unsigned char y = 128-32;// 64;
-
-static void bonus_level();
 
 void engine_screen_manager_init()
 {
