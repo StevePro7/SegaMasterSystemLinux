a3dc99d3ceba9306cb28844e3813363a891ad7bd
StevePro7 <steveprozune@hotmail.com>
2023-02-05 16:11:47 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/dead_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/dead_screen.c
@@ -8,14 +8,12 @@
 
 void screen_dead_screen_load()
 {
-	devkit_SMS_mapROMBank( bggame_tiles__tiles__psgcompr_bank );
-	engine_font_manager_text( "DEAD", 1, 10 );
+	engine_font_manager_text( "DEAD SCREEN", 1, 10 );
 	engine_player_manager_draw();
 }
 
 void screen_dead_screen_update( unsigned char *screen_type )
 {
-	engine_scroll_manager_update( 0 );
 	engine_player_manager_draw();
 	*screen_type = screen_type_dead;
 }
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/load_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/load_screen.c
@@ -73,7 +73,8 @@ void screen_load_screen_update( unsigned char *screen_type )
 	//engine_player_manager_draw();
 
 	//*screen_type = screen_type_ready;
-	*screen_type = screen_type_play;
+	//*screen_type = screen_type_play;
+	*screen_type = screen_type_dead;
 }
 
 //
