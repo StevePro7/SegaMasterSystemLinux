64c8c043b2ffe183bcd7a8cff608c59e7e4f258f
StevePro7 <steveprozune@hotmail.com>
2023-02-15 20:12:30 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/demo_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/demo_screen.c
@@ -25,12 +25,12 @@ static unsigned char landingY;
 static unsigned char index;
 static unsigned int posY2;
 
-//static int jumps[] =
+//static signed int jumps[] =
 //{
 //	-368,-351,-335,-318,-302,-285,-268,-252,-235,-219,-202,-185,-169,-152,-136,-119,-102,-86,-69,-53,-36,-20,-3,14,30,47,63,80,97,113,130,146,163,180,196,213,229,246,262,279,296,312,329,345,362,379,395,412,428,409,462,478,495,511,528,545,561,578,594,611,627,644,
 //};
 
-static int jumps[] =
+static signed int jumps[] =
 {
 	-736, -702, -670, -636, -604, -570, -536, -504, -470, -438, -404, -370, -338, -304, -272, -238, -204, -172, -138, -106, -72, -40, -6, 28, 60, 94, 126, 160, 194, 226, 260, 292, 326, 360, 392, 426, 458, 492, 524, 558, 592, 624, 658, 690, 724, 758, 790, 824, 856, 818, 924, 956, 990, 1022, 1056, 1090, 1122, 1156, 1188, 1222, 1254, 1288,
 };
@@ -70,6 +70,7 @@ void screen_demo_screen_load()
 	frame = 0;
 	flag = 0;
 
+	engine_font_manager_data( po->player_state, 20, 8 );
 	engine_font_manager_data( frame, 20, 9 );
 	engine_font_manager_data( index, 20, 10 );
 	engine_font_manager_data( 0, 20, 11 );
@@ -82,6 +83,31 @@ void screen_demo_screen_load()
 
 void screen_demo_screen_update( unsigned char *screen_type )
 {
+	struct_player_object *po = &global_player_object;
+	unsigned int input1, input2;
+	//signed int deltaY;
+
+	input1 = devkit_SMS_getKeysPressed();			// hold
+	input2 = devkit_SMS_getKeysHeld();				// move
+	if( player_state_isonground == po->player_state )
+	{
+		if( input1 & input_type_fire1 || input1 & input_type_fire2 )
+		{
+			engine_font_manager_data( input1, 20, 7 );
+			po->player_state = player_state_isintheair;
+		}
+	}
+	if( player_state_isintheair == po->player_state )
+	{
+		engine_font_manager_data( po->player_state, 20, 8 );
+		engine_font_manager_data( frame, 20, 9 );
+		engine_font_manager_data( index, 20, 10 );
+		engine_font_manager_data( 0, 20, 11 );
+		engine_font_manager_data( 0, 20, 12 );
+		engine_font_manager_data( po->posnY, 20, 13 );
+		engine_font_manager_data( po->drawY, 20, 14 );
+	}
+
 	engine_player_manager_draw();
 	*screen_type = screen_type_demo;
 }
