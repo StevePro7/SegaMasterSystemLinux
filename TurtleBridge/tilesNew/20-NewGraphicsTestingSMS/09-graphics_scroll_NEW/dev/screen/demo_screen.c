c62d6d303d1f9f4e795232595b03f854c9186b93
StevePro7 <steveprozune@hotmail.com>
2023-02-15 21:39:07 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/demo_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/demo_screen.c
@@ -25,15 +25,18 @@ static unsigned char landingY;
 static unsigned char index;
 //static unsigned int posY2;
 
-//static signed int jumps[] =
-//{
-//	-368,-351,-335,-318,-302,-285,-268,-252,-235,-219,-202,-185,-169,-152,-136,-119,-102,-86,-69,-53,-36,-20,-3,14,30,47,63,80,97,113,130,146,163,180,196,213,229,246,262,279,296,312,329,345,362,379,395,412,428,409,462,478,495,511,528,545,561,578,594,611,627,644,
-//};
-
 static signed int jumps[] =
 {
-	-736, -702, -670, -636, -604, -570, -536, -504, -470, -438, -404, -370, -338, -304, -272, -238, -204, -172, -138, -106, -72, -40, -6, 28, 60, 94, 126, 160, 194, 226, 260, 292, 326, 360, 392, 426, 458, 492, 524, 558, 592, 624, 658, 690, 724, 758, 790, 824, 856, 818, 924, 956, 990, 1022, 1056, 1090, 1122, 1156, 1188, 1222, 1254, 1288,
+	-368,-351,-335,-318,-302,-285,-268,-252,-235,-219,-202,-185,-169,-152,-136,-119,-102,-86,-69,-53,-36,-20,-3,14,30,47,63,80,97,113,130,146,163,180,196,213,229,246,262,279,296,312,329,345,362,379,395,412,428,409,462,478,495,511,528,545,561,578,594,611,627,644,
 };
+
+//static signed int jumps[] =
+//{
+//	-736, -702, -670, -636, -604, -570, -536, -504, -470, -438, -404, -370, -338, -304, -272, -238, -204, -172, -138, -106, -72, -40, -6, 28, 60, 94, 126, 160, 194, 226, 260, 292, 326, 360, 392, 426, 458, 492, 524, 558, 592, 624, 658, 690, 724, 758, 790, 824, 856, 818, 924, 956, 990, 1022, 1056, 1090, 1122, 1156, 1188, 1222, 1254, 1288,
+//};
+
+static void swap_player_frame();
+static void flip_player_frame( unsigned char input3, unsigned char input4 );
 
 void screen_demo_screen_load()
 {
@@ -46,7 +49,7 @@ void screen_demo_screen_load()
 
 	engine_level_manager_init( go->game_level );
 	engine_player_manager_initX( go->game_difficulty );
-
+	po->player_frame = 8;
 
 	devkit_SMS_displayOff();
 	engine_asm_manager_clear_VRAM();
@@ -68,6 +71,7 @@ void screen_demo_screen_load()
 	index = 0;
 	frame = 0;
 
+	engine_font_manager_data( po->player_frame, 20, 5 );
 	engine_font_manager_data( po->player_state, 20, 8 );
 	engine_font_manager_data( frame, 20, 9 );
 	engine_font_manager_data( index, 20, 10 );
@@ -77,29 +81,50 @@ void screen_demo_screen_load()
 	engine_font_manager_data( po->drawY, 20, 14 );
 
 	landingY = player_loadY * 8;
+	
 }
 
 void screen_demo_screen_update( unsigned char *screen_type )
 {
 	struct_player_object *po = &global_player_object;
 	unsigned int input1, input2;
+	unsigned int input3, input4;	// TODO - use later to move right when in the air
 	signed int deltaY;
 //	unsigned int leapY;
 
-	input1 = devkit_SMS_getKeysPressed();			// hold
-	input2 = devkit_SMS_getKeysHeld();				// move
+	//input1 = devkit_SMS_getKeysPressed();			// hold
+	input1 = engine_input_manager_hold( input_type_fire1 );
+	input2 = engine_input_manager_hold( input_type_fire2 );
+	//input2 = devkit_SMS_getKeysHeld();				// move
 	if( player_state_isonground == po->player_state )
 	{
-		if( input1 & input_type_fire1 || input1 & input_type_fire2 )
+		//if( input_type_fire1 & input1 || input_type_fire2 & input1 )
+		if (input1 || input2 )
 		{
 			frame = 0;
 			engine_font_manager_data( input1, 20, 7 );
 			po->player_state = player_state_isintheair;
+			//po->player_frame = 4;
 			index = 0;
 		}
 	}
-	if( player_state_isintheair == po->player_state )
+	else if( player_state_isintheair == po->player_state )
 	{
+		//if( input_type_fire1 & input1 )
+		if( input1 )
+		{
+			engine_font_manager_data( input1, 20, 7 );
+			swap_player_frame();
+		}
+		if( input2 )
+		{
+			input3 = engine_input_manager_move( input_type_left );
+			input4 = engine_input_manager_move( input_type_right );
+			engine_font_manager_data( input1, 20, 7 );
+			flip_player_frame( input3 , input4 );
+		}
+
+		// TODO pull joystick down on descent to double next code.
 		deltaY = jumps[ index ];
 		po->leapY += deltaY;
 		po->posnY = UFIX( po->leapY );
@@ -108,7 +133,7 @@ void screen_demo_screen_update( unsigned char *screen_type )
 			//po->posnY = player_loadY * 8;
 			po->posnY = landingY;
 			po->leapY = po->posnY << 8;
-			po->player_frame = 0;
+			//po->player_frame = 0;
 			po->player_state = player_state_isonground;
 			index = 0;
 		}
@@ -120,10 +145,10 @@ void screen_demo_screen_update( unsigned char *screen_type )
 			//po->leapY = po->posnY << 8;
 			index++;
 		}
-
 		frame++;
 		po->drawY = po->posnY - 32;
 
+		engine_font_manager_data( po->player_frame, 20, 5 );
 		engine_font_manager_data( po->player_state, 20, 8 );
 		engine_font_manager_data( frame, 20, 9 );
 		engine_font_manager_data( index, 20, 10 );
@@ -135,4 +160,91 @@ void screen_demo_screen_update( unsigned char *screen_type )
 
 	engine_player_manager_draw();
 	*screen_type = screen_type_demo;
+}
+
+static void swap_player_frame()
+{
+	struct_player_object *po = &global_player_object;
+	if( po->player_frame < 4 )
+	{
+		po->player_frame = 2 - po->player_frame;
+	}
+	else if( po->player_frame < 8 )
+	{
+		po->player_frame += 4;
+	}
+	else if( po->player_frame < 12 )
+	{
+		po->player_frame -= 4;
+	}
+}
+
+static void flip_player_frame( unsigned char input3, unsigned char input4 )
+{
+	// TODO enum??
+	struct_player_object *po = &global_player_object;
+	if( 0 == po->player_frame )
+	{
+		po->player_frame = 4;
+	}
+	else if( 2 == po->player_frame )
+	{
+		po->player_frame = 8;
+	}
+	else if( po->player_frame < 8 )
+	{
+		if( input3 )
+		{
+			if( 4 == po->player_frame )
+			{
+				po->player_frame = 7;
+			}
+			else
+			{
+				po->player_frame--;
+			}
+		}
+		else
+		{
+			if( 7 == po->player_frame )
+			{
+				po->player_frame = 4;
+			}
+			else
+			{
+				po->player_frame++;
+			}
+		}
+	}
+	else if( po->player_frame < 12 )
+	{
+		if( input4 )
+		{
+			if( 11 == po->player_frame )
+			{
+				po->player_frame = 8;
+			}
+			else
+			{
+				po->player_frame++;
+			}
+		}
+		else
+		{
+			if( 8 == po->player_frame )
+			{
+				po->player_frame = 11;
+			}
+			else
+			{
+				po->player_frame--;
+			}
+		}
+	}
+
+	po->drawX = po->initX - 16;
+	if( 5 == po->player_frame || 7 == po->player_frame || 9 == po->player_frame || 11 == po->player_frame )
+	{
+		po->drawX = po->initX - 16 + 4;
+	}
 }
