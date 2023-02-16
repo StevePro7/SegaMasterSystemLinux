37aac8c5c4deb94960eb6761a5e36c2f76a5cc33
StevePro7 <steveprozune@hotmail.com>
2023-02-16 15:04:19 +0000

    no message

--- TurtleBridge/maths/20-BankBuildSMS/dev/engine/enum_manager.h
+++ TurtleBridge/maths/20-BankBuildSMS/dev/engine/enum_manager.h
@@ -1,6 +1,33 @@
 #ifndef _ENUM_MANAGER_H_
 #define _ENUM_MANAGER_H_
 
+typedef enum tag_enum_screen_type
+{
+	screen_type_none = 0,
+	screen_type_splash = 1,
+	screen_type_begin = 2,
+	screen_type_intro = 3,
+	screen_type_title = 4,
+	screen_type_demo = 5,
+	screen_type_diff = 6,
+	screen_type_level = 7,
+	screen_type_start = 8,
+	screen_type_init = 9,
+	screen_type_load = 10,
+	screen_type_ready = 11,
+	screen_type_play = 12,
+	screen_type_pass = 13,
+	screen_type_dead = 14,
+	screen_type_cont = 15,
+	screen_type_over = 16,
+	screen_type_boss = 17,
+	screen_type_beat = 18,
+	screen_type_option = 19,
+	screen_type_test = 20,
+	screen_type_func = 21,
+
+} enum_screen_type;
+
 typedef enum tag_enum_tile_type
 {
 	tile_type_no_tiles = 0,
--- TurtleBridge/maths/20-BankBuildSMS/dev/engine/screen_manager.c
+++ TurtleBridge/maths/20-BankBuildSMS/dev/engine/screen_manager.c
@@ -1,12 +1,32 @@
 #include "screen_manager.h"
-#include "content_manager.h"
-#include "enum_manager.h"
-#include "font_manager.h"
 #include "global_manager.h"
-#include "input_manager.h"
+#include "enum_manager.h"
 
-void engine_screen_manager_init()
+// Screens
+#include "../screen/none_screen.h"
+#include "../screen/test_screen.h"
+#include "../screen/func_screen.h"
+
+static unsigned char curr_screen_type;
+static unsigned char next_screen_type;
+
+static void( *load_method[ MAX_SCREEENS ] )( );
+static void( *update_method[ MAX_SCREEENS ] )( unsigned char *screen_type );
+
+void engine_screen_manager_init( unsigned char open_screen_type )
 {
+	next_screen_type = open_screen_type;
+	curr_screen_type = screen_type_none;
+
+	// Set load methods.
+	load_method[ screen_type_none ] = screen_none_screen_load;
+	load_method[ screen_type_test ] = screen_test_screen_load;
+	load_method[ screen_type_func ] = screen_func_screen_load;
+
+	// Set update methods.
+	update_method[ screen_type_none ] = screen_none_screen_update;
+	update_method[ screen_type_test ] = screen_test_screen_update;
+	update_method[ screen_type_func ] = screen_func_screen_update;
 }
 
 void engine_screen_manager_update()
