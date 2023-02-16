f57ab29633a05384f740c50dbc3b42b727717e55
StevePro7 <steveprozune@hotmail.com>
2023-02-16 21:09:46 +0000

    no message

--- TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank45.bat
+++ TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank45.bat
@@ -3,7 +3,13 @@ REM bank45
 
 cd ..
 cd banks
+cd bank45
+cd raw
 
+"C:/Program Files/SDCC/bin/pcmenc" -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff__Title__Screen__05.wav
+mv Riff__Title__Screen__05.wav.pcmenc ../
+
+cd ../..
 "C:/Program Files/SDCC/bin/folder2c" bank45 bank45 45
 
 "C:/Program Files/SDCC/bin/sdcc" --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK45 bank45.c
--- TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank48.bat
+++ TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank48.bat
@@ -3,7 +3,13 @@ REM bank48
 
 cd ..
 cd banks
+cd bank48
+cd raw
 
+"C:/Program Files/SDCC/bin/pcmenc" -rto 1 -dt1 12 -dt2 12 -dt3 423 Riff__Title__Screen__08.wav
+mv Riff__Title__Screen__08.wav.pcmenc ../
+
+cd ../..
 "C:/Program Files/SDCC/bin/folder2c" bank48 bank48 48
 
 "C:/Program Files/SDCC/bin/sdcc" --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK48 bank48.c
