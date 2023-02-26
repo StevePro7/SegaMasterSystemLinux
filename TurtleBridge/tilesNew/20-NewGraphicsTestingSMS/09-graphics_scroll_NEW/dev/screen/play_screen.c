d4085f7d5ca545b75c85d537fdf47ebc1448f031
StevePro7 <steveprozune@hotmail.com>
2023-02-26 13:03:33 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/play_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/play_screen.c
@@ -26,7 +26,7 @@ static signed int deltaY;
 void screen_play_screen_load()
 {
 	engine_frame_manager_load();
-	//engine_frame_manager_draw();
+	engine_frame_manager_draw();
 
 	//19-Feb-2023
 	// TODO - iron this out but IMPORTANT - I don't think I want to play music in same function as draw title etc. as causes screen flicker??
@@ -67,9 +67,9 @@ void screen_play_screen_update( unsigned char *screen_type )
 		//{
 		//	command = engine_command_manager_build( po->player_state, 0, 0, 0, 0, 0, 0 );
 		//}
-		if( 0 == fo->frame_count )
+		if( 10 == fo->frame_count )
 		{
-			command = engine_command_manager_build( po->player_state, 0, 0, 0, 0, 1, 0 );
+			command = engine_command_manager_build( po->player_state, 0, 0, 0, 0, 0, 1 );
 		}
 		else
 		{
@@ -78,8 +78,6 @@ void screen_play_screen_update( unsigned char *screen_type )
 
 		//command = engine_command_manager_build( po->player_state, 0, 1, 0, 0, 0, 0 );
 
-		//engine_frame_manager_update();
-		//engine_frame_manager_draw();
 		//if( 60 == fo->frame_count )
 		//{
 		//	command = engine_command_manager_build( po->player_state, 0, 1, 0, 0, 1, 0 );
@@ -105,7 +103,7 @@ void screen_play_screen_update( unsigned char *screen_type )
 		//command = engine_command_manager_build( po->player_state, 0, 1, 0, 0, 0, 0 );
 
 		engine_frame_manager_update();
-		//engine_frame_manager_draw();
+		engine_frame_manager_draw();
 		//if( 60 == fo->frame_count )
 		//{
 		//	command = engine_command_manager_build( po->player_state, 0, 1, 0, 0, 1, 0 );
