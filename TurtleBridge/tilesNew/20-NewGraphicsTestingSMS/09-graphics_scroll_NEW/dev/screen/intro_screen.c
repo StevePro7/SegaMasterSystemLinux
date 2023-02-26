a052932cd5ccccf1e1e81e8236d2b0dda18b3460
StevePro7 <steveprozune@hotmail.com>
2023-02-26 15:38:02 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/intro_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/intro_screen.c
@@ -50,35 +50,140 @@ void screen_intro_screen_update( unsigned char *screen_type )
 	unsigned char input1, input2, input3, input4, input5, input6;
 	unsigned char command;
 
-	//unsigned char deltaX;
-	//unsigned char loops;
-	//enum_scroll_state scroll_state;
+	unsigned char deltaX;
+	unsigned char loops;
+	enum_scroll_state scroll_state;
 	enum_player_state player_state;
 
 	command = COMMAND_NONE_MASK;
 	player_state = po->player_state;
 
-	input1 = engine_input_manager_move( input_type_left );
-	input2 = engine_input_manager_move( input_type_right );
-	input3 = engine_input_manager_move( input_type_up );
-	input4 = engine_input_manager_move( input_type_down );
-	//input4 = engine_input_manager_hold( input_type_down );			// increment frame counter.
-	input5 = engine_input_manager_hold( input_type_fire1 );
-	input6 = engine_input_manager_hold( input_type_fire2 );
-
-	command = engine_command_manager_build( po->player_state, input1, input2, input3, 0, input5, input6 );
-	engine_font_manager_data( po->player_state, 31, 1 );
-	engine_font_manager_data( command, 31, 2 );
-
-	if( command != co->prev_command )
+	if( !complete )
 	{
-		engine_command_manager_steven( fo->frame_count, command );
-	//	engine_command_manager_draw();
+		input1 = engine_input_manager_move( input_type_left );
+		input2 = engine_input_manager_move( input_type_right );
+		input3 = engine_input_manager_move( input_type_up );
+		input4 = engine_input_manager_move( input_type_down );
+		//input4 = engine_input_manager_hold( input_type_down );			// increment frame counter.
+		input5 = engine_input_manager_hold( input_type_fire1 );
+		input6 = engine_input_manager_hold( input_type_fire2 );
+
+		command = engine_command_manager_build( po->player_state, input1, input2, input3, 0, input5, input6 );
+		engine_font_manager_data( po->player_state, 31, 1 );
+		engine_font_manager_data( command, 31, 2 );
+
+		if( command != co->prev_command )
+		{
+			engine_command_manager_steven( fo->frame_count, command );
+			//	engine_command_manager_draw();
+		}
+
+		engine_frame_manager_update();
+		engine_frame_manager_draw();
+
+
+		if( COMMAND_NONE_MASK != command )
+		{
+			// Get horizontal movement.
+			deltaX = engine_player_manager_get_deltaX( po->player_state, command );
+
+			// Get button action.
+			engine_player_manager_set_action( po->player_frame, command );
+
+			// No scroll.
+			if( 0 == deltaX )
+			{
+				engine_scroll_manager_update( 0 );
+			}
+			else
+			{
+				//if( !complete ) {}
+				for( loops = 0; loops < deltaX; loops++ )
+				{
+					scroll_state = engine_scroll_manager_update( 1 );
+					if( scroll_state_tile == scroll_state )
+					{
+						engine_level_manager_draw_column( so->scrollColumn );
+					}
+					else if( scroll_state_comp == scroll_state )
+					{
+						complete = scroll_state_comp == scroll_state;
+						if( complete )
+						{
+							break;
+						}
+					}
+				}
+
+				// Set horizontal movement.
+				engine_player_manager_horz( deltaX );
+
+				// Get / set vertical movement.
+				deltaY = 0;
+				if( player_state_isintheair == po->player_state )
+				{
+					deltaY = engine_player_manager_get_deltaY();
+					engine_player_manager_vert( deltaY );
+					engine_player_manager_bounds( deltaY, po->posnY, go->game_isgod );
+				}
+				else if( player_state_isonground == po->player_state )
+				{
+					engine_player_manager_animate( po->player_frame );
+				}
+
+				// General all-purpose collision detection routine.
+				player_state = engine_player_manager_collision( po->player_state, po->lookX, po->tileY, deltaY, po->posnY, go->game_isgod );
+
+				// Finally, check if player forcing downward drop.
+				if( player_state_isintheair == po->player_state )
+				{
+					// If player forces down while in the air then only apply on the descent!
+					if( ( COMMAND_DOWN_MASK & command ) == COMMAND_DOWN_MASK )
+					{
+						if( deltaY > 0 )
+						{
+							deltaY = engine_player_manager_get_deltaY();
+							engine_player_manager_vert( deltaY );
+							engine_player_manager_bounds( deltaY, po->posnY, go->game_isgod );
+						}
+					}
+
+					// General all-purpose collision detection routine.
+					player_state = engine_player_manager_collision( po->player_state, po->lookX, po->tileY, deltaY, po->posnY, go->game_isgod );
+				}
+			}
+
+			// TODO - this is done up above ^^
+			// Store command for future use.
+			//engine_command_manager_update( command );
+		}
+		else
+		{
+			engine_scroll_manager_update( 0 );
+		}
+
+		engine_player_manager_draw();
+		//engine_debug_manager_printout();
+		//	engine_font_manager_data( deltaY, 30, 2 );
+		//	engine_font_manager_data( po->posnY, 30, 3 );
+
+		// Check to see if player completes level.
+		if( complete )
+		{
+			engine_scroll_manager_update( 0 );
+			//*screen_type = screen_type_pass;
+			return;
+		}
+
+		// Check if moving on to the dying sequence.
+		if( player_state_isnowdying == player_state )
+		{
+			engine_scroll_manager_update( 0 );
+			*screen_type = screen_type_dead;
+			return;
+		}
+
 	}
-
-	engine_frame_manager_update();
-	engine_frame_manager_draw();
-
 	engine_player_manager_draw();
 	*screen_type = screen_type_intro;
 }
