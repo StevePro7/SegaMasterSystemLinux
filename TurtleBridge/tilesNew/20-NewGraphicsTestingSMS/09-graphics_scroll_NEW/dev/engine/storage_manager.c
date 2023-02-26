478e518a4cda0dc60f44423085072ad489d0ec66
StevePro7 <steveprozune@hotmail.com>
2023-02-26 17:37:32 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/storage_manager.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/storage_manager.c
@@ -33,8 +33,10 @@ void engine_storage_manager_read()
 	devkit_SMS_enableSRAM();
 	command_frame_index[ 0 ] = 0;// command_frame_index[ 0 ];
 	command_frame_index[ 1 ] = 4;// command_frame_index[ 1 ];
+	command_frame_index[ 2 ] = 5;// command_frame_index[ 2 ];
 	command_this_command[ 0 ] = 2;// command_this_command[ 0 ];
 	command_this_command[ 1 ] = 34;// command_this_command[ 1 ];
+	command_this_command[ 2 ] = 1;// command_this_command[ 2 ];
 
 	//command_frame_index[ 0 ] = so->storage_frame_index[ 0 ];
 	//command_frame_index[ 1 ] = so->storage_frame_index[ 1 ];
@@ -53,8 +55,10 @@ void engine_storage_manager_write()
 	//so->flag = 0x48;
 	so->storage_frame_index[ 0 ] = 0;// command_frame_index[ 0 ];
 	so->storage_frame_index[ 1 ] = 4;// command_frame_index[ 1 ];
+	so->storage_frame_index[ 2 ] = 5;// command_frame_index[ 2 ];
 	so->storage_this_command[ 0 ] = 2;// command_this_command[ 0 ];
 	so->storage_this_command[ 1 ] = 34;// command_this_command[ 1 ];
+	so->storage_this_command[ 2 ] = 1;// command_this_command[ 2 ];
 	//so->num_jumps = 0xBEEF;
 	//so->jump_high[ 0 ] = 1;
 	//so->jump_high[ 1 ] = 2;
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/test_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/test_screen.c
@@ -45,24 +45,24 @@ void screen_test_screen_update( unsigned char *screen_type )
 	// TODO delete
 	struct_frame_object *fo = &global_frame_object;
 
-	//struct_scroll_object *so = &global_scroll_object;
+	struct_scroll_object *so = &global_scroll_object;
 	struct_player_object *po = &global_player_object;
-	//struct_level_object *lo = &global_level_object;
-	//struct_game_object *go = &global_game_object;
+	struct_level_object *lo = &global_level_object;
+	struct_game_object *go = &global_game_object;
 	struct_command_object *co = &global_command_object;
 
 	unsigned char input1;// input2, input3, input4, input5, input6;
 	unsigned char input2;
-	//unsigned char deltaX;
-	////signed int deltaY;
-	//unsigned char loops;
-	////signed char collision;
-	//enum_scroll_state scroll_state;
+	unsigned char deltaX;
+	//signed int deltaY;
+	unsigned char loops;
+	//signed char collision;
+	enum_scroll_state scroll_state;
 	enum_player_state player_state;
 
 	unsigned char command = COMMAND_NONE_MASK;
 	player_state = po->player_state;
-
+		
 	if( !complete )
 	{
 		input1 = engine_input_manager_hold( input_type_left );
@@ -86,6 +86,106 @@ void screen_test_screen_update( unsigned char *screen_type )
 
 			engine_frame_manager_update();
 			engine_frame_manager_draw();
+		}
+
+		if( COMMAND_NONE_MASK != command )
+		{
+			// Get horizontal movement.
+			deltaX = engine_player_manager_get_deltaX( po->player_state, command );
+
+			// Get button action.
+			engine_player_manager_set_action( po->player_frame, command );
+
+			// No scroll.
+			if( 0 == deltaX )
+			{
+				engine_scroll_manager_update( 0 );
+			}
+			else
+			{
+				//if( !complete ) {}
+				for( loops = 0; loops < deltaX; loops++ )
+				{
+					scroll_state = engine_scroll_manager_update( 1 );
+					if( scroll_state_tile == scroll_state )
+					{
+						engine_level_manager_draw_column( so->scrollColumn );
+					}
+					else if( scroll_state_comp == scroll_state )
+					{
+						complete = scroll_state_comp == scroll_state;
+						if( complete )
+						{
+							break;
+						}
+					}
+				}
+
+				// Set horizontal movement.
+				engine_player_manager_horz( deltaX );
+
+				// Get / set vertical movement.
+				deltaY = 0;
+				if( player_state_isintheair == po->player_state )
+				{
+					deltaY = engine_player_manager_get_deltaY();
+					engine_player_manager_vert( deltaY );
+					engine_player_manager_bounds( deltaY, po->posnY, go->game_isgod );
+				}
+				else if( player_state_isonground == po->player_state )
+				{
+					engine_player_manager_animate( po->player_frame );
+				}
+
+				// General all-purpose collision detection routine.
+				player_state = engine_player_manager_collision( po->player_state, po->lookX, po->tileY, deltaY, po->posnY, go->game_isgod );
+
+				// Finally, check if player forcing downward drop.
+				if( player_state_isintheair == po->player_state )
+				{
+					// If player forces down while in the air then only apply on the descent!
+					if( ( COMMAND_DOWN_MASK & command ) == COMMAND_DOWN_MASK )
+					{
+						if( deltaY > 0 )
+						{
+							deltaY = engine_player_manager_get_deltaY();
+							engine_player_manager_vert( deltaY );
+							engine_player_manager_bounds( deltaY, po->posnY, go->game_isgod );
+						}
+					}
+
+					// General all-purpose collision detection routine.
+					player_state = engine_player_manager_collision( po->player_state, po->lookX, po->tileY, deltaY, po->posnY, go->game_isgod );
+				}
+			}
+
+			// Store command for future use.
+			//engine_command_manager_update( command );
+		}
+		else
+		{
+			engine_scroll_manager_update( 0 );
+		}
+
+		engine_player_manager_draw();
+		//engine_debug_manager_printout();
+		//	engine_font_manager_data( deltaY, 30, 2 );
+		//	engine_font_manager_data( po->posnY, 30, 3 );
+
+		// Check to see if player completes level.
+		if( complete )
+		{
+			engine_scroll_manager_update( 0 );
+			//*screen_type = screen_type_pass;
+			return;
+		}
+
+		// Check if moving on to the dying sequence.
+		if( player_state_isnowdying == player_state )
+		{
+			engine_scroll_manager_update( 0 );
+			//*screen_type = screen_type_dead;
+			return;
 		}
 	}
 
