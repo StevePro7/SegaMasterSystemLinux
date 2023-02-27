31efc0a85eef3fe77f3bd790f0ea39e311e8fb84
StevePro7 <steveprozune@hotmail.com>
2023-02-27 21:33:17 +0000

    no message

--- TurtleBridge/maths/16-AudioDump/Program.cs
+++ TurtleBridge/maths/16-AudioDump/Program.cs
@@ -8,7 +8,9 @@ namespace AudioDump
 		{
 			var fm = new FileManager();
 			fm.DumpFiles("SampleTest01");
-			//fm.DumpFile("bank3.h");
+			fm.DumpFiles("SampleTest02");
+			fm.DumpFiles("SampleTest03");
+			fm.DumpFiles("SampleTest04");
 
 			Console.WriteLine("Press [ RETURN ]");
 			Console.Read();
