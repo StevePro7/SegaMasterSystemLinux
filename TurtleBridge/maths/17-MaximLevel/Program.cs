3d34d5ade88f4fe0dbbec805ba53eb7c5c5c56bc
StevePro7 <steveprozune@hotmail.com>
2023-02-28 20:35:29 +0000

    no message

---
+++ TurtleBridge/maths/17-MaximLevel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MaximLevel
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fm = new FileManager();
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
---
+++ TurtleBridge/maths/17-MaximLevel/bank6.h
@@ -0,0 +1,3 @@
+extern const unsigned char level1116_txt[];
+#define				level1116_txt_size 1024
+#define				level1116_txt_bank 6
