22a824054da4b1c58dee2a68cb490ef73f807c9c
StevePro7 <steveprozune@hotmail.com>
2023-02-28 20:44:14 +0000

    no message

--- TurtleBridge/maths/20-BankBuildSMS/dev/banks/bank4.h
+++ TurtleBridge/maths/20-BankBuildSMS/dev/banks/bank4.h
@@ -1,4 +1,38 @@
-extern const unsigned char	bank4_txt[];
-#define				bank4_txt_size 7
-#define				bank4_txt_bank 4
+extern const unsigned char stevepro01[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
 
+
+extern const unsigned char stevepro02[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro03[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro04[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro05[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro06[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro07[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro08[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
--- TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank4.bat
+++ TurtleBridge/maths/20-BankBuildSMS/dev/scripts/bank4.bat
@@ -3,8 +3,9 @@ REM bank4
 
 cd ..
 cd banks
+::cd bank4
 
-"C:/Program Files/SDCC/bin/folder2c" bank4 bank4 4
+:: "C:/Program Files/SDCC/bin/folder2c" bank4 bank4 4
 
 "C:/Program Files/SDCC/bin/sdcc" --debug -c --no-std-crt0 -mz80 --Werror --opt-code-speed --constseg BANK4 bank4.c
 
