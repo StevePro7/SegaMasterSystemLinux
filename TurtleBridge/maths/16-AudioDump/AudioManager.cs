587635c847f8137392e75894aa9315a53da913e3
StevePro7 <steveprozune@hotmail.com>
2023-02-28 20:23:51 +0000

    no message

--- TurtleBridge/maths/16-AudioDump/AudioManager.cs
+++ TurtleBridge/maths/16-AudioDump/AudioManager.cs
@@ -12,19 +12,24 @@ namespace AudioDump
 		public AudioManager()
 		{
 			Lines = new List<string>();
+			Names = new List<string>();
+			Datas = new List<int>();
 		}
 
 		public void Calculate(List<FileObject> fileObjs)
 		{
 			var fileWips = new List<FileObject>();
 			int count = fileObjs.Count;
-			int index = 0;
+			//int index = 0;
 			for (int outer = 0; outer < count; outer++)
 			{
 				fileWips.Clear();
+				Datas.Clear();
 				var objOuter = fileObjs[outer];
 				fileWips.Add(objOuter);
+				Datas.Add(outer);
 				int maxim = objOuter.Size;
+				Names.Add(objOuter.Name);
 				for (int inner = 0; inner < count; inner++)
 				{
 					if (inner == outer)
@@ -33,12 +38,17 @@ namespace AudioDump
 					}
 
 					var objInner = fileObjs[inner];
+					if (Names.Contains(objInner.Name))
+					{
+						continue;
+					}
 					int value = maxim + objInner.Size;
 					if (value < SIXTEEN)
 					{
 						if (value > maxim)
 						{
 							fileWips.Add(objInner);
+							Datas.Add(inner);
 							maxim = value;
 						}
 					}
@@ -47,11 +57,19 @@ namespace AudioDump
 				// Chance for banking...
 				if (fileWips.Count() > 1)
 				{
-					index++;
-					Lines.Add(index.ToString().PadLeft(2, '0') + "," + objOuter.Name);
-					foreach (var obj in fileWips)
+					//if (!Datas.Contains(maxim))
+					//{
+
+					//index++;
+					Lines.Add((outer + 1).ToString().PadLeft(2, '0') + "," + objOuter.Name + "[" + fileWips.Count() + "]");
+					//foreach (var obj in fileWips)
+					for (int idx = 0; idx < fileWips.Count(); idx++)
 					{
-						string line = String.Format("{0},{1},{2},{3}",
+						var obj = fileWips[idx];
+						var dat = Datas[idx];
+						Names.Add(obj.Name);
+						string line = String.Format("{0},{1},{2},{3},{4}",
+							(dat + 1).ToString().PadLeft(2, '0'),
 						obj.Proj,
 						obj.Bank,
 						obj.Name,
@@ -60,6 +78,11 @@ namespace AudioDump
 					}
 					Lines.Add("TOTAL," + maxim);
 					Lines.Add("");
+					//}
+					//else
+					//{
+					//	Console.WriteLine("bobo");
+					//}
 				}
 			}
 
@@ -69,5 +92,7 @@ namespace AudioDump
 		}
 
 		public List<string> Lines { get; private set; }
+		public List<string> Names { get; private set; }
+		public List<int> Datas { get; private set; }
 	}
 }
--- TurtleBridge/maths/16-AudioDump/Program.cs
+++ TurtleBridge/maths/16-AudioDump/Program.cs
@@ -12,10 +12,10 @@ namespace AudioDump
 			fm.DumpFiles("SampleTest03");
 			fm.DumpFiles("SampleTest04");
 
-			fm.Sporadic("SampleTest05", "mus_cont_psg", 6, 530);
-			fm.Sporadic("SampleTest05", "mus_level_psg", 6, 4135);
-			fm.Sporadic("SampleTest05", "mus_over_psg", 6, 881);
-			fm.Sporadic("SampleTest05", "mus_beat_psg", 6, 1573);
+			//fm.Sporadic("SampleTest05", "mus_cont_psg", 6, 530);
+			//fm.Sporadic("SampleTest05", "mus_level_psg", 6, 4135);
+			//fm.Sporadic("SampleTest05", "mus_over_psg", 6, 881);
+			//fm.Sporadic("SampleTest05", "mus_beat_psg", 6, 1573);
 
 			fm.Sort();
 			//fm.Save();
