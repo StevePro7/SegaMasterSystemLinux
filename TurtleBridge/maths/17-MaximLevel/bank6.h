733160908d755310790b1db6e76f51f9ea6be7db
StevePro7 <steveprozune@hotmail.com>
2023-02-28 20:38:47 +0000

    no message

--- TurtleBridge/maths/17-MaximLevel/bank6.h
+++ TurtleBridge/maths/17-MaximLevel/bank6.h
@@ -1,3 +1,38 @@
-extern const unsigned char level1116_txt[];
-#define				level1116_txt_size 1024
-#define				level1116_txt_bank 6
+extern const unsigned char stevepro01[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro02[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro03[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro04[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro05[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro06[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro07[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
+
+
+extern const unsigned char stevepro08[];
+#define				stevepro01_size 2048
+#define				stevepro01_bank 4
