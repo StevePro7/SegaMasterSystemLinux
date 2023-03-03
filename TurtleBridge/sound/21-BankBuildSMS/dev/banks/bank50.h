6ec9d49491ab98820a6ef3672fcfb11bbd4c63aa
StevePro7 <steveprozune@hotmail.com>
2023-03-03 20:27:12 +0000

    no message

--- TurtleBridge/sound/21-BankBuildSMS/dev/banks/bank50.h
+++ TurtleBridge/sound/21-BankBuildSMS/dev/banks/bank50.h
@@ -1,4 +1,4 @@
-extern const unsigned char	bank50_txt[];
-#define				bank50_txt_size 8
-#define				bank50_txt_bank 50
+extern const unsigned char	Riff_Sound_VHshriek_01_wav_pcmenc[];
+#define				Riff_Sound_VHshriek_01_wav_pcmenc_size 15027
+#define				Riff_Sound_VHshriek_01_wav_pcmenc_bank 50
 
