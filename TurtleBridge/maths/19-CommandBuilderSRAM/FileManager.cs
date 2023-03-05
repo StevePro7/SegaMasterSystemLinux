7d9222358e462fee2c610f2073c5898c8ed4356d
StevePro7 <steveprozune@hotmail.com>
2023-03-05 17:16:24 +0000

    no message

---
+++ TurtleBridge/maths/19-CommandBuilderSRAM/FileManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace CommandBuilderSRAM
+{
+	public class FileManager
+	{
+		public void Init()
+		{
+			if (!Directory.Exists("output"))
+			{
+				Directory.CreateDirectory("output");
+			}
+
+			var files = Directory.GetFiles("output");
+			foreach (var file in files)
+			{
+				File.Delete(file);
+			}
+		}
+
+		public void Process(string path)
+		{
+			Bytes = File.ReadAllBytes("input/" + path);
+		}
+
+		public byte[] Bytes { get; private set; }
+	}
+}
--- TurtleBridge/maths/19-CommandBuilderSRAM/Program.cs
+++ TurtleBridge/maths/19-CommandBuilderSRAM/Program.cs
@@ -1,30 +1,13 @@
 ﻿using System;
 
-namespace FunctionMgr
+namespace CommandBuilderSRAM
 {
 	class Program
 	{
 		static void Main()
 		{
-			var fm = new FunctionManager();
-			byte divide = 10;
-			byte x = 2;
-			byte y = 8;
-			byte z = 0;
-			fm.CovertXYtoZ(divide, x, y, ref z);
-			z = 75;
-			fm.ConvertZtoXY(divide, z, ref x, ref y);
-
-			//12 * 16 = 192 + 12 = 204
-			// 2 + 8
-			byte b = 255;
-			byte upper = 0;
-			byte lower = 0;
-			fm.ConvertByteToNibbles(b, ref upper, ref lower);
-
-			upper = 15;
-			lower = 13;
-			fm.ConvertNibblesToByte(upper, lower, ref b);
+			var fm = new FileManager();
+			fm.Init();
 			Console.WriteLine("Press [ RETURN ]");
 			Console.Read();
 		}
