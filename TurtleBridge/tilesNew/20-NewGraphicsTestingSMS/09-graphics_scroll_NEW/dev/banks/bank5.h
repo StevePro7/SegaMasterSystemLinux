0708ad16b92f232a41c24caf3a416a99b13cac66
StevePro7 <steveprozune@hotmail.com>
2023-03-05 15:42:33 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank5.h
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank5.h
@@ -1,44 +1,4 @@
-extern const unsigned char	mastertracker_psg[];
-#define				mastertracker_psg_size 4825
-#define				mastertracker_psg_bank 5
-
-extern const unsigned char	sound0_psg[];
-#define				sound0_psg_size 23
-#define				sound0_psg_bank 5
-
-extern const unsigned char	sound1_psg[];
-#define				sound1_psg_size 21
-#define				sound1_psg_bank 5
-
-extern const unsigned char	sound2_psg[];
-#define				sound2_psg_size 34
-#define				sound2_psg_bank 5
-
-extern const unsigned char	sound3_psg[];
-#define				sound3_psg_size 34
-#define				sound3_psg_bank 5
-
-extern const unsigned char	sound4_psg[];
-#define				sound4_psg_size 21
-#define				sound4_psg_bank 5
-
-extern const unsigned char	sound5_psg[];
-#define				sound5_psg_size 32
-#define				sound5_psg_bank 5
-
-extern const unsigned char	sound6_psg[];
-#define				sound6_psg_size 35
-#define				sound6_psg_bank 5
-
-extern const unsigned char	sound7_psg[];
-#define				sound7_psg_size 35
-#define				sound7_psg_bank 5
-
-extern const unsigned char	sound8_psg[];
-#define				sound8_psg_size 19
-#define				sound8_psg_bank 5
-
-extern const unsigned char	sound9_psg[];
-#define				sound9_psg_size 97
-#define				sound9_psg_bank 5
+extern const unsigned char	usas_jubaruins_psg[];
+#define				usas_jubaruins_psg_size 14126
+#define				usas_jubaruins_psg_bank 5
 
