d8c3a6507720ea351d3f73c58eae7eb1076c2301
StevePro7 <steveprozune@hotmail.com>
2023-03-05 17:08:46 +0000

    no message

---
+++ TurtleBridge/maths/19-CommandBuilderSRAM/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FunctionMgr
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fm = new FunctionManager();
+			byte divide = 10;
+			byte x = 2;
+			byte y = 8;
+			byte z = 0;
+			fm.CovertXYtoZ(divide, x, y, ref z);
+			z = 75;
+			fm.ConvertZtoXY(divide, z, ref x, ref y);
+
+			//12 * 16 = 192 + 12 = 204
+			// 2 + 8
+			byte b = 255;
+			byte upper = 0;
+			byte lower = 0;
+			fm.ConvertByteToNibbles(b, ref upper, ref lower);
+
+			upper = 15;
+			lower = 13;
+			fm.ConvertNibblesToByte(upper, lower, ref b);
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
