78eb4c9dc2d1423abd361a272ad8ba38b9b9e58d
StevePro7 <steveprozune@hotmail.com>
2023-03-08 17:37:46 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/function_manager.adb
+++
@@ -1,1 +0,0 @@
-M:function_manager
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/main.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/main.c
@@ -76,12 +76,12 @@ static void start()
 
 	open_screen_type = screen_type_splash;
 	open_screen_type = screen_type_title;
-	open_screen_type = screen_type_option;
+	//open_screen_type = screen_type_option;
 	//open_screen_type = screen_type_func;
 	//open_screen_type = screen_type_test;
 	//open_screen_type = screen_type_init;
 	//open_screen_type = screen_type_level;
-	open_screen_type = screen_type_intro;
+	//open_screen_type = screen_type_intro;
 	//open_screen_type = screen_type_play;
 	//open_screen_type = screen_type_load;
 	//open_screen_type = screen_type_ready;
@@ -89,9 +89,9 @@ static void start()
 //	open_screen_type = screen_type_test;
 
 	//open_screen_type = screen_type_pass;
-	open_screen_type = screen_type_begin;
+	//open_screen_type = screen_type_begin;
 	//open_screen_type = screen_type_over;
-	open_screen_type = screen_type_cont;
+	//open_screen_type = screen_type_cont;
 	
 	//open_screen_type = screen_type_func;
 	//open_screen_type = screen_type_start;
