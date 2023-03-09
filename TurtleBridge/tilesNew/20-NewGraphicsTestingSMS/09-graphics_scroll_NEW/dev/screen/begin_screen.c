439eefa404e5c8d7bf7eff7de33377a38164b123
StevePro7 <steveprozune@hotmail.com>
2023-03-09 14:30:34 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/begin_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/begin_screen.c
@@ -1,22 +1,29 @@
 #include "begin_screen.h"
 #include "../engine/asm_manager.h"
-#include "../engine/audio_manager.h"
 #include "../engine/content_manager.h"
 #include "../engine/debug_manager.h"
 #include "../engine/enum_manager.h"
-#include "../engine/font_manager.h"
-
+//#include "../engine/font_manager.h"
+#include "../engine/graphics_manager.h"
+#include "../devkit/_sms_manager.h"
 
 void screen_begin_screen_load()
 {
-	engine_font_manager_text( "BEGIN SCREEN!!", 10, 2 );
+	//engine_graphics_manager_common();
+	devkit_SMS_displayOff();
+	engine_asm_manager_clear_VRAM();
+	engine_content_manager_bggame();
+	engine_content_manager_sprite();
+	engine_graphics_manager_title();
+	engine_graphics_manager_sea();
+	devkit_SMS_displayOn();
 
-	
+	// TODO delete this.
+	engine_debug_manager_initgame();
 }
 
 void screen_begin_screen_update( unsigned char *screen_type )
 {
-
-
-	*screen_type = screen_type_begin;
+	*screen_type = screen_type_start;
+	//*screen_type = screen_type_begin;
 }
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/start_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/start_screen.c
@@ -33,13 +33,14 @@ static bool flag;
 void screen_start_screen_load()
 {
 	//engine_graphics_manager_common();
-	devkit_SMS_displayOff();
-	engine_asm_manager_clear_VRAM();
-	engine_content_manager_bggame();
-	engine_content_manager_sprite();
-	engine_graphics_manager_title();
-	engine_graphics_manager_sea();
-	devkit_SMS_displayOn();
+	//devkit_SMS_displayOff();
+	//engine_asm_manager_clear_VRAM();
+	//engine_content_manager_bggame();
+	//engine_content_manager_sprite();
+	//engine_graphics_manager_title();
+	//engine_graphics_manager_sea();
+	//devkit_SMS_displayOn();
+
 	devkit_SMS_setSpriteMode( devkit_SPRITEMODE_NORMAL() );
 
 	engine_util_manager_locale_texts( 4, 9, 7 );
