21d2dead7e22a9927f945aaf56a920e0eefdcac9
StevePro7 <steveprozune@hotmail.com>
2023-03-09 20:58:43 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/beat_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/beat_screen.c
@@ -1,12 +1,9 @@
 #include "beat_screen.h"
-#include "../engine/cartoon_manager.h"
-#include "../engine/command_manager.h"
-#include "../engine/debug_manager.h"
 #include "../engine/enum_manager.h"
 #include "../engine/font_manager.h"
-#include "../engine/graphics_manager.h"
+#include "../engine/global_manager.h"
 #include "../engine/input_manager.h"
-#include "../engine/player_manager.h"
+#include "../engine/util_manager.h"
 
 #ifdef _CONSOLE
 #else
@@ -16,13 +13,20 @@
 void screen_beat_screen_load()
 {
 	engine_font_manager_text( "BEAT SCREEN!!", 10, 0 );
-
-	
 }
 
 void screen_beat_screen_update( unsigned char *screen_type )
 {
-	
+	unsigned char input;
 
+	unsigned char value;
+	input = engine_input_manager_hold( input_type_right );
+	if( input )
+	{
+		value = engine_random_manager_next( SPRITE_TILES );
+		engine_font_manager_data( value, 10, 10 );
+	}
+
+	engine_random_manager_rand();
 	*screen_type = screen_type_beat;
 }
