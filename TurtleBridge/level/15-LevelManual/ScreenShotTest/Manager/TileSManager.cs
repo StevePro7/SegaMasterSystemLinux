07a0db092ce0cdf67032af6a31c2f6981775ab4e
StevePro7 <steveprozune@hotmail.com>
2023-03-11 21:22:32 +0000

    no message

--- TurtleBridge/level/15-LevelManual/ScreenShotTest/Manager/TileSManager.cs
+++ TurtleBridge/level/15-LevelManual/ScreenShotTest/Manager/TileSManager.cs
@@ -9,37 +9,32 @@ namespace ScreenShotTest
 {
 	public class TilesManager
 	{
+		private List<int> lines;
+
 		public TilesManager()
 		{
 			int cols = 16;
 			Tiles = new int[cols];
+			lines = new List<int>();
 		}
 
 		public void Initialize()
 		{
+			// https://coderwall.com/p/app3ya/read-excel-file-in-c
+			string path = @"D:\GitHub\StevePro8\SegaMasterSystemLinux\TurtleBridge\level\15-LevelManual\ScreenShotTest\MyTiles.xls";
 			Excel.Application xlApp = new Excel.Application();
-			Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"D:\GitHub\StevePro8\SegaMasterSystemLinux\TurtleBridge\level\15-LevelManual\ScreenShotTest\bin\Windows\x86\Debug\MyTiles.xls");
+			Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(path);
 			Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
 			Excel.Range xlRange = xlWorksheet.UsedRange;
 
-			var r1 = xlRange.Cells.Rows;
-			int b = r1.Count;
-			var r2 = xlRange.Cells.Columns;
-			int rowCount = 3;
-			int colCount = 2;
-			for (int i = 1; i <= rowCount; i++)
+			int rowCount = xlRange.Cells.Rows.Count;
+			int colCount = xlRange.Cells.Columns.Count;
+			int tileStart = 0;
+			for (int row = 1; row <= rowCount; row++)
 			{
-				for (int j = 1; j <= colCount; j++)
-				{
-					if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-					{
-						string text = xlRange.Cells[i, j].Value2.ToString();
-						Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-					}
-						
+				var type = xlRange.Cells[row, 1].Value2.ToString();
+				var valu = Convert.ToInt32(xlRange.Cells[row, 2].Value2.ToString());
 
-					//add useful things here!   
-				}
 			}
 		}
 
