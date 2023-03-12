3319d5ff85a20ebbc69d1e04cd9e43d0ca1a5e95
StevePro7 <steveprozune@hotmail.com>
2023-03-12 17:51:51 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/debug_manager.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/debug_manager.c
@@ -17,7 +17,7 @@ void engine_debug_manager_initgame()
 	go->game_difficulty = difficulty_type_easier;
 	//go->game_difficulty = difficulty_type_normal;
 	//go->game_difficulty = difficulty_type_harder;
-	//go->game_difficulty = difficulty_type_insane;
+	go->game_difficulty = difficulty_type_insane;
 
 	go->game_world = 5;	// TODO - currently not aligned with game_level but used for testing...
 	go->game_point = 4;
