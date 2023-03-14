86e731680133f894340033bca622996c68f32e4d
StevePro7 <steveprozune@hotmail.com>
2023-03-14 21:53:49 +0000

    refactor levels

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/debug_manager.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/engine/debug_manager.c
@@ -28,15 +28,15 @@ void engine_debug_manager_initgame()
 	go->game_round = 0;
 	// 9 = turtles
 	// 3 = long512
-	go->game_level = 2;
-	go->game_level = 9;
+	go->game_level = 0;
+	//go->game_level = 9;
 	//go->game_level = 0;
 	//go->game_level = 1;
 	//go->game_level = 3;
 	//go->game_level = 9;
 	//go->game_level = 17;// 64x screens
 	//go->game_level = 20;	// testing large jumps
-	go->game_level = 24;	// bank8
+	//go->game_level = 24;	// bank8
 	go->game_level = 0;
 
 	//go->game_level = 3;	// trees
