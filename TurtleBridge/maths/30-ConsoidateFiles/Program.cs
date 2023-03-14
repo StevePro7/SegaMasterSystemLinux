4de8a850a2f83909e1d86052da53f327d66a0bec
StevePro7 <steveprozune@hotmail.com>
2023-03-14 22:29:56 +0000

    no message

---
+++ TurtleBridge/maths/30-ConsoidateFiles/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FunctionMgr
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fm = new FileManager();
+			fm.Process("level_0104");
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
