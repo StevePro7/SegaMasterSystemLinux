708e3c07b211be85875c80ee1ec78c57e50c34d6
StevePro7 <steveprozune@hotmail.com>
2023-03-15 22:40:51 +0000

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/begin_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/begin_screen.c
@@ -42,10 +42,10 @@ void screen_begin_screen_update( unsigned char *screen_type )
 	//*screen_type = screen_type_init;
 	//*screen_type = screen_type_intro;
 	//*screen_type = screen_type_level;
-	*screen_type = screen_type_option;
+	//*screen_type = screen_type_option;
 	//*screen_type = screen_type_diff;
 	//*screen_type = screen_type_beat;
-	//*screen_type = screen_type_demo;
+	*screen_type = screen_type_func;
 	//*screen_type = screen_type_start;
 	//*screen_type = screen_type_record;
 	//*screen_type = screen_type_repeat;
