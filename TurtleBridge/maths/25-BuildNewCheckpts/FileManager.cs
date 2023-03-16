e5d3e34825b651ad77081b61636eca1e47d8ce62
StevePro7 <steveprozune@hotmail.com>
2023-03-16 21:41:45 +0000

    no message

--- TurtleBridge/maths/25-BuildNewCheckpts/FileManager.cs
+++ TurtleBridge/maths/25-BuildNewCheckpts/FileManager.cs
@@ -26,16 +26,28 @@ namespace ExtractCheckpoints
 
 		public void Process()
 		{
-			var line = "AA-bridge01,BB-bridge03,CC-bridge01,DD-bridge01";
-			var info = line.Split(new char[] { ',' });
-			string a = info[0] + ".csv";
-			string b = info[1] + ".csv";
-			string c = info[2] + ".csv";
-			string d = info[3] + ".csv";
-			Build(a, b, c, d);
+			int world = 1;
+			int round = 1;
+			var lines = GetLines();
+			foreach (var line in lines)
+			{
+				//var line = "AA-bridge01,BB-bridge03,CC-bridge01,DD-bridge01";
+				var info = line.Split(new char[] { ',' });
+				string a = info[0] + ".csv";
+				string b = info[1] + ".csv";
+				string c = info[2] + ".csv";
+				string d = info[3] + ".csv";
+				Build(a, b, c, d, world, round);
+				round++;
+				if (round > 8)
+				{
+					round = 1;
+					world++;
+				}
+			}
 		}
 
-		private void Build(string a, string b, string c, string d)
+		private void Build(string a, string b, string c, string d, int w, int r)
 		{
 			output.Clear();
 			var fa = File.ReadAllLines("input/" + a);
@@ -56,9 +68,50 @@ namespace ExtractCheckpoints
 			string nc = c.Replace(".csv", "");
 			string nd = d.Replace(".csv", "");
 
-			string outFile = String.Format("{0}_{1}_{2}_{3}.csv", na, nb, nc, nd);
+			string wd = w.ToString().PadLeft(2, '0');
+			string rd = r.ToString().PadLeft(2, '0');
+			string outFile = String.Format("level_{0}{1}_{2}_{3}_{4}_{5}.csv", wd, rd, na, nb, nc, nd);
 			var contents = output.ToArray();
 			File.WriteAllLines("output/" + outFile, contents);
+		}
+
+		private string[] GetLines()
+		{
+			return new string[]
+			{
+				"AA-bridge01,BB-bridge03,CC-bridge01,DD-bridge01",
+				"AA-island03,BB-island03,CC-island01,DD-island01",
+				"AA-bridge02,BB-island01,CC-bridge01,DD-island01",
+				"AA-mixerd01,BB-bridge02,CC-island01,DD-bridge01",
+				"AA-island01,BB-island03,CC-mixerd02,DD-bridge01",
+				"AA-island02,BB-mixerd01,CC-bridge01,DD-island01",
+				"AA-bridge03,BB-bridge01,CC-island01,DD-island01",
+				"AA-bridge02,BB-island02,CC-bridge01,DD-mixerd02",
+				"AA-mixerd01,BB-bridge02,CC-bridge01,DD-island01",
+				"AA-bridge01,BB-mixerd02,CC-bridge01,DD-island01",
+				"AA-mixerd02,BB-bridge03,CC-island01,DD-bridge01",
+				"AA-island03,BB-island03,CC-island01,DD-bridge01",
+				"AA-bridge02,BB-mixerd01,CC-bridge01,DD-island01",
+				"AA-island01,BB-bridge02,CC-mixerd01,DD-island01",
+				"AA-bridge02,BB-island02,CC-island01,DD-mixerd02",
+				"AA-island02,BB-bridge03,CC-island01,DD-bridge01",
+				"AA-island01,BB-island03,CC-mixerd01,DD-bridge01",
+				"AA-bridge01,BB-mixerd01,CC-mixerd02,DD-island01",
+				"AA-mixerd01,BB-island02,CC-bridge01,DD-mixerd01",
+				"AA-island03,BB-bridge03,CC-bridge01,DD-mixerd02",
+				"AA-bridge02,BB-bridge02,CC-mixerd02,DD-bridge01",
+				"AA-island02,BB-mixerd02,CC-bridge01,DD-island01",
+				"AA-dropsd01,BB-bridge03,CC-bridge01,DD-bridge01",
+				"AA-bridge01,BB-dropsd02,CC-island01,DD-mixerd02",
+				"AA-dropsd02,BB-mixerd01,CC-bridge01,DD-island01",
+				"AA-island01,BB-dropsd01,CC-island01,DD-dropsd02",
+				"AA-dropsd01,BB-bridge02,CC-mixerd02,DD-mixerd02",
+				"AA-mixerd02,BB-mixerd01,CC-island01,DD-bridge01",
+				"AA-mixerd01,BB-bridge03,CC-dropsd01,DD-island01",
+				"AA-bridge01,BB-bridge02,CC-bridge01,DD-dropsd01",
+				"AA-island02,BB-island02,CC-dropsd01,DD-island01",
+				"AA-bridge02,BB-dropsd01,CC-mixerd01,DD-bridge01",
+			};
 		}
 	}
 }
