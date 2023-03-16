261fe7a719c60303d097b20bbe95a6206c9de959
StevePro7 <steveprozune@hotmail.com>
2023-03-16 15:26:07 +0000

    no message

--- TurtleBridge/maths/24-ExtractCheckpoints/Program.cs
+++ TurtleBridge/maths/24-ExtractCheckpoints/Program.cs
@@ -11,8 +11,8 @@ namespace ExtractCheckpoints
 			var fm = new FileManager();
 			fm.Initialize();
 			//fm.Process("Atiles_0102_01");
-			fm.Process("Btiles_0102_01");
-			//fm.Process("tiles_0103_01");
+			//fm.Process("Btiles_0102_01");
+			fm.Process("tiles_0103_01");
 			//fm.Process("tiles_0104_01");
 
 			//Console.WriteLine("Processed \"" + cm.Directory + "\"");
