2152d07b169f6c8ac6b586c8aea095e6bd0a49a7
StevePro7 <steveprozune@hotmail.com>
2023-03-21 21:00:15 +0000

    no message

---
+++ TurtleBridge/maths/32-ManualEditor/FileManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoidateFiles
+{
+	public class FileManager
+	{
+		private List<string> output;
+
+		public FileManager()
+		{
+			output = new List<string>();
+		}
+
+		public void Initialize()
+		{
+			if (!Directory.Exists("output"))
+			{
+				Directory.CreateDirectory("output");
+			}
+		}
+
+		public void Process(int screen)
+		{
+			output.Clear();
+			var inpFile = File.ReadAllLines("input/file.csv");
+			var outFile = screen.ToString().PadLeft(2, '0');
+			var outFileA = outFile + "A.csv";
+			var outFileB = outFile + "B.csv";
+			File.WriteAllLines("output/" + outFileA, inpFile.ToArray());
+			File.WriteAllLines("output/" + outFileB, inpFile.ToArray());
+			//File.WriteAllLines(path, contents);
+		}
+	}
+}
---
+++ TurtleBridge/maths/32-ManualEditor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoidateFiles
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fm = new FileManager();
+			fm.Initialize();
+			fm.Process(2);
+
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
