aef543bf506a899c9ec3e6380a385951bf0bb41f
StevePro7 <steveprozune@hotmail.com>
2023-03-22 09:04:18 +0000

    no message

--- TurtleBridge/maths/34-NewCheckpointEx/FileManager.cs
+++ TurtleBridge/maths/34-NewCheckpointEx/FileManager.cs
@@ -10,10 +10,22 @@ namespace ExtractCheckpoints
 		private const string root2 = @"E:/GitHub/StevePro8/SegaMasterSystemLinux/TurtleBridge/level/levels/lego/10-points_output/";
 
 		private List<string> output;
+		private List<string>[] points;
+		//private List<string> point2;
+		//private List<string> point3;
+		//private List<string> point4;
 
 		public FileManager()
 		{
 			output = new List<string>();
+			points = new List<string>[4];
+			for (int i = 0; i < 4; i++)
+			{
+				points[i] = new List<string>();
+			}
+			//point2 = new List<string>();
+			//point3 = new List<string>();
+			//point4 = new List<string>();
 		}
 
 		public void Initialize()
@@ -27,6 +39,78 @@ namespace ExtractCheckpoints
 				var j = i + 1;
 				var k = j.ToString().PadLeft(2, '0');
 				Directory.CreateDirectory("output/" + k);
+			}
+		}
+
+		public void Process1()
+		{
+			var files = Directory.GetFiles(root1);
+			var file = files[0];
+			var text = file.Replace(root1, String.Empty);
+			var name = text.Substring(0, 10);
+			var dirX = name.Substring(6, 2);
+			Build(file, dirX, name);
+		}
+
+		public void Build(string file, string dirX, string name)
+		{
+			int dist = 7;
+			if ("03" == dirX || "04" == dirX || "05" == dirX)
+			{
+				dist = 15;
+			}
+
+			var outDir = String.Format("output/{0}/{1}", dirX, name);
+			for (int i = 0; i<4;i++)
+			{
+				points[i].Clear();
+				var j = (i + 1).ToString().PadLeft(2, '0');
+				var subDir = String.Format("{0}_{1}", name, j);
+				if (!Directory.Exists(outDir + "/" + subDir))
+				{
+					Directory.CreateDirectory(outDir + "/" + subDir);
+				}
+			}
+
+			//point2.Clear();
+			//point3.Clear();
+			//point4.Clear();
+			
+			var linesX = File.ReadAllLines(file);
+			int looper = 0;
+			foreach (var line in linesX)
+			{
+				if (line.StartsWith("// AA"))
+				{
+					looper = 0;
+				}
+				else if (line.StartsWith("// BB"))
+				{
+					looper = 1;
+				}
+				else if (line.StartsWith("// CC"))
+				{
+					looper = 2;
+				}
+				else if (line.StartsWith("// DD"))
+				{
+					looper = 3;
+				}
+
+				string text = line;
+				if ("WavesBlock,8" == text)
+				{
+					text = "WavesBlock," + dist * 8;
+				}
+				points[looper].Add(text);
+			}
+
+			for (int i = 0; i < 4; i++)
+			{
+				var j = (i + 1).ToString().PadLeft(2, '0');
+				var subDir = String.Format("{0}_{1}", name, j);
+				var path = String.Format("{0}/{1}/{1}.csv", outDir, subDir);
+				File.WriteAllLines(path, points[i]);
 			}
 		}
 
--- TurtleBridge/maths/34-NewCheckpointEx/Program.cs
+++ TurtleBridge/maths/34-NewCheckpointEx/Program.cs
@@ -9,12 +9,17 @@ namespace ExtractCheckpoints
 			var fm = new FileManager();
 			//fm.Initialize();
 
+			fm.Process1();
+			//fm.Process2("02");
+			//fm.Process2("03");
+			//fm.Process2("04");
+			//fm.Process2("05");
+
 			//fm.Process2("01");
-			fm.Process2("02");
-			fm.Process2("03");
-			fm.Process2("04");
-			fm.Process2("05");
-			//fm.Build("02", "level_0201_AA-mixerd01_BB-bridge02_CC-bridge01_DD-island01");
+			//fm.Process2("02");
+			//fm.Process2("03");
+			//fm.Process2("04");
+			//fm.Process2("05");
 
 			//Console.WriteLine("Processed \"" + cm.Directory + "\"");
 			Console.WriteLine("Press [ RETURN ]");
