6272532752b6bba9b1e7ed0cb8c200274506b52e
StevePro7 <steveprozune@hotmail.com>
2023-03-22 09:10:10 +0000

    no message

---
+++ TurtleBridge/level/levels/lego/10-points_output/05_old/level_0503_AA-dropsd02_BB-mixerd02_CC-dropsd01_DD-bridge01/level_0503_AA-dropsd02_BB-mixerd02_CC-dropsd01_DD-bridge01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_0503_txt[];
+#define				level_0503_txt_size 256
+#define				level_0503_txt_bank 11
---
+++ TurtleBridge/level/levels/lego/10-points_output/05_old/level_0506_AA-bridge03_BB-island01_CC-mixerd02_DD-dropsd01/level_0506_AA-bridge03_BB-island01_CC-mixerd02_DD-dropsd01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_0506_txt[];
+#define				level_0506_txt_size 256
+#define				level_0506_txt_bank 11
