35a281b2348670940e4be9270478ef0f2a81bc44
StevePro7 <steveprozune@hotmail.com>
2023-03-22 08:27:57 +0000

    no message

---
+++ TurtleBridge/maths/34-NewCheckpointEx/FileManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExtractCheckpoints
+{
+	public class FileManager
+	{
+		private const string root = @"E:/GitHub/StevePro8/SegaMasterSystemLinux/TurtleBridge/level/levels/lego/10-points_output/";
+
+		private List<string> output;
+
+		public FileManager()
+		{
+			output = new List<string>();
+		}
+
+		public void Initialize()
+		{
+			if (!Directory.Exists("output"))
+			{
+				Directory.CreateDirectory("output");
+			}
+			for (int i = 0; i < 5; i++)
+			{
+				var j = i + 1;
+				var k = j.ToString().PadLeft(2, '0');
+				Directory.CreateDirectory("output/" + k);
+			}
+		}
+
+		public void Process(string dirX)
+		{
+			string path = String.Format("{0}{1}/", root, dirX);
+			var dirs = Directory.GetDirectories(path);
+			var dirn = dirs[0];
+			var dirt = dirn.Replace(path, String.Empty);
+			var name = dirt.Substring(0, 10);
+			Build(dirX, dirn, dirt, name);
+		}
+
+		public void Build(string dirX, string dirn, string dirt, string name)
+		{
+			var outDir = String.Format("output/{0}/{1}", dirX, name);
+			if (!Directory.Exists(outDir))
+			{
+				Directory.CreateDirectory(outDir);
+			}
+			var pngFile = Directory.GetFiles(dirn, "*.png");
+			var pngSave = String.Format("{0}/{1}.png", outDir, name);
+			File.Copy(pngFile[0], pngSave);
+
+			string path = String.Format("{0}{1}/{2}", root, dirn, name);
+			var inpFile = File.ReadAllLines(path);
+		}
+	}
+}
---
+++ TurtleBridge/maths/34-NewCheckpointEx/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ExtractCheckpoints
+{
+	class Program
+	{
+		static void Main()
+		{
+			var fm = new FileManager();
+			//fm.Initialize();
+
+			fm.Process("01");
+			//fm.Build("02", "level_0201_AA-mixerd01_BB-bridge02_CC-bridge01_DD-island01");
+
+			//Console.WriteLine("Processed \"" + cm.Directory + "\"");
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
