d70a04db4b320f75bf49eb9e509426ab0eff2b22
StevePro7 <steveprozune@hotmail.com>
2023-03-22 19:21:01 +0000

    no message

--- TurtleBridge/level/levels/legoIII/manual01/level_5011_01.h
+++
@@ -1,3 +0,0 @@
-extern const unsigned char level_5011_txt[];
-#define				level_5011_txt_size 512
-#define				level_5011_txt_bank 12
---
+++ TurtleBridge/level/levels/legoIII/manual_bridge/level_9901/level_9901_01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_9901_txt[];
+#define				level_9901_txt_size 256
+#define				level_9901_txt_bank 12
