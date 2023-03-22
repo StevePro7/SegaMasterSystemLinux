0c4e78363cd3849f07ab3a79e2dbdca275e9da59
StevePro7 <steveprozune@hotmail.com>
2023-03-22 18:30:34 +0000

    no message

---
+++ TurtleBridge/level/levels/legoIII/manual_trees/level_2004/level_2002_01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_2002[];
+#define				level_2002_size 384
+#define				level_2002_bank 12
---
+++ TurtleBridge/level/levels/legoIII/manual_trees/level_2017/level_2017_01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_2017[];
+#define				level_2017_size 384
+#define				level_2017_bank 12
