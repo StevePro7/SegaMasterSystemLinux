43f03736baa3061c1115511eda34ef577c3027ca
StevePro7 <steveprozune@hotmail.com>
2023-03-26 13:40:46 +0100

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/level_screen.c
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/screen/level_screen.c
@@ -213,8 +213,8 @@ void screen_level_screen_update( unsigned char *screen_type )
 		//engine_game_manager_set_level_test( game_level );
 
 		// TODO confirm will not go here unless surrounded by hack flag
-		*screen_type = screen_type_option;		// use for testing!	TODO - remove
-		//*screen_type = screen_type_init;
+		//*screen_type = screen_type_option;		// use for testing!	TODO - remove
+		*screen_type = screen_type_init;
 
 		engine_sound_manager_play( 2 );
 		engine_player_manager_draw();
