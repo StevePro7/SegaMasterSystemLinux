5093d83c9adf5bccba51d957d9f8530976d81213
StevePro7 <steveprozune@hotmail.com>
2023-03-26 14:35:42 +0100

    no message

--- TurtleBridge/level/15-LevelManual/ScreenShotTest/bankX.h
+++ TurtleBridge/level/15-LevelManual/ScreenShotTest/bankX.h
@@ -1,3 +1,3 @@
-extern const unsigned char level_0203_txt[];
-#define				level_0203_txt_size 256
-#define				level_0203_txt_bank 8
+extern const unsigned char level_0401_txt[];
+#define				level_0401_txt_size 512
+#define				level_0401_txt_bank 10
