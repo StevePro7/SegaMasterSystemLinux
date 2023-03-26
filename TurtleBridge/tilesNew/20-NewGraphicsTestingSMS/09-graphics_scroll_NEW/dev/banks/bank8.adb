28bfd858378e95add2a1c31a8eddcc543c46cdbc
StevePro7 <steveprozune@hotmail.com>
2023-03-26 12:01:28 +0100

    no message

--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank8.adb
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank8.adb
@@ -1,5 +1,5 @@
 M:bank8
-S:G$level_0101_txt$0_0$0({256}DA256d,SC:U),D,0,0
+S:G$level_0101_txt$0_0$0({1024}DA1024d,SC:U),D,0,0
 S:G$level_0102_txt$0_0$0({256}DA256d,SC:U),D,0,0
 S:G$level_0103_txt$0_0$0({256}DA256d,SC:U),D,0,0
 S:G$level_0104_txt$0_0$0({256}DA256d,SC:U),D,0,0
--- TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank8.h
+++ TurtleBridge/tilesNew/20-NewGraphicsTestingSMS/09-graphics_scroll_NEW/dev/banks/bank8.h
@@ -1,5 +1,5 @@
 extern const unsigned char level_0101_txt[];
-#define				level_0101_txt_size 256
+#define				level_0101_txt_size 1024
 #define				level_0101_txt_bank 8
 
 extern const unsigned char level_0102_txt[];
