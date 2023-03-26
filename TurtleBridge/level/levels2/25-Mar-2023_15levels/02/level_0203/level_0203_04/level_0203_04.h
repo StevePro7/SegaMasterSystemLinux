15f6c69b7d21c30a25a1f247045e3b993523eae1
StevePro7 <steveprozune@hotmail.com>
2023-03-26 11:43:44 +0100

    no message

---
+++ TurtleBridge/level/levels2/25-Mar-2023_15levels/02/level_0203/level_0203_04/level_0203_04.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_0203_txt[];
+#define				level_0203_txt_size 256
+#define				level_0203_txt_bank 8
---
+++ TurtleBridge/level/levels2/25-Mar-2023_15levels/03/level_0303/level_0303_01/level_0303_01.h
@@ -0,0 +1,3 @@
+extern const unsigned char level_0303_txt[];
+#define				level_0303_txt_size 512
+#define				level_0303_txt_bank 9
