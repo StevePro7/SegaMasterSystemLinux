71a4f6056ea3d13296d72a23a6460fafba9e327c
StevePro7 <steveprozune@hotmail.com>
2023-03-28 21:02:53 +0100

    no message

---
+++ TurtleBridge/maths/31-ConsoidateFilesRoot/FileManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoidateFiles
+{
+	public class FileManager
+	{
+		private List<string> output;
+
+		public FileManager()
+		{
+			output = new List<string>();
+		}
+
+		public void Initialize()
+		{
+			if (!Directory.Exists("output"))
+			{
+				Directory.CreateDirectory("output");
+			}
+		}
+
+		public void Process(string directory)
+		{
+			output.Clear();
+			output.Add("const unsigned char " + directory + "_txt[] =");
+			output.Add("{");
+
+			var searchPattern = directory + "*.c";
+			var files = Directory.GetFiles("input/", searchPattern);
+			foreach (var file in files)
+			{
+				var lines = File.ReadAllLines(file);
+				foreach (var line in lines)
+				{
+					if (line.StartsWith("const") || line.StartsWith("{") || line.StartsWith("}"))
+					{
+						continue;
+					}
+
+					output.Add(line);
+				}
+			}
+
+			output.Add("};");
+
+			var contents = output.ToArray();
+			var path = String.Format("output/{0}_NEW.c", directory);
+			File.WriteAllLines(path, contents);
+		}
+	}
+}
---
+++ TurtleBridge/maths/31-ConsoidateFilesRoot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoidateFiles
+{
+	class Program
+	{
+		static void Main()
+		{
+			var cm = new ConfigManager();
+			cm.Initialize();
+			var fm = new FileManager();
+			fm.Initialize();
+			fm.Process(cm.Directory);
+
+			Console.WriteLine("Processed \"" + cm.Directory + "\"");
+			Console.WriteLine("Press [ RETURN ]");
+			Console.Read();
+		}
+
+	}
+}
