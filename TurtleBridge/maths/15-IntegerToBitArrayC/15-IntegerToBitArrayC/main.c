8bd94a9c10527c59937af2c80b0dd9a22e18584f
StevePro7 <steveprozune@hotmail.com>
2023-03-31 23:38:15 +0100

    func to func

--- TurtleBridge/maths/15-IntegerToBitArrayC/15-IntegerToBitArrayC/main.c
+++ TurtleBridge/maths/15-IntegerToBitArrayC/15-IntegerToBitArrayC/main.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
+// https://stackoverflow.com/questions/9410/how-do-you-pass-a-function-as-a-parameter-in-c
 
-// https://stackoverflow.com/questions/8011700/how-do-i-extract-specific-n-bits-of-a-32-bit-unsigned-integer-in-c
-void main()
+
+void foo();
+void bar();
+void execute( void( *f )( ) );
+
+int main()
 {
-	unsigned char value;
-	unsigned char mask;
-	unsigned char X;
-	unsigned char startBit;
-	//unsigned char lastXbits;
-	unsigned char isolatedXbits;
-
-	value = 255;
-	X = 3;
-	startBit = 2;
-	mask = ( ( 1 << X ) - 1 ) << startBit;
-	isolatedXbits = value & mask;
-
-	printf( "val	%d\n", value );
-	printf( "start	%d\n", startBit );
-	printf( "X	%d\n", X );
-	
-	printf( "res	%d\n", isolatedXbits );
-	printf( "\n" );
-	return;
+	execute( foo );
+	execute( bar );
+	return 0;
 }
+
+void foo()
+{
+	printf( "foo\n" );
+}
+
+void bar()
+{
+	printf( "bar\n" );
+}
+
+void execute( void( *f )( ) ) // receive address of print
+{
+	f();
+}
+//
+//// https://stackoverflow.com/questions/8011700/how-do-i-extract-specific-n-bits-of-a-32-bit-unsigned-integer-in-c
+//void main()
+//{
+//	unsigned char value;
+//	unsigned char mask;
+//	unsigned char X;
+//	unsigned char startBit;
+//	//unsigned char lastXbits;
+//	unsigned char isolatedXbits;
+//
+//	value = 255;
+//	X = 3;
+//	startBit = 2;
+//	mask = ( ( 1 << X ) - 1 ) << startBit;
+//	isolatedXbits = value & mask;
+//
+//	printf( "val	%d\n", value );
+//	printf( "start	%d\n", startBit );
+//	printf( "X	%d\n", X );
+//	
+//	printf( "res	%d\n", isolatedXbits );
+//	printf( "\n" );
+//	return;
+//}
