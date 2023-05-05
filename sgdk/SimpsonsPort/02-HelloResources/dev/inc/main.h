f3c7275553628cf4efa5161b05d6d4bef93dc36a
StevePro7 <steveprozune@hotmail.com>
2023-05-05 17:00:40 +0100

    no message

--- sgdk/SimpsonsPort/02-HelloResources/dev/inc/main.h
+++ sgdk/SimpsonsPort/02-HelloResources/dev/inc/main.h
@@ -7,4 +7,12 @@
 #include <genesis.h>
 #endif
 
+#include "gfx_actors.h"
+#include "gfx_sprites.h"
+#include "gfx_tiles.h"
+
+#include "sfx_effects.h"
+#include "sfx_musics.h"
+#include "sfx_results.h"
+
 #endif//__MAIN__
