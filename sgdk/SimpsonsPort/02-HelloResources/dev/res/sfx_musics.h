ea06b0e25c1594cbf03eb85bd662ac35a7e2224f
StevePro7 <steveprozune@hotmail.com>
2023-05-05 16:48:30 +0100

    no message

--- sgdk/SimpsonsPort/02-HelloResources/dev/res/sfx_musics.h
+++ sgdk/SimpsonsPort/02-HelloResources/dev/res/sfx_musics.h
@@ -1,12 +1,6 @@
 #ifndef _RES_SFX_MUSICS_H_
 #define _RES_SFX_MUSICS_H_
 
-#ifdef _CONSOLE
-#include "_genesis.h"
-#else
-#include <genesis.h>
-#endif
-
 extern const u8 sfx_gameover[34816];
 extern const u8 sfx_titlemusic[977152];
 
