665762bd54acaa4b3783a432e73beddb85c75e25
StevePro7 <steveprozune@hotmail.com>
2023-05-12 17:21:57 +0100

    no message

--- sgdk/SkazkaPort/04-ConvertAPIs/dev/inc/main.h
+++ sgdk/SkazkaPort/04-ConvertAPIs/dev/inc/main.h
@@ -7,4 +7,15 @@
 #include <genesis.h>
 #endif
 
+#include "audio_manager.h"
+#include "enemy_manager.h"
+#include "enum_manager.h"
+#include "game_manager.h"
+#include "hack_manager.h"
+#include "graphics_manager.h"
+#include "input_manager.h"
+#include "player_manager.h"
+#include "screen_manager.h"
+#include "select_manager.h"
+
 #endif//__MAIN__
--- sgdk/SkazkaPort/04-ConvertAPIs/dev/src/main.c
+++ sgdk/SkazkaPort/04-ConvertAPIs/dev/src/main.c
@@ -2,9 +2,32 @@
 
 int main()
 {
-	VDP_drawText( "Hello Genny Folders!", 10, 13 );
+	unsigned char open_screen_type;
+
+	open_screen_type = screen_type_splash;
+	//open_screen_type = screen_type_title;
+
+	// Initialize player first!
+	engine_player_manager_init();
+	engine_hack_manager_init();
+	engine_hack_manager_load();
+	engine_hack_manager_invert();
+
+	engine_enemy_manager_init();
+	engine_game_manager_init();
+	engine_audio_manager_init();
+
+	engine_graphics_manager_init();
+	engine_graphics_manager_load();
+	engine_graphics_manager_clear_full();
+
+	engine_select_manager_init();
+	engine_screen_manager_init( open_screen_type );
 	while( 1 )
 	{
+		engine_input_manager_update();
+		engine_screen_manager_update();
+
 		VDP_waitVSync();
 	}
 
