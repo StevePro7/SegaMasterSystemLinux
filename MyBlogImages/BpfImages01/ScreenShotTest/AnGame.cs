3e181745cbc23dc49b15a963a888d0ded8aca621
StevePro7 <steveprozune@hotmail.com>
2023-06-01 21:30:31 +0100

    no message

--- MyBlogImages/BpfImages01/ScreenShotTest/AnGame.cs
+++ MyBlogImages/BpfImages01/ScreenShotTest/AnGame.cs
@@ -1,92 +1,57 @@
-﻿using System;
-using System.Configuration;
-using System.IO;
+﻿using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace ScreenShotTest
 {
-	/// <summary>
-	/// This is the main type for your game
-	/// </summary>
 	public class AnGame : Microsoft.Xna.Framework.Game
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		RenderTarget2D renderTarget;
-		private Texture2D[] images;
-		private const int offset = 0;
-		private const int max = 3;
-		private const int val = 0;
-		private string[] names = new string[max] { "cursor01", "cursor02", "cursor03" };
 
-		private bool save;
-
-		private int width;
-		private int height;
+		private ConfigManager configManager;
+		private ImageManager ImageManager;
+		private string name;
+		private int wide;
+		private int high;
 
 		public AnGame()
 		{
 			graphics = new GraphicsDeviceManager(this);
-			graphics.PreferredBackBufferWidth = 48;
-			graphics.PreferredBackBufferHeight = 24;
 			Content.RootDirectory = "Content";
+
+			configManager = new ConfigManager();
+			ImageManager = new ImageManager();
 		}
 
-		/// <summary>
-		/// Allows the game to perform any initialization it needs to before starting to run.
-		/// This is where it can query for any required services and load any non-graphic
-		/// related content.  Calling base.Initialize will enumerate through any components
-		/// and initialize them as well.
-		/// </summary>
 		protected override void Initialize()
 		{
-			save = false;
-			//if (null != ConfigurationManager.AppSettings["save"])
-			//{
-			//	save = Convert.ToBoolean(ConfigurationManager.AppSettings["save"]);
-			//}
-			save = true;
+			configManager.Init();
+			name = configManager.Name;
 			IsMouseVisible = true;
 			base.Initialize();
 		}
 
-		/// <summary>
-		/// LoadContent will be called once per game and is the place to load
-		/// all of your content.
-		/// </summary>
 		protected override void LoadContent()
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
-			images = new Texture2D[max];
-			for (int idx = 0; idx < max; idx++)
-			{
-				images[idx] = Content.Load<Texture2D>(names[idx]);
-			}
+			ImageManager.Load(Content);
 
 			PresentationParameters pp = GraphicsDevice.PresentationParameters;
-			width = pp.BackBufferWidth;
-			height = pp.BackBufferHeight;
+			wide = pp.BackBufferWidth;
+			high = pp.BackBufferHeight;
 			//renderTarget = new RenderTarget2D(GraphicsDevice, width, height, 1, GraphicsDevice.DisplayMode.Format);
-			renderTarget = new RenderTarget2D(GraphicsDevice, width, height, false, SurfaceFormat.Color, DepthFormat.Depth24);
+			renderTarget = new RenderTarget2D(GraphicsDevice, wide, high, false, SurfaceFormat.Color, DepthFormat.Depth24);
 		}
 
-		/// <summary>
-		/// UnloadContent will be called once per game and is the place to unload
-		/// all content.
-		/// </summary>
 		protected override void UnloadContent()
 		{
 			Content.Unload();
 		}
 
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
 			// Allows the game to exit
@@ -96,13 +61,9 @@ namespace ScreenShotTest
 			base.Update(gameTime);
 		}
 
-		/// <summary>
-		/// This is called when the game should draw itself.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			if (save)
+			if (configManager.Save)
 			{
 				//GraphicsDevice.SetRenderTarget(0, renderTarget);
 				GraphicsDevice.SetRenderTarget(renderTarget);
@@ -111,15 +72,11 @@ namespace ScreenShotTest
 				Draw();
 				base.Draw(gameTime);
 
-				//GraphicsDevice.SetRenderTarget(0, null);
 				GraphicsDevice.SetRenderTarget(null);
-				//Texture2D resolvedTexture = renderTarget.GetTexture();
-				Texture2D resolvedTexture = (Texture2D)renderTarget;
-				//resolvedTexture.Save("00.jpg", ImageFileFormat.Jpg);
-				//Stream stream = File.Create(names[val] + ".png");
-				Stream stream = File.Create("stevepro.png");
-				//resolvedTexture.SaveAsJpeg(stream, width, height);
-				resolvedTexture.SaveAsPng(stream, width, height);
+				Texture2D resolvedTexture = renderTarget;
+
+				Stream stream = File.Create(name + ".png");
+				resolvedTexture.SaveAsPng(stream, wide, high);
 		
 				Exit();
 			}
@@ -134,23 +91,8 @@ namespace ScreenShotTest
 		{
 			graphics.GraphicsDevice.Clear(Color.Black);
 			spriteBatch.Begin();
-			spriteBatch.Draw(images[2], new Vector2(0, 0), Color.OrangeRed);
-			//spriteBatch.Draw(images[4], new Vector2(8, 0), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(16, 0), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(24, 0), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(32, 0), Color.White);
-			//spriteBatch.Draw(images[1], new Vector2(40, 0), Color.White);
-
-			//spriteBatch.Draw(images[5], new Vector2(0, 8), Color.White);
-			//spriteBatch.Draw(images[5], new Vector2(40, 8), Color.White);
-
-			//spriteBatch.Draw(images[2], new Vector2(0, 16), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(8, 16), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(16, 16), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(24, 16), Color.White);
-			//spriteBatch.Draw(images[4], new Vector2(32, 16), Color.White);
-			//spriteBatch.Draw(images[3], new Vector2(40, 16), Color.White);
-
+			ImageManager.Draw(name);
+			//spriteBatch.Draw(images[2], new Vector2(0, 0), Color.OrangeRed);
 			spriteBatch.End();
 		}
 
---
+++ MyBlogImages/BpfImages01/ScreenShotTest/ImageManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace ScreenShotTest
+{
+	public class ImageManager
+	{
+		private const int MAX = 6;
+
+		private string[] names = new string[MAX] 
+		{
+			"HelloWorld01",
+			"HelloWorld02",
+			"HelloWorld03",
+			"HelloWorld04",
+			"HelloMaps01",
+			"HelloMaps02"
+		};
+
+		public void Load(ContentManager content)
+		{
+			Images = new Dictionary<string, Texture2D>();
+			for (int idx = 0; idx < MAX; idx++)
+			{
+				string name = names[idx];
+				Images[name] = content.Load<Texture2D>(names[idx]);
+			}
+		}
+
+		public void Draw(string name)
+		{
+		}
+
+		public IDictionary<string, Texture2D> Images { get; private set; }
+	}
+}
