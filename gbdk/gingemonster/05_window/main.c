53472bef8155dd2a7ca4887e7638d6748b05e0fc
StevePro7 <steveprozune@hotmail.com>
2023-06-27 20:40:58 +0100

    no message

--- gbdk/gingemonster/05_window/main.c
+++ gbdk/gingemonster/05_window/main.c
@@ -5,38 +5,38 @@
 #include "windowmap.h"
 
 void interruptLCD(){
-    HIDE_WIN;
+	HIDE_WIN;
 }
 
 void main(){
-    font_t min_font;
+	font_t min_font;
 
-    STAT_REG = 0x45;
-    LYC_REG = 0x08;  //  Fire LCD Interupt on the 8th scan line (just first row)
+	STAT_REG = 0x45;
+	LYC_REG = 0x08;  //  Fire LCD Interupt on the 8th scan line (just first row)
 
-    disable_interrupts();
+	disable_interrupts();
 
-    font_init();
-    min_font = font_load(font_min); // 36 tile
-    font_set(min_font);
+	font_init();
+	min_font = font_load(font_min); // 36 tile
+	font_set(min_font);
 
-    set_bkg_data(37, 7, backgroundtiles);
-    set_bkg_tiles(0, 0, 40, 18, backgroundmap);
+	set_bkg_data(37, 7, backgroundtiles);
+	set_bkg_tiles(0, 0, 40, 18, backgroundmap);
 
-    set_win_tiles(0,0,5,1,windowmap);
-    move_win(7,0);
+	set_win_tiles(0,0,5,1,windowmap);
+	move_win(7,0);
 
-    SHOW_BKG;
-    SHOW_WIN;
-    DISPLAY_ON;
+	SHOW_BKG;
+	SHOW_WIN;
+	DISPLAY_ON;
 
-    add_LCD(interruptLCD);
-    enable_interrupts();
-    set_interrupts(VBL_IFLAG | LCD_IFLAG);    
+	add_LCD(interruptLCD);
+	enable_interrupts();
+	set_interrupts(VBL_IFLAG | LCD_IFLAG);    
 
-    while(1){
-        SHOW_WIN;
-        scroll_bkg(1,0);
-        wait_vbl_done();
-    }
+	while(1){
+		SHOW_WIN;
+		scroll_bkg(1,0);
+		wait_vbl_done();
+	}
 }
