e01f5caecdb1676e4d3dd3fc7f2e618d1dc11804
StevePro7 <steveprozune@hotmail.com>
2023-07-01 21:42:10 +0100

    no message

--- gbdk/examples/wav_sample/src/samptest.c
+++ gbdk/examples/wav_sample/src/samptest.c
@@ -10,29 +10,30 @@
 void main()
 {
 
-    NR52_REG = 0x80u;
-    NR51_REG = 0xffu;
-    NR50_REG = 0x77u;
+	NR52_REG = 0x80u;
+	NR51_REG = 0xffu;
+	NR50_REG = 0x77u;
 
-    __critical {
-        TMA_REG = 0xC0u, TAC_REG = 0x07u;
-        add_TIM(play_isr);    
-        set_interrupts(VBL_IFLAG | TIM_IFLAG);
-    }
+	__critical{
+		TMA_REG = 0xC0u, TAC_REG = 0x07u;
+		add_TIM( play_isr );
+		set_interrupts( VBL_IFLAG | TIM_IFLAG );
+	}
 
-    puts("PRESS A/B TO PLAY\n");
+	puts( "PRESS A/B TO PLAY\n" );
 
-    while(1)
-    {
-        UINT8 j = joypad();
-        if (j & J_A) {
-            play_sample1();
-            while (joypad() & J_A) wait_vbl_done();
-        } else
-        if (j & J_B) {
-            play_sample2();
-            while (joypad() & J_B) wait_vbl_done();
-        }
-        wait_vbl_done();
-    }
+	while( 1 )
+	{
+		UINT8 j = joypad();
+		if( j & J_A ) {
+			play_sample1();
+			while( joypad() & J_A ) wait_vbl_done();
+		}
+		else
+			if( j & J_B ) {
+				play_sample2();
+				while( joypad() & J_B ) wait_vbl_done();
+			}
+		wait_vbl_done();
+	}
 }
