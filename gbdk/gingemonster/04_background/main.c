2d19deeb630b71ec5fccd857e3c4d2ee27836449
StevePro7 <steveprozune@hotmail.com>
2023-07-01 12:11:41 +0100

    no message

--- gbdk/gingemonster/04_background/main.c
+++ gbdk/gingemonster/04_background/main.c
@@ -3,14 +3,14 @@
 #include "simplebackgroundmap.h"
 
 void main(){
-    set_bkg_data(0, 7, backgroundtiles);
-    set_bkg_tiles(0, 0, 40, 18, backgroundmap);
+	set_bkg_data(0, 7, backgroundtiles);
+	set_bkg_tiles(0, 0, 40, 18, backgroundmap);
 
-    SHOW_BKG;
-    DISPLAY_ON;
+	SHOW_BKG;
+	DISPLAY_ON;
 
-    while(1){
-        scroll_bkg(1,0);
-        delay(100);
-    }
+	while(1){
+		scroll_bkg(1,0);
+		delay(100);
+	}
 }
