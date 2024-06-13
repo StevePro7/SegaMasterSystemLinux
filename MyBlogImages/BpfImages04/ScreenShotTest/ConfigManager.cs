6d611390da12d778c52bd27543afe3b5ea4b92d7
StevePro7 <steveprozune@hotmail.com>
2024-06-13 09:32:04 +0100

    no message

---
+++ MyBlogImages/BpfImages04/ScreenShotTest/ConfigManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace ScreenShotTest
+{
+	public class ConfigManager
+	{
+		public void Init()
+		{
+			Save = Convert.ToBoolean(ConfigurationManager.AppSettings["Save"]);
+			Name = Convert.ToString(ConfigurationManager.AppSettings["Name"]);
+			Left = Convert.ToInt32(ConfigurationManager.AppSettings["Left"]);
+			Rght = Convert.ToInt32(ConfigurationManager.AppSettings["Rght"]);
+			UpXX = Convert.ToInt32(ConfigurationManager.AppSettings["UpXX"]);
+			Down = Convert.ToInt32(ConfigurationManager.AppSettings["Down"]);
+		}
+
+		public bool Save { get; private set; }
+		public string Name { get; private set; }
+		public int Left { get; private set; }
+		public int Rght { get; private set; }
+		public int UpXX { get; private set; }
+		public int Down { get; private set; }
+	}
+}
