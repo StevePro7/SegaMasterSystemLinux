01b5991a9ab0c088248588cd037ce06e4614b341
StevePro7 <steveprozune@hotmail.com>
2024-06-24 13:19:50 +0100

    05

---
+++ MyBlogImages/BpfImages05/ScreenShotTest/ImageManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace ScreenShotTest
+{
+	public class ImageManager
+	{
+		private const int MAX = 3;
+
+		private string[] names = new string[MAX] 
+		{
+			"BlankProject",
+			"CICDvariables",
+			"NewProject",
+		};
+
+		public void Load(ContentManager content)
+		{
+			Images = new Dictionary<string, Texture2D>();
+			for (int idx = 0; idx < MAX; idx++)
+			{
+				string name = names[idx];
+				Images[name] = content.Load<Texture2D>(names[idx]);
+			}
+		}
+
+		public void Draw(string name)
+		{
+		}
+
+		public IDictionary<string, Texture2D> Images { get; private set; }
+	}
+}
