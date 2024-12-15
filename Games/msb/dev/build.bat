5a83637ed0ad910a3b05ae0efc017c0a8043cb11
StevePro7 <steveprozune@hotmail.com>
2024-12-15 20:31:33 +0000

    no message

--- Games/msb/dev/build.bat
+++ Games/msb/dev/build.bat
@@ -36,7 +36,7 @@ echo.
 sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
 ../crt0/crt0_sms.rel game.rel ^
  -Wl-b_BANK2=0x8000 -Wl-b_BANK3=0x8000 -Wl-b_BANK4=0x8000 ^
-../lib/SMSlib.lib ../lib/PSGLib.rel ../lib/GSLib.rel ^
+../libs/SMSlib.lib ../libs/PSGLib.rel ../libs/GSLib.rel ^
 bank2.rel bank3.rel bank4.rel ^
 montylib.rel gamelogic.rel resources.rel spriteengine.rel player.rel soundengine.rel
 
@@ -46,10 +46,6 @@ ihx2sms output.ihx output.sms
 
 
 :: Delete
-cd devkit
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
-cd ..
-
 if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
 if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
 :: if exist "*.noi"  del "*.noi"  > nul
