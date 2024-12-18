32208d0c78887da4b285df921ddc9dfd0d448db9
StevePro7 <steveprozune@hotmail.com>
2024-12-18 20:08:29 +0000

    no message

--- Games/msb/msb04/dev/main.c
+++ Games/msb/msb04/dev/main.c
@@ -7,7 +7,9 @@ void main( void )
 	devkit_SMS_useFirstHalfTilesforSprites( false );
 	devkit_SMS_VDPturnOnFeature( devkit_VDPFEATURE_HIDEFIRSTCOL() );
 
-	devkit_SMS_setSpritePaletteColor( 0, devkit_RGB( 3, 3, 2 ) );
+	devkit_SMS_displayOff();
+	devkit_SMS_setSpritePaletteColor( 0, devkit_RGB( 3, 0, 0 ) );
+	devkit_SMS_displayOn();
 	for( ;; )
 	{
 		devkit_SMS_waitForVBlank();
