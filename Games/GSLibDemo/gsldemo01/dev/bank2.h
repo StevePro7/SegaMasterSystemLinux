1387c0d7b8ef8d1758840847a5de3fc81b2f04e3
StevePro7 <steveprozune@hotmail.com>
2024-12-26 16:59:16 +0000

    no message

--- Games/GSLibDemo/gsldemo01/dev/bank2.h
+++ Games/GSLibDemo/gsldemo01/dev/bank2.h
@@ -1,6 +1,6 @@
-extern const unsigned char	sprite_tiles_bin[6144];
-#define				sprite_tiles_bin_size 6144
-#define				sprite_tiles_bin_bank 2
+extern const unsigned char	metatiles_bin[ 528 ];
+#define				metatiles_bin_size 528
+#define				metatiles_bin_bank 3
 extern const unsigned char	scrolltable_bin[4109];
 #define				scrolltable_bin_size 4109
 #define				scrolltable_bin_bank 2
--- Games/GSLibDemo/gsldemo01/dev/bank3.h
+++ Games/GSLibDemo/gsldemo01/dev/bank3.h
@@ -4,12 +4,12 @@ extern const unsigned char	tiles_bin[2816];
 extern const unsigned char	village_psg[1053];
 #define				village_psg_size 1053
 #define				village_psg_bank 3
-extern const unsigned char	metatiles_bin[528];
-#define				metatiles_bin_size 528
-#define				metatiles_bin_bank 3
 extern const unsigned char	palette_bin[13];
 #define				palette_bin_size 13
 #define				palette_bin_bank 3
 extern const unsigned char	sprite_palette_bin[7];
 #define				sprite_palette_bin_size 7
 #define				sprite_palette_bin_bank 3
+extern const unsigned char	sprite_tiles_bin[ 6144 ];
+#define				sprite_tiles_bin_size 6144
+#define				sprite_tiles_bin_bank 2
