196c357959bab99eb8f0de0e45692ec26aa6c6f6
StevePro7 <steveprozune@hotmail.com>
2024-12-26 19:02:09 +0000

    no message

--- Games/GSLibDemo/gsldemo03/dev/build.bat
+++ Games/GSLibDemo/gsldemo03/dev/build.bat
@@ -1,3 +1,14 @@
-sdcc -c -mz80 main.c 
-sdcc -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 -Wl-b_BANK2=0x4000 -Wl-b_BANK3=0x4000 libs\crt0_sms.rel main.rel SMSlib.lib bank2.rel bank3.rel
-ihx2sms output.ihx GSLib_Demo.sms 
+sdcc -c -mz80 main.c
+
+sdcc -o output.ihx -mz80 --no-std-crt0 --data-loc 0xC000 ^
+-Wl-b_BANK2=0x4000 -Wl-b_BANK3=0x4000 ^
+../crt/crt0_sms.rel main.rel ^
+../lib/SMSlib.lib ^
+bank2.rel bank3.rel
+
+ihx2sms output.ihx output.sms
+
+if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
+if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+
+output.sms
---
+++ Games/GSLibDemo/gsldemo03/dev/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN__
+#define __MAIN__
+
+#include "bank2.h"
+#include "bank3.h"
+#include "resources.c"
+#include "../lib/SMSlib.h"
+#include "../lib/GSLib.c"
+#include "../lib/PSGlib.c"
+
+#endif//__MAIN__
