a107cc06126c609f43ddd730d44f0ae17b1f1868
StevePro7 <steveprozune@hotmail.com>
2024-12-26 17:00:50 +0000

    no message

--- Games/GSLibDemo/gsldemo01/dev/bank2.adb
+++ Games/GSLibDemo/gsldemo01/dev/bank2.adb
@@ -1,3 +1,3 @@
 M:bank2
-S:G$sprite_tiles_bin$0_0$0({6144}DA6144d,SC:U),D,0,0
+S:G$metatiles_bin$0_0$0({528}DA528d,SC:U),D,0,0
 S:G$scrolltable_bin$0_0$0({4109}DA4109d,SC:U),D,0,0
--- Games/GSLibDemo/gsldemo01/dev/bank3.adb
+++ Games/GSLibDemo/gsldemo01/dev/bank3.adb
@@ -1,6 +1,6 @@
 M:bank3
 S:G$tiles_bin$0_0$0({2816}DA2816d,SC:U),D,0,0
 S:G$village_psg$0_0$0({1053}DA1053d,SC:U),D,0,0
-S:G$metatiles_bin$0_0$0({528}DA528d,SC:U),D,0,0
 S:G$palette_bin$0_0$0({13}DA13d,SC:U),D,0,0
 S:G$sprite_palette_bin$0_0$0({7}DA7d,SC:U),D,0,0
+S:G$sprite_tiles_bin$0_0$0({6144}DA6144d,SC:U),D,0,0
