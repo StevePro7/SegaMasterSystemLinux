885f8875e8b03dce2bd4dbb2622a1af1e29b1ae8
StevePro7 <steveprozune@hotmail.com>
2025-12-07 17:01:46 +0000

    attempt

--- ExamplesBlog/DD-WAVsamples/10-MultipeExampleAsync/dev/engine/sample_manager.adb
+++ ExamplesBlog/DD-WAVsamples/10-MultipeExampleAsync/dev/engine/sample_manager.adb
@@ -2,10 +2,10 @@ M:sample_manager
 F:G$engine_sample_manager_init$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
 S:Lsample_manager.engine_sample_manager_init$psginit$1_0$6({2}DG,SV:S),R,0,0,[]
 F:G$engine_sample_manager_play$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-S:Lsample_manager.engine_sample_manager_play$sample$1_0$8({2}DG,SV:S),R,0,0,[]
-F:G$engine_sample_manager_update$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
-F:G$engine_sample_manager_isPlaying$0_0$0({2}DF,SC:U),Z,0,0,0,0,0
+S:Lsample_manager.engine_sample_manager_play$sample$1_0$0({2}DG,SV:S),R,0,0,[l,h]
 F:G$engine_sample_manager_stop$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
+F:G$engine_sample_manager_isPlaying$0_0$0({2}DF,SC:U),Z,0,0,0,0,0
+F:G$engine_sample_manager_update$0_0$0({2}DF,SV:S),Z,0,0,0,0,0
 S:Fsample_manager$sm_pcm_ptr$0_0$0({2}DG,SC:U),E,0,0
 S:Fsample_manager$sm_pcm_len$0_0$0({2}SI:U),E,0,0
 S:Fsample_manager$sm_pcm_playing$0_0$0({1}SC:U),E,0,0
