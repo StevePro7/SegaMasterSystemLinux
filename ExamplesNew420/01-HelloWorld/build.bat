039ea8d9a469783f76ebe798386a0c4e8d93b434
StevePro7 <steveprozune@hotmail.com>
2026-09-16 17:45:31 +0100

    vscode

--- ExamplesNew420/01-HelloWorld/build.bat
+++ ExamplesNew420/01-HelloWorld/build.bat
@@ -3,8 +3,7 @@ echo Build
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
 echo Link
-sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 ^
-crt0_sms.rel main.rel SMSlib.lib
+sdcc --debug -o output.ihx --Werror --opt-code-speed -mz80 --no-std-crt0 --data-loc 0xC000 crt0_sms.rel main.rel SMSlib.lib
 
 echo Run
 ihx2sms output.ihx output.sms
