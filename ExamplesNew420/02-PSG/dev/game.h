070007db2ee84213f40edc7b21e901d3bf524e86
StevePro7 <steveprozune@hotmail.com>
2026-09-19 18:28:55 +0100

    psg

---
+++ ExamplesNew420/02-PSG/dev/game.h
@@ -0,0 +1,16 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#define productCode	9999
+#define revision	0
+
+#define verMaj		1
+#define verMin		0
+#define dateYear	2026
+#define dateMonth	9
+#define dateDay		19
+#define author		"Steven Boland"
+#define name		"Hello PSG"
+#define descr		"Hello PSG tesing"
+
+#endif//_GAME_H_
