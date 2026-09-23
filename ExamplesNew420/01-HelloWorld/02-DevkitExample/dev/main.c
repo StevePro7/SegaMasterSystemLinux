3a130aab9266da0b8df6b1bbc58d2d6314e5ca33
StevePro7 <steveprozune@hotmail.com>
2026-09-23 19:22:18 +0100

    HW

---
+++ ExamplesNew420/01-HelloWorld/02-DevkitExample/dev/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void main( void )
+{
+	devkit_SMS_init();
+	devkit_SMS_displayOff();
+	//engine_asm_manager_clear_VRAM();
+	devkit_SMS_setBGPaletteColor( 0, devkit_RGB( 0, 0, 0 ) );
+	devkit_SMS_setSpritePaletteColor( 0, devkit_RGB( 3, 0, 0 ) );
+	devkit_SMS_displayOn();
+	for( ;; )
+	{
+		devkit_SMS_waitForVBlank();
+	}
+}
