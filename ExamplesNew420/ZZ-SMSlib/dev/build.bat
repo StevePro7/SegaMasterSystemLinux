da9c6b6234ad37f2d74da7d8a37b395c4bdb7b2b
StevePro7 <steveprozune@hotmail.com>
2026-09-23 19:20:00 +0100

    red

--- ExamplesNew420/ZZ-SMSlib/dev/build.bat
+++ ExamplesNew420/ZZ-SMSlib/dev/build.bat
@@ -7,10 +7,10 @@ set /a _hours=100%_time:~0,2%%%100,_min=100%_time:~3,2%%%100,_sec=100%_time:~6,2
 set /a _started=_hours*60*60*100+_min*60*100+_sec*100+_cs
 
 :: Compile
-cd devkit
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
-sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
-cd ..
+::cd devkit
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _sms_manager.c
+::sdcc --debug -c -mz80 --opt-code-speed --peep-file ../peep-rules.txt --std-c99 _snd_manager.c
+::cd ..
 
 sdcc --debug -c -mz80 --opt-code-speed --peep-file peep-rules.txt --std-c99 main.c
 
@@ -35,10 +35,10 @@ devkit/_snd_manager.rel
 ihx2sms output.ihx output.sms
 
 :: Cleanup
-cd devkit
-if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
-if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
-cd ..
+::cd devkit
+::if exist "*.asm" del "*.asm" > nul; if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul
+::if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
+::cd ..
 
 if exist "*.asm" del "*.asm" > nul; if exist "*.ihx" del "*.ihx" > nul; if exist "*.lk"  del "*.lk"  > nul
 if exist "*.lst" del "*.lst" > nul; if exist "*.sym" del "*.sym" > nul;
